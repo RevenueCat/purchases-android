061a503f4c86cbe0a6eada86a803c60fc0945ee3
Jacob Eiting <jacob.eiting@gmail.com>
2018-03-30 16:48:39 -0700

    HTTPClient basics tested.

--- purchases/src/main/java/com/revenuecat/purchases/HTTPClient.java
+++ purchases/src/main/java/com/revenuecat/purchases/HTTPClient.java
@@ -1,24 +1,116 @@
 package com.revenuecat.purchases;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Map;
 
 class HTTPClient {
     class Result {
-        final Integer responseCode;
-        final Map body;
+        Integer responseCode;
+        JSONObject body;
+    }
 
-        Result(Integer responseCode, Map body) {
-            this.responseCode = responseCode;
-            this.body = body;
+    class HTTPErrorException extends Exception {}
+
+    private final URL baseURL;
+
+    HTTPClient() {
+        try {
+            this.baseURL = new URL("https://api.revenuecat.com/v1");
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
         }
     }
 
-    private class HTTPErrorException extends Exception {}
+    HTTPClient(URL baseURL) {
+        this.baseURL = baseURL;
+    }
+
+    /** Buffers the given {@code InputStream}. */
+    private static BufferedReader buffer(InputStream is) {
+        return new BufferedReader(new InputStreamReader(is));
+    }
+
+    /** Reads the given {@code InputStream} into a String. */
+    private static String readFully(InputStream is) throws IOException {
+        return readFully(buffer(is));
+    }
+
+    /** Reads the given {@code BufferedReader} into a String. */
+    private static String readFully(BufferedReader reader) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        for (String line; (line = reader.readLine()) != null; ) {
+            sb.append(line);
+        }
+        return sb.toString();
+    }
+
+    private static InputStream getInputStream(HttpURLConnection connection) {
+        try {
+            return connection.getInputStream();
+        } catch (IOException e) {
+            return connection.getErrorStream();
+        }
+    }
 
     public Result performRequest(final String path,
                                  final Map body,
                                  final Map<String, String> headers)
             throws HTTPErrorException {
-        throw new UnsupportedOperationException();
+        URL fullURL = null;
+        try {
+            fullURL = new URL(baseURL, path);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+
+        HttpURLConnection connection = null;
+
+        try {
+            connection = (HttpURLConnection)fullURL.openConnection();
+
+            if (headers != null) {
+                for (Map.Entry<String, String> entry : headers.entrySet()) {
+                    connection.addRequestProperty(entry.getKey(), entry.getValue());
+                }
+            }
+            connection.addRequestProperty("Content-Type", "application/json");
+            connection.addRequestProperty("X-Platform", "android");
+            connection.addRequestProperty("X-Platform-Version", Integer.toString(android.os.Build.VERSION.SDK_INT));
+            connection.addRequestProperty("X-Version", "0.1.0-SNAPSHOT"); // FIXME
+        } catch (IOException e) {
+            throw new HTTPErrorException();
+        }
+
+        InputStream in = getInputStream(connection);
+        HTTPClient.Result result = new HTTPClient.Result();
+
+        String payload = null;
+        try {
+            result.responseCode = connection.getResponseCode();
+            payload = readFully(in);
+        } catch (IOException e) {
+            throw new HTTPErrorException();
+        } finally {
+            connection.disconnect();
+        }
+
+        try {
+            result.body = new JSONObject(payload);
+        } catch (JSONException e) {
+            throw new HTTPErrorException();
+        }
+
+        return result;
     }
 }
--- purchases/src/test/java/com/revenuecat/purchases/HTTPClientTest.java
+++ purchases/src/test/java/com/revenuecat/purchases/HTTPClientTest.java
@@ -1,10 +1,110 @@
 package com.revenuecat.purchases;
 
-import org.junit.Test;
+import android.util.Log;
 
+import junit.framework.TestCase;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.logging.Logger;
+
+import okhttp3.HttpUrl;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import okhttp3.mockwebserver.RecordedRequest;
+
+import static junit.framework.TestCase.assertEquals;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(manifest = Config.NONE)
 public class HTTPClientTest {
+    private static MockWebServer server;
+    private static URL baseURL;
+
+    @BeforeClass
+    public static void setup() throws IOException {
+        server = new MockWebServer();
+        baseURL = server.url("/v1").url();
+    }
+
     @Test
     public void canBeCreated() {
-        new HTTPClient();
+        new HTTPClient(baseURL);
+    }
+
+    @Test
+    public void canPerformASimpleGet() throws HTTPClient.HTTPErrorException, InterruptedException {
+        MockResponse response = new MockResponse().setBody("{}");
+        server.enqueue(response);
+
+        HTTPClient client = new HTTPClient(baseURL);
+        client.performRequest("/resource", null, null);
+
+        RecordedRequest request = server.takeRequest();
+        assertEquals(request.getMethod(), "GET");
+        assertEquals(request.getPath(), "/resource");
+    }
+
+    // Errors
+
+    @Test(expected = HTTPClient.HTTPErrorException.class)
+    public void reWrapsBadJSONError() throws HTTPClient.HTTPErrorException, InterruptedException {
+        MockResponse response = new MockResponse().setBody("not uh jason");
+        server.enqueue(response);
+
+        HTTPClient client = new HTTPClient(baseURL);
+        client.performRequest("/resource", null, null);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void badURLsThrowRuntimeException() throws HTTPClient.HTTPErrorException {
+        HTTPClient client = new HTTPClient(baseURL);
+        client.performRequest(" https://", null, null);
+    }
+
+    // Headers
+    @Test
+    public void addsHeadersToRequest() throws HTTPClient.HTTPErrorException, InterruptedException {
+        MockResponse response = new MockResponse().setBody("{}");
+        server.enqueue(response);
+
+        HashMap<String, String> headers = new HashMap<>();
+        headers.put("Authentication", "Bearer todd");
+
+        HTTPClient client = new HTTPClient(baseURL);
+        client.performRequest("/resource", null, headers);
+
+        RecordedRequest request = server.takeRequest();
+        assertEquals(request.getHeader("Authentication"), "Bearer todd");
+    }
+
+    @Test
+    public void addsDefaultHeadersToRequest() throws HTTPClient.HTTPErrorException, InterruptedException {
+        MockResponse response = new MockResponse().setBody("{}");
+        server.enqueue(response);
+
+        HTTPClient client = new HTTPClient(baseURL);
+        client.performRequest("/resource", null, null);
+
+        RecordedRequest request = server.takeRequest();
+
+        assertEquals(request.getHeader("Content-Type"), "application/json");
+        assertEquals(request.getHeader("X-Platform"), "android");
+        assertEquals(request.getHeader("X-Platform-Version"), Integer.toString(android.os.Build.VERSION.SDK_INT));
+        assertEquals(request.getHeader("X-Version"), "0.1.0-SNAPSHOT");
+    }
+
+    @AfterClass
+    public static void teardown() throws IOException {
+        server.shutdown();
     }
 }
