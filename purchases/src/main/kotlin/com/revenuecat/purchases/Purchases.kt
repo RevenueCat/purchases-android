7cfe566aee42205b6901279b6f639f64a1980626
Cesar de la Vega <cesarvegaro@gmail.com>
2019-05-30 23:40:32 -0700

    Prepare next development version

--- purchases/src/main/kotlin/com/revenuecat/purchases/Purchases.kt
+++ purchases/src/main/kotlin/com/revenuecat/purchases/Purchases.kt
@@ -986,7 +986,7 @@ class Purchases @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE) intern
          * Current version of the Purchases SDK
          */
         @JvmStatic
-        val frameworkVersion = "2.2.4"
+        val frameworkVersion = "2.3.0-SNAPSHOT"
 
         /**
          * Configures an instance of the Purchases SDK with a specified API key. The instance will
