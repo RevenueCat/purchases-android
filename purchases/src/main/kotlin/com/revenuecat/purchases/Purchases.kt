f062d0b791d3e32369dd742f3416aa1397f5523c
Cesar de la Vega <cesarvegaro@gmail.com>
2020-03-03 15:52:10 -0800

    Version 3.0.7

--- purchases/src/main/kotlin/com/revenuecat/purchases/Purchases.kt
+++ purchases/src/main/kotlin/com/revenuecat/purchases/Purchases.kt
@@ -1087,7 +1087,7 @@ class Purchases @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE) intern
          * Current version of the Purchases SDK
          */
         @JvmStatic
-        val frameworkVersion = "3.1.0-SNAPSHOT"
+        val frameworkVersion = "3.0.7"
 
         /**
          * Configures an instance of the Purchases SDK with a specified API key. The instance will
