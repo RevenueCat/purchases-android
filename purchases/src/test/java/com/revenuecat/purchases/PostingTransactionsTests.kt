49061838111cd2aa405c87e47fa34541d14886c0
Cesar de la Vega <cesarvegaro@gmail.com>
2020-06-08 17:34:32 -0700

    fixes tests

--- purchases/src/test/java/com/revenuecat/purchases/PostingTransactionsTests.kt
+++ purchases/src/test/java/com/revenuecat/purchases/PostingTransactionsTests.kt
@@ -103,7 +103,12 @@ class PostingTransactionsTests {
                 every { currentAppUserID } returns appUserId
             },
             subscriberAttributesManager = subscriberAttributesManagerMock,
-            appConfig = AppConfig("en-US", "1.0", PlatformInfo("native", "3.2.0"), true)
+            appConfig = AppConfig(
+                context = mockk(relaxed = true),
+                observerMode = false,
+                platformInfo = PlatformInfo(flavor = "native", version = "3.2.0"),
+                proxyURL = null
+            )
         )
     }
 
