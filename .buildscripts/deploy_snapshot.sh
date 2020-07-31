abd3e0aeba048d28c735f56b294457d159e32323
Cesar de la Vega <cesarvegaro@gmail.com>
2020-07-30 17:57:15 -0700

    fixes version parsing

--- .buildscripts/deploy_snapshot.sh
+++ .buildscripts/deploy_snapshot.sh
@@ -9,7 +9,7 @@
 REPO="purchases-android"
 USERNAME="RevenueCat"
 JDK="oraclejdk8"
-VERSION=$(grep "versionName" purchases/build.gradle | awk '{print $2}')
+VERSION=$(grep "versionName" purchases/build.gradle | awk '{print $2}' | sed -e 's/^"//' -e 's/"$//')
 
 if [ "$CIRCLE_PROJECT_REPONAME" != "$REPO" ]; then
   echo "Skipping snapshot deployment: wrong repository. Expected '$REPO' but was '$CIRCLE_PROJECT_REPONAME'."
@@ -18,7 +18,7 @@ elif [ "$CIRCLE_PROJECT_USERNAME" != "$USERNAME" ]; then
 elif [ "$CIRCLE_JDK_VERSION" != "$JDK" ]; then
   # $CIRCLE_JDK_VERSION must be manually set in circle.yml
   echo "Skipping snapshot deployment: wrong JDK. Expected '$JDK' but was '$CIRCLE_JDK_VERSION'."
-elif [ "$CIRCLE_BRANCH" == "main" ] || [ "$CIRCLE_BRANCH" == "bug/fixes_deploys" ] && [[ "$VERSION" == *SNAPSHOT ]]; then
+elif [ "$CIRCLE_BRANCH" == "main" ] || [ "$CIRCLE_BRANCH" == "bug/fixes-deploys" ] && [[ "$VERSION" == *SNAPSHOT ]]; then
   echo "Deploying snapshot..."
  ./gradlew androidSourcesJar androidJavadocsJar uploadArchives -P signing.keyId=$GPG_SIGNING_KEY_ID -Psigning.password=$GPG_SIGNING_KEY_PW -Psigning.secretKeyRingFile=./secring.gpg \
                           -PSONATYPE_NEXUS_USERNAME=$SONATYPE_NEXUS_USERNAME -PSONATYPE_NEXUS_PASSWORD=$SONATYPE_NEXUS_PASSWORD -PRELEASE_SIGNING_ENABLED=true
