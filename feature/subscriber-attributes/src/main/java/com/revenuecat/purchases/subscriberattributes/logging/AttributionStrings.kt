00eb43d676f1e06c6ffb13cb555729c6271fda9c
Cesar de la Vega <cesarvegaro@gmail.com>
2020-09-24 11:17:03 -0700

    Logging proposal (#188)
    * attempt with error codes
    
    * Revert "attempt with error codes"
    
    This reverts commit 94a4d7dc9608fa1ccc1a2d33d5e1770bfa53751e.
    
    * Uses constatns

---
+++ feature/subscriber-attributes/src/main/java/com/revenuecat/purchases/subscriberattributes/logging/AttributionStrings.kt
@@ -0,0 +1,5 @@
+package com.revenuecat.purchases.subscriberattributes.logging
+
+object AttributionStrings {
+    const val NO_SUBSCRIBER_ATTRIBUTES_TO_SYNCHRONIZE = "No subscriber attributes to synchronize."
+}
