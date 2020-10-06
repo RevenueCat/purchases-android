77a85775cda4c5642360e68517fee587c783a203
Cesar de la Vega <cesarvegaro@gmail.com>
2020-10-06 06:36:45 -0700

    Catch IOException when getAdvertisingIdInfo (#197)
    * changes exception to catch
    
    * catches timeoutexcecption too, just in case

--- feature/subscriber-attributes/src/test/java/com/revenuecat/purchases/subscriberattributes/AttributionFetcherTests.kt
+++ feature/subscriber-attributes/src/test/java/com/revenuecat/purchases/subscriberattributes/AttributionFetcherTests.kt
@@ -11,6 +11,7 @@ import io.mockk.mockkStatic
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
 import org.junit.Test
+import java.io.IOException
 import java.util.concurrent.TimeoutException
 
 class AttributionFetcherTests {
@@ -93,6 +94,27 @@ class AttributionFetcherTests {
             expectedAdID = "12345",
             expectedAndroidID = "androidid",
             expectedIsLimitAdTrackingEnabled = true
+        )
+
+        var completionCalled = false
+        underTest.getDeviceIdentifiers(mockContext) { advertisingID, androidID ->
+            completionCalled = true
+
+            assertThat(advertisingID).isNull()
+            assertThat(androidID).isEqualTo("androidid")
+        }
+
+        assertThat(completionCalled).isTrue()
+    }
+
+    @Test
+    fun `IOException when getting device identifiers`() {
+        val mockContext = mockk<Application>(relaxed = true)
+        mockAdvertisingInfo(
+            mockContext = mockContext,
+            expectedAdID = "12345",
+            expectedAndroidID = "androidid",
+            expectedException = IOException(TimeoutException())
         )
 
         var completionCalled = false
