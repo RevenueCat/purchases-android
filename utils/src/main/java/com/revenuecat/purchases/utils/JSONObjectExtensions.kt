a434ea84c9658cf316874d9290e60a7a30800e5e
Cesar de la Vega <cesarvegaro@gmail.com>
2020-10-11 08:20:50 -0700

    Management url json fix (#203)
    * attempts to fix management url crash
    
    * Replaces takeUnless with takeIf

--- utils/src/main/java/com/revenuecat/purchases/utils/JSONObjectExtensions.kt
+++ utils/src/main/java/com/revenuecat/purchases/utils/JSONObjectExtensions.kt
@@ -43,6 +43,6 @@ fun JSONObject.getDate(jsonKey: String): Date = Iso8601Utils.parse(getString(jso
 
 fun JSONObject.optDate(jsonKey: String): Date? = takeUnless { this.isNull(jsonKey) }?.getDate(jsonKey)
 
-fun JSONObject.getNullableString(name: String): String? = this.getString(name).takeUnless { this.isNull(name) }
+fun JSONObject.getNullableString(name: String): String? = takeUnless { this.isNull(name) }?.getString(name)
 
-fun JSONObject.optNullableString(name: String): String? = this.optString(name).takeUnless { this.isNull(name) }
+fun JSONObject.optNullableString(name: String): String? = takeIf { this.has(name) }?.getNullableString(name)
