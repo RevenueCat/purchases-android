c62842c7899e3217a12388772792903ff288a23c
Cesar de la Vega <cesarvegaro@gmail.com>
2020-10-13 13:06:44 -0700

    moves AttributionStrings to its own module (#202)

--- strings/src/main/java/com/revenuecat/purchases/strings/AttributionStrings.kt
+++ strings/src/main/java/com/revenuecat/purchases/strings/AttributionStrings.kt
@@ -1,4 +1,4 @@
-package com.revenuecat.purchases.subscriberattributes.logging
+package com.revenuecat.purchases.strings
 
 object AttributionStrings {
     const val NO_SUBSCRIBER_ATTRIBUTES_TO_SYNCHRONIZE = "No subscriber attributes to synchronize."
