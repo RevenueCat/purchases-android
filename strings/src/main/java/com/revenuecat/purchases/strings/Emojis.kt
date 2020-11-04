dc98370568352b36b723f403d8ebfb9ea8ac0974
Tina Nguyen <tiinanguyen@berkeley.edu>
2020-11-03 18:06:52 -0800

    adds emojis file

---
+++ strings/src/main/java/com/revenuecat/purchases/strings/Emojis.kt
@@ -0,0 +1,13 @@
+package com.revenuecat.purchases.strings
+
+object Emojis {
+   const val APPLE = "🍎";
+   const val ERROR = "\u203C\uFE0F";
+   const val HEART_CAT_EYES = "😻";
+   const val INFO = "\u2139\uFE0F";
+   const val MONEY = "💰";
+   const val ROBOT = "🤖"
+   const val SAD_CAT_EYES = "😿";
+   const val USER = "👤";
+   const val WARNING = "\u26A0\uFE0F";
+}
