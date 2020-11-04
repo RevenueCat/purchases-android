0912a7e0de373e5942d712c5d77f1820698d130c
tiinanguyen <36222014+tiinanguyen@users.noreply.github.com>
2020-11-04 12:59:30 -0800

    adds emojis file (#217)
    * adds emojis file
    
    * adds missing semicolon
    
    * Removes semicolons
    
    * adds comment for unicode emojis

---
+++ strings/src/main/java/com/revenuecat/purchases/strings/Emojis.kt
@@ -0,0 +1,13 @@
+package com.revenuecat.purchases.strings
+
+object Emojis {
+   const val APPLE = "🍎"
+   const val ERROR = "\u203C\uFE0F" // double exclamation mark
+   const val HEART_CAT_EYES = "😻"
+   const val INFO = "\u2139\uFE0F" // information 
+   const val MONEY = "💰"
+   const val ROBOT = "🤖"
+   const val SAD_CAT_EYES = "😿"
+   const val USER = "👤"
+   const val WARNING = "\u26A0\uFE0F" // yellow warning sign
+}
