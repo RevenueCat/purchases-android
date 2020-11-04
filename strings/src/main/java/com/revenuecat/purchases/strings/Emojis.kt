419aa18b97a2ba3f23c17e94f995072ff1b9b51c
Tina Nguyen <tiinanguyen@berkeley.edu>
2020-11-04 08:57:35 -0800

    adds comment for unicode emojis

--- strings/src/main/java/com/revenuecat/purchases/strings/Emojis.kt
+++ strings/src/main/java/com/revenuecat/purchases/strings/Emojis.kt
@@ -2,12 +2,12 @@ package com.revenuecat.purchases.strings
 
 object Emojis {
    const val APPLE = "🍎"
-   const val ERROR = "\u203C\uFE0F"
+   const val ERROR = "\u203C\uFE0F" // double exclamation mark
    const val HEART_CAT_EYES = "😻"
-   const val INFO = "\u2139\uFE0F"
+   const val INFO = "\u2139\uFE0F" // information 
    const val MONEY = "💰"
    const val ROBOT = "🤖"
    const val SAD_CAT_EYES = "😿"
    const val USER = "👤"
-   const val WARNING = "\u26A0\uFE0F"
+   const val WARNING = "\u26A0\uFE0F" // yellow warning sign
 }
