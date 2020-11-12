125d9aa569e1b7e9735e3d2588d8f306a56f7409
Tina Nguyen <tiinanguyen@berkeley.edu>
2020-11-12 10:17:04 -0800

    change cascading if to when

--- common/src/main/java/com/revenuecat/purchases/common/errorLogUtils.kt
+++ common/src/main/java/com/revenuecat/purchases/common/errorLogUtils.kt
@@ -5,96 +5,80 @@ import com.revenuecat.purchases.strings.Emojis
 fun googleErrorLog(level: LogLevel, message: String) {
     val emojiList = listOf(Emojis.GOOGLE_ERROR, Emojis.DOUBLE_EXCLAMATION)
     val emojifiedMessage = "${emojiList.joinToString("")} $message"
-    if (level == LogLevel.DEBUG) {
-        debugLog(emojifiedMessage)
-    } else if (level == LogLevel.INFO) {
-        log(emojifiedMessage)
-    } else {
-        errorLog(emojifiedMessage)
+    when (level) {
+        LogLevel.DEBUG -> debugLog(emojifiedMessage)
+        LogLevel.INFO -> log(emojifiedMessage)
+        LogLevel.ERROR -> errorLog(emojifiedMessage)
     }
 }
 
 fun infoLog(level: LogLevel, message: String) {
     val emojiList = listOf(Emojis.INFO)
     val emojifiedMessage = "${emojiList.joinToString("")} $message"
-    if (level == LogLevel.DEBUG) {
-        debugLog(emojifiedMessage)
-    } else if (level == LogLevel.INFO) {
-        log(emojifiedMessage)
-    } else {
-        errorLog(emojifiedMessage)
+    when (level) {
+        LogLevel.DEBUG -> debugLog(emojifiedMessage)
+        LogLevel.INFO -> log(emojifiedMessage)
+        LogLevel.ERROR -> errorLog(emojifiedMessage)
     }
 }
 
 fun purchaseLog(level: LogLevel, message: String) {
     val emojiList = listOf(Emojis.PURCHASE)
     val emojifiedMessage = "${emojiList.joinToString("")} $message"
-    if (level == LogLevel.DEBUG) {
-        debugLog(emojifiedMessage)
-    } else if (level == LogLevel.INFO) {
-        log(emojifiedMessage)
-    } else {
-        errorLog(emojifiedMessage)
+    when (level) {
+        LogLevel.DEBUG -> debugLog(emojifiedMessage)
+        LogLevel.INFO -> log(emojifiedMessage)
+        LogLevel.ERROR -> errorLog(emojifiedMessage)
     }
 }
 
 fun rcSuccessLog(level: LogLevel, message: String) {
     val emojiList = listOf(Emojis.RC_SUCCESS)
     val emojifiedMessage = "${emojiList.joinToString("")} $message"
-    if (level == LogLevel.DEBUG) {
-        debugLog(emojifiedMessage)
-    } else if (level == LogLevel.INFO) {
-        log(emojifiedMessage)
-    } else {
-        errorLog(emojifiedMessage)
+    when (level) {
+        LogLevel.DEBUG -> debugLog(emojifiedMessage)
+        LogLevel.INFO -> log(emojifiedMessage)
+        LogLevel.ERROR -> errorLog(emojifiedMessage)
     }
 }
 
 fun rcPurchaseSuccessLog(level: LogLevel, message: String) {
     val emojiList = listOf(Emojis.RC_SUCCESS, Emojis.PURCHASE)
     val emojifiedMessage = "${emojiList.joinToString("")} $message"
-    if (level == LogLevel.DEBUG) {
-        debugLog(emojifiedMessage)
-    } else if (level == LogLevel.INFO) {
-        log(emojifiedMessage)
-    } else {
-        errorLog(emojifiedMessage)
+    when (level) {
+        LogLevel.DEBUG -> debugLog(emojifiedMessage)
+        LogLevel.INFO -> log(emojifiedMessage)
+        LogLevel.ERROR -> errorLog(emojifiedMessage)
     }
 }
 
 fun rcErrorLog(level: LogLevel, message: String) {
     val emojiList = listOf(Emojis.RC_ERROR, Emojis.DOUBLE_EXCLAMATION)
     val emojifiedMessage = "${emojiList.joinToString("")} $message"
-    if (level == LogLevel.DEBUG) {
-        debugLog(emojifiedMessage)
-    } else if (level == LogLevel.INFO) {
-        log(emojifiedMessage)
-    } else {
-        errorLog(emojifiedMessage)
+    when (level) {
+        LogLevel.DEBUG -> debugLog(emojifiedMessage)
+        LogLevel.INFO -> log(emojifiedMessage)
+        LogLevel.ERROR -> errorLog(emojifiedMessage)
     }
 }
 
 fun userLog(level: LogLevel, message: String) {
     val emojiList = listOf(Emojis.APP_USER_ID)
     val emojifiedMessage = "${emojiList.joinToString("")} $message"
-    if (level == LogLevel.DEBUG) {
-        debugLog(emojifiedMessage)
-    } else if (level == LogLevel.INFO) {
-        log(emojifiedMessage)
-    } else {
-        errorLog(emojifiedMessage)
+    when (level) {
+        LogLevel.DEBUG -> debugLog(emojifiedMessage)
+        LogLevel.INFO -> log(emojifiedMessage)
+        LogLevel.ERROR -> errorLog(emojifiedMessage)
     }
 }
 
 fun warningLog(level: LogLevel, message: String) {
     val emojiList = listOf(Emojis.WARNING)
     val emojifiedMessage = "${emojiList.joinToString("")} $message"
-    if (level == LogLevel.DEBUG) {
-        debugLog(emojifiedMessage)
-    } else if (level == LogLevel.INFO) {
-        log(emojifiedMessage)
-    } else {
-        errorLog(emojifiedMessage)
+    when (level) {
+        LogLevel.DEBUG -> debugLog(emojifiedMessage)
+        LogLevel.INFO -> log(emojifiedMessage)
+        LogLevel.ERROR -> errorLog(emojifiedMessage)
     }
 }
 
