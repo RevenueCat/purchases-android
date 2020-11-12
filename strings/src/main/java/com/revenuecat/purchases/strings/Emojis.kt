73a568f6ce4991a04fd07ee2c3485aa0a2a3d34c
Tina Nguyen <tiinanguyen@berkeley.edu>
2020-11-11 16:16:47 -0800

    adds logs based on intent

--- strings/src/main/java/com/revenuecat/purchases/strings/Emojis.kt
+++ strings/src/main/java/com/revenuecat/purchases/strings/Emojis.kt
@@ -2,12 +2,11 @@ package com.revenuecat.purchases.strings
 
 object Emojis {
    const val APP_USER_ID = "👤"
-   const val APPLE_ERROR = "🍎"
-   const val DOUBLE_EXCLAMATION = "\u203C\uFE0F" //red double exclamation marks
+   const val DOUBLE_EXCLAMATION = "\u203C\uFE0F" // red double exclamation marks
    const val GOOGLE_ERROR = "🤖"
-   const val INFO = "\u2139\uFE0F" //information
+   const val INFO = "\u2139\uFE0F" // information
    const val PURCHASE = "💰"
    const val RC_ERROR = "😿"
    const val RC_SUCCESS = "😻"
-   const val WARNING = "\u26A0\uFE0F" //yellow warning sign
+   const val WARNING = "\u26A0\uFE0F" // yellow warning sign
 }
