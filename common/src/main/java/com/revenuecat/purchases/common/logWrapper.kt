66ad0eb2019772d70e59a372670f0f451ff08506
Tina Nguyen <tiinanguyen@berkeley.edu>
2020-11-19 16:05:01 -0800

    update LogIntent names

--- common/src/main/java/com/revenuecat/purchases/common/logWrapper.kt
+++ common/src/main/java/com/revenuecat/purchases/common/logWrapper.kt
@@ -5,14 +5,14 @@ import com.revenuecat.purchases.strings.Emojis
 fun log(intent: LogIntent, message: String) {
     val emojifiedMessage = "${intent.emojiList.joinToString("")} $message"
     when (intent) {
-        LogIntent.GOOGLEERROR -> errorLog(emojifiedMessage)
-        LogIntent.GOOGLEINFO -> infoLog(emojifiedMessage)
+        LogIntent.GOOGLE_ERROR -> errorLog(emojifiedMessage)
+        LogIntent.GOOGLE_INFO -> infoLog(emojifiedMessage)
         LogIntent.INFO -> infoLog(emojifiedMessage)
-        LogIntent.DEBUGINFO -> debugLog(emojifiedMessage)
+        LogIntent.DEBUG_INFO -> debugLog(emojifiedMessage)
         LogIntent.PURCHASE -> debugLog(emojifiedMessage)
-        LogIntent.RCERROR -> errorLog(emojifiedMessage)
-        LogIntent.RCPURCHASESUCCESS -> debugLog(emojifiedMessage)
-        LogIntent.RCSUCCESS -> debugLog(emojifiedMessage)
+        LogIntent.RC_ERROR -> errorLog(emojifiedMessage)
+        LogIntent.RC_PURCHASE_SUCCESS -> debugLog(emojifiedMessage)
+        LogIntent.RC_SUCCESS -> debugLog(emojifiedMessage)
         LogIntent.USER -> debugLog(emojifiedMessage)
         LogIntent.WARNING -> warnLog(emojifiedMessage)
     }
@@ -25,11 +25,11 @@ enum class LogIntent(val emojiList: List<String>) {
     /**
      * Emojis for Google error messages.
      */
-    GOOGLEERROR(listOf(Emojis.GOOGLE_ERROR, Emojis.DOUBLE_EXCLAMATION)),
+    GOOGLE_ERROR(listOf(Emojis.GOOGLE_ERROR, Emojis.DOUBLE_EXCLAMATION)),
     /**
      * Emojis for Google info messages.
      */
-    GOOGLEINFO(listOf(Emojis.GOOGLE_ERROR, Emojis.DOUBLE_EXCLAMATION)),
+    GOOGLE_INFO(listOf(Emojis.GOOGLE_ERROR, Emojis.DOUBLE_EXCLAMATION)),
     /**
      * Emoji for information messages on the info level.
      */
@@ -37,7 +37,7 @@ enum class LogIntent(val emojiList: List<String>) {
     /**
      * Emoji for information messages on the debug level.
      */
-    DEBUGINFO(listOf(Emojis.INFO)),
+    DEBUG_INFO(listOf(Emojis.INFO)),
     /**
      * Emoji for purchase messages.
      */
@@ -45,15 +45,15 @@ enum class LogIntent(val emojiList: List<String>) {
     /**
      * Emojis for RevenueCat error messages.
      */
-    RCERROR(listOf(Emojis.RC_ERROR, Emojis.DOUBLE_EXCLAMATION)),
+    RC_ERROR(listOf(Emojis.RC_ERROR, Emojis.DOUBLE_EXCLAMATION)),
     /**
      * Emojis for RevenueCat purchase success messages.
      */
-    RCPURCHASESUCCESS(listOf(Emojis.RC_SUCCESS, Emojis.PURCHASE)),
+    RC_PURCHASE_SUCCESS(listOf(Emojis.RC_SUCCESS, Emojis.PURCHASE)),
     /**
      * Emoji for RevenueCat success messages.
      */
-    RCSUCCESS(listOf(Emojis.RC_SUCCESS)),
+    RC_SUCCESS(listOf(Emojis.RC_SUCCESS)),
     /**
      * Emoji for App User ID related messages.
      */
