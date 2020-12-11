acbc24be462038811041230c3c0d76685e29d309
Tina Nguyen <tiinanguyen@berkeley.edu>
2020-12-10 17:01:27 -0800

    removing [QueryPurchases]

--- strings/src/main/java/com/revenuecat/purchases/strings/PurchaseStrings.kt
+++ strings/src/main/java/com/revenuecat/purchases/strings/PurchaseStrings.kt
@@ -3,7 +3,7 @@ package com.revenuecat.purchases.strings
 object PurchaseStrings {
     const val ACKNOWLEDGING_PURCHASE = "Acknowledging purchase with token %s"
     const val ACKNOWLEDGING_PURCHASE_ERROR = "Error acknowledging purchase. Will retry next queryPurchases. %s"
-    const val BILLING_CLIENT_NOT_CONNECTED = "[QueryPurchases] Skipping updating pending purchase queue since " +
+    const val BILLING_CLIENT_NOT_CONNECTED = "Skipping updating pending purchase queue since " +
             "BillingClient is not connected yet."
     const val CONSUMING_PURCHASE = "Consuming purchase with token %s"
     const val CONSUMING_PURCHASE_ERROR = "Error consuming purchase. Will retry next queryPurchases. %s"
@@ -20,7 +20,7 @@ object PurchaseStrings {
     const val SYNCING_PURCHASES = "Syncing purchases"
     const val SYNCING_PURCHASES_ERROR = "Error syncing purchases %s"
     const val SYNCING_PURCHASES_ERROR_DETAILS = "Error syncing purchases %s. Error: %s"
-    const val UPDATING_PENDING_PURCHASE_QUEUE = "[QueryPurchases] Updating pending purchase queue"
+    const val UPDATING_PENDING_PURCHASE_QUEUE = "Updating pending purchase queue"
     const val UPGRADING_SKU = "Moving from old SKU %s to sku %s"
     const val UPGRADING_SKU_ERROR = "There was an error trying to upgrade. BillingResponseCode: %s"
 }
--- strings/src/main/java/com/revenuecat/purchases/strings/RestoreStrings.kt
+++ strings/src/main/java/com/revenuecat/purchases/strings/RestoreStrings.kt
@@ -4,9 +4,9 @@ object RestoreStrings {
     const val PURCHASE_HISTORY_EMPTY = "Purchase history is empty."
     const val PURCHASE_HISTORY_RETRIEVED = "Purchase history retrieved %s"
     const val PURCHASE_RESTORED = "Purchase %s restored"
-    const val QUERYING_PURCHASE = "[QueryPurchases] Querying %s"
-    const val QUERYING_PURCHASE_WITH_HASH = "[QueryPurchases] Purchase of type %s with hash %s"
-    const val QUERYING_PURCHASE_WITH_TYPE = "[QueryPurchases] Querying Purchase with %s and type %s"
+    const val QUERYING_PURCHASE = "Querying %s"
+    const val QUERYING_PURCHASE_WITH_HASH = "Purchase of type %s with hash %s"
+    const val QUERYING_PURCHASE_WITH_TYPE = "Querying Purchase with %s and type %s"
     const val RESTORING_PURCHASE = "Restoring purchases"
     const val RESTORING_PURCHASE_ERROR = "Error restoring purchase: %s. Error: %s"
     const val SHARING_ACC_RESTORE_FALSE = "allowSharingPlayStoreAccount is set to false and restorePurchases " +
