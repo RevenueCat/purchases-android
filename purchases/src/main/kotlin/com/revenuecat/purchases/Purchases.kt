cbf14180f2c771fc13df631082a5b1c4c0b4d562
Andy Boedo <andresboedo@gmail.com>
2021-02-19 14:01:23 -0300

    fixed missing closing bracket in replaceWith

--- purchases/src/main/kotlin/com/revenuecat/purchases/Purchases.kt
+++ purchases/src/main/kotlin/com/revenuecat/purchases/Purchases.kt
@@ -707,7 +707,7 @@ class Purchases @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE) intern
      */
     @Deprecated(
         "Use logIn instead",
-        ReplaceWith("Purchases.sharedInstance.logIn(newAppUserID, LogInCallback?")
+        ReplaceWith("Purchases.sharedInstance.logIn(newAppUserID, LogInCallback?)")
     )
     @JvmOverloads
     fun identify(
@@ -792,7 +792,7 @@ class Purchases @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE) intern
      */
     @Deprecated(
         "Use logOut instead",
-        ReplaceWith("Purchases.sharedInstance.logOut(ReceivePurchaserInfoListener")
+        ReplaceWith("Purchases.sharedInstance.logOut(ReceivePurchaserInfoListener?)")
     )
     @JvmOverloads
     fun reset(
