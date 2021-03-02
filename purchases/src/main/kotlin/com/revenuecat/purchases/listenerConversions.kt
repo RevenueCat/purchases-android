d95e10065845558128574a607a7fd23be4466840
Andy Boedo <andresboedo@gmail.com>
2021-03-02 16:56:53 -0300

    made logInWith and logOutWith public

--- purchases/src/main/kotlin/com/revenuecat/purchases/listenerConversions.kt
+++ purchases/src/main/kotlin/com/revenuecat/purchases/listenerConversions.kt
@@ -329,7 +329,7 @@ fun Purchases.identifyWith(
  * @param [onError] Will be called after the call has completed with an error.
  */
 @Suppress("unused")
-internal fun Purchases.logInWith(
+fun Purchases.logInWith(
     appUserID: String,
     onError: ErrorFunction = ON_ERROR_STUB,
     onSuccess: ReceiveLogInSuccessFunction
@@ -344,7 +344,7 @@ internal fun Purchases.logInWith(
  * @param [onError] Will be called after the call has completed with an error.
  */
 @Suppress("unused")
-internal fun Purchases.logOutWith(
+fun Purchases.logOutWith(
     onError: ErrorFunction = ON_ERROR_STUB,
     onSuccess: ReceivePurchaserInfoSuccessFunction
 ) {
