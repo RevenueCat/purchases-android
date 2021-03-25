099577ec8ee623220609b4114ff236a247423f6d
Maddie Beyl <maddie@revenuecat.com>
2021-03-25 12:40:21 -0700

    Add crude expanding animation upon clicking PurchaserInfo card

--- purchases-sample/src/main/java/com/revenuecat/sample/OverviewActivity.kt
+++ purchases-sample/src/main/java/com/revenuecat/sample/OverviewActivity.kt
@@ -1,11 +1,14 @@
 package com.revenuecat.sample
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.transition.TransitionManager
 import com.revenuecat.purchases.Purchases
 import com.revenuecat.purchases.getPurchaserInfoWith
 import com.revenuecat.sample.databinding.ActivityOverviewBinding
+import kotlinx.android.synthetic.main.activity_overview.view.*
 
 class OverviewActivity : AppCompatActivity() {
 
@@ -14,6 +17,11 @@ class OverviewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_overview)
+
+        binding.purchaserInfoCard.setOnClickListener { card ->
+            TransitionManager.beginDelayedTransition(card.purchaser_info_card_content)
+            card.purchaser_info_details_container.visibility = View.VISIBLE
+        }
     }
 
     override fun onResume() {
