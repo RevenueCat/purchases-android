6b0c1ae91239ef43f633c9a7143482fcc9a5a0d2
Maddie Beyl <maddie@revenuecat.com>
2021-03-24 17:09:36 -0700

    Add blank OverviewActivity to eventually contain new UI and make it launcher activity

---
+++ purchases-sample/src/main/java/com/revenuecat/sample/OverviewActivity.kt
@@ -0,0 +1,12 @@
+package com.revenuecat.sample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class OverviewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_overview)
+    }
+}
