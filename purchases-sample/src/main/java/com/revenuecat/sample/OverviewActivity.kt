85f9c01f4a99477d20c5fcc3fd4a5fedac4a37eb
Maddie Beyl <maddie@revenuecat.com>
2021-03-25 11:45:01 -0700

    Add databinding and use it for originalAppUserId in PurchaserInfo card

--- purchases-sample/src/main/java/com/revenuecat/sample/OverviewActivity.kt
+++ purchases-sample/src/main/java/com/revenuecat/sample/OverviewActivity.kt
@@ -2,11 +2,24 @@ package com.revenuecat.sample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.revenuecat.purchases.Purchases
+import com.revenuecat.purchases.getPurchaserInfoWith
+import com.revenuecat.sample.databinding.ActivityOverviewBinding
 
 class OverviewActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityOverviewBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_overview)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_overview)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Purchases.sharedInstance.getPurchaserInfoWith(::showError) { purchaserInfo ->
+            binding.purchaserInfo = purchaserInfo
+        }
     }
 }
