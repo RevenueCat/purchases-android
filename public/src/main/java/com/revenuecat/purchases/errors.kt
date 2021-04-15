4eb84499dc2eee1ea3519214a978c7198a656604
beylmk <maddie@revenuecat.com>
2021-04-14 17:21:08 -0700

    Update InvalidPackageNameError mapping (#298)
    * Create new ConfigurationError, map BackendInvalidPackageName to it
    
    * Map apple-specific errors to UnknownBackendResponseError

--- public/src/main/java/com/revenuecat/purchases/errors.kt
+++ public/src/main/java/com/revenuecat/purchases/errors.kt
@@ -46,4 +46,5 @@ enum class PurchasesErrorCode(val code: Int, val description: String) {
     PaymentPendingError(20, "The payment is pending."),
     InvalidSubscriberAttributesError(21, "One or more of the attributes sent could not be saved."),
     LogOutWithAnonymousUserError(22, "Called logOut but the current user is anonymous."),
+    ConfigurationError(23, "There is an issue with your configuration. Check the underlying error for more details.")
 }
