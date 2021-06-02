18feedb71d059d03354e67812a8215a8fb0498f5
vegaro <cesarvegaro@gmail.com>
2021-06-01 17:43:35 -0700

    adds some logs and javadocs around just one sku assumption

--- feature/google/src/main/java/com/revenuecat/purchases/google/extensions.kt
+++ feature/google/src/main/java/com/revenuecat/purchases/google/extensions.kt
@@ -2,9 +2,30 @@ package com.revenuecat.purchases.google
 
 import com.android.billingclient.api.Purchase
 import com.android.billingclient.api.PurchaseHistoryRecord
+import com.revenuecat.purchases.common.LogIntent
+import com.revenuecat.purchases.common.log
+import com.revenuecat.purchases.strings.BillingStrings
 
+/**
+ * **Important**: until multi-line subscriptions are released,
+ * we assume that there will be only a single sku in the purchase.
+ * https://android-developers.googleblog.com/2021/05/whats-new-in-google-play-2021.html
+ */
 val PurchaseHistoryRecord.sku: String
-    get() = skus[0]
+    get() = skus[0].also {
+        if (skus.size > 0) {
+            log(LogIntent.GOOGLE_WARNING, BillingStrings.BILLING_PURCHASE_HISTORY_RECORD_MORE_THAN_ONE_SKU)
+        }
+    }
 
+/**
+ * **Important**: until multi-line subscriptions are released,
+ * we assume that there will be only a single sku in the purchase.
+ * https://android-developers.googleblog.com/2021/05/whats-new-in-google-play-2021.html
+ */
 val Purchase.sku: String
-    get() = skus[0]
+    get() = skus[0].also {
+        if (skus.size > 0) {
+            log(LogIntent.GOOGLE_WARNING, BillingStrings.BILLING_PURCHASE_MORE_THAN_ONE_SKU)
+        }
+    }
--- strings/src/main/java/com/revenuecat/purchases/strings/BillingStrings.kt
+++ strings/src/main/java/com/revenuecat/purchases/strings/BillingStrings.kt
@@ -11,4 +11,8 @@ object BillingStrings {
     const val BILLING_UNAVAILABLE = "Billing is not available in this device. %s"
     const val BILLING_WRAPPER_PURCHASES_ERROR = "BillingWrapper purchases failed to update: %s"
     const val BILLING_WRAPPER_PURCHASES_UPDATED = "BillingWrapper purchases updated: %s"
+    const val BILLING_PURCHASE_HISTORY_RECORD_MORE_THAN_ONE_SKU = "There's more than one sku in the " +
+        "PurchaseHistoryRecord, but only one will be used."
+    const val BILLING_PURCHASE_MORE_THAN_ONE_SKU = "There's more than one sku in the PurchaseHistoryRecord, " +
+        "but only one will be used."
 }
