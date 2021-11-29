1d4308be98e18bce386f9f819c4d9b0482a5155c
NachoSoto <NachoSoto@users.noreply.github.com>
2021-11-29 15:12:57 -0800

    IdentityManager.configure: warn on empty App User ID and convert it to anonymous (#384)
    Fixes [sc-10159].
    See also https://github.com/RevenueCat/purchases-ios/pull/995 for iOS equivalent.

--- strings/src/main/java/com/revenuecat/purchases/strings/IdentityStrings.kt
+++ strings/src/main/java/com/revenuecat/purchases/strings/IdentityStrings.kt
@@ -9,6 +9,8 @@ object IdentityStrings {
     const val CREATING_ALIAS_SUCCESS = "Alias created"
     const val IDENTIFYING_ANON_ID = "Identifying from an anonymous ID: %s. An alias will be created."
     const val IDENTIFYING_APP_USER_ID = "Identifying App User ID: %s"
+    const val EMPTY_APP_USER_ID_WILL_BECOME_ANONYMOUS = "Identifying with empty App User ID will be " +
+        "treated as anonymous."
     const val SETTING_NEW_ANON_ID = "Setting new anonymous App User ID - %s"
     const val LOG_OUT_CALLED_ON_ANONYMOUS_USER = "Called logOut but the current user is anonymous"
     const val LOG_OUT_SUCCESSFUL = "Logged out successfully"
