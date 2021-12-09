b9db57d5568f9eaac30dd24cdd08f413c99222f2
Cesar de la Vega <cesarvegaro@gmail.com>
2021-12-09 12:04:56 -0500

    ReceiveOfferingsListener to Kotlin

--- purchases/src/main/java/com/revenuecat/purchases/interfaces/ReceiveOfferingsListener.kt
+++ purchases/src/main/java/com/revenuecat/purchases/interfaces/ReceiveOfferingsListener.kt
@@ -2,31 +2,26 @@
 //
 //  Copyright © 2019 RevenueCat, Inc. All rights reserved.
 //
+package com.revenuecat.purchases.interfaces
 
-package com.revenuecat.purchases.interfaces;
-
-
-import androidx.annotation.NonNull;
-
-import com.revenuecat.purchases.Offerings;
-import com.revenuecat.purchases.PurchasesError;
+import com.revenuecat.purchases.Offerings
+import com.revenuecat.purchases.PurchasesError
 
 /**
  * Interface to be implemented when making calls to fetch [Offering].
  */
-public interface ReceiveOfferingsListener {
-
+interface ReceiveOfferingsListener {
     /**
      * Will be called after a successful fetch of offerings.
      *
      * @param offerings
      */
-    void onReceived(@NonNull Offerings offerings);
+    fun onReceived(offerings: Offerings)
 
     /**
      * Will be called after an error fetching offerings
      *
      * @param error A [PurchasesError] containing the reason for the failure when fetching offerings.
      */
-    void onError(@NonNull PurchasesError error);
+    fun onError(error: PurchasesError)
 }
