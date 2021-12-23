a322b1bee574422f23f7fb4a0f52d0bed03a9269
NachoSoto <ignaciosoto90@gmail.com>
2021-12-23 10:54:59 -0800

    Package

---
+++ api-tester-kotlin/src/main/java/com/revenuecat/apitesterkotlin/PackageAPI.kt
@@ -0,0 +1,30 @@
+package com.revenuecat.apitesterkotlin
+
+import com.android.billingclient.api.SkuDetails
+import com.revenuecat.purchases.Package
+import com.revenuecat.purchases.PackageType
+
+@Suppress("unused")
+private class PackageAPI {
+    fun check(p: Package) {
+        val identifier: String = p.identifier
+        val packageType: PackageType = p.packageType
+        val product: SkuDetails = p.product
+        val offering: String = p.offering
+    }
+
+    fun check(type: PackageType) {
+        when (type) {
+            PackageType.UNKNOWN,
+            PackageType.CUSTOM,
+            PackageType.LIFETIME,
+            PackageType.ANNUAL,
+            PackageType.SIX_MONTH,
+            PackageType.THREE_MONTH,
+            PackageType.TWO_MONTH,
+            PackageType.MONTHLY,
+            PackageType.WEEKLY
+            -> {}
+        }
+    }
+}
