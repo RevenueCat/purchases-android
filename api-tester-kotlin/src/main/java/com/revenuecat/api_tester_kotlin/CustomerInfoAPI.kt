8b58695db7730ccf3e2c201cf824e95f880efcd9
NachoSoto <ignaciosoto90@gmail.com>
2021-12-22 17:41:21 -0800

    Added api-tester-kotlin

---
+++ api-tester-kotlin/src/main/java/com/revenuecat/api_tester_kotlin/CustomerInfoAPI.kt
@@ -0,0 +1,29 @@
+package com.revenuecat.api_tester_kotlin
+
+import android.net.Uri
+import com.revenuecat.purchases.CustomerInfo
+import com.revenuecat.purchases.EntitlementInfos
+import com.revenuecat.purchases.models.Transaction
+import java.util.Date
+
+@Suppress("unused")
+private class CustomerInfoAPI {
+    fun check() {
+        val customerInfo: CustomerInfo? = null
+
+        val entitlementInfo: EntitlementInfos = customerInfo!!.entitlements
+        val asubs = customerInfo.activeSubscriptions
+        val skus: Set<String> = customerInfo.allPurchasedSkus
+        val led: Date? = customerInfo.latestExpirationDate
+        val nst: List<Transaction> = customerInfo.nonSubscriptionTransactions
+        val opd: Date? = customerInfo.originalPurchaseDate
+        val rd: Date = customerInfo.requestDate
+        val fs: Date = customerInfo.firstSeen
+        val oaui: String = customerInfo.originalAppUserId
+        val mu: Uri? = customerInfo.managementURL
+        val eds: Date? = customerInfo.getExpirationDateForSku("")
+        val pds: Date? = customerInfo.getPurchaseDateForSku("")
+        val ede: Date? = customerInfo.getExpirationDateForEntitlement("")
+        val pde: Date? = customerInfo.getPurchaseDateForEntitlement("")
+    }
+}
