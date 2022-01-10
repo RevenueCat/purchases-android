2b8c0fa9eb020aa0fe9076bb13eec0d65289531b
Cesar de la Vega <cesarvegaro@gmail.com>
2022-01-10 18:45:02 +0100

    Update AmazonStrings.kt

--- feature/amazon/src/main/java/com/revenuecat/purchases/amazon/AmazonStrings.kt
+++ feature/amazon/src/main/java/com/revenuecat/purchases/amazon/AmazonStrings.kt
@@ -32,7 +32,7 @@ object AmazonStrings {
         "Failed to get user data. Call is not supported."
     const val ERROR_USER_DATA_STORE_PROBLEM =
         "Failed to get user data. There was an Amazon store problem."
-    const val PRICE_EXTRACTION_ITERATING_OVER_AVAILABLE_CURRENCIES = "Currency symbol is not the same as Locale's. " +
+    const val PRICE_EXTRACTION_ITERATING_OVER_AVAILABLE_CURRENCIES = "Currency symbol from Amazon price is not the same as Locale's. " +
         "Iterating over all available currencies to find the currency code."
     const val PRICE_EXTRACTION_USING_CURRENCY_SYMBOL = "Couldn't determine currencyCode. Setting currencyCode to " +
         "symbol sent by Amazon"
