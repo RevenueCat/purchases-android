5b7ebb979472dc177b0a44fc3dc75fd3d2e8bb7d
Cesar de la Vega <cesarvegaro@gmail.com>
2022-01-11 23:01:49 +0100

    adds missing JvmSynthetic

--- purchases/src/main/kotlin/com/revenuecat/purchases/Purchases.kt
+++ purchases/src/main/kotlin/com/revenuecat/purchases/Purchases.kt
@@ -105,7 +105,7 @@ class Purchases @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE) intern
     private val dispatcher: Dispatcher,
     private val identityManager: IdentityManager,
     private val subscriberAttributesManager: SubscriberAttributesManager,
-    @JvmSynthetic internal var appConfig: AppConfig
+    @get:JvmSynthetic internal var appConfig: AppConfig
 ) : LifecycleDelegate {
 
     /** @suppress */
