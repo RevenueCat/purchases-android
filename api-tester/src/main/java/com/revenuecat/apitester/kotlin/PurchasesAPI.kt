c65f4dcd32bf59b3614cec75c86f841e604f6929
Cesar de la Vega <cesarvegaro@gmail.com>
2022-01-12 16:19:01 +0100

    Fix api tester for Amazon branch (#440)
    * created DeprecatedPurchasesAPI
    
    * Fixed storeproduct and storetransaction
    
    * fix PurchasesAPI
    
    * GetStoreProductsCallback
    
    * clean up DeprecatedPurchasesAPI
    
    * Fix PaymentTransaction
    
    * created DeprecatedPurchasesAPI
    
    * fix Java api-testers

--- api-tester/src/main/java/com/revenuecat/apitester/kotlin/PurchasesAPI.kt
+++ api-tester/src/main/java/com/revenuecat/apitester/kotlin/PurchasesAPI.kt
@@ -2,28 +2,29 @@ package com.revenuecat.apitester.kotlin
 
 import android.app.Activity
 import android.content.Context
-import com.android.billingclient.api.Purchase
-import com.android.billingclient.api.SkuDetails
 import com.revenuecat.purchases.BillingFeature
 import com.revenuecat.purchases.CustomerInfo
 import com.revenuecat.purchases.Offerings
 import com.revenuecat.purchases.Package
 import com.revenuecat.purchases.Purchases
+import com.revenuecat.purchases.PurchasesConfiguration
 import com.revenuecat.purchases.PurchasesError
 import com.revenuecat.purchases.UpgradeInfo
 import com.revenuecat.purchases.getCustomerInfoWith
 import com.revenuecat.purchases.getNonSubscriptionSkusWith
 import com.revenuecat.purchases.getOfferingsWith
 import com.revenuecat.purchases.getSubscriptionSkusWith
-import com.revenuecat.purchases.interfaces.GetSkusResponseListener
+import com.revenuecat.purchases.interfaces.GetStoreProductsCallback
 import com.revenuecat.purchases.interfaces.LogInCallback
-import com.revenuecat.purchases.interfaces.MakePurchaseListener
-import com.revenuecat.purchases.interfaces.ProductChangeListener
-import com.revenuecat.purchases.interfaces.ReceiveCustomerInfoListener
-import com.revenuecat.purchases.interfaces.ReceiveOfferingsListener
+import com.revenuecat.purchases.interfaces.ProductChangeCallback
+import com.revenuecat.purchases.interfaces.PurchaseCallback
+import com.revenuecat.purchases.interfaces.ReceiveCustomerInfoCallback
+import com.revenuecat.purchases.interfaces.ReceiveOfferingsCallback
 import com.revenuecat.purchases.interfaces.UpdatedCustomerInfoListener
 import com.revenuecat.purchases.logInWith
 import com.revenuecat.purchases.logOutWith
+import com.revenuecat.purchases.models.StoreProduct
+import com.revenuecat.purchases.models.StoreTransaction
 import com.revenuecat.purchases.purchasePackageWith
 import com.revenuecat.purchases.purchaseProductWith
 import com.revenuecat.purchases.restorePurchasesWith
@@ -36,28 +37,28 @@ private class PurchasesAPI {
     fun check(
         purchases: Purchases,
         activity: Activity,
-        skuDetails: SkuDetails,
+        storeProduct: StoreProduct,
         packageToPurchase: Package,
         upgradeInfo: UpgradeInfo
     ) {
         val skus = ArrayList<String>()
-        val receiveOfferingsListener = object : ReceiveOfferingsListener {
+        val receiveOfferingsCallback = object : ReceiveOfferingsCallback {
             override fun onReceived(offerings: Offerings) {}
             override fun onError(error: PurchasesError) {}
         }
-        val skusResponseListener = object : GetSkusResponseListener {
-            override fun onReceived(skus: List<SkuDetails>) {}
+        val skusResponseCallback = object : GetStoreProductsCallback {
+            override fun onReceived(storeProducts: List<StoreProduct>) {}
             override fun onError(error: PurchasesError) {}
         }
-        val purchaseChangeListener = object : ProductChangeListener {
-            override fun onCompleted(purchase: Purchase?, customerInfo: CustomerInfo) {}
+        val purchaseChangeCallback = object : ProductChangeCallback {
+            override fun onCompleted(storeTransaction: StoreTransaction?, customerInfo: CustomerInfo) {}
             override fun onError(error: PurchasesError, userCancelled: Boolean) {}
         }
-        val makePurchaseListener = object : MakePurchaseListener {
-            override fun onCompleted(purchase: Purchase, customerInfo: CustomerInfo) {}
+        val purchaseCallback = object : PurchaseCallback {
+            override fun onCompleted(storeTransaction: StoreTransaction, customerInfo: CustomerInfo) {}
             override fun onError(error: PurchasesError, userCancelled: Boolean) {}
         }
-        val receiveCustomerInfoListener = object : ReceiveCustomerInfoListener {
+        val receiveCustomerInfoCallback = object : ReceiveCustomerInfoCallback {
             override fun onReceived(customerInfo: CustomerInfo) {}
             override fun onError(error: PurchasesError) {}
         }
@@ -66,20 +67,20 @@ private class PurchasesAPI {
             override fun onError(error: PurchasesError) {}
         }
         purchases.syncPurchases()
-        purchases.getOfferings(receiveOfferingsListener)
-        purchases.getSubscriptionSkus(skus, skusResponseListener)
-        purchases.getNonSubscriptionSkus(skus, skusResponseListener)
-        purchases.purchaseProduct(activity, skuDetails, upgradeInfo, purchaseChangeListener)
-        purchases.purchaseProduct(activity, skuDetails, makePurchaseListener)
-        purchases.purchasePackage(activity, packageToPurchase, upgradeInfo, purchaseChangeListener)
-        purchases.purchasePackage(activity, packageToPurchase, makePurchaseListener)
-        purchases.restorePurchases(receiveCustomerInfoListener)
+        purchases.getOfferings(receiveOfferingsCallback)
+        purchases.getSubscriptionSkus(skus, skusResponseCallback)
+        purchases.getNonSubscriptionSkus(skus, skusResponseCallback)
+        purchases.purchaseProduct(activity, storeProduct, upgradeInfo, purchaseChangeCallback)
+        purchases.purchaseProduct(activity, storeProduct, purchaseCallback)
+        purchases.purchasePackage(activity, packageToPurchase, upgradeInfo, purchaseChangeCallback)
+        purchases.purchasePackage(activity, packageToPurchase, purchaseCallback)
+        purchases.restorePurchases(receiveCustomerInfoCallback)
         purchases.logIn("", logInCallback)
         purchases.logOut()
-        purchases.logOut(receiveCustomerInfoListener)
+        purchases.logOut(receiveCustomerInfoCallback)
         val appUserID: String = purchases.appUserID
 
-        purchases.getCustomerInfo(receiveCustomerInfoListener)
+        purchases.getCustomerInfo(receiveCustomerInfoCallback)
         purchases.removeUpdatedCustomerInfoListener()
         purchases.invalidateCustomerInfoCache()
         purchases.close()
@@ -100,7 +101,7 @@ private class PurchasesAPI {
         purchases: Purchases,
         activity: Activity,
         packageToPurchase: Package,
-        skuDetails: SkuDetails,
+        storeProduct: StoreProduct,
         upgradeInfo: UpgradeInfo
     ) {
         purchases.getOfferingsWith(
@@ -109,29 +110,29 @@ private class PurchasesAPI {
         )
         purchases.purchaseProductWith(
             activity,
-            skuDetails,
+            storeProduct,
             onError = { _: PurchasesError, _: Boolean -> },
-            onSuccess = { _: Purchase, _: CustomerInfo -> }
+            onSuccess = { _: StoreTransaction, _: CustomerInfo -> }
         )
         purchases.purchaseProductWith(
             activity,
-            skuDetails,
+            storeProduct,
             upgradeInfo,
             onError = { _: PurchasesError, _: Boolean -> },
-            onSuccess = { _: Purchase?, _: CustomerInfo -> }
+            onSuccess = { _: StoreTransaction?, _: CustomerInfo -> }
         )
         purchases.purchasePackageWith(
             activity,
             packageToPurchase,
             upgradeInfo,
             onError = { _: PurchasesError, _: Boolean -> },
-            onSuccess = { _: Purchase?, _: CustomerInfo -> }
+            onSuccess = { _: StoreTransaction?, _: CustomerInfo -> }
         )
         purchases.purchasePackageWith(
             activity,
             packageToPurchase,
             onError = { _: PurchasesError, _: Boolean -> },
-            onSuccess = { _: Purchase?, _: CustomerInfo -> }
+            onSuccess = { _: StoreTransaction, _: CustomerInfo -> }
         )
         purchases.restorePurchasesWith(
             onError = { _: PurchasesError -> },
@@ -153,12 +154,12 @@ private class PurchasesAPI {
         purchases.getSubscriptionSkusWith(
             ArrayList<String>(),
             onError = { _: PurchasesError -> },
-            onReceiveSkus = { _: List<SkuDetails> -> }
+            onReceiveSkus = { _: List<StoreProduct> -> }
         )
         purchases.getNonSubscriptionSkusWith(
             ArrayList<String>(),
             onError = { _: PurchasesError -> },
-            onReceiveSkus = { _: List<SkuDetails> -> }
+            onReceiveSkus = { _: List<StoreProduct> -> }
         )
     }
 
@@ -190,10 +191,14 @@ private class PurchasesAPI {
         val features: List<BillingFeature> = ArrayList()
         val configured: Boolean = Purchases.isConfigured
 
-        Purchases.configure(context, "")
-        Purchases.configure(context, "", "")
-        Purchases.configure(context, "", "", true)
-        Purchases.configure(context, "", "", false, executorService)
+        val build = PurchasesConfiguration.Builder(context, apiKey = "")
+            .appUserID("")
+            .observerMode(true)
+            .observerMode(false)
+            .service(executorService)
+            .build()
+
+        Purchases.configure(build)
 
         Purchases.canMakePayments(context, features) { _: Boolean -> }
         Purchases.canMakePayments(context) { _: Boolean -> }
@@ -205,8 +210,6 @@ private class PurchasesAPI {
         val url: URL? = Purchases.proxyURL
 
         val instance: Purchases = Purchases.sharedInstance
-
-        // TODO: add the builder version once amazon is merged
     }
 
     fun check(network: Purchases.AttributionNetwork) {
