80b2bfc650913c2385adb0f9f2ccac25ee6dd800
Maddie Beyl <maddie@revenuecat.com>
2022-01-31 15:16:53 -0800

    remove unused imports

--- examples/purchase-tester/src/main/java/com/revenuecat/purchasetester/OverviewFragment.kt
+++ examples/purchase-tester/src/main/java/com/revenuecat/purchasetester/OverviewFragment.kt
@@ -1,13 +1,10 @@
 package com.revenuecat.purchasetester
 
-import android.content.ClipData
-import android.content.ClipboardManager
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat.getSystemService
 import androidx.core.view.doOnPreDraw
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.FragmentNavigatorExtras
@@ -18,8 +15,8 @@ import com.revenuecat.purchases.EntitlementInfo
 import com.revenuecat.purchases.Offering
 import com.revenuecat.purchases.Offerings
 import com.revenuecat.purchases.Purchases
-import com.revenuecat.purchases.getOfferingsWith
 import com.revenuecat.purchases.getCustomerInfoWith
+import com.revenuecat.purchases.getOfferingsWith
 import com.revenuecat.purchases.logOutWith
 import com.revenuecat.purchasetester.databinding.FragmentOverviewBinding
 
