6f7a69b97e1f7fddca2a61de7a1e536d564cbc5e
Toni Rico <antonio.rico.diez@revenuecat.com>
2022-06-15 10:23:03 +0200

    Setup comments to each of the cache fetch policies for better documentation

--- purchases/src/main/kotlin/com/revenuecat/purchases/CacheFetchPolicy.kt
+++ purchases/src/main/kotlin/com/revenuecat/purchases/CacheFetchPolicy.kt
@@ -1,19 +1,30 @@
 package com.revenuecat.purchases
 
 /**
- * Specifies behavior for a caching API. Current values:
- * - CACHE_ONLY: Returns values from the cache, or throws an error if not available. It won't initiate a fetch.
- * - FETCH_CURRENT: Ignore whether the cache has a value or not and fetch the most up-to-date data. This will return an
- *  error if the fetch request fails
- * - NOT_STALE_CACHED_OR_CURRENT: Returns the cached data if available and not stale. If not available or stale,
- *  it fetches up-to-date data. Note that this won't return stale data even if fetch request fails
- * - CACHED_OR_FETCHED: (default) returns the cached data if available (even if stale). If not available, fetches
- *  up-to-date data. If cached data is staled, it initiates a fetch in the background.
+ * Specifies behavior for a caching API.
  */
 enum class CacheFetchPolicy {
+    /**
+     * Returns values from the cache, or throws an error if not available. It won't initiate a fetch.
+     */
     CACHE_ONLY,
+
+    /**
+     * Ignore whether the cache has a value or not and fetch the most up-to-date data. This will return an
+     *  error if the fetch request fails
+     */
     FETCH_CURRENT,
+
+    /**
+     * Returns the cached data if available and not stale. If not available or stale,
+     *  it fetches up-to-date data. Note that this won't return stale data even if fetch request fails
+     */
     NOT_STALE_CACHED_OR_CURRENT,
+
+    /**
+     * (default) returns the cached data if available (even if stale). If not available, fetches
+     *  up-to-date data. If cached data is stale, it initiates a fetch in the background.
+     */
     CACHED_OR_FETCHED;
 
     companion object {
