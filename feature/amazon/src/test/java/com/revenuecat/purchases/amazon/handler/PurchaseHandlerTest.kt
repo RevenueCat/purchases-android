bfed36f8d8feef7486037676a679a8a7cd7811ae
Cesar de la Vega <cesarvegaro@gmail.com>
2022-07-01 15:23:14 +0200

    fixes test

--- feature/amazon/src/test/java/com/revenuecat/purchases/amazon/handler/PurchaseHandlerTest.kt
+++ feature/amazon/src/test/java/com/revenuecat/purchases/amazon/handler/PurchaseHandlerTest.kt
@@ -319,7 +319,7 @@ class PurchaseHandlerTest {
         assertThat(broadcastIntentSlot.isCaptured).isTrue
         val captured = broadcastIntentSlot.captured
         assertThat(captured.`package`).isEqualTo(packageName)
-        assertThat(captured.action).isEqualTo(ProxyAmazonBillingActivity.EXTRAS_REQUEST_ID)
+        assertThat(captured.action).isEqualTo(ProxyAmazonBillingActivityBroadcastReceiver.PURCHASE_FINISHED_ACTION)
     }
 
     @Test
