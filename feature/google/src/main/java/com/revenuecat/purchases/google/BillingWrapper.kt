ef704205fa6c65e84a6da92d62b5324e11eb1a7e
Maddie Beyl <maddie@revenuecat.com>
2022-08-15 10:06:44 -0500

    Revert "example of refactoring querypurchases"
    This reverts commit 4a9c68b2ecacffb4da7737cbfe43415a977767b4.

--- feature/google/src/main/java/com/revenuecat/purchases/google/BillingWrapper.kt
+++ feature/google/src/main/java/com/revenuecat/purchases/google/BillingWrapper.kt
@@ -389,44 +389,35 @@ class BillingWrapper(
         onSuccess: (Map<String, StoreTransaction>) -> Unit,
         onError: (PurchasesError) -> Unit
     ) {
-        queryPurchasesBySkuType(SkuType.SUBS,
-            { mapOfActiveSubscriptions ->
-                queryPurchasesBySkuType(SkuType.INAPP,
-                    { mapOfUnconsumedInAppPurchases ->
-                        onSuccess(mapOfActiveSubscriptions + mapOfUnconsumedInAppPurchases)
-                    },
-                    {
-                    return@queryPurchasesBySkuType
-                    },
-                )
-            },
-            {
-                return@queryPurchasesBySkuType
-            }
-        )
-    }
-
-    private fun queryPurchasesBySkuType(
-        @SkuType skuType: String,
-        onSuccess: (Map<String, StoreTransaction>) -> Unit,
-        onError: (PurchasesError) -> Unit
-    ) {
         withConnectedClient {
             log(LogIntent.DEBUG, RestoreStrings.QUERYING_PURCHASE)
-            this.queryPurchasesAsync(skuType) { billingResult, purchases ->
-                if (!billingResult.isSuccessful()) {
-                    val purchasesError = billingResult.responseCode.billingResponseToPurchasesError(
-                        RestoreStrings.QUERYING_PURCHASES_ERROR.format(
-                            skuType,
-                            billingResult.toHumanReadableDescription()
-                        )
+
+            this.queryPurchasesAsync(SkuType.SUBS) querySubPurchasesAsync@{ activeSubsResult, activeSubsPurchases ->
+                if (!activeSubsResult.isSuccessful()) {
+                    val purchasesError = activeSubsResult.responseCode.billingResponseToPurchasesError(
+                        RestoreStrings.QUERYING_SUBS_ERROR.format(activeSubsResult.toHumanReadableDescription())
                     )
                     onError(purchasesError)
-                    return@queryPurchasesAsync
+                    return@querySubPurchasesAsync
                 }
 
-                val mapOfPurchases = purchases.toMapOfGooglePurchaseWrapper(skuType)
-                onSuccess(mapOfPurchases)
+                val mapOfActiveSubscriptions = activeSubsPurchases.toMapOfGooglePurchaseWrapper(SkuType.SUBS)
+
+                this.queryPurchasesAsync(SkuType.INAPP) queryInAppsPurchasesAsync@{
+                        unconsumedInAppsResult, unconsumedInAppsPurchases ->
+                    if (!unconsumedInAppsResult.isSuccessful()) {
+                        val purchasesError = unconsumedInAppsResult.responseCode.billingResponseToPurchasesError(
+                            RestoreStrings.QUERYING_INAPP_ERROR.format(
+                                unconsumedInAppsResult.toHumanReadableDescription()
+                            )
+                        )
+                        onError(purchasesError)
+                        return@queryInAppsPurchasesAsync
+                    }
+                    val mapOfUnconsumedInApps =
+                        unconsumedInAppsPurchases.toMapOfGooglePurchaseWrapper(SkuType.INAPP)
+                    onSuccess(mapOfActiveSubscriptions + mapOfUnconsumedInApps)
+                }
             }
         }
     }
