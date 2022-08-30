baa6e978169c6ec76c971808013732129a00fbc0
Maddie Beyl <maddie@revenuecat.com>
2022-08-30 10:59:25 -0500

    fix stubs for bc5

--- test-utils/src/unityIAP/java/com/revenuecat/purchases/utils/stubsBillingClient4.kt
+++ test-utils/src/unityIAP/java/com/revenuecat/purchases/utils/stubsBillingClient4.kt
@@ -6,35 +6,6 @@ import com.android.billingclient.api.PurchaseHistoryRecord
 import com.android.billingclient.api.SkuDetails
 import org.json.JSONArray
 
-@SuppressWarnings("LongParameterList", "MagicNumber")
-fun stubSkuDetails(
-    productId: String = "monthly_intro_pricing_one_week",
-    @BillingClient.ProductType type: String = BillingClient.ProductType.SUBS,
-    price: Double = 4.99,
-    subscriptionPeriod: String = "P1M",
-    freeTrialPeriod: String? = null,
-    introductoryPricePeriod: String? = null
-): SkuDetails = SkuDetails("""
-            {
-              "skuDetailsToken":"AEuhp4KxWQR-b-OAOXVicqHM4QqnqK9vkPnOXw0vSB9zWPBlTsW8TmtjSEJ_rJ6f0_-i",
-              "productId":"$productId",
-              "type":"$type",
-              "price":"${'$'}$price",
-              "price_amount_micros":${price.times(1_000_000)},
-              "price_currency_code":"USD",
-              "subscriptionPeriod":"$subscriptionPeriod",
-              "freeTrialPeriod":"$freeTrialPeriod",
-              ${ introductoryPricePeriod?.let { """
-                  "introductoryPricePeriod":"$it",
-                  "introductoryPriceAmountMicros":990000,
-                  "introductoryPrice":"${'$'}0.99",
-                  "introductoryPriceCycles":2,
-              """.trimIndent() } ?: "" }
-              "title":"Monthly Product Intro Pricing One Week (PurchasesSample)",
-              "description":"Monthly Product Intro Pricing One Week"
-            }    
-        """.trimIndent())
-
 fun stubGooglePurchase(
     productIds: List<String> = listOf("com.revenuecat.lifetime"),
     purchaseTime: Long = System.currentTimeMillis(),
@@ -71,3 +42,33 @@ fun stubPurchaseHistoryRecord(
                 "purchaseToken": "$purchaseToken"
             }
         """.trimIndent(), signature)
+
+@SuppressWarnings("LongParameterList", "MagicNumber")
+fun stubSkuDetails(
+    productId: String = "monthly_intro_pricing_one_week",
+    @BillingClient.SkuType type: String = BillingClient.SkuType.SUBS,
+    price: Double = 4.99,
+    subscriptionPeriod: String = "P1M",
+    freeTrialPeriod: String? = null,
+    introductoryPricePeriod: String? = null
+): SkuDetails = SkuDetails("""
+            {
+              "skuDetailsToken":"AEuhp4KxWQR-b-OAOXVicqHM4QqnqK9vkPnOXw0vSB9zWPBlTsW8TmtjSEJ_rJ6f0_-i",
+              "productId":"$productId",
+              "type":"$type",
+              "price":"${'$'}$price",
+              "price_amount_micros":${price.times(1_000_000)},
+              "price_currency_code":"USD",
+              "subscriptionPeriod":"$subscriptionPeriod",
+              "freeTrialPeriod":"$freeTrialPeriod",
+              ${ introductoryPricePeriod?.let { """
+                  "introductoryPricePeriod":"$it",
+                  "introductoryPriceAmountMicros":990000,
+                  "introductoryPrice":"${'$'}0.99",
+                  "introductoryPriceCycles":2,
+              """.trimIndent() } ?: "" }
+              "title":"Monthly Product Intro Pricing One Week (PurchasesSample)",
+              "description":"Monthly Product Intro Pricing One Week"
+            }    
+        """.trimIndent())
+
