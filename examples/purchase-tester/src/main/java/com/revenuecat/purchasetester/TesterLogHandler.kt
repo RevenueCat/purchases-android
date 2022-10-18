04f3f2b4bb1041ff0d8386d04c4faf3326573479
Toni Rico <antonio.rico.diez@revenuecat.com>
2022-10-18 11:57:24 +0200

    Remove duplicated prefix

--- examples/purchase-tester/src/main/java/com/revenuecat/purchasetester/TesterLogHandler.kt
+++ examples/purchase-tester/src/main/java/com/revenuecat/purchasetester/TesterLogHandler.kt
@@ -20,19 +20,19 @@ class TesterLogHandler(
     @Synchronized
     override fun d(tag: String, msg: String) {
         Log.d(tag, msg)
-        mutableStoredLogs.add(LogMessage(LogLevel.DEBUG, "DEBUG: $tag: $msg"))
+        mutableStoredLogs.add(LogMessage(LogLevel.DEBUG, "$tag: $msg"))
     }
 
     @Synchronized
     override fun i(tag: String, msg: String) {
         Log.i(tag, msg)
-        mutableStoredLogs.add(LogMessage(LogLevel.INFO, "INFO: $tag: $msg"))
+        mutableStoredLogs.add(LogMessage(LogLevel.INFO, "$tag: $msg"))
     }
 
     @Synchronized
     override fun w(tag: String, msg: String) {
         Log.w(tag, msg)
-        mutableStoredLogs.add(LogMessage(LogLevel.WARNING, "WARN: $tag: $msg"))
+        mutableStoredLogs.add(LogMessage(LogLevel.WARNING, "$tag: $msg"))
     }
 
     @Synchronized
@@ -42,7 +42,7 @@ class TesterLogHandler(
         } else {
             Log.e(tag, msg)
         }
-        mutableStoredLogs.add(LogMessage(LogLevel.ERROR, "ERROR: $tag: $msg, ${throwable?.localizedMessage}"))
+        mutableStoredLogs.add(LogMessage(LogLevel.ERROR, "$tag: $msg, ${throwable?.localizedMessage}"))
         mainHandler.post {
             Toast.makeText(applicationContext, "ERROR: $msg", Toast.LENGTH_LONG).show()
         }
