195621bfca0403be46eb9c3abbddc98db747a8b3
Maddie Beyl <maddie@revenuecat.com>
2022-10-27 13:07:49 -0400

    update package and storeproduct in apitesters

--- api-tester/src/main/java/com/revenuecat/apitester/java/StoreProductAPI.java
+++ api-tester/src/main/java/com/revenuecat/apitester/java/StoreProductAPI.java
@@ -1,6 +1,7 @@
 package com.revenuecat.apitester.java;
 
 import com.revenuecat.purchases.ProductType;
+import com.revenuecat.purchases.models.Price;
 import com.revenuecat.purchases.models.StoreProduct;
 
 import org.json.JSONObject;
@@ -8,23 +9,12 @@ import org.json.JSONObject;
 @SuppressWarnings({"unused"})
 final class StoreProductAPI {
     static void check(final StoreProduct product) {
-        final String sku = product.getSku();
+        final String sku = product.getStoreProductId();
         final ProductType type = product.getType();
-        final String price = product.getPrice();
-        final long priceAmountMicros = product.getPriceAmountMicros();
-        final String priceCurrencyCode = product.getPriceCurrencyCode();
-        final String originalPrice = product.getOriginalPrice();
-        final long originalPriceAmountMicros = product.getOriginalPriceAmountMicros();
+        final Price price = product.getOneTimeProductPrice();
         final String title = product.getTitle();
         final String description = product.getDescription();
         final String subscriptionPeriod = product.getSubscriptionPeriod();
-        final String freeTrialPeriod = product.getFreeTrialPeriod();
-        final String introductoryPrice = product.getIntroductoryPrice();
-        final long introductoryPriceAmountMicros = product.getIntroductoryPriceAmountMicros();
-        final String introductoryPricePeriod = product.getIntroductoryPricePeriod();
-        final int introductoryPriceCycles = product.getIntroductoryPriceCycles();
-        final String iconUrl = product.getIconUrl();
-        final JSONObject originalJson = product.getOriginalJson();
     }
 
     static void check(final ProductType type) {
