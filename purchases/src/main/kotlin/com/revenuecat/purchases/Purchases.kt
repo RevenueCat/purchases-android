80880f3d850bc446219119c738362bc4926aab2b
Cesar de la Vega <cesarvegaro@gmail.com>
2022-10-31 20:06:19 +0100

    List<StoreProduct>

--- purchases/src/main/kotlin/com/revenuecat/purchases/Purchases.kt
+++ purchases/src/main/kotlin/com/revenuecat/purchases/Purchases.kt
@@ -1218,7 +1218,7 @@ class Purchases internal constructor(
 
     private fun logMissingProducts(
         offerings: Offerings,
-        storeProductByID: Map<String, StoreProduct>
+        storeProductByID: Map<String, List<StoreProduct>>
     ) = offerings.all.values
         .flatMap { it.availablePackages }
         .map { it.product.sku }
