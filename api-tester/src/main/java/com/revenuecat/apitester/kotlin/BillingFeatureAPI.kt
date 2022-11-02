66ed4c92edc5d520a27bf8bdaae1668064eab5aa
Maddie Beyl <maddie@revenuecat.com>
2022-11-02 10:42:21 -0400

    add back billingfeatureapi

---
+++ api-tester/src/main/java/com/revenuecat/apitester/kotlin/BillingFeatureAPI.kt
@@ -0,0 +1,15 @@
+package com.revenuecat.apitester.kotlin
+
+import com.revenuecat.purchases.models.BillingFeature
+
+@Suppress("unused", "UNUSED_VARIABLE")
+private class BillingFeatureAPI {
+    fun check(billingFeature: BillingFeature) {
+        when (billingFeature) {
+            BillingFeature.PRICE_CHANGE_CONFIRMATION,
+            BillingFeature.SUBSCRIPTIONS,
+            BillingFeature.SUBSCRIPTIONS_UPDATE -> {
+            }
+        }.exhaustive
+    }
+}
