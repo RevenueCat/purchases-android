RevenueCat/purchases-android

commit 01d74de7708adafc92c7550a5603e6378de93e36

Wait to obtain unsubscribed attributes until finishing getting device identifiers (#604)
* Wait to obtain unsubscribed attributes until finishing getting device identifiers

* Remove test code

* Synchronize listeners access and update

* Fix and add tests for making getting attributes depend on getting device identifiers

* Cleaner way to mark listeners access as synchronized

Co-authored-by: Cesar de la Vega <cesarvegaro@gmail.com>

Co-authored-by: Cesar de la Vega <cesarvegaro@gmail.com>

feature/subscriber-attributes/src/main/java/com/revenuecat/purchases/subscriberattributes/SubscriberAttributesManager.kt
@@ -3,18 +3,22 @@ package com.revenuecat.purchases.subscriberattributes
 import android.app.Application
 import com.revenuecat.purchases.common.LogIntent
 import com.revenuecat.purchases.common.SubscriberAttributeError
-import com.revenuecat.purchases.common.subscriberattributes.DeviceIdentifiersFetcher
 import com.revenuecat.purchases.common.log
+import com.revenuecat.purchases.common.subscriberattributes.DeviceIdentifiersFetcher
 import com.revenuecat.purchases.common.subscriberattributes.SubscriberAttributeKey
 import com.revenuecat.purchases.strings.AttributionStrings
 import com.revenuecat.purchases.subscriberattributes.caching.AppUserID
+import com.revenuecat.purchases.subscriberattributes.caching.SubscriberAttributeMap
 import com.revenuecat.purchases.subscriberattributes.caching.SubscriberAttributesCache
+import java.util.Observable
 
 class SubscriberAttributesManager(
     val deviceCache: SubscriberAttributesCache,
     val backend: SubscriberAttributesPoster,
     private val deviceIdentifiersFetcher: DeviceIdentifiersFetcher
 ) {
+
+    private val obtainingDeviceIdentifiersObservable = ObtainDeviceIdentifiersObservable()
 
     @Synchronized
     fun setAttributes(attributesToSet: Map<String, String?>, appUserID: String) {
@@ -52,51 +56,59 @@ class SubscriberAttributesManager(
         currentAppUserID: AppUserID,
         completion: (() -> Unit)? = null
     ) {
-        val unsyncedStoredAttributesForAllUsers =
-            deviceCache.getUnsyncedSubscriberAttributes()
-        if (unsyncedStoredAttributesForAllUsers.isEmpty()) {
-            log(LogIntent.DEBUG, AttributionStrings.NO_SUBSCRIBER_ATTRIBUTES_TO_SYNCHRONIZE)
-            if (completion != null) {
-                completion()
-            }
-            return
-        }
-
-        val unsyncedStoredAttributesCount = unsyncedStoredAttributesForAllUsers.size
-        var currentSyncedAttributeCount = 0
-
-        unsyncedStoredAttributesForAllUsers.forEach { (syncingAppUserID, unsyncedAttributesForUser) ->
-            backend.postSubscriberAttributes(
-                unsyncedAttributesForUser.toBackendMap(),
-                syncingAppUserID,
-                {
-                    markAsSynced(syncingAppUserID, unsyncedAttributesForUser, emptyList())
-                    log(LogIntent.RC_SUCCESS, AttributionStrings.ATTRIBUTES_SYNC_SUCCESS.format(syncingAppUserID))
-                    if (currentAppUserID != syncingAppUserID) {
-                        deviceCache.clearSubscriberAttributesIfSyncedForSubscriber(syncingAppUserID)
-                    }
-                    currentSyncedAttributeCount++
-                    if (completion != null && currentSyncedAttributeCount == unsyncedStoredAttributesCount) {
-                        completion()
-                    }
-                },
-                { error, didBackendGetAttributes, attributeErrors ->
-                    if (didBackendGetAttributes) {
-                        markAsSynced(syncingAppUserID, unsyncedAttributesForUser, attributeErrors)
-                    }
-                    log(LogIntent.RC_ERROR, AttributionStrings.ATTRIBUTES_SYNC_ERROR.format(syncingAppUserID, error))
-                    currentSyncedAttributeCount++
-                    if (completion != null && currentSyncedAttributeCount == unsyncedStoredAttributesCount) {
-                        completion()
-                    }
+        obtainingDeviceIdentifiersObservable.waitUntilIdle {
+            val unsyncedStoredAttributesForAllUsers =
+                deviceCache.getUnsyncedSubscriberAttributes()
+            if (unsyncedStoredAttributesForAllUsers.isEmpty()) {
+                log(LogIntent.DEBUG, AttributionStrings.NO_SUBSCRIBER_ATTRIBUTES_TO_SYNCHRONIZE)
+                if (completion != null) {
+                    completion()
                 }
-            )
+                return@waitUntilIdle
+            }
+
+            val unsyncedStoredAttributesCount = unsyncedStoredAttributesForAllUsers.size
+            var currentSyncedAttributeCount = 0
+
+            unsyncedStoredAttributesForAllUsers.forEach { (syncingAppUserID, unsyncedAttributesForUser) ->
+                backend.postSubscriberAttributes(
+                    unsyncedAttributesForUser.toBackendMap(),
+                    syncingAppUserID,
+                    {
+                        markAsSynced(syncingAppUserID, unsyncedAttributesForUser, emptyList())
+                        log(LogIntent.RC_SUCCESS, AttributionStrings.ATTRIBUTES_SYNC_SUCCESS.format(syncingAppUserID))
+                        if (currentAppUserID != syncingAppUserID) {
+                            deviceCache.clearSubscriberAttributesIfSyncedForSubscriber(syncingAppUserID)
+                        }
+                        currentSyncedAttributeCount++
+                        if (completion != null && currentSyncedAttributeCount == unsyncedStoredAttributesCount) {
+                            completion()
+                        }
+                    },
+                    { error, didBackendGetAttributes, attributeErrors ->
+                        if (didBackendGetAttributes) {
+                            markAsSynced(syncingAppUserID, unsyncedAttributesForUser, attributeErrors)
+                        }
+                        log(
+                            LogIntent.RC_ERROR,
+                            AttributionStrings.ATTRIBUTES_SYNC_ERROR.format(syncingAppUserID, error)
+                        )
+                        currentSyncedAttributeCount++
+                        if (completion != null && currentSyncedAttributeCount == unsyncedStoredAttributesCount) {
+                            completion()
+                        }
+                    }
+                )
+            }
         }
     }
 
     @Synchronized
-    fun getUnsyncedSubscriberAttributes(appUserID: String) =
-        deviceCache.getUnsyncedSubscriberAttributes(appUserID)
+    fun getUnsyncedSubscriberAttributes(appUserID: String, completion: (SubscriberAttributeMap) -> Unit) {
+        obtainingDeviceIdentifiersObservable.waitUntilIdle {
+            completion(deviceCache.getUnsyncedSubscriberAttributes(appUserID))
+        }
+    }
 
     @Synchronized
     fun markAsSynced(
@@ -111,7 +123,8 @@ class SubscriberAttributesManager(
             return
         }
         log(
-            LogIntent.INFO, AttributionStrings.MARKING_ATTRIBUTES_SYNCED.format(appUserID) +
+            LogIntent.INFO,
+            AttributionStrings.MARKING_ATTRIBUTES_SYNCED.format(appUserID) +
                 attributesToMarkAsSynced.values.joinToString("\n")
         )
         val currentlyStoredAttributes = deviceCache.getAllStoredSubscriberAttributes(appUserID)
@@ -160,8 +173,48 @@ class SubscriberAttributesManager(
         applicationContext: Application,
         completion: (deviceIdentifiers: Map<String, String?>) -> Unit
     ) {
+        obtainingDeviceIdentifiersObservable.numberOfProcesses++
         deviceIdentifiersFetcher.getDeviceIdentifiers(applicationContext) { deviceIdentifiers ->
             completion(deviceIdentifiers)
+            obtainingDeviceIdentifiersObservable.numberOfProcesses--
+        }
+    }
+
+    // This was added to resolve: https://revenuecats.atlassian.net/browse/CSDK-433. To summarize it,
+    // setting advertising IDs happens on a non-main thread. If that process is delayed for any reason, the attributes
+    // won't be stored in the device cache in time for the sync with the backend. This is a mechanism added so we can
+    // wait until the device identifiers have been obtained before continuing with other processes.
+    private class ObtainDeviceIdentifiersObservable : Observable() {
+        var numberOfProcesses: Int = 0
+            @Synchronized
+            get
+            @Synchronized
+            set(value) {
+                if (field == value) return
+                field = value
+                setChanged()
+                notifyObservers()
+            }
+
+        @get:Synchronized
+        private val listeners: ArrayList<() -> Unit> = arrayListOf()
+
+        init {
+            addObserver { observable, _ ->
+                val numberOfProcesses = (observable as ObtainDeviceIdentifiersObservable).numberOfProcesses
+                if (numberOfProcesses == 0) {
+                    synchronized(this) {
+                        listeners.forEach { it() }
+                        listeners.clear()
+                    }
+                }
+            }
+        }
+
+        @Synchronized
+        fun waitUntilIdle(completion: () -> Unit) {
+            if (numberOfProcesses == 0) completion()
+            listeners.add { completion() }
         }
     }
 }

feature/subscriber-attributes/src/test/java/com/revenuecat/purchases/subscriberattributes/SubscriberAttributesManagerTests.kt
@@ -18,6 +18,8 @@ import io.mockk.mockk
 import io.mockk.slot
 import io.mockk.verify
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertNull
 import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
@@ -313,7 +315,7 @@ class SubscriberAttributesManagerTests {
     }
 
     @Test
-    fun `getting unsynchronized attributes`() {
+    fun `getting unsynchronized attributes finishes immediately if not currently getting device identifiers`() {
         val subscriberAttribute = SubscriberAttribute("key", null, isSynced = false)
         val expected = mapOf(
             "key" to subscriberAttribute
@@ -322,9 +324,105 @@ class SubscriberAttributesManagerTests {
             mockDeviceCache.getUnsyncedSubscriberAttributes(appUserID)
         } returns expected
 
-        val actual = underTest.getUnsyncedSubscriberAttributes(appUserID)
+        var unsyncedAttributes: Map<String, SubscriberAttribute>? = null
+        underTest.getUnsyncedSubscriberAttributes(appUserID) {
+            unsyncedAttributes = it
+        }
 
-        assertThat(actual).isEqualTo(expected)
+        assertNotNull(unsyncedAttributes)
+        assertThat(unsyncedAttributes).isEqualTo(expected)
+    }
+
+    @Test
+    fun `getting unsynchronized attributes waits if currently getting device identifiers`() {
+        val mockContext = mockk<Application>(relaxed = true)
+        val subscriberAttribute = SubscriberAttribute("key", null, isSynced = false)
+        val expected = mapOf(
+            "key" to subscriberAttribute
+        )
+        every {
+            mockDeviceCache.getUnsyncedSubscriberAttributes(appUserID)
+        } returns expected
+
+        mockSettingAttributesOnEmptyCache()
+
+        var deviceIdentifiersLambda: ((Map<String, String>) -> Unit)? = null
+        every {
+            mockDeviceIdentifiersFetcher.getDeviceIdentifiers(mockContext, captureLambda())
+        } answers {
+            deviceIdentifiersLambda = lambda<(Map<String, String>) -> Unit>().captured
+        }
+
+        underTest.setAttributionID(
+            SubscriberAttributeKey.AttributionIds.Adjust,
+            "test-adjust-id",
+            appUserID,
+            mockContext
+        )
+
+        var unsyncedAttributes: Map<String, SubscriberAttribute>? = null
+        underTest.getUnsyncedSubscriberAttributes(appUserID) {
+            unsyncedAttributes = it
+        }
+
+        assertNull(unsyncedAttributes)
+        deviceIdentifiersLambda?.invoke(mapOf("random-key" to "random-value"))
+        assertNotNull(unsyncedAttributes)
+        assertThat(unsyncedAttributes).isEqualTo(expected)
+    }
+
+    @Test
+    fun `getting unsynchronized attributes waits if currently getting device identifiers multiple times`() {
+        val mockContext = mockk<Application>(relaxed = true)
+        val subscriberAttribute = SubscriberAttribute("key", null, isSynced = false)
+        val expected = mapOf(
+            "key" to subscriberAttribute
+        )
+        every {
+            mockDeviceCache.getUnsyncedSubscriberAttributes(appUserID)
+        } returns expected
+
+        mockSettingAttributesOnEmptyCache()
+
+        var deviceIdentifiersFirstCallLambda: ((Map<String, String>) -> Unit)? = null
+        var deviceIdentifiersSecondCallLambda: ((Map<String, String>) -> Unit)? = null
+        var deviceIdentifiersFetchInvocationCount = 0
+        every {
+            mockDeviceIdentifiersFetcher.getDeviceIdentifiers(mockContext, captureLambda())
+        } answers {
+            deviceIdentifiersFetchInvocationCount++
+            if (deviceIdentifiersFetchInvocationCount == 1) {
+                deviceIdentifiersFirstCallLambda = lambda<(Map<String, String>) -> Unit>().captured
+            } else if (deviceIdentifiersFetchInvocationCount == 2) {
+                deviceIdentifiersSecondCallLambda = lambda<(Map<String, String>) -> Unit>().captured
+            }
+        }
+
+        underTest.setAttributionID(
+            SubscriberAttributeKey.AttributionIds.Adjust,
+            "test-adjust-id",
+            appUserID,
+            mockContext
+        )
+
+        underTest.setAttributionID(
+            SubscriberAttributeKey.AttributionIds.Facebook,
+            "test-facebook-id",
+            appUserID,
+            mockContext
+        )
+
+        var unsyncedAttributes: Map<String, SubscriberAttribute>? = null
+        underTest.getUnsyncedSubscriberAttributes(appUserID) {
+            unsyncedAttributes = it
+        }
+
+        assertNull(unsyncedAttributes)
+        deviceIdentifiersFirstCallLambda?.invoke(mapOf("random-key" to "random-value"))
+        assertNull(unsyncedAttributes)
+        deviceIdentifiersSecondCallLambda?.invoke(mapOf("random-key-2" to "random-value-2"))
+        assertNotNull(unsyncedAttributes)
+        assertThat(unsyncedAttributes).isEqualTo(expected)
     }
 
     @Test

commit e90c48cb502a081af32b7150932ddd0ed7734e51

Fix for calling getUnsyncedSubscriberAttributes callback twice (#687)
### Description
https://revenuecats.atlassian.net/browse/CSDK-520

This issue could happen in any situation in which we try to get unsynced
subscriber attributes before getting device identifiers. For example, if
we called `collectDeviceIdentifiers` or one of the set attribution ID
methods after calling `logIn`. In those cases, the completion blocks
from getting unsynced subscriber attributes could be called twice. This
could also cause us to send these attributes multiple times to the
backend among other side effects. This issue was introduced in #604.

feature/subscriber-attributes/src/main/java/com/revenuecat/purchases/subscriberattributes/SubscriberAttributesManager.kt
@@ -214,7 +214,7 @@ class SubscriberAttributesManager(
         @Synchronized
         fun waitUntilIdle(completion: () -> Unit) {
             if (numberOfProcesses == 0) completion()
-            listeners.add { completion() }
+            else listeners.add { completion() }
         }
     }
 }

feature/subscriber-attributes/src/test/java/com/revenuecat/purchases/subscriberattributes/SubscriberAttributesManagerTests.kt
@@ -18,6 +18,7 @@ import io.mockk.mockk
 import io.mockk.slot
 import io.mockk.verify
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
 import org.junit.Assert.assertNull
 import org.junit.Assert.assertTrue
@@ -423,6 +424,45 @@ class SubscriberAttributesManagerTests {
         deviceIdentifiersSecondCallLambda?.invoke(mapOf("random-key-2" to "random-value-2"))
         assertNotNull(unsyncedAttributes)
         assertThat(unsyncedAttributes).isEqualTo(expected)
+    }
+
+    @Test
+    fun `getting unsynchronized attributes calls completion only once`() {
+        val mockContext = mockk<Application>(relaxed = true)
+        val subscriberAttribute = SubscriberAttribute("key", null, isSynced = false)
+        val expected = mapOf(
+            "key" to subscriberAttribute
+        )
+        every {
+            mockDeviceCache.getUnsyncedSubscriberAttributes(appUserID)
+        } returns expected
+
+        mockSettingAttributesOnEmptyCache()
+
+        var deviceIdentifiersCallLambda: ((Map<String, String>) -> Unit)? = null
+        every {
+            mockDeviceIdentifiersFetcher.getDeviceIdentifiers(mockContext, captureLambda())
+        } answers {
+            deviceIdentifiersCallLambda = lambda<(Map<String, String>) -> Unit>().captured
+        }
+
+        var callbackCallCount = 0
+        underTest.getUnsyncedSubscriberAttributes(appUserID) {
+            callbackCallCount += 1
+        }
+
+        underTest.setAttributionID(
+            SubscriberAttributeKey.AttributionIds.Facebook,
+            "test-facebook-id",
+            appUserID,
+            mockContext
+        )
+
+        assertEquals(1, callbackCallCount)
+
+        deviceIdentifiersCallLambda!!.invoke(emptyMap())
+
+        assertEquals(1, callbackCallCount)
     }
 
     @Test