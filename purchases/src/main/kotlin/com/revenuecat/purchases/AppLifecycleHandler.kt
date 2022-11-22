be5a24769bbaf11579668928c952ca62e0b9561b
Cesar de la Vega <cesarvegaro@gmail.com>
2022-11-22 15:00:50 +0100

    adds implementations

--- purchases/src/main/kotlin/com/revenuecat/purchases/AppLifecycleHandler.kt
+++ purchases/src/main/kotlin/com/revenuecat/purchases/AppLifecycleHandler.kt
@@ -3,16 +3,30 @@ package com.revenuecat.purchases
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 
+@SuppressWarnings("EmptyFunctionBlock")
 internal class AppLifecycleHandler(
     private val lifecycleDelegate: LifecycleDelegate,
 ) : DefaultLifecycleObserver {
+    // Some functions are implemented and left empty to prevent issues with Java default interfaces
+
+    override fun onCreate(owner: LifecycleOwner) {
+    }
 
     override fun onStart(owner: LifecycleOwner) {
         lifecycleDelegate.onAppForegrounded()
     }
 
+    override fun onResume(owner: LifecycleOwner) {
+    }
+
+    override fun onPause(owner: LifecycleOwner) {
+    }
+
     override fun onStop(owner: LifecycleOwner) {
         lifecycleDelegate.onAppBackgrounded()
+    }
+
+    override fun onDestroy(owner: LifecycleOwner) {
     }
 }
 
