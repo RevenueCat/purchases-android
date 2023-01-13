62d153558eec08e9df2d063d9b615ea6317805aa
Maddie Beyl <maddie@revenuecat.com>
2023-01-13 09:13:19 -0500

    Set default and up date doc

--- purchases/src/main/kotlin/com/revenuecat/purchases/UpgradeInfo.kt
+++ purchases/src/main/kotlin/com/revenuecat/purchases/UpgradeInfo.kt
@@ -5,9 +5,11 @@ import com.android.billingclient.api.BillingFlowParams
 /**
  * This object holds the information used when upgrading from another sku.
  * @property oldProductId The old product ID to upgrade from.
- * @property prorationMode The [BillingFlowParams.ProrationMode] to use when upgrading the given oldSku.
+ * @property prorationMode The [BillingFlowParams.ProrationMode] to use when upgrading the given oldSku. Defaults to
+ * [BillingFlowParams.ProrationMode.IMMEDIATE_WITHOUT_PRORATION].
  */
 data class UpgradeInfo @JvmOverloads constructor(
     val oldProductId: String,
-    @BillingFlowParams.ProrationMode val prorationMode: Int? = null
+    @BillingFlowParams.ProrationMode val prorationMode: Int? =
+        BillingFlowParams.ProrationMode.IMMEDIATE_WITHOUT_PRORATION
 )
