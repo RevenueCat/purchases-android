8560b707bdde6d970459ce9d516bffec66887e67
Maddie Beyl <maddie@revenuecat.com>
2023-01-19 17:49:24 -0500

    Make ReceiptInfo data class now that it doesn't have SkuDetails

--- common/src/main/java/com/revenuecat/purchases/common/ReceiptInfo.kt
+++ common/src/main/java/com/revenuecat/purchases/common/ReceiptInfo.kt
@@ -3,7 +3,7 @@ package com.revenuecat.purchases.common
 import com.revenuecat.purchases.models.PricingPhase
 import com.revenuecat.purchases.models.StoreProduct
 
-class ReceiptInfo(
+data class ReceiptInfo(
     val productIDs: List<String>,
     val offeringIdentifier: String? = null,
     val subscriptionOptionId: String? = null,
@@ -16,39 +16,4 @@ class ReceiptInfo(
     val duration: String? = storeProduct?.subscriptionPeriod?.takeUnless { it.isEmpty() }
     val pricingPhases: List<PricingPhase>? =
         storeProduct?.subscriptionOptions?.firstOrNull { it.id == subscriptionOptionId }?.pricingPhases
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as ReceiptInfo
-
-        if (productIDs != other.productIDs) return false
-        if (offeringIdentifier != other.offeringIdentifier) return false
-        if (price != other.price) return false
-        if (currency != other.currency) return false
-        if (subscriptionOptionId != other.subscriptionOptionId) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = productIDs.hashCode()
-        result = 31 * result + (offeringIdentifier?.hashCode() ?: 0)
-        result = 31 * result + (storeProduct?.hashCode() ?: 0)
-        result = 31 * result + (subscriptionOptionId?.hashCode() ?: 0)
-        return result
-    }
-
-    override fun toString(): String {
-        return "ReceiptInfo(" +
-            "productIDs='${productIDs.joinToString()}', " +
-            "offeringIdentifier=$offeringIdentifier, " +
-            "storeProduct=$storeProduct, " +
-            "subscriptionOptionId=$subscriptionOptionId, " +
-            "pricingPhases=$pricingPhases, " +
-            "price=$price, " +
-            "currency=$currency, " +
-            "duration=$duration)"
-    }
 }
