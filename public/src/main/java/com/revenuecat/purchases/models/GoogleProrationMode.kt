bdc69c7bd8dbbdd7227caf3612882a1da8660e0d
Maddie Beyl <maddie@revenuecat.com>
2023-01-24 10:24:23 -0500

    rename playbillingClientMode, update docs for clarity

--- public/src/main/java/com/revenuecat/purchases/models/GoogleProrationMode.kt
+++ public/src/main/java/com/revenuecat/purchases/models/GoogleProrationMode.kt
@@ -4,13 +4,13 @@ import com.android.billingclient.api.BillingFlowParams
 
 /**
  * Enum of possible proration modes to be passed to a Google Play purchase.
- * Ignored for Amazon purchases. <-- TODOBC5 confirm
+ * Ignored for Amazon purchases.
  *
  * See https://developer.android.com/google/play/billing/subscriptions#proration for examples
  */
-enum class GoogleProrationMode(@BillingFlowParams.ProrationMode val playBillingClientName: Int) {
+enum class GoogleProrationMode(@BillingFlowParams.ProrationMode val playBillingClientMode: Int) {
     /**
-     * New subscription benefits take effect immediately.
+     * Old subscription is cancelled, and new subscription takes effect immediately.
      * User is charged for the full price of the new subscription on the old subscription's expiration date.
      *
      * This is the default behavior.
@@ -18,11 +18,12 @@ enum class GoogleProrationMode(@BillingFlowParams.ProrationMode val playBillingC
     IMMEDIATE_WITHOUT_PRORATION(BillingFlowParams.ProrationMode.IMMEDIATE_WITHOUT_PRORATION),
 
     /**
-     * New subscription benefits take effect immediately.
+     * Old subscription is cancelled, and new subscription takes effect immediately.
      * Any time remaining on the old subscription is used to push out the first payment date for the new subscription.
      * User is charged the full price of new subscription once that prorated time has passed.
      *
-     * The purchase will fail if this mode is used when switching between [PurchaseOption]s of the same [StoreProduct].
+     * The purchase will fail if this mode is used when switching between [SubscriptionOption]s
+     * of the same [StoreProduct].
      */
     IMMEDIATE_WITH_TIME_PRORATION(BillingFlowParams.ProrationMode.IMMEDIATE_WITH_TIME_PRORATION)
 }
