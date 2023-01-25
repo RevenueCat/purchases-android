c4afc89a4cf020bad4be1a123b15e3dccbcb45f9
Maddie Beyl <maddie@revenuecat.com>
2023-01-25 13:29:32 -0500

    add javadocs for PricingPhase, add a couple todos for recurrencemode

--- public/src/main/java/com/revenuecat/purchases/models/PricingPhase.kt
+++ public/src/main/java/com/revenuecat/purchases/models/PricingPhase.kt
@@ -3,13 +3,34 @@ package com.revenuecat.purchases.models
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
+/**
+ * Encapsulates how a user pays for a subscription at a given point in time.
+ */
 @Parcelize
-// TODO javadocs
 data class PricingPhase(
+    /**
+     * Billing period for which the [PricingPhase] applies, in ISO 8601 format (i.e. one month -> P1M)
+     */
     val billingPeriod: String,
+
+    /**
+     * The currency code in ISO 4217 format.
+     */
     val priceCurrencyCode: String,
+
+    /**
+     * Formatted price for the [PricingPhase], including currency sign.
+     */
     val formattedPrice: String,
+
+    /**
+     * Price for the payment cycle in micro-units, where 1M micro-units == one unit in the currency
+     */
     val priceAmountMicros: Long,
+
+    /**
+     * [RecurrenceMode] of the [PricingPhase]
+     */
     val recurrenceMode: RecurrenceMode,
 
     /**
