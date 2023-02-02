ae534283caa51d551869135e7c4f6e3167fa59e6
Josh Holtz <me@joshholtz.com>
2023-02-02 05:12:37 -0600

    Magic Weather now works with BC5 alpha.2 but its not the prettiest yet

--- examples/MagicWeather/app/src/main/java/com/revenuecat/sample/ui/paywall/PaywallAdapter.kt
+++ examples/MagicWeather/app/src/main/java/com/revenuecat/sample/ui/paywall/PaywallAdapter.kt
@@ -4,21 +4,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.revenuecat.purchases.Offering
 import com.revenuecat.purchases.Package
+import com.revenuecat.purchases.models.StoreProduct
+import com.revenuecat.purchases.models.SubscriptionOption
 import com.revenuecat.sample.R
 
 class PaywallAdapter(
     var offering: Offering?,
-    var didChoosePackage: (Package) -> Unit
+    var didChoosePaywallItem: (PaywallItem) -> Unit
 ) : RecyclerView.Adapter<PaywallAdapter.PackageViewHolder>() {
 
     class PackageViewHolder(
         val view: View,
-        val didChoosePackage: (Package) -> Unit
+        val didChoosePaywallItem: (PaywallItem) -> Unit
     ) : RecyclerView.ViewHolder(view), View.OnClickListener {
-        var item: Package? = null
+        var item: PaywallItem? = null
 
         init {
             view.setOnClickListener(this)
@@ -26,7 +29,7 @@ class PaywallAdapter(
 
         override fun onClick(v: View) {
             item?.let {
-                didChoosePackage(it)
+                didChoosePaywallItem(it)
             }
         }
     }
@@ -35,21 +38,90 @@ class PaywallAdapter(
         val view = LayoutInflater.from(viewGroup.context)
             .inflate(R.layout.paywall_item, viewGroup, false)
 
-        return PackageViewHolder(view, didChoosePackage)
+        return PackageViewHolder(view, didChoosePaywallItem)
     }
 
     override fun onBindViewHolder(viewHolder: PackageViewHolder, position: Int) {
-        offering?.availablePackages?.get(position)?.let {
+        theItems[position].let {
             val item = it
             viewHolder.item = item
 
-            val price = item.product.defaultOption?.pricingPhases?.lastOrNull()?.formattedPrice ?: item.product.oneTimeProductPrice?.formattedPrice ?: "???"
+            when (it) {
+                is PaywallItem.Title -> {
+                    viewHolder.view.findViewById<TextView>(R.id.paywall_item_options_title).visibility = View.VISIBLE
+                    viewHolder.view.findViewById<TextView>(R.id.paywall_item_options_title).text = it.title
+                    viewHolder.view.findViewById<ConstraintLayout>(R.id.purchasableLayout).visibility = View.GONE
+                }
+                is PaywallItem.Product -> {
+                    viewHolder.view.findViewById<TextView>(R.id.paywall_item_options_title).visibility = View.GONE
+                    viewHolder.view.findViewById<ConstraintLayout>(R.id.purchasableLayout).visibility = View.VISIBLE
 
-            viewHolder.view.findViewById<TextView>(R.id.paywall_item_title).text = item.product.title
-            viewHolder.view.findViewById<TextView>(R.id.paywall_item_description).text = item.product.productId
-            viewHolder.view.findViewById<TextView>(R.id.paywall_item_price).text = price
+                    val product = it.storeProduct
+                    val price = product.oneTimeProductPrice?.formattedPrice ?: "???"
+
+                    viewHolder.view.findViewById<TextView>(R.id.paywall_item_price).text = price
+                    viewHolder.view.findViewById<TextView>(R.id.paywall_item_best_offer).visibility = View.GONE
+                }
+                is PaywallItem.Option -> {
+                    viewHolder.view.findViewById<TextView>(R.id.paywall_item_options_title).visibility = View.GONE
+                    viewHolder.view.findViewById<ConstraintLayout>(R.id.purchasableLayout).visibility = View.VISIBLE
+
+                    val option = it.subscriptionOption
+                    val price = option.pricingPhases.map { phase ->
+                        "${phase.formattedPrice} for ${phase.billingPeriod}"
+                    }.joinToString(separator = " -> ")
+
+                    viewHolder.view.findViewById<TextView>(R.id.paywall_item_price).text = price
+                    viewHolder.view.findViewById<TextView>(R.id.paywall_item_best_offer).visibility =  if (it.defaultOffer) View.VISIBLE else View.GONE
+                }
+            }
         }
     }
 
-    override fun getItemCount() = offering?.availablePackages?.size ?: 0
+    override fun getItemCount() = theItems.size ?: 0
+
+    private val theItems: List<PaywallItem>
+        get() = offering?.availablePackages?.flatMap {
+
+            if (it.product.subscriptionOptions.isEmpty()) {
+                listOf(
+                    PaywallItem.Title(it.product.title),
+                    PaywallItem.Product(it.product)
+                )
+            } else {
+                val product = it.product
+                val title = product.defaultOption?.pricingPhases?.lastOrNull()?.billingPeriod?.let { period ->
+                    when (period) {
+                        "P1W" -> "Weekly"
+                        "P4W" -> "Every 4  Weeks"
+                        "P1M" -> "Monthly"
+                        "P3M" -> "Every 3 Months"
+                        "P6M" -> "Every 6 Months"
+                        "P1Y" -> "Yearly"
+                        else -> {
+                            null
+                        }
+                    }
+                } ?: product.title
+
+                mutableListOf(PaywallItem.Title(title)) + it.product.subscriptionOptions.map { option ->
+                    PaywallItem.Option(option, option == it.product.defaultOption)
+                }.sortedBy { option -> !option.defaultOffer }
+            }
+        } ?: emptyList()
+}
+
+sealed class PaywallItem {
+    data class Title(
+        val title: String
+    ) : PaywallItem()
+
+    data class Product(
+        val storeProduct: StoreProduct
+    ) : PaywallItem()
+
+    data class Option(
+        val subscriptionOption: SubscriptionOption,
+        val defaultOffer: Boolean
+    ) : PaywallItem()
 }
