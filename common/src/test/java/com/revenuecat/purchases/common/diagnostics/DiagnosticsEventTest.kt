6dfbc41fee53bbca2edae91850084a088a2b904d
Toni Rico <antonio.rico.diez@revenuecat.com>
2023-02-09 15:52:00 +0100

    Automatically add timestamp

--- common/src/test/java/com/revenuecat/purchases/common/diagnostics/DiagnosticsEventTest.kt
+++ common/src/test/java/com/revenuecat/purchases/common/diagnostics/DiagnosticsEventTest.kt
@@ -1,21 +1,36 @@
 package com.revenuecat.purchases.common.diagnostics
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.revenuecat.purchases.common.DateProvider
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.annotation.Config
+import java.util.Date
 
 @RunWith(AndroidJUnit4::class)
 @Config(manifest = Config.NONE)
 class DiagnosticsEventTest {
+
+    private val testDate = Date(1675954145L) // Thursday, February 9, 2023 2:49:05 PM GMT
+
+    private lateinit var testDateProvider: DateProvider
+
+    @Before
+    fun setup() {
+        testDateProvider = object : DateProvider {
+            override val now: Date
+                get() = testDate
+        }
+    }
 
     @Test
     fun `toString transforms log event to correct JSON`() {
         val event = DiagnosticsEvent.Log(
             name = DiagnosticsLogEventName.ENDPOINT_HIT,
             properties = mapOf("test-key-1" to "test-value-1", "test-key-2" to 123, "test-key-3" to true),
-            timestamp = "timestamp"
+            dateProvider = testDateProvider
         )
         val eventAsString = event.toString()
         val expectedString = "{" +
@@ -23,7 +38,7 @@ class DiagnosticsEventTest {
             "\"type\":\"log\"," +
             "\"name\":\"endpoint_hit\"," +
             "\"properties\":{\"test-key-1\":\"test-value-1\",\"test-key-2\":123,\"test-key-3\":true}," +
-            "\"timestamp\":\"timestamp\"" +
+            "\"timestamp\":1675954145" +
             "}"
         assertThat(eventAsString).isEqualTo(expectedString)
     }
@@ -52,7 +67,7 @@ class DiagnosticsEventTest {
             exceptionClass = "TestClass.kt",
             message = "test message",
             location = "DiagnosticsEvent:121",
-            timestamp = "timestamp"
+            dateProvider = testDateProvider
         )
         val eventAsString = event.toString()
         val expectedString = "{" +
@@ -61,7 +76,7 @@ class DiagnosticsEventTest {
             "\"exc_class\":\"TestClass.kt\"," +
             "\"message\":\"test message\"," +
             "\"location\":\"DiagnosticsEvent:121\"," +
-            "\"timestamp\":\"timestamp\"" +
+            "\"timestamp\":1675954145" +
             "}"
         assertThat(eventAsString).isEqualTo(expectedString)
     }
