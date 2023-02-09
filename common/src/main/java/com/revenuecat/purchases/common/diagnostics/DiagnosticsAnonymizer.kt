5f6e061af8711442163172be2395de4c9fd25f4f
Toni Rico <antonio.rico.diez@revenuecat.com>
2023-02-09 11:52:16 +0100

    Diagnostics: Anonymize events (#782)

--- common/src/main/java/com/revenuecat/purchases/common/diagnostics/DiagnosticsAnonymizer.kt
+++ common/src/main/java/com/revenuecat/purchases/common/diagnostics/DiagnosticsAnonymizer.kt
@@ -1,8 +1,27 @@
 package com.revenuecat.purchases.common.diagnostics
 
-class DiagnosticsAnonymizer {
+import com.revenuecat.purchases.common.Anonymizer
+
+class DiagnosticsAnonymizer(
+    private val anonymizer: Anonymizer
+) {
     fun anonymizeEventIfNeeded(diagnosticsEvent: DiagnosticsEvent): DiagnosticsEvent {
-        // WIP: Anonymize
-        return diagnosticsEvent
+        return when (diagnosticsEvent) {
+            is DiagnosticsEvent.Log -> anonymizeLog(diagnosticsEvent)
+            is DiagnosticsEvent.Exception -> anonymizeException(diagnosticsEvent)
+            is DiagnosticsEvent.Metric -> diagnosticsEvent
+        }
+    }
+
+    private fun anonymizeLog(diagnosticsLog: DiagnosticsEvent.Log): DiagnosticsEvent {
+        return diagnosticsLog.copy(
+            properties = anonymizer.anonymizedMap(diagnosticsLog.properties)
+        )
+    }
+
+    private fun anonymizeException(diagnosticsException: DiagnosticsEvent.Exception): DiagnosticsEvent {
+        return diagnosticsException.copy(
+            message = anonymizer.anonymizedString(diagnosticsException.message)
+        )
     }
 }
