72f33d4bfa6f538c454676f57fad3a743c1f2026
Toni Rico <antonio.rico.diez@revenuecat.com>
2023-02-09 11:52:16 +0100

    Anonymize diagnostics data

--- common/src/main/java/com/revenuecat/purchases/common/diagnostics/DiagnosticsAnonymizer.kt
+++ common/src/main/java/com/revenuecat/purchases/common/diagnostics/DiagnosticsAnonymizer.kt
@@ -1,8 +1,38 @@
 package com.revenuecat.purchases.common.diagnostics
 
-class DiagnosticsAnonymizer {
+import com.revenuecat.purchases.common.Anonymizer
+
+class DiagnosticsAnonymizer(
+    private val anonymizer: Anonymizer
+) {
     fun anonymizeEventIfNeeded(diagnosticsEvent: DiagnosticsEvent): DiagnosticsEvent {
-        // WIP: Anonymize
-        return diagnosticsEvent
+        return when(diagnosticsEvent) {
+            is DiagnosticsEvent.Log -> anonymizeLog(diagnosticsEvent)
+            is DiagnosticsEvent.Exception -> anonymizeException(diagnosticsEvent)
+            is DiagnosticsEvent.Metric -> diagnosticsEvent
+        }
+    }
+
+    private fun anonymizeLog(diagnosticsLog: DiagnosticsEvent.Log): DiagnosticsEvent {
+        return diagnosticsLog.copy(
+            properties = anonymizeMap(diagnosticsLog.properties)
+        )
+    }
+
+    private fun anonymizeException(diagnosticsException: DiagnosticsEvent.Exception): DiagnosticsEvent {
+        return diagnosticsException.copy(
+            message = anonymizeString(diagnosticsException.message)
+        )
+    }
+
+    private fun anonymizeMap(mapToAnonymize: Map<String, Any>): Map<String, Any> {
+        return mapToAnonymize.mapValues { (_, value) ->
+            if (value is String) anonymizeString(value)
+            else value
+        }
+    }
+
+    private fun anonymizeString(textToAnonymize: String): String {
+        return anonymizer.anonymizeString(textToAnonymize)
     }
 }
