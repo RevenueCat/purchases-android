336ebd07ec55a84aa102f6067a4567af0adb28c6
Toni Rico <antonio.rico.diez@revenuecat.com>
2023-02-10 16:41:50 +0100

    Cleanup comment

--- common/src/main/java/com/revenuecat/purchases/common/AppConfig.kt
+++ common/src/main/java/com/revenuecat/purchases/common/AppConfig.kt
@@ -22,7 +22,7 @@ class AppConfig(
     val baseURL: URL = proxyURL?.also {
         log(LogIntent.INFO, ConfigureStrings.CONFIGURING_PURCHASES_PROXY_URL_SET)
     } ?: URL("https://api.revenuecat.com/")
-    val dianosticsURL = URL("https://api-telemetry.revenuecat.com/") // WIP: Update URL and path
+    val dianosticsURL = URL("https://api-telemetry.revenuecat.com/") // WIP: Update URL
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
