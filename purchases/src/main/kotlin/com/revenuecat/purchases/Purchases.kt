36a3cc10d03bc3c40298448f56c46282cb9f12d8
Toni Rico <antonio.rico.diez@revenuecat.com>
2023-02-16 10:49:45 +0100

    Diagnostics: Sync diagnostics when configuring SDK if needed (#791)
    ### Description
    Based on #787 
    
    This PR adds support to enable/disable diagnostics (disabled by default)
    and adds all the plumbing to sync diagnostics when configuring the SDK.

--- purchases/src/main/kotlin/com/revenuecat/purchases/Purchases.kt
+++ purchases/src/main/kotlin/com/revenuecat/purchases/Purchases.kt
@@ -31,6 +31,7 @@ import com.revenuecat.purchases.common.caching.DeviceCache
 import com.revenuecat.purchases.common.createOfferings
 import com.revenuecat.purchases.common.currentLogHandler
 import com.revenuecat.purchases.common.debugLogsEnabled
+import com.revenuecat.purchases.common.diagnostics.DiagnosticsManager
 import com.revenuecat.purchases.common.errorLog
 import com.revenuecat.purchases.common.log
 import com.revenuecat.purchases.common.sha1
@@ -103,6 +104,7 @@ class Purchases internal constructor(
     private val subscriberAttributesManager: SubscriberAttributesManager,
     @set:JvmSynthetic @get:JvmSynthetic internal var appConfig: AppConfig,
     private val customerInfoHelper: CustomerInfoHelper,
+    diagnosticsManager: DiagnosticsManager?,
     // This is nullable due to: https://github.com/RevenueCat/purchases-flutter/issues/408
     private val mainHandler: Handler? = Handler(Looper.getMainLooper())
 ) : LifecycleDelegate {
@@ -173,6 +175,8 @@ class Purchases internal constructor(
         if (!appConfig.dangerousSettings.autoSyncPurchases) {
             log(LogIntent.WARNING, AUTO_SYNC_PURCHASES_DISABLED)
         }
+
+        diagnosticsManager?.syncDiagnosticsFileIfNeeded()
     }
 
     /** @suppress */
