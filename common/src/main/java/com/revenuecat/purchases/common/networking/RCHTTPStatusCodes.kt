bd15e3965e3e76b297276b77a3602485ad00efec
Toni Rico <antonio.rico.diez@revenuecat.com>
2023-02-17 09:04:01 +0100

    Rename to BAD_REQUEST

--- common/src/main/java/com/revenuecat/purchases/common/networking/RCHTTPStatusCodes.kt
+++ common/src/main/java/com/revenuecat/purchases/common/networking/RCHTTPStatusCodes.kt
@@ -5,9 +5,9 @@ object RCHTTPStatusCodes {
     const val CREATED = 201
     const val UNSUCCESSFUL = 300
     const val NOT_MODIFIED = 304
-    const val REQUEST_ERROR = 400
+    const val BAD_REQUEST = 400
     const val NOT_FOUND = 404
     const val ERROR = 500
 
-    fun isSuccessful(statusCode: Int) = statusCode < REQUEST_ERROR
+    fun isSuccessful(statusCode: Int) = statusCode < BAD_REQUEST
 }
