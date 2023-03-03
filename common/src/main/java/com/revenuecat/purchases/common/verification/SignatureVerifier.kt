f0d3f21a4478d01cf90ae5754e49d5b8eef1158e
Toni Rico <antonio.rico.diez@revenuecat.com>
2023-03-03 10:13:27 +0100

    Entitlement verification: Add Tink dependency to perform signature verification (#823)
    ### Description
    This PR adds Tink as a dependency to perform signature verification. It
    also adds our production public key so we can verify responses from the
    backend.

--- common/src/main/java/com/revenuecat/purchases/common/verification/SignatureVerifier.kt
+++ common/src/main/java/com/revenuecat/purchases/common/verification/SignatureVerifier.kt
@@ -1,6 +1,8 @@
 package com.revenuecat.purchases.common.verification
 
 import android.util.Base64
+import com.google.crypto.tink.subtle.Ed25519Verify
+import java.security.GeneralSecurityException
 
 interface SignatureVerifier {
     fun verify(signatureToVerify: ByteArray, messageToVerify: ByteArray): Boolean
@@ -8,14 +10,19 @@ interface SignatureVerifier {
 
 class DefaultSignatureVerifier(publicKey: String = DEFAULT_PUBLIC_KEY) : SignatureVerifier {
     companion object {
-        private const val DEFAULT_PUBLIC_KEY = "" // WIP: Add B64 encoded public key here
+        private const val DEFAULT_PUBLIC_KEY = "UC1upXWg5QVmyOSwozp755xLqquBKjjU+di6U8QhMlM="
     }
 
-    @Suppress("UnusedPrivateMember") // WIP: Remove suppress
     private val publicKeyBytes = Base64.decode(publicKey, Base64.DEFAULT)
 
+    private val verifier = Ed25519Verify(publicKeyBytes)
+
     override fun verify(signatureToVerify: ByteArray, messageToVerify: ByteArray): Boolean {
-        // WIP: Add library
-        return true
+        return try {
+            verifier.verify(signatureToVerify, messageToVerify)
+            true
+        } catch (_: GeneralSecurityException) {
+            false
+        }
     }
 }
