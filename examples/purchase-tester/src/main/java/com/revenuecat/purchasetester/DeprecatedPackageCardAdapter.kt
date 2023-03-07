d1c97f13b30048899abfe66b7dc269205fd77d88
Maddie Beyl <maddie@revenuecat.com>
2023-03-07 18:10:43 -0500

    Add deprecated versions of offering fragment to test old purchasing methods

---
+++ examples/purchase-tester/src/main/java/com/revenuecat/purchasetester/DeprecatedPackageCardAdapter.kt
@@ -0,0 +1,130 @@
+package com.revenuecat.purchasetester
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.RadioButton
+import androidx.core.view.children
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.revenuecat.purchases.Package
+import com.revenuecat.purchases.PackageType
+import com.revenuecat.purchases.ProductType
+import com.revenuecat.purchases.models.googleProduct
+import com.revenuecat.purchases.models.SubscriptionOption
+import com.revenuecat.purchases.models.StoreProduct
+import com.revenuecat.purchases_sample.databinding.PackageCardBinding
+
+class DeprecatedPackageCardAdapter(
+    private val packages: List<Package>,
+    private val activeSubscriptions: Set<String>,
+    private val listener: PackageCardAdapterListener
+) :
+    RecyclerView.Adapter<DeprecatedPackageCardAdapter.PackageViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PackageViewHolder {
+        val binding = PackageCardBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return PackageViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int = packages.size
+
+    override fun onBindViewHolder(holder: PackageViewHolder, position: Int) {
+        holder.bind(packages[position])
+    }
+
+    inner class PackageViewHolder(private val binding: PackageCardBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        private val nothingCheckedIndex = -1
+
+        fun bind(currentPackage: Package) {
+            val product = currentPackage.product
+            binding.currentPackage = currentPackage
+            binding.isSubscription = product.type == ProductType.SUBS
+            binding.isActive = activeSubscriptions.contains(product.id)
+
+            binding.packageBuyButton.setOnClickListener {
+                listener.onPurchasePackageClicked(
+                    binding.root,
+                    currentPackage,
+                    binding.isUpgradeCheckbox.isChecked
+                )
+            }
+
+            binding.productBuyButton.setOnClickListener {
+                listener.onPurchaseProductClicked(
+                    binding.root,
+                    product,
+                    binding.isUpgradeCheckbox.isChecked
+                )
+            }
+
+            binding.packageType.detail = if (currentPackage.packageType == PackageType.CUSTOM) {
+                "custom -> ${currentPackage.packageType.identifier}"
+            } else {
+                currentPackage.packageType.toString()
+            }
+
+            binding.packageDetailsJsonObject.detail = product.googleProduct?.productDetails?.toString() ?: "TODO Amazon"
+
+            bindSubscriptionOptions(product)
+
+            binding.root.setOnClickListener {
+                with(binding.packageDetailsContainer) {
+                    visibility = if (visibility == View.GONE) View.VISIBLE else View.GONE
+                }
+            }
+        }
+
+        private fun bindSubscriptionOptions(product: StoreProduct) {
+            binding.packageSubscriptionOptionGroup.removeAllViews()
+            val numberOfSubscriptionOptions = product.subscriptionOptions?.size ?: 0
+            val defaultOption = product.defaultOption
+            product.subscriptionOptions?.forEach { subscriptionOption ->
+                val radioButton = RadioButton(binding.root.context).apply {
+                    text = subscriptionOption.toButtonString(subscriptionOption == defaultOption)
+                    tag = subscriptionOption
+                }
+                binding.packageSubscriptionOptionGroup.addView(radioButton)
+                if (numberOfSubscriptionOptions == 1) binding.packageSubscriptionOptionGroup.check(radioButton.id)
+            }
+        }
+
+        private fun getSelectedSubscriptionOption(): SubscriptionOption? {
+            val selectedButtonId = binding.packageSubscriptionOptionGroup.checkedRadioButtonId
+            return binding.packageSubscriptionOptionGroup.children
+                .filter { it.id == selectedButtonId }
+                .firstOrNull()
+                ?.tag as? SubscriptionOption
+        }
+
+        private fun validateStartPurchase(product: StoreProduct): String? {
+            if (product.type == ProductType.SUBS &&
+                binding.packageSubscriptionOptionGroup.checkedRadioButtonId == nothingCheckedIndex) {
+                return "Please choose subscription option first"
+            }
+            return null
+        }
+
+        private fun showErrorMessage(errorMessage: String) {
+            MaterialAlertDialogBuilder(binding.root.context)
+                .setMessage(errorMessage)
+                .setPositiveButton("OK") { dialog, _ -> dialog.dismiss() }
+                .show()
+        }
+    }
+
+    interface PackageCardAdapterListener {
+        fun onPurchasePackageClicked(
+            cardView: View,
+            currentPackage: Package,
+            isUpgrade: Boolean
+        )
+        fun onPurchaseProductClicked(
+            cardView: View,
+            currentProduct: StoreProduct,
+            isUpgrade: Boolean
+        )
+    }
+}
--- examples/purchase-tester/src/main/java/com/revenuecat/purchasetester/OverviewFragment.kt
+++ examples/purchase-tester/src/main/java/com/revenuecat/purchasetester/OverviewFragment.kt
@@ -92,7 +92,9 @@ class OverviewFragment : Fragment(), OfferingCardAdapter.OfferingCardAdapterList
 
         val offeringCardTransitionName = getString(R.string.offering_fragment_transition)
         val extras = FragmentNavigatorExtras(cardView to offeringCardTransitionName)
-        val directions = OverviewFragmentDirections.actionOverviewFragmentToOfferingFragment(offering)
+//        val directions = OverviewFragmentDirections.actionOverviewFragmentToOfferingFragment(offering)
+        // uncomment line below and comment line above to test deprecated purchase methods
+        val directions = OverviewFragmentDirections.actionOverviewFragmentToDeprecatedOfferingFragment(offering)
         findNavController().navigate(directions, extras)
     }
 
