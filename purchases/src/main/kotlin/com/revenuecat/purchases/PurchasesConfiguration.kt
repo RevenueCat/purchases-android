00061891f8abc175223434c56f59135b44611d54
Toni Rico <antonio.rico.diez@revenuecat.com>
2023-03-14 17:05:31 +0100

    Update docs after review
    Co-authored-by: NachoSoto <NachoSoto@users.noreply.github.com>

--- purchases/src/main/kotlin/com/revenuecat/purchases/PurchasesConfiguration.kt
+++ purchases/src/main/kotlin/com/revenuecat/purchases/PurchasesConfiguration.kt
@@ -71,7 +71,7 @@ open class PurchasesConfiguration(builder: Builder) {
          * **Feature in beta**. Defines how strict [EntitlementInfo] verification ought to be.
          *
          * When changing from [EntitlementVerificationMode.DISABLED] to [EntitlementVerificationMode.INFORMATIONAL],
-         * we will clear the CustomerInfo cache. This means users will need to connect to the internet to get back
+         * the SDK will clear the CustomerInfo cache. This means users will need to connect to the internet to get back
          * their entitlements.
          *
          * The result of the verification can be obtained from [EntitlementInfos.verification] or
