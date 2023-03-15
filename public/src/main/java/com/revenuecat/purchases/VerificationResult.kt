ae77cd079d36e703b1ebfbaa9491c2e860b7c805
Toni Rico <antonio.rico.diez@revenuecat.com>
2023-03-15 20:26:27 +0100

    Modify VerificationResult docs to try to avoid mentioning entitlements

--- public/src/main/java/com/revenuecat/purchases/VerificationResult.kt
+++ public/src/main/java/com/revenuecat/purchases/VerificationResult.kt
@@ -5,9 +5,9 @@ package com.revenuecat.purchases
  *
  * This is accomplished by preventing MiTM attacks between the SDK and the RevenueCat server.
  * With verification enabled, the SDK ensures that the response created by the server was not
- * modified by a third-party, and the entitlements received are exactly what was sent.
+ * modified by a third-party, and the response received is exactly what was sent.
  *
- * - Note: Entitlements are only verified if enabled using PurchasesConfiguration.Builder's
+ * - Note: Verification is only performed if enabled using PurchasesConfiguration.Builder's
  * entitlementVerificationMode property. This is disabled by default.
  */
 enum class VerificationResult {
@@ -19,12 +19,12 @@ enum class VerificationResult {
     NOT_REQUESTED,
 
     /**
-     * Entitlements were verified with our server.
+     * Verification with our server was performed successfully.
      */
     VERIFIED,
 
     /**
-     * Entitlement verification failed, possibly due to a MiTM attack.
+     * Verification failed, possibly due to a MiTM attack.
      */
     FAILED
 }
