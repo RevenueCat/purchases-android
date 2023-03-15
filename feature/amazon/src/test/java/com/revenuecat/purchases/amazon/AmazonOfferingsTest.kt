3546352dbd6fe12330a140e9fd38ccf27583a593
Maddie Beyl <maddie@revenuecat.com>
2023-03-15 15:26:09 -0400

    create test file for amazon offerings with todo

---
+++ feature/amazon/src/test/java/com/revenuecat/purchases/amazon/AmazonOfferingsTest.kt
@@ -0,0 +1,33 @@
+//  Purchases
+//
+//  Copyright © 2019 RevenueCat, Inc. All rights reserved.
+//
+
+package com.revenuecat.purchases.amazon
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.android.billingclient.api.ProductDetails
+import com.revenuecat.purchases.OfferingParserFactory
+import com.revenuecat.purchases.Package
+import com.revenuecat.purchases.PackageType
+import com.revenuecat.purchases.Store
+import com.revenuecat.purchases.models.Period
+import com.revenuecat.purchases.models.StoreProduct
+import com.revenuecat.purchases.utils.stubINAPPStoreProduct
+import com.revenuecat.purchases.utils.stubPricingPhase
+import com.revenuecat.purchases.utils.stubStoreProduct
+import com.revenuecat.purchases.utils.stubSubscriptionOption
+import org.assertj.core.api.Assertions.assertThat
+import org.json.JSONArray
+import org.json.JSONObject
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
+
+@RunWith(AndroidJUnit4::class)
+@Config(manifest = Config.NONE)
+class AmazonOfferingsTest {
+
+// TODOBC5 add tests once AmazonStoreProduct changes are in
+
+}
