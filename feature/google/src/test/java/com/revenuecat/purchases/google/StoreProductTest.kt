66d41d095c9a7dcf3eeb3db99a6291f0703f3c77
Maddie Beyl <maddie@revenuecat.com>
2023-03-31 14:49:17 -0400

    Add StoreProductTest

--- feature/google/src/test/java/com/revenuecat/purchases/google/StoreProductTest.kt
+++ feature/google/src/test/java/com/revenuecat/purchases/google/StoreProductTest.kt
@@ -222,4 +222,85 @@ class StoreProductTest {
 
         assertThat(storeProduct1.hashCode()).isEqualTo(storeProduct2.hashCode())
     }
+
+    @Test
+    fun `copyWithOfferingId copies product with offeringId applied to itself and all subscriptionOptions`() {
+        val productDetails = mockProductDetails()
+        val price = Price(
+            formatted = "$1.00",
+            amountMicros = 100,
+            currencyCode = "USD"
+        )
+
+        val subscriptionOption1 = GoogleSubscriptionOption(
+            productId = productId,
+            basePlanId = basePlanId,
+            offerId = null,
+            pricingPhases = listOf(PricingPhase(
+                billingPeriod = Period.create("P1M"),
+                recurrenceMode = RecurrenceMode.INFINITE_RECURRING,
+                billingCycleCount = 0,
+                price = Price(
+                    formatted = "",
+                    amountMicros = 0L,
+                    currencyCode = "",
+                )
+            )),
+            tags = emptyList(),
+            productDetails,
+            offerToken
+        )
+
+        val subscriptionOption2 = GoogleSubscriptionOption(
+            productId = productId,
+            basePlanId = basePlanId,
+            offerId = null,
+            pricingPhases = listOf(PricingPhase(
+                billingPeriod = Period.create("P1M"),
+                recurrenceMode = RecurrenceMode.INFINITE_RECURRING,
+                billingCycleCount = 0,
+                price = Price(
+                    formatted = "",
+                    amountMicros = 0L,
+                    currencyCode = "",
+                )
+            )),
+            tags = emptyList(),
+            productDetails,
+            offerToken
+        )
+
+        val storeProduct = GoogleStoreProduct(
+            productId = "product_id",
+            basePlanId = "base_plan_id",
+            type = ProductType.SUBS,
+            price = price,
+            title = "TITLE",
+            description = "DESCRIPTION",
+            period = Period.create("P1M"),
+            subscriptionOptions = SubscriptionOptions(listOf(subscriptionOption1, subscriptionOption2)),
+            defaultOption = subscriptionOption1,
+            productDetails = productDetails,
+            presentedOfferingIdentifier = "originalOfferingId"
+        )
+
+        val expectedOfferingId = "expectedOfferingId"
+        val copiedStoreProduct = storeProduct.copyWithOfferingId(expectedOfferingId) as GoogleStoreProduct
+
+        assertThat(copiedStoreProduct.id).isEqualTo(storeProduct.id)
+        assertThat(copiedStoreProduct.productId).isEqualTo(storeProduct.productId)
+        assertThat(copiedStoreProduct.basePlanId).isEqualTo(storeProduct.basePlanId)
+        assertThat(copiedStoreProduct.type).isEqualTo(storeProduct.type)
+        assertThat(copiedStoreProduct.price).isEqualTo(storeProduct.price)
+        assertThat(copiedStoreProduct.title).isEqualTo(storeProduct.title)
+        assertThat(copiedStoreProduct.description).isEqualTo(storeProduct.description)
+        assertThat(copiedStoreProduct.period).isEqualTo(storeProduct.period)
+        assertThat(copiedStoreProduct.productDetails).isEqualTo(storeProduct.productDetails)
+
+        assertThat(copiedStoreProduct.presentedOfferingIdentifier).isEqualTo(expectedOfferingId)
+        assertThat(copiedStoreProduct.defaultOption!!.presentedOfferingIdentifier).isEqualTo(expectedOfferingId)
+        copiedStoreProduct.subscriptionOptions?.forEach {
+            assertThat(it.presentedOfferingIdentifier).isEqualTo(expectedOfferingId)
+        }
+    }
 }
