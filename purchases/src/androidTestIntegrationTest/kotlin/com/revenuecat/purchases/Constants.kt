e4bef8e96e8020498696e7011fa3cf173808df23
Toni Rico <antonio.rico.diez@revenuecat.com>
2023-04-05 17:52:39 +0200

    Add verification for entitlement granting to integration tests V4

--- purchases/src/androidTestIntegrationTest/kotlin/com/revenuecat/purchases/Constants.kt
+++ purchases/src/androidTestIntegrationTest/kotlin/com/revenuecat/purchases/Constants.kt
@@ -5,4 +5,6 @@ object Constants {
     const val proxyUrl = "NO_PROXY_URL"
     const val googlePurchaseToken = "GOOGLE_PURCHASE_TOKEN"
     const val productIdToPurchase = "PRODUCT_ID_TO_PURCHASE"
+    // comma separated list of active entitlements to verify
+    const val activeEntitlementIdsToVerify = "ACTIVE_ENTITLEMENT_IDS_TO_VERIFY"
 }
