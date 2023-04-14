9ce7d7582a69ba9eeb79fec01c5b036459847640
Andy Boedo <andresboedo@gmail.com>
2023-04-14 18:41:56 -0300

    updated the code so that it only runs on android v4

--- common/src/androidTest/kotlin/JSONArrayBugInstrumentedTest.kt
+++ common/src/androidTest/kotlin/JSONArrayBugInstrumentedTest.kt
@@ -1,5 +1,5 @@
+import android.os.Build
 import com.revenuecat.purchases.common.networking.MapConverter
-import org.json.JSONArray
 import org.json.JSONObject
 import org.junit.Assert.assertEquals
 
@@ -7,20 +7,26 @@ import org.junit.Assert.assertEquals
 class JSONArrayBugInstrumentedTest {
     @org.junit.Test
     fun testReproduceIncorrectJSONArray() {
+        // these tests should only be run on Android 4, since that's the version that has the bug.
+        val osLevel = Build.VERSION.SDK_INT
+        if (!(osLevel >= Build.VERSION_CODES.ICE_CREAM_SANDWICH && osLevel <= Build.VERSION_CODES.KITKAT)) {
+            return
+        }
+
         val inputMap = mapOf(
             "key1" to listOf("value2", "value3"),
         )
         val incorrectJSONObject = JSONObject(inputMap)
         assertEquals(
             incorrectJSONObject
-                .toString(), ("""{"key1":["value2, value3"]}""")
+                .toString(), ("{\"key1\":[\"value2, value3\"]}")
         )
         val mapConverter = MapConverter()
 
         val correctJSONObject = mapConverter.convertToJSON(inputMap)
         assertEquals(
             correctJSONObject
-                .toString(), ("""{"key1":["value2","value3"]}""")
+                .toString(), ("{\"key1\":[\"value2\",\"value3\"]}")
         )
     }
 }
