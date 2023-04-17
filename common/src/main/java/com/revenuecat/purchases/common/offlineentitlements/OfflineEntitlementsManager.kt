21e6e39019c1df8795a55d4923facb0b2889c5cd
Toni Rico <antonio.rico.diez@revenuecat.com>
2023-04-17 12:51:10 +0200

    Only calculate offline customer info once at the same time and multi-threading support

--- common/src/main/java/com/revenuecat/purchases/common/offlineentitlements/OfflineEntitlementsManager.kt
+++ common/src/main/java/com/revenuecat/purchases/common/offlineentitlements/OfflineEntitlementsManager.kt
@@ -19,26 +19,32 @@ class OfflineEntitlementsManager(
     // We cache the offline customer info in memory, so it's not persisted.
     val offlineCustomerInfo: CustomerInfo?
         get() = _offlineCustomerInfo
-    // TODO make sure we handle concurrency
+    @get:Synchronized @set:Synchronized
     private var _offlineCustomerInfo: CustomerInfo? = null
 
+    private val offlineCustomerInfoCallbackCache = mutableMapOf<String, List<OfflineCustomerInfoCallback>>()
+
     fun resetOfflineCustomerInfoCache() {
-        if (_offlineCustomerInfo != null) {
-            // TODO improve logs
-            warnLog("Resetting offline customer info cache")
+        synchronized(this) {
+            if (_offlineCustomerInfo != null) {
+                warnLog(OfflineEntitlementsStrings.RESETTING_OFFLINE_CUSTOMER_INFO_CACHE)
+                _offlineCustomerInfo = null
+            }
         }
-        _offlineCustomerInfo = null
     }
 
     fun shouldCalculateOfflineCustomerInfoInGetCustomerInfoRequest(
         isServerError: Boolean,
         appUserId: String
     ): Boolean {
-        return appConfig.areOfflineEntitlementsEnabled && isServerError && deviceCache.getCachedCustomerInfo(appUserId) == null
+        return appConfig.areOfflineEntitlementsEnabled &&
+            isServerError &&
+            deviceCache.getCachedCustomerInfo(appUserId) == null
     }
 
     fun shouldCalculateOfflineCustomerInfoInPostReceipt(isServerError: Boolean): Boolean {
-        return appConfig.areOfflineEntitlementsEnabled && isServerError
+        return appConfig.areOfflineEntitlementsEnabled &&
+            isServerError
     }
 
     @Suppress("FunctionOnlyReturningConstant")
@@ -47,17 +53,34 @@ class OfflineEntitlementsManager(
         onSuccess: (CustomerInfo) -> Unit,
         onError: (PurchasesError) -> Unit
     ) {
+        synchronized(this) {
+            val alreadyProcessing = offlineCustomerInfoCallbackCache.containsKey(appUserId)
+            val callbacks = offlineCustomerInfoCallbackCache[appUserId] ?: emptyList()
+            offlineCustomerInfoCallbackCache[appUserId] = callbacks + listOf(onSuccess to onError)
+            if (alreadyProcessing) {
+                debugLog(OfflineEntitlementsStrings.ALREADY_CALCULATING_OFFLINE_CUSTOMER_INFO.format(appUserId))
+                return
+            }
+        }
         offlineCustomerInfoCalculator.computeOfflineCustomerInfo(
             appUserId,
             onSuccess = { customerInfo ->
-                // TODO improve logs
-                warnLog("Updating offline customer info cache")
-                _offlineCustomerInfo = customerInfo
-                onSuccess(customerInfo)
+                synchronized(this) {
+                    warnLog("Updating offline customer info cache")
+                    _offlineCustomerInfo = customerInfo
+                    val callbacks = offlineCustomerInfoCallbackCache.remove(appUserId)
+                    callbacks?.forEach { (onSuccess, _) ->
+                        onSuccess(customerInfo)
+                    }
+                }
             },
             onError = {
-                errorLog("Error calculating offline customer info: $it")
-                onError(it)
+                synchronized(this) {
+                    val callbacks = offlineCustomerInfoCallbackCache.remove(appUserId)
+                    callbacks?.forEach { (_, onError) ->
+                        onError(it)
+                    }
+                }
             }
         )
     }
@@ -77,3 +100,5 @@ class OfflineEntitlementsManager(
         }
     }
 }
+
+private typealias OfflineCustomerInfoCallback = Pair<(CustomerInfo) -> Unit, (PurchasesError) -> Unit>
--- strings/src/main/java/com/revenuecat/purchases/strings/OfflineEntitlementsStrings.kt
+++ strings/src/main/java/com/revenuecat/purchases/strings/OfflineEntitlementsStrings.kt
@@ -4,4 +4,6 @@ object OfflineEntitlementsStrings {
     const val UPDATING_PRODUCT_ENTITLEMENT_MAPPING = "Product entitlement mappings are stale. Updating."
     const val SUCCESSFULLY_UPDATED_PRODUCT_ENTITLEMENTS = "Successfully updated product entitlement mappings."
     const val ERROR_UPDATING_PRODUCT_ENTITLEMENTS = "Error updating product entitlement mappings. Error: %s"
+    const val RESETTING_OFFLINE_CUSTOMER_INFO_CACHE = "Resetting offline customer info cache"
+    const val ALREADY_CALCULATING_OFFLINE_CUSTOMER_INFO = "Already calculating offline customer info for %s"
 }
