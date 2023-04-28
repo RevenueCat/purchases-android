e1672bafbde898e1bc91ca725b18740191c60f83
Cesar de la Vega <cesarvegaro@gmail.com>
2023-04-28 14:20:45 -0700

    revert changes around original_purchase_date

--- common/src/main/java/com/revenuecat/purchases/common/EntitlementInfoFactories.kt
+++ common/src/main/java/com/revenuecat/purchases/common/EntitlementInfoFactories.kt
@@ -87,9 +87,8 @@ internal fun JSONObject.buildEntitlementInfo(
     val expirationDate = optDate(EntitlementsResponseJsonKeys.EXPIRES_DATE)
     val unsubscribeDetectedAt = productData.optDate(ProductResponseJsonKeys.UNSUBSCRIBE_DETECTED_AT)
     val billingIssueDetectedAt = productData.optDate(ProductResponseJsonKeys.BILLING_ISSUES_DETECTED_AT)
-    val store = productData.getStore(ProductResponseJsonKeys.STORE)
 
-    val entitlementPurchaseDate = getDate(ProductResponseJsonKeys.PURCHASE_DATE)
+    val store = productData.getStore(ProductResponseJsonKeys.STORE)
 
     return EntitlementInfo(
         identifier = identifier,
@@ -97,8 +96,8 @@ internal fun JSONObject.buildEntitlementInfo(
         willRenew = getWillRenew(store, expirationDate, unsubscribeDetectedAt,
             billingIssueDetectedAt),
         periodType = productData.optPeriodType(ProductResponseJsonKeys.PERIOD_TYPE),
-        latestPurchaseDate = entitlementPurchaseDate,
-        originalPurchaseDate = this.calculateOriginalPurchaseDate(productData),
+        latestPurchaseDate = getDate(ProductResponseJsonKeys.PURCHASE_DATE),
+        originalPurchaseDate = productData.getDate(ProductResponseJsonKeys.ORIGINAL_PURCHASE_DATE),
         expirationDate = expirationDate,
         store = store,
         productIdentifier = getString(EntitlementsResponseJsonKeys.PRODUCT_IDENTIFIER),
@@ -112,40 +111,6 @@ internal fun JSONObject.buildEntitlementInfo(
         // verificationResult = verificationResult
     )
 }
-
-// If a product's base plans grants different entitlements, the product data section of the customerInfo response
-// might not match the information for the entitlement. If will have the latest purchased plan's information only.
-// This means we only have the original_purchase_date for the latest base plan purchased.
-//
-// For example:
-//
-// An entitlement could have this response from the backend (note the purchase is for p1m):
-// {
-//   "expires_date":"2023-04-11T17:59:03.285Z",
-//   "product_identifier":"prod_1",
-//   "purchase_date":"2023-04-11T16:59:03.285Z",
-//   "product_plan_identifier":"p1m"
-// }
-//
-// But the product data could be for a more recent purchase for another plan (not_bw):
-// {
-//   "billing_issues_detected_at":null,
-//   "is_sandbox":false,
-//   "original_purchase_date":"2023-04-11T17:59:03.285Z",
-//   "purchase_date":"2023-04-11T17:59:03.285Z",
-//   "store":"play_store",
-//   "unsubscribe_detected_at":null,
-//   "product_plan_identifier":"not_bw",
-//   "expires_date":"2023-04-12T18:59:01.115Z",
-//   "period_type":"normal"
-// }
-private fun JSONObject.calculateOriginalPurchaseDate(productData: JSONObject): Date =
-    if (productData.optNullableString(ProductResponseJsonKeys.PRODUCT_PLAN_IDENTIFIER) ==
-        this.optNullableString(EntitlementsResponseJsonKeys.PRODUCT_PLAN_IDENTIFIER)) {
-        productData.getDate(ProductResponseJsonKeys.ORIGINAL_PURCHASE_DATE)
-    } else {
-        this.getDate(EntitlementsResponseJsonKeys.PURCHASE_DATE)
-    }
 
 private fun isDateActive(
     identifier: String,
