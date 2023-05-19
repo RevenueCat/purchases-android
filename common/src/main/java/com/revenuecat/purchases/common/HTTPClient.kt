c21e0c7cf45b204bf709fd8cec2daa5465d46065
Andy Boedo <andresboedo@gmail.com>
2023-05-19 17:42:30 -0300

    Workaround bug in android 4 for JSON objects with List<String> (#942)
    Fixes
    https://linear.app/revenuecat/issue/SDK-3052/400-from-post-receipts-because-product-did-not-have-quotes
    https://revenuecats.atlassian.net/browse/CSDK-681
    
    Works around the following bug in android 4:
    https://stackoverflow.com/q/37317669
    http://fupeg.blogspot.com/2011/07/android-json-bug.html
    
    
    Essentially, this bug means that when you get a list of product_ids,
    instead of sending a list of strings, it sends a list with a string in
    it.
    
    | Before | After |
    | :-: | :-: |
    | "product_ids": "[item1, item2]" | "product_ids": ["item1, "item2"] |

--- common/src/main/java/com/revenuecat/purchases/common/HTTPClient.kt
+++ common/src/main/java/com/revenuecat/purchases/common/HTTPClient.kt
@@ -14,6 +14,7 @@ import com.revenuecat.purchases.common.networking.ETagManager
 import com.revenuecat.purchases.common.networking.Endpoint
 import com.revenuecat.purchases.common.networking.HTTPRequest
 import com.revenuecat.purchases.common.networking.HTTPResult
+import com.revenuecat.purchases.common.networking.MapConverter
 import com.revenuecat.purchases.common.networking.RCHTTPStatusCodes
 import com.revenuecat.purchases.common.verification.SignatureVerificationException
 import com.revenuecat.purchases.common.verification.SignatureVerificationMode
@@ -21,7 +22,6 @@ import com.revenuecat.purchases.common.verification.SigningManager
 import com.revenuecat.purchases.strings.NetworkStrings
 import com.revenuecat.purchases.utils.filterNotNullValues
 import org.json.JSONException
-import org.json.JSONObject
 import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.IOException
@@ -41,7 +41,8 @@ class HTTPClient(
     private val eTagManager: ETagManager,
     private val diagnosticsTrackerIfEnabled: DiagnosticsTracker?,
     val signingManager: SigningManager,
-    private val dateProvider: DateProvider = DefaultDateProvider()
+    private val dateProvider: DateProvider = DefaultDateProvider(),
+    private val mapConverter: MapConverter = MapConverter()
 ) {
     @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
     internal companion object {
@@ -135,7 +136,7 @@ class HTTPClient(
         requestHeaders: Map<String, String>,
         refreshETag: Boolean
     ): HTTPResult? {
-        val jsonBody = body?.convert()
+        val jsonBody = body?.let { mapConverter.convertToJSON(it) }
         val path = endpoint.getPath()
         val urlPathWithVersion = "/v1$path"
         val connection: HttpURLConnection
@@ -244,30 +245,6 @@ class HTTPClient(
             .plus(authenticationHeaders)
             .plus(eTagManager.getETagHeaders(urlPath, refreshETag))
             .filterNotNullValues()
-    }
-
-    private fun Map<String, Any?>.convert(): JSONObject {
-        val mapWithoutInnerMaps = mapValues { (_, value) ->
-            value.tryCast<Map<String, Any?>>(ifSuccess = { convert() })
-        }
-        return JSONObject(mapWithoutInnerMaps)
-    }
-
-    // To avoid Java type erasure, we use a Kotlin inline function with a reified parameter
-    // so that we can check the type on runtime.
-    //
-    // Doing something like:
-    // if (value is Map<*, *>) (value as Map<String, Any?>).convert()
-    //
-    // Would give an unchecked cast warning due to Java type erasure
-    private inline fun <reified T> Any?.tryCast(
-        ifSuccess: T.() -> Any?
-    ): Any? {
-        return if (this is T) {
-            this.ifSuccess()
-        } else {
-            this
-        }
     }
 
     private fun getConnection(request: HTTPRequest): HttpURLConnection {
