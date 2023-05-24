69e4fe12924497091cc18825e4d9ad663bec30e9
Toni Rico <antonio.rico.diez@revenuecat.com>
2023-05-24 11:04:10 +0200

    Add some comments for clarity

--- common/src/integrationTest/kotlin/com/revenuecat/purchases/common/DispatcherConstants.kt
+++ common/src/integrationTest/kotlin/com/revenuecat/purchases/common/DispatcherConstants.kt
@@ -2,6 +2,7 @@ package com.revenuecat.purchases.common
 
 import kotlin.time.Duration.Companion.milliseconds
 
+// These values are reduced from the values used in the original code to make the tests run faster.
 object DispatcherConstants {
     val jitterDelay = 500L.milliseconds
     val jitterLongDelay = 1000L.milliseconds
