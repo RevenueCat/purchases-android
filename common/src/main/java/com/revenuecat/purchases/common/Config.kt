3950500a16342bc84bad985443604eb952c9d0bc
revenuecat-ops <ops@revenuecat.com>
2023-05-25 17:50:34 +0000

    Preparing for next version

--- common/src/main/java/com/revenuecat/purchases/common/Config.kt
+++ common/src/main/java/com/revenuecat/purchases/common/Config.kt
@@ -5,5 +5,5 @@ import com.revenuecat.purchases.LogLevel
 object Config {
     var logLevel = LogLevel.debugLogsEnabled(BuildConfig.DEBUG)
 
-    const val frameworkVersion = "6.3.0"
+    const val frameworkVersion = "6.4.0-SNAPSHOT"
 }
