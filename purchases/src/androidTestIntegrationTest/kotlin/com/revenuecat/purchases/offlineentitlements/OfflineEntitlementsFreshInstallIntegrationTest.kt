8d8e2f287e6beeda2341c8add449fb3cde792c2c
Toni Rico <antonio.rico.diez@revenuecat.com>
2023-06-23 03:52:21 -0700

    Fix offline entitlements integration tests (#1085)
    ### Description
    #1073 broke some more integration tests. I fixed one in #1080, but
    didn't run the offline entitlements integration tests 🤦‍♂️ .
    
    Most of these make sense after that functionality was added

--- purchases/src/androidTestIntegrationTest/kotlin/com/revenuecat/purchases/offlineentitlements/OfflineEntitlementsFreshInstallIntegrationTest.kt
+++ purchases/src/androidTestIntegrationTest/kotlin/com/revenuecat/purchases/offlineentitlements/OfflineEntitlementsFreshInstallIntegrationTest.kt
@@ -4,6 +4,7 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.revenuecat.purchases.Purchases
 import com.revenuecat.purchases.PurchasesErrorCode
 import com.revenuecat.purchases.getCustomerInfoWith
+import com.revenuecat.purchases.helpers.mockQueryProductDetails
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.fail
 import org.junit.Before
@@ -12,7 +13,37 @@ import org.junit.runner.RunWith
 
 @Suppress("TooManyFunctions")
 @RunWith(AndroidJUnit4::class)
-class OfflineEntitlementsFreshInstallIntegrationTest : BaseOfflineEntitlementsIntegrationTest() {
+class OfflineEntitlementsFreshInstallIntegrationNoPurchasesTest : BaseOfflineEntitlementsIntegrationTest() {
+
+    override val initialForceServerErrors = true
+
+    @Before
+    fun setUp() {
+        ensureBlockFinishes { latch ->
+            setUpTest {
+                latch.countDown()
+            }
+        }
+    }
+
+    @Test
+    fun doesNotEnterOfflineEntitlementsModeIfNoProductEntitlementMappingAvailable() {
+        ensureBlockFinishes { latch ->
+            Purchases.sharedInstance.getCustomerInfoWith(
+                onError = {
+                    assertThat(it.code).isEqualTo(PurchasesErrorCode.UnknownBackendError)
+                    latch.countDown()
+                },
+                onSuccess = {
+                    fail("Expected error")
+                },
+            )
+        }
+    }
+}
+
+@RunWith(AndroidJUnit4::class)
+class OfflineEntitlementsFreshInstallInitialPurchasesIntegrationTest : BaseOfflineEntitlementsIntegrationTest() {
 
     override val initialActivePurchasesToUse = initialActivePurchases
     override val initialForceServerErrors = true
@@ -21,6 +52,7 @@ class OfflineEntitlementsFreshInstallIntegrationTest : BaseOfflineEntitlementsIn
     fun setUp() {
         ensureBlockFinishes { latch ->
             setUpTest {
+                mockBillingAbstract.mockQueryProductDetails()
                 latch.countDown()
             }
         }
