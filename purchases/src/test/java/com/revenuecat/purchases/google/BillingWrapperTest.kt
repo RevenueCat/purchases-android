RevenueCat/purchases-android

commit e414de2b3920cea2e13104c0f5d0d31b7606df74

Sync pending purchases before getting customer info (#1073)
### Description
Followup to #1052 and #1058 

This changes the behavior of fetching customer info. Now, when fetching
customer info, we will first try to sync any pending purchases if any.
If there are pending purchases, we will return the customer info from
the last request sent. If there aren't, we will then hit the current
customer info endpoint to get the latest customer info.

purchases/src/main/kotlin/com/revenuecat/purchases/Purchases.kt
@@ -204,6 +204,7 @@ class Purchases internal constructor(
                 identityManager.currentAppUserID,
                 fetchPolicy = CacheFetchPolicy.FETCH_CURRENT,
                 appInBackground = false,
+                allowSharingPlayStoreAccount = allowSharingPlayStoreAccount,
             )
         }
         offeringsManager.onAppForeground(identityManager.currentAppUserID)
@@ -635,6 +636,7 @@ class Purchases internal constructor(
                 identityManager.currentAppUserID,
                 CacheFetchPolicy.default(),
                 state.appInBackground,
+                allowSharingPlayStoreAccount,
                 receiveCustomerInfoCallback(
                     onSuccess = { customerInfo ->
                         dispatch { callback?.onReceived(customerInfo, false) }
@@ -708,6 +710,7 @@ class Purchases internal constructor(
             identityManager.currentAppUserID,
             fetchPolicy,
             state.appInBackground,
+            allowSharingPlayStoreAccount,
             callback,
         )
     }
@@ -1112,6 +1115,7 @@ class Purchases internal constructor(
                 appUserID,
                 CacheFetchPolicy.FETCH_CURRENT,
                 appInBackground,
+                allowSharingPlayStoreAccount,
                 completion,
             )
             offeringsManager.fetchAndCacheOfferings(appUserID, appInBackground)

purchases/src/main/kotlin/com/revenuecat/purchases/CustomerInfoHelper.kt
@@ -17,6 +17,7 @@ internal class CustomerInfoHelper(
     private val backend: Backend,
     private val offlineEntitlementsManager: OfflineEntitlementsManager,
     private val customerInfoUpdateHandler: CustomerInfoUpdateHandler,
+    private val postPendingTransactionsHelper: PostPendingTransactionsHelper,
     private val handler: Handler = Handler(Looper.getMainLooper()),
 ) {
 
@@ -24,24 +25,28 @@ internal class CustomerInfoHelper(
         appUserID: String,
         fetchPolicy: CacheFetchPolicy,
         appInBackground: Boolean,
+        allowSharingPlayStoreAccount: Boolean,
         callback: ReceiveCustomerInfoCallback? = null,
     ) {
         debugLog(CustomerInfoStrings.RETRIEVING_CUSTOMER_INFO.format(fetchPolicy))
         when (fetchPolicy) {
             CacheFetchPolicy.CACHE_ONLY -> getCustomerInfoCacheOnly(appUserID, callback)
-            CacheFetchPolicy.FETCH_CURRENT -> getCustomerInfoFetchOnly(
+            CacheFetchPolicy.FETCH_CURRENT -> postPendingPurchasesAndFetchCustomerInfo(
                 appUserID,
                 appInBackground,
+                allowSharingPlayStoreAccount,
                 callback,
             )
             CacheFetchPolicy.CACHED_OR_FETCHED -> getCustomerInfoCachedOrFetched(
                 appUserID,
                 appInBackground,
+                allowSharingPlayStoreAccount,
                 callback,
             )
             CacheFetchPolicy.NOT_STALE_CACHED_OR_CURRENT -> getCustomerInfoNotStaledCachedOrFetched(
                 appUserID,
                 appInBackground,
+                allowSharingPlayStoreAccount,
                 callback,
             )
         }
@@ -64,6 +69,26 @@ internal class CustomerInfoHelper(
             errorLog(error)
             dispatch { callback.onError(error) }
         }
+    }
+
+    private fun postPendingPurchasesAndFetchCustomerInfo(
+        appUserID: String,
+        appInBackground: Boolean,
+        allowSharingPlayStoreAccount: Boolean,
+        callback: ReceiveCustomerInfoCallback? = null,
+    ) {
+        postPendingTransactionsHelper.syncPendingPurchaseQueue(
+            allowSharingPlayStoreAccount,
+            onError = { getCustomerInfoFetchOnly(appUserID, appInBackground, callback) },
+            onSuccess = { customerInfo ->
+                if (customerInfo == null) {
+                    getCustomerInfoFetchOnly(appUserID, appInBackground, callback)
+                } else {
+                    log(LogIntent.RC_SUCCESS, CustomerInfoStrings.CUSTOMERINFO_UPDATED_FROM_SYNCING_PENDING_PURCHASES)
+                    dispatch { callback?.onReceived(customerInfo) }
+                }
+            },
+        )
     }
 
     private fun getCustomerInfoFetchOnly(
@@ -109,28 +134,40 @@ internal class CustomerInfoHelper(
     private fun getCustomerInfoCachedOrFetched(
         appUserID: String,
         appInBackground: Boolean,
+        allowSharingPlayStoreAccount: Boolean,
         callback: ReceiveCustomerInfoCallback? = null,
     ) {
         val cachedCustomerInfo = getCachedCustomerInfo(appUserID)
         if (cachedCustomerInfo != null) {
             log(LogIntent.DEBUG, CustomerInfoStrings.VENDING_CACHE)
             dispatch { callback?.onReceived(cachedCustomerInfo) }
-            updateCachedCustomerInfoIfStale(appUserID, appInBackground)
+            updateCachedCustomerInfoIfStale(appUserID, appInBackground, allowSharingPlayStoreAccount)
         } else {
             log(LogIntent.DEBUG, CustomerInfoStrings.NO_CACHED_CUSTOMERINFO)
-            getCustomerInfoFetchOnly(appUserID, appInBackground, callback)
+            postPendingPurchasesAndFetchCustomerInfo(
+                appUserID,
+                appInBackground,
+                allowSharingPlayStoreAccount,
+                callback,
+            )
         }
     }
 
     private fun getCustomerInfoNotStaledCachedOrFetched(
         appUserID: String,
         appInBackground: Boolean,
+        allowSharingPlayStoreAccount: Boolean,
         callback: ReceiveCustomerInfoCallback? = null,
     ) {
         if (deviceCache.isCustomerInfoCacheStale(appUserID, appInBackground)) {
-            getCustomerInfoFetchOnly(appUserID, appInBackground, callback)
+            postPendingPurchasesAndFetchCustomerInfo(
+                appUserID,
+                appInBackground,
+                allowSharingPlayStoreAccount,
+                callback,
+            )
         } else {
-            getCustomerInfoCachedOrFetched(appUserID, appInBackground, callback)
+            getCustomerInfoCachedOrFetched(appUserID, appInBackground, allowSharingPlayStoreAccount, callback)
         }
     }
 
@@ -142,6 +179,7 @@ internal class CustomerInfoHelper(
     private fun updateCachedCustomerInfoIfStale(
         appUserID: String,
         appInBackground: Boolean,
+        allowSharingPlayStoreAccount: Boolean,
     ) {
         if (deviceCache.isCustomerInfoCacheStale(appUserID, appInBackground)) {
             log(
@@ -150,7 +188,7 @@ internal class CustomerInfoHelper(
                     CustomerInfoStrings.CUSTOMERINFO_STALE_UPDATING_BACKGROUND
                 } else CustomerInfoStrings.CUSTOMERINFO_STALE_UPDATING_FOREGROUND,
             )
-            getCustomerInfoFetchOnly(appUserID, appInBackground)
+            postPendingPurchasesAndFetchCustomerInfo(appUserID, appInBackground, allowSharingPlayStoreAccount)
         }
     }
 

purchases/src/main/kotlin/com/revenuecat/purchases/PostPendingTransactionsHelper.kt
@@ -31,39 +31,34 @@ internal class PostPendingTransactionsHelper(
             onSuccess?.invoke(null)
             return
         }
-        if (billing.isConnected()) {
-            log(LogIntent.DEBUG, PurchaseStrings.UPDATING_PENDING_PURCHASE_QUEUE)
-            val appUserID = identityManager.currentAppUserID
-            dispatcher.enqueue({
-                billing.queryPurchases(
-                    appUserID,
-                    onSuccess = { purchasesByHashedToken ->
-                        purchasesByHashedToken.forEach { (hash, purchase) ->
-                            log(
-                                LogIntent.DEBUG,
-                                RestoreStrings.QUERYING_PURCHASE_WITH_HASH.format(purchase.type, hash),
-                            )
-                        }
-                        deviceCache.cleanPreviouslySentTokens(purchasesByHashedToken.keys)
-                        val transactionsToSync = deviceCache.getActivePurchasesNotInCache(purchasesByHashedToken)
-                        postTransactionsWithCompletion(
-                            transactionsToSync,
-                            allowSharingPlayStoreAccount,
-                            appUserID,
-                            onError,
-                            onSuccess,
+        log(LogIntent.DEBUG, PurchaseStrings.UPDATING_PENDING_PURCHASE_QUEUE)
+        val appUserID = identityManager.currentAppUserID
+        dispatcher.enqueue({
+            billing.queryPurchases(
+                appUserID,
+                onSuccess = { purchasesByHashedToken ->
+                    purchasesByHashedToken.forEach { (hash, purchase) ->
+                        log(
+                            LogIntent.DEBUG,
+                            RestoreStrings.QUERYING_PURCHASE_WITH_HASH.format(purchase.type, hash),
                         )
-                    },
-                    onError = { error ->
-                        log(LogIntent.GOOGLE_ERROR, error.toString())
-                        onError?.invoke(error)
-                    },
-                )
-            })
-        } else {
-            log(LogIntent.DEBUG, PurchaseStrings.BILLING_CLIENT_NOT_CONNECTED)
-            onError?.invoke(PurchasesError(PurchasesErrorCode.StoreProblemError, "Billing client disconnected"))
-        }
+                    }
+                    deviceCache.cleanPreviouslySentTokens(purchasesByHashedToken.keys)
+                    val transactionsToSync = deviceCache.getActivePurchasesNotInCache(purchasesByHashedToken)
+                    postTransactionsWithCompletion(
+                        transactionsToSync,
+                        allowSharingPlayStoreAccount,
+                        appUserID,
+                        onError,
+                        onSuccess,
+                    )
+                },
+                onError = { error ->
+                    log(LogIntent.GOOGLE_ERROR, error.toString())
+                    onError?.invoke(error)
+                },
+            )
+        })
     }
 
     private fun postTransactionsWithCompletion(

purchases/src/main/kotlin/com/revenuecat/purchases/PurchasesFactory.kt
@@ -155,25 +155,6 @@ internal class PurchasesFactory(
                 offlineEntitlementsManager,
             )
 
-            val customerInfoHelper = CustomerInfoHelper(
-                cache,
-                backend,
-                offlineEntitlementsManager,
-                customerInfoUpdateHandler,
-            )
-            val offeringParser = OfferingParserFactory.createOfferingParser(store)
-
-            var diagnosticsSynchronizer: DiagnosticsSynchronizer? = null
-            if (diagnosticsFileHelper != null && diagnosticsTracker != null) {
-                diagnosticsSynchronizer = DiagnosticsSynchronizer(
-                    diagnosticsFileHelper,
-                    diagnosticsTracker,
-                    backend,
-                    diagnosticsDispatcher,
-                    DiagnosticsSynchronizer.initializeSharedPreferences(context),
-                )
-            }
-
             val postReceiptHelper = PostReceiptHelper(
                 appConfig,
                 backend,
@@ -182,19 +163,6 @@ internal class PurchasesFactory(
                 cache,
                 subscriberAttributesManager,
                 offlineEntitlementsManager,
-            )
-
-            val syncPurchasesHelper = SyncPurchasesHelper(
-                billing,
-                identityManager,
-                customerInfoHelper,
-                postReceiptHelper,
-            )
-
-            val offeringsManager = OfferingsManager(
-                offeringsCache,
-                backend,
-                OfferingsFactory(billing, offeringParser),
             )
 
             val postTransactionWithProductDetailsHelper = PostTransactionWithProductDetailsHelper(
@@ -209,6 +177,39 @@ internal class PurchasesFactory(
                 dispatcher,
                 identityManager,
                 postTransactionWithProductDetailsHelper,
+            )
+
+            val customerInfoHelper = CustomerInfoHelper(
+                cache,
+                backend,
+                offlineEntitlementsManager,
+                customerInfoUpdateHandler,
+                postPendingTransactionsHelper,
+            )
+            val offeringParser = OfferingParserFactory.createOfferingParser(store)
+
+            var diagnosticsSynchronizer: DiagnosticsSynchronizer? = null
+            if (diagnosticsFileHelper != null && diagnosticsTracker != null) {
+                diagnosticsSynchronizer = DiagnosticsSynchronizer(
+                    diagnosticsFileHelper,
+                    diagnosticsTracker,
+                    backend,
+                    diagnosticsDispatcher,
+                    DiagnosticsSynchronizer.initializeSharedPreferences(context),
+                )
+            }
+
+            val syncPurchasesHelper = SyncPurchasesHelper(
+                billing,
+                identityManager,
+                customerInfoHelper,
+                postReceiptHelper,
+            )
+
+            val offeringsManager = OfferingsManager(
+                offeringsCache,
+                backend,
+                OfferingsFactory(billing, offeringParser),
             )
 
             return Purchases(

purchases/src/main/kotlin/com/revenuecat/purchases/SyncPurchasesHelper.kt
@@ -37,7 +37,7 @@ internal class SyncPurchasesHelper(
                         if (currentPurchase == lastPurchase) {
                             if (errors.isEmpty()) {
                                 debugLog(PurchaseStrings.SYNCED_PURCHASES_SUCCESSFULLY)
-                                retrieveCustomerInfo(appUserID, appInBackground, onSuccess, onError)
+                                retrieveCustomerInfo(appUserID, appInBackground, isRestore, onSuccess, onError)
                             } else {
                                 errorLog(PurchaseStrings.SYNCING_PURCHASES_ERROR.format(errors))
                                 onError(errors.first())
@@ -69,7 +69,7 @@ internal class SyncPurchasesHelper(
                         )
                     }
                 } else {
-                    retrieveCustomerInfo(appUserID, appInBackground, onSuccess, onError)
+                    retrieveCustomerInfo(appUserID, appInBackground, isRestore, onSuccess, onError)
                 }
             },
             onReceivePurchaseHistoryError = {
@@ -82,6 +82,7 @@ internal class SyncPurchasesHelper(
     private fun retrieveCustomerInfo(
         appUserID: String,
         appInBackground: Boolean,
+        isRestore: Boolean,
         onSuccess: (CustomerInfo) -> Unit,
         onError: (PurchasesError) -> Unit,
     ) {
@@ -89,6 +90,7 @@ internal class SyncPurchasesHelper(
             appUserID,
             CacheFetchPolicy.CACHED_OR_FETCHED,
             appInBackground = appInBackground,
+            allowSharingPlayStoreAccount = isRestore,
             callback = object : ReceiveCustomerInfoCallback {
                 override fun onReceived(customerInfo: CustomerInfo) {
                     onSuccess(customerInfo)

purchases/src/test/java/com/revenuecat/purchases/BasePurchasesTest.kt
@@ -216,14 +216,20 @@ open class BasePurchasesTest {
     // region Protected methods
     protected fun mockCustomerInfoHelper(errorGettingCustomerInfo: PurchasesError? = null) {
         with(mockCustomerInfoHelper) {
+            val slotList = mutableListOf<ReceiveCustomerInfoCallback?>()
             every {
-                retrieveCustomerInfo(any(), any(), false, any())
+                retrieveCustomerInfo(
+                    any(),
+                    any(),
+                    appInBackground = false,
+                    allowSharingPlayStoreAccount = false,
+                    captureNullable(slotList),
+                )
             } answers {
-                val callback = arg<ReceiveCustomerInfoCallback?>(3)
                 if (errorGettingCustomerInfo == null) {
-                    callback?.onReceived(mockInfo)
+                    slotList.firstOrNull()?.onReceived(mockInfo)
                 } else {
-                    callback?.onError(errorGettingCustomerInfo)
+                    slotList.firstOrNull()?.onError(errorGettingCustomerInfo)
                 }
             }
         }

purchases/src/test/java/com/revenuecat/purchases/PostPendingTransactionsHelperTest.kt
@@ -66,21 +66,6 @@ class PostPendingTransactionsHelperTest {
     // region syncPendingPurchaseQueue
 
     @Test
-    fun `if billing client not connected do not query purchases`() {
-        changeBillingConnected(false)
-        syncAndAssertError(PurchasesError(PurchasesErrorCode.StoreProblemError, "Billing client disconnected"))
-        verify(exactly = 0) {
-            billing.queryPurchases(any(), any(), any())
-        }
-    }
-
-    @Test
-    fun `if billing client not connected error callback called`() {
-        changeBillingConnected(false)
-        syncAndAssertError(PurchasesError(PurchasesErrorCode.StoreProblemError, "Billing client disconnected"))
-    }
-
-    @Test
     fun `skip posting pending purchases if autosync is off`() {
         changeAutoSyncEnabled(false)
         syncAndAssertSuccessful(null)

commit c360ee9f5a93b24576f5b73ff840c05909bff2b1

Fix issue updating customer info on app open (#1128)
### Description
In some situations, we weren't refreshing the customer info on app open.
This was caused by #1073. In that PR we changed to fetch current
purchases before getting customer info, so we would sync purchases
before getting customer info. However, when we fetch customer info
during the app open's `onAppForeground` callback, the
`billing.purchasesUpdatedListener` was not set. Currently, if that
listener isn't set, we would just [skip the
query](https://github.com/RevenueCat/purchases-android/blob/ef34062996a17aa6a47d6eb2ca0903d66a51ab5e/purchases/src/main/kotlin/com/revenuecat/purchases/google/BillingWrapper.kt#L142)
altogether so we would never get a response.

Thanks @joshdholtz  for bringing it up!

purchases/src/main/kotlin/com/revenuecat/purchases/Purchases.kt
@@ -160,16 +160,19 @@ class Purchases internal constructor(
     init {
         identityManager.configure(backingFieldAppUserID)
 
-        dispatch {
-            ProcessLifecycleOwner.get().lifecycle.addObserver(lifecycleHandler)
-        }
-
         billing.stateListener = object : BillingAbstract.StateListener {
             override fun onConnected() {
                 postPendingTransactionsHelper.syncPendingPurchaseQueue(allowSharingPlayStoreAccount)
             }
         }
         billing.purchasesUpdatedListener = getPurchasesUpdatedListener()
+
+        dispatch {
+            // This needs to happen after the billing client listeners have been set. This is because
+            // we perform operations with the billing client in the lifecycle observer methods.
+            ProcessLifecycleOwner.get().lifecycle.addObserver(lifecycleHandler)
+        }
+
         if (!appConfig.dangerousSettings.autoSyncPurchases) {
             log(LogIntent.WARNING, AUTO_SYNC_PURCHASES_DISABLED)
         }

purchases/src/main/kotlin/com/revenuecat/purchases/google/BillingWrapper.kt
@@ -146,6 +146,9 @@ internal class BillingWrapper(
             } else {
                 executePendingRequests()
             }
+        } else {
+            // This shouldn't happen, but if it does, we want to propagate an error instead of hanging.
+            request(PurchasesError(PurchasesErrorCode.UnknownError, "BillingWrapper is not attached to a listener"))
         }
     }
 

purchases/src/test/java/com/revenuecat/purchases/google/BillingWrapperTest.kt
@@ -198,6 +198,25 @@ class BillingWrapperTest {
     }
 
     @Test
+    fun `if no listener is set, we fail`() {
+        wrapper.purchasesUpdatedListener = null
+
+        var error: PurchasesError? = null
+        wrapper.queryPurchaseHistoryAsync(
+            ProductType.SUBS.toGoogleProductType()!!,
+            {
+                fail("call should not succeed")
+            },
+            {
+                error = it
+            }
+        )
+        assertThat(error).isNotNull
+        assertThat(error?.code).isEqualTo(PurchasesErrorCode.UnknownError)
+        assertThat(error?.underlyingErrorMessage).isEqualTo("BillingWrapper is not attached to a listener")
+    }
+
+    @Test
     fun defersCallUntilConnected() {
         every { mockClient.isReady } returns false
 