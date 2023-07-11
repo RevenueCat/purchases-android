9d446d0ea709718efdf381906a567f7510ebf979
Toni Rico <antonio.rico.diez@revenuecat.com>
2023-07-11 16:58:52 +0200

    Improve signature verification error log

--- purchases/src/main/kotlin/com/revenuecat/purchases/errors.kt
+++ purchases/src/main/kotlin/com/revenuecat/purchases/errors.kt
@@ -57,5 +57,6 @@ enum class PurchasesErrorCode(val code: Int, val description: String) {
     ),
     EmptySubscriberAttributesError(25, "A request for subscriber attributes returned none."),
     CustomerInfoError(28, "There was a problem related to the customer info."),
-    SignatureVerificationError(36, "Request failed signature verification."),
+    SignatureVerificationError(36, "Request failed signature verification. " +
+        "Please see https://www.revenuecat.com/docs/trusted-entitlements for more info."),
 }
