19246692b4324fb2329f109cd9652dc2bb5cf22e
Toni Rico <antonio.rico.diez@revenuecat.com>
2023-07-25 10:50:16 +0200

    CustomEntitlementComputation: Integration tests (#1164)
    ### Description
    This PR adds integration tests for the custom entitlement computation
    feature

---
+++ purchases/src/androidTestDefaults/kotlin/com/revenuecat/purchases/PurchasesTestConfigurationExtensions.kt
@@ -0,0 +1,28 @@
+package com.revenuecat.purchases
+
+import android.content.Context
+import com.revenuecat.purchases.common.BillingAbstract
+
+@Suppress("LongParameterList")
+internal fun Purchases.Companion.configureSdk(
+    context: Context,
+    appUserID: String,
+    billingAbstract: BillingAbstract,
+    entitlementVerificationMode: EntitlementVerificationMode? = null,
+    forceServerErrors: Boolean = false,
+    forceSigningErrors: Boolean = false,
+) {
+    Purchases.configure(
+        PurchasesConfiguration.Builder(context, Constants.apiKey)
+            .appUserID(appUserID)
+            .apply {
+                if (entitlementVerificationMode != null) {
+                    entitlementVerificationMode(entitlementVerificationMode)
+                }
+            }
+            .build(),
+        billingAbstract,
+        forceServerErrors,
+        forceSigningErrors,
+    )
+}
