2ca4245d7333dae180820aa3a82eb95fcee9866c
Toni Rico <antonio.rico.diez@revenuecat.com>
2023-07-27 18:11:28 +0200

    Add header to menu

--- debugview/src/debug/kotlin/com/revenuecat/purchases/debugview/InternalDebugRevenueCatScreen.kt
+++ debugview/src/debug/kotlin/com/revenuecat/purchases/debugview/InternalDebugRevenueCatScreen.kt
@@ -2,12 +2,16 @@ package com.revenuecat.purchases.debugview
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.revenuecat.purchases.debugview.models.InternalDebugRevenueCatScreenViewModel
 import com.revenuecat.purchases.debugview.models.SettingGroupState
@@ -25,6 +29,11 @@ internal fun InternalDebugRevenueCatScreen(
             .verticalScroll(rememberScrollState())
             .fillMaxWidth(),
     ) {
+        Text(
+            text = "RevenueCat Debug Menu",
+            style = MaterialTheme.typography.h5,
+            modifier = Modifier.padding(horizontal = 16.dp, vertical = 16.dp),
+        )
         screenViewModel.state.collectAsState().value.toGroupStates().forEach { SettingGroup(it) }
     }
 }
