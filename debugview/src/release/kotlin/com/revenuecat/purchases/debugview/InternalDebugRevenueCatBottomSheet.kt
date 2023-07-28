ac9034de9a4eae11c14322af5a71e4dae96d767e
Toni Rico <antonio.rico.diez@revenuecat.com>
2023-07-28 10:25:09 +0200

    Fix tests

--- debugview/src/release/kotlin/com/revenuecat/purchases/debugview/InternalDebugRevenueCatBottomSheet.kt
+++ debugview/src/release/kotlin/com/revenuecat/purchases/debugview/InternalDebugRevenueCatBottomSheet.kt
@@ -1,8 +1,10 @@
 package com.revenuecat.purchases.debugview
 
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.SheetState
 import androidx.compose.runtime.Composable
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Suppress("EmptyFunctionBlock", "UnusedParameter")
 @Composable
 internal fun InternalDebugRevenueCatBottomSheet(sheetState: SheetState) {}
