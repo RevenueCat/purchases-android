2e1dbd2b6340ce6bc6d91595bc7cc3ac310f038c
Toni Rico <antonio.rico.diez@revenuecat.com>
2023-08-01 11:49:04 +0200

    Add documentation

--- debugview/src/main/kotlin/com/revenuecat/purchases/debugview/DebugRevenueCatScreen.kt
+++ debugview/src/main/kotlin/com/revenuecat/purchases/debugview/DebugRevenueCatScreen.kt
@@ -4,6 +4,12 @@ import androidx.compose.runtime.Composable
 import com.revenuecat.purchases.PurchasesTransactionException
 import com.revenuecat.purchases.models.StoreTransaction
 
+/**
+ * Composable function that allows to display the debug screen as any part of the screen in your composable tree.
+ *
+ * @param onPurchaseCompleted Callback that will be called when a purchase is completed within the debug screen.
+ * @param onPurchaseErrored Callback that will be called when a purchase fails or is cancelled within the debug screen.
+ */
 @Composable
 fun DebugRevenueCatScreen(
     onPurchaseCompleted: (StoreTransaction) -> Unit,
