dfc9cab4fa35822a36963ff5792be50516d5998e
Toni Rico <antonio.rico.diez@revenuecat.com>
2023-09-04 12:18:49 +0200

    Create paywalls tester app

---
+++ examples/paywall-tester/src/main/java/com/revenuecat/paywallstester/PaywallTesterApp.kt
@@ -0,0 +1,9 @@
+package com.revenuecat.paywallstester
+
+import androidx.compose.runtime.Composable
+import com.revenuecat.paywallstester.ui.screens.main.MainScreen
+
+@Composable
+fun PaywallTesterApp() {
+    MainScreen()
+}
