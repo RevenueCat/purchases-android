f617e361bc114123aa5404f79f10ce0d70da5efd
Toni Rico <toni.rico.diez@revenuecat.com>
2023-09-06 09:29:34 +0200

    Add RevenueCatUI module and initial API (#1213)
    ### Description
    This PR creates a new module `:ui:revenuecatui` as a starting point for
    paywalls. Additionally, it adds some initial code for the public API.

---
+++ ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/PaywallView.kt
@@ -0,0 +1,13 @@
+package com.revenuecat.purchases.ui.revenuecatui
+
+import androidx.compose.runtime.Composable
+
+/**
+ * Composable offering a full screen Paywall UI configured from the RevenueCat dashboard.
+ * @param options The options to configure the PaywallView if needed.
+ */
+@Suppress("unused")
+@Composable
+fun PaywallView(options: PaywallViewOptions = PaywallViewOptions.Builder().build()) {
+    // WIP
+}
---
+++ ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/PaywallViewOptions.kt
@@ -0,0 +1,38 @@
+package com.revenuecat.purchases.ui.revenuecatui
+
+import com.revenuecat.purchases.Offering
+
+class PaywallViewOptions(builder: Builder) {
+
+    val offering: Offering?
+    val shouldDisplayDismissButton: Boolean
+    val listener: PaywallViewListener?
+
+    init {
+        this.offering = builder.offering
+        this.shouldDisplayDismissButton = builder.shouldDisplayDismissButton
+        this.listener = builder.listener
+    }
+
+    class Builder {
+        internal var offering: Offering? = null
+        internal var shouldDisplayDismissButton: Boolean = false
+        internal var listener: PaywallViewListener? = null
+
+        fun setOffering(offering: Offering?) = apply {
+            this.offering = offering
+        }
+
+        fun setShouldDisplayDismissButton(shouldDisplayDismissButton: Boolean) = apply {
+            this.shouldDisplayDismissButton = shouldDisplayDismissButton
+        }
+
+        fun setListener(listener: PaywallViewListener) = apply {
+            this.listener = listener
+        }
+
+        fun build(): PaywallViewOptions {
+            return PaywallViewOptions(this)
+        }
+    }
+}
