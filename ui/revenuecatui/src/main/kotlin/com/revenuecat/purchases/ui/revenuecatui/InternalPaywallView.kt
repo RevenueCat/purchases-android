104cb2be567c46ada6d4c95f1bf6c8daa72e1567
Toni Rico <toni.rico.diez@revenuecat.com>
2023-09-07 17:25:14 +0200

    Paywalls: Use paywall data in paywall (#1230)
    ### Description
    This just adds a couple of text views as a PoC of setting a paywall in
    the dashboard and seeing the data in the UI.
    
    ![image](https://github.com/RevenueCat/purchases-android/assets/808417/ec95eef0-63e1-4ece-8432-9b521bcfdbdf)

--- ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/InternalPaywallView.kt
+++ ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/InternalPaywallView.kt
@@ -10,15 +10,18 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.revenuecat.purchases.Offering
+import java.util.Locale
 
 @Composable
 internal fun InternalPaywallView(
@@ -40,9 +43,24 @@ internal fun InternalPaywallView(
                 Text(text = "Error: ${state.errorMessage}")
             }
             is PaywallViewState.Loaded -> {
-                Text(text = "Paywall for offeringId: ${state.offering.identifier}")
+                val offering = state.offering
+                Text(text = "Paywall for offeringId: ${offering.identifier}")
+                // TODO-PAYWALLS: Use device locale and move logic to view model
+                offering.paywall?.configForLocale(Locale.ENGLISH)?.let { localizedConfiguration ->
+                    Text(
+                        style = MaterialTheme.typography.h5,
+                        textAlign = TextAlign.Center,
+                        text = localizedConfiguration.title,
+                    )
+                    Text(
+                        style = MaterialTheme.typography.subtitle1,
+                        textAlign = TextAlign.Center,
+                        text = localizedConfiguration.subtitle ?: "",
+                    )
+                }
                 val activity = LocalContext.current.getActivity() ?: error("Error finding activity")
-                state.offering.availablePackages.forEach { aPackage ->
+
+                offering.availablePackages.forEach { aPackage ->
                     Button(onClick = { viewModel.purchasePackage(activity, aPackage) }) {
                         Text(text = "Purchase ${aPackage.identifier}. Price: ${aPackage.product.price.formatted}")
                     }
