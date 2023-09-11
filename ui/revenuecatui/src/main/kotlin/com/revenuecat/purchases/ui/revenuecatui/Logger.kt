c0e90cc18ea4d4383a1151d9022f5ea52e2601c5
Toni Rico <antonio.rico.diez@revenuecat.com>
2023-09-11 11:33:20 +0200

    Fix detekt

--- ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/Logger.kt
+++ ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/Logger.kt
@@ -2,21 +2,19 @@ package com.revenuecat.purchases.ui.revenuecatui
 
 import android.util.Log
 
-internal class Logger {
-    companion object {
-        private const val TAG = "RevenueCatUI"
+object Logger {
+    private const val TAG = "RevenueCatUI"
 
-        // TODO-PAYWALLS, allow hooking up a custom log handler
-        fun e(message: String) {
-            Log.e(TAG, message)
-        }
+    // TODO-PAYWALLS, allow hooking up a custom log handler
+    fun e(message: String) {
+        Log.e(TAG, message)
+    }
 
-        fun e(message: String, throwable: Throwable) {
-            Log.e(TAG, message, throwable)
-        }
+    fun e(message: String, throwable: Throwable) {
+        Log.e(TAG, message, throwable)
+    }
 
-        fun i(message: String) {
-            Log.i(TAG, message)
-        }
+    fun i(message: String) {
+        Log.i(TAG, message)
     }
 }
