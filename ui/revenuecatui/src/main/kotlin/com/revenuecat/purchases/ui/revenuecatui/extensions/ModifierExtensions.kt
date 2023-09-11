fed1b08484858c691cb89f8939a96df960801c4e
NachoSoto <ignaciosoto90@gmail.com>
2023-09-11 16:39:43 -0700

    `Paywalls`: created `PaywallBackground`
    This creates a generic `PaywallBackground` that can be used in any template.
    
    - Created `Modifier.conditional`
    - Changed `PaywallData.getUrlStringFromImage` to return `String?` (images are optional)
    - Added `PaywallBackground` to `Template2`

---
+++ ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/extensions/ModifierExtensions.kt
@@ -0,0 +1,11 @@
+package com.revenuecat.purchases.ui.revenuecatui.extensions
+
+import androidx.compose.ui.Modifier
+
+fun Modifier.conditional(condition: Boolean, modifier: Modifier.() -> Modifier): Modifier {
+    return if (condition) {
+        then(modifier(Modifier))
+    } else {
+        this
+    }
+}
