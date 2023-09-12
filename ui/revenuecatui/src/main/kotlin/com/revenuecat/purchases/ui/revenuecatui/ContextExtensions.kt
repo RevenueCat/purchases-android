3ee58ca1cfb9baf0fddd4f48ca3fd0da2ea0d2e6
Toni Rico <toni.rico.diez@revenuecat.com>
2023-09-12 08:42:23 +0200

    Update ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/ContextExtensions.kt
    Co-authored-by: NachoSoto <NachoSoto@users.noreply.github.com>

--- ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/ContextExtensions.kt
+++ ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/ContextExtensions.kt
@@ -5,7 +5,7 @@ import android.content.Context
 import android.content.ContextWrapper
 
 /**
- * This function gets the activity from a given context. Most times, the context itself will be
+ * Returns the activity from a given context. Most times, the context itself will be
  * an activity, but in the case it's not, it will iterate through the context wrappers until it
  * finds one that is an activity.
  */
