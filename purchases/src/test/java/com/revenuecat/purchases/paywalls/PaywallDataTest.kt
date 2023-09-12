f2965432b21e489b61c050ef8d632acbb32f2236
Cesar de la Vega <cesarvegaro@gmail.com>
2023-09-12 11:52:07 +0200

    config for locale fixes

--- purchases/src/test/java/com/revenuecat/purchases/paywalls/PaywallDataTest.kt
+++ purchases/src/test/java/com/revenuecat/purchases/paywalls/PaywallDataTest.kt
@@ -8,6 +8,8 @@ import io.mockk.unmockkConstructor
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.annotation.Config
