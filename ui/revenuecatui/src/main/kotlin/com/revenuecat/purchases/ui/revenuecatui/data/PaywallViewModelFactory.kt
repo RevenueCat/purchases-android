799614ade762b679c26f0bfeed2c1668d33e3922
Cesar de la Vega <cesarvegaro@gmail.com>
2023-09-13 13:56:03 +0200

    missing import

--- ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/data/PaywallViewModelFactory.kt
+++ ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/data/PaywallViewModelFactory.kt
@@ -3,6 +3,7 @@ package com.revenuecat.purchases.ui.revenuecatui.data
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.revenuecat.purchases.Offering
+import com.revenuecat.purchases.ui.revenuecatui.PaywallViewListener
 
 internal class PaywallViewModelFactory(
     private val offering: Offering?,
