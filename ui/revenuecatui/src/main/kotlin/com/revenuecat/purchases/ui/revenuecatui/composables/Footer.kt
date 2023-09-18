bf66f648ca0b2438d608fb14dc2a27079f8e007d
NachoSoto <ignaciosoto90@gmail.com>
2023-09-18 13:05:35 -0700

    Improved separators

--- ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/composables/Footer.kt
+++ ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/composables/Footer.kt
@@ -1,7 +1,8 @@
 package com.revenuecat.purchases.ui.revenuecatui.composables
 
-import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
@@ -10,11 +11,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -97,14 +101,20 @@ internal fun Footer(
 }
 
 @Composable
-private fun Separator(color: Color) {
-    Canvas(
+private fun RowScope.Separator(color: Color) {
+    Column(
         modifier = Modifier
-            .size(10.dp),
-        onDraw = {
-            drawCircle(color = color)
-        }
-    )
+            .fillMaxWidth()
+            .wrapContentSize(Alignment.Center)
+            .weight(0.5f)
+    ) {
+        Box(
+            modifier = Modifier
+                .size(10.dp)
+                .clip(CircleShape)
+                .background(color)
+        )
+    }
 }
 
 @Composable
