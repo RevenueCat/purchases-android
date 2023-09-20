b9dca647081f0dcfe61e58d3357fb1e2ad0c2ae7
NachoSoto <ignaciosoto90@gmail.com>
2023-09-20 10:41:54 -0700

    Updated default padding
    See https://m3.material.io/foundations/layout/understanding-layout/spacing#5893f0f7-ac4a-41fc-a109-00f0daafd82a

--- ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/UIConstant.kt
+++ ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/UIConstant.kt
@@ -3,6 +3,6 @@ package com.revenuecat.purchases.ui.revenuecatui
 import androidx.compose.ui.unit.dp
 
 internal object UIConstant {
-    val defaultHorizontalPadding = 10.dp
-    val defaultVerticalSpacing = 10.dp
+    val defaultHorizontalPadding = 12.dp
+    val defaultVerticalSpacing = 12.dp
 }
