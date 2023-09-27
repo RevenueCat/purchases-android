d1cb93aa75af11b3c1889913bfd105cef7c0afb0
Cesar de la Vega <cesarvegaro@gmail.com>
2023-09-27 16:13:24 +0200

    enable Template3

--- ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/InternalPaywallView.kt
+++ ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/InternalPaywallView.kt
@@ -16,6 +16,7 @@ import com.revenuecat.purchases.ui.revenuecatui.helpers.isInPreviewMode
 import com.revenuecat.purchases.ui.revenuecatui.helpers.toAndroidContext
 import com.revenuecat.purchases.ui.revenuecatui.templates.Template1
 import com.revenuecat.purchases.ui.revenuecatui.templates.Template2
+import com.revenuecat.purchases.ui.revenuecatui.templates.Template3
 
 @Composable
 internal fun InternalPaywallView(
@@ -39,7 +40,7 @@ internal fun InternalPaywallView(
             when (state.templateConfiguration.template) {
                 PaywallTemplate.TEMPLATE_1 -> Template1(state = state, viewModel = viewModel)
                 PaywallTemplate.TEMPLATE_2 -> Template2(state = state, viewModel = viewModel)
-                PaywallTemplate.TEMPLATE_3 -> Text(text = "Error: Template 3 not supported")
+                PaywallTemplate.TEMPLATE_3 -> Template3(state = state, viewModel = viewModel)
                 PaywallTemplate.TEMPLATE_4 -> Text(text = "Error: Template 4 not supported")
                 PaywallTemplate.TEMPLATE_5 -> Text(text = "Error: Template 5 not supported")
             }
