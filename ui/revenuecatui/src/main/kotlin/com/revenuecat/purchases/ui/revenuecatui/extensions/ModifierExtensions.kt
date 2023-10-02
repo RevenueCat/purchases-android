4797a4e10d8cfb17d255dd872018e45ad3f5e59d
NachoSoto <NachoSoto@users.noreply.github.com>
2023-10-02 14:59:54 -0500

    `Paywalls`: Intro Eligibility dependent composable (#1286)
    This is equivalent to
    https://github.com/RevenueCat/purchases-ios/blob/main/RevenueCatUI/Views/IntroEligibilityStateView.swift.
    
    It allows us to display different text depending on:
    - Whether there is a special `_with_intro_offer` version of the text
    - Has eligible offers
    
    This will be used when displaying `call_to_action` and `offer_details`.
    And later on we can expand this to support multiple phases.

--- ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/extensions/ModifierExtensions.kt
+++ ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/extensions/ModifierExtensions.kt
@@ -2,7 +2,7 @@ package com.revenuecat.purchases.ui.revenuecatui.extensions
 
 import androidx.compose.ui.Modifier
 
-fun Modifier.conditional(condition: Boolean, modifier: Modifier.() -> Modifier): Modifier {
+internal fun Modifier.conditional(condition: Boolean, modifier: Modifier.() -> Modifier): Modifier {
     return if (condition) {
         then(modifier(Modifier))
     } else {
