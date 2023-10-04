a6507bcedb8c32f4412fb183ab8eff410aa68c15
Cesar de la Vega <cesarvegaro@gmail.com>
2023-10-04 18:09:48 +0200

    creates file OfferingWithMultiPackagePaywall

--- ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/data/testdata/TestData.kt
+++ ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/data/testdata/TestData.kt
@@ -16,7 +16,6 @@ import com.revenuecat.purchases.models.RecurrenceMode
 import com.revenuecat.purchases.models.StoreProduct
 import com.revenuecat.purchases.models.SubscriptionOption
 import com.revenuecat.purchases.models.SubscriptionOptions
-import com.revenuecat.purchases.paywalls.PaywallColor
 import com.revenuecat.purchases.paywalls.PaywallData
 import com.revenuecat.purchases.ui.revenuecatui.PaywallViewMode
 import com.revenuecat.purchases.ui.revenuecatui.R
@@ -128,46 +127,6 @@ internal object TestData {
         metadata = mapOf(),
         paywall = template3,
         serverDescription = "",
-    )
-
-    val offeringWithMultiPackagePaywall = Offering(
-        identifier = "offeringWithMultiPackagePaywall",
-        serverDescription = "Offering",
-        metadata = mapOf(),
-        paywall = PaywallData(
-            templateName = PaywallTemplate.TEMPLATE_2.id,
-            config = PaywallData.Configuration(
-                packages = listOf(PackageType.ANNUAL.identifier!!, PackageType.MONTHLY.identifier!!),
-                images = Constants.images,
-                colors = PaywallData.Configuration.ColorInformation(
-                    light = PaywallData.Configuration.Colors(
-                        background = PaywallColor("#FFFFFF"),
-                        text1 = PaywallColor("#111111"),
-                        callToActionBackground = PaywallColor("#EC807C"),
-                        callToActionForeground = PaywallColor("#FFFFFF"),
-                        accent1 = PaywallColor("#BC66FF"),
-                        accent2 = PaywallColor("#111100"),
-                    ),
-                    dark = PaywallData.Configuration.Colors(
-                        background = PaywallColor("#000000"),
-                        text1 = PaywallColor("#EEEEEE"),
-                        callToActionBackground = PaywallColor("#ACD27A"),
-                        callToActionForeground = PaywallColor("#000000"),
-                        accent1 = PaywallColor("#B022BB"),
-                        accent2 = PaywallColor("#EEDDEE"),
-                    ),
-                ),
-                blurredBackgroundImage = true,
-                privacyURL = URL("https://revenuecat.com/tos"),
-            ),
-            localization = mapOf("en_US" to Constants.localization),
-            assetBaseURL = Constants.assetBaseURL,
-        ),
-        availablePackages = listOf(
-            Packages.weekly,
-            Packages.monthly,
-            Packages.annual,
-        ),
     )
 
     object Packages {
