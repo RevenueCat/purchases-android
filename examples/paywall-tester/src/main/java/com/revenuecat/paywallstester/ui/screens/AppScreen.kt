58c3502f97f3711a1f4142daf15cf2123a991757
Toni Rico <toni.rico.diez@revenuecat.com>
2023-10-06 12:30:26 +0200

    Paywalls: Update paywall tester to be able to display paywall footer (#1315)
    ### Description
    Followup to #1314 in order to be able to use the new `PaywallFooter` in
    the paywalls tester app

--- examples/paywall-tester/src/main/java/com/revenuecat/paywallstester/ui/screens/AppScreen.kt
+++ examples/paywall-tester/src/main/java/com/revenuecat/paywallstester/ui/screens/AppScreen.kt
@@ -3,4 +3,5 @@ package com.revenuecat.paywallstester.ui.screens
 sealed class AppScreen(val route: String) {
     object Main : AppScreen("main")
     object Paywall : AppScreen("paywall")
+    object PaywallFooter : AppScreen("paywall_footer")
 }
