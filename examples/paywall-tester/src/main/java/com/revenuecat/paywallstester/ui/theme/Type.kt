f8e9f0bfe35617fa72385e42c00d6be303df801f
Toni Rico <antonio.rico.diez@revenuecat.com>
2023-10-09 09:39:32 +0200

    PoC of overriding fonts through the theme

--- examples/paywall-tester/src/main/java/com/revenuecat/paywallstester/ui/theme/Type.kt
+++ examples/paywall-tester/src/main/java/com/revenuecat/paywallstester/ui/theme/Type.kt
@@ -2,19 +2,50 @@ package com.revenuecat.paywallstester.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.revenuecat.paywallstester.R
+
+val superMarioFontFamily =
+    FontFamily(
+        Font(
+            R.font.super_mario,
+        ),
+    )
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = superMarioFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp,
     ),
+    displaySmall = TextStyle(
+        fontFamily = superMarioFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp,
+    ),
+    titleLarge = TextStyle(
+        fontFamily = superMarioFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp,
+    ),
+    titleMedium = TextStyle(
+        fontFamily = superMarioFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 18.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.sp,
+    ),
+
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
