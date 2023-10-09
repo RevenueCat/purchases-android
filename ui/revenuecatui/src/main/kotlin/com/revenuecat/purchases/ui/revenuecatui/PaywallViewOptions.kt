5968c4466b0945eae3073337fc35b855e4727b68
Toni Rico <antonio.rico.diez@revenuecat.com>
2023-10-09 09:01:55 +0200

    Add PaywallViewOptions with different ways to select an offering

--- ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/PaywallViewOptions.kt
+++ ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/PaywallViewOptions.kt
@@ -2,33 +2,52 @@ package com.revenuecat.purchases.ui.revenuecatui
 
 import com.revenuecat.purchases.Offering
 
-class PaywallViewOptions(builder: Builder) {
+internal sealed class OfferingSelection {
+    data class OfferingType(val offeringType: Offering) : OfferingSelection()
+    data class OfferingId(val offeringId: String) : OfferingSelection()
+    object None : OfferingSelection()
 
     val offering: Offering?
-    internal val offeringId: String?
+        get() = when (this) {
+            is OfferingType -> offeringType
+            is OfferingId -> null
+            None -> null
+        }
+
+    val offeringIdentifier: String?
+        get() = when (this) {
+            is OfferingType -> offeringType.identifier
+            is OfferingId -> offeringId
+            None -> null
+        }
+}
+
+class PaywallViewOptions(builder: Builder) {
+
+    internal val offeringSelection: OfferingSelection
     val shouldDisplayDismissButton: Boolean
     val listener: PaywallViewListener?
     internal var mode: PaywallViewMode = PaywallViewMode.default
 
     init {
-        this.offering = builder.offering
-        this.offeringId = builder.offeringId
+        this.offeringSelection = builder.offeringSelection
         this.shouldDisplayDismissButton = builder.shouldDisplayDismissButton
         this.listener = builder.listener
     }
 
     class Builder {
-        internal var offering: Offering? = null
-        internal var offeringId: String? = null
+        internal var offeringSelection: OfferingSelection = OfferingSelection.None
         internal var shouldDisplayDismissButton: Boolean = false
         internal var listener: PaywallViewListener? = null
 
         fun setOffering(offering: Offering?) = apply {
-            this.offering = offering
+            this.offeringSelection = offering?.let { OfferingSelection.OfferingType(it) }
+                ?: OfferingSelection.None
         }
 
         internal fun setOfferingId(offeringId: String?) = apply {
-            this.offeringId = offeringId
+            this.offeringSelection = offeringId?.let { OfferingSelection.OfferingId(it) }
+                ?: OfferingSelection.None
         }
 
         fun setShouldDisplayDismissButton(shouldDisplayDismissButton: Boolean) = apply {
