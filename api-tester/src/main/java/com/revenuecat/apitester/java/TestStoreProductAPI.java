fa43b0585c8efc1dcb540ac3517ab9d68b609fe3
NachoSoto <NachoSoto@users.noreply.github.com>
2023-10-11 09:18:32 -0700

    Expose new `TestStoreProduct` (#1333)
    This can be used to create mock data for tests or Jetpack Compose
    previews.

---
+++ api-tester/src/main/java/com/revenuecat/apitester/java/TestStoreProductAPI.java
@@ -0,0 +1,17 @@
+package com.revenuecat.apitester.java;
+
+import com.revenuecat.purchases.models.Period;
+import com.revenuecat.purchases.models.Price;
+import com.revenuecat.purchases.models.TestStoreProduct;
+
+@SuppressWarnings({"unused"})
+final class TestStoreProductAPI {
+    static void check(final Price price, final Period period) {
+        new TestStoreProduct(
+             "id", "title", "description", price, period, null, null
+        );
+        new TestStoreProduct(
+            "id", "title", "description", price, period, period, price
+        );
+    }
+}
--- purchases/src/test/java/com/revenuecat/purchases/PurchaseParamsTest.kt
+++ purchases/src/test/java/com/revenuecat/purchases/PurchaseParamsTest.kt
@@ -5,15 +5,17 @@
 
 package com.revenuecat.purchases
 
-import android.app.Activity
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.revenuecat.purchases.models.SubscriptionOption
+import com.revenuecat.purchases.models.Period
+import com.revenuecat.purchases.models.Price
+import com.revenuecat.purchases.models.TestStoreProduct
 import com.revenuecat.purchases.utils.STUB_OFFERING_IDENTIFIER
 import com.revenuecat.purchases.utils.stubOfferings
 import com.revenuecat.purchases.utils.stubStoreProduct
 import com.revenuecat.purchases.utils.stubSubscriptionOption
 import io.mockk.mockk
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.fail
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -45,6 +47,23 @@ class PurchaseParamsTest {
     }
 
     @Test
+    fun `Initializing with TestStoreProduct throws error`() {
+        val storeProduct = TestStoreProduct(
+            "id",
+            "title",
+            "description",
+            Price("$1.99", 1_990_000, "US"),
+            Period(1, Period.Unit.MONTH, "P1M")
+        )
+        try {
+            PurchaseParams.Builder(mockk(), storeProduct).build()
+            fail("Expected error")
+        } catch (e: PurchasesException) {
+            assertThat(e.code).isEqualTo(PurchasesErrorCode.ProductNotAvailableForPurchaseError)
+        }
+    }
+
+    @Test
     fun `Initializing with SubscriptionOption sets proper presentedOfferingIdentifier`() {
         val storeProduct = stubStoreProduct("abc", presentedOfferingId = STUB_OFFERING_IDENTIFIER)
         val (_, offerings) = stubOfferings(storeProduct)
@@ -69,6 +88,24 @@ class PurchaseParamsTest {
 
         val expectedPurchasingData = packageToPurchase.product.purchasingData
         assertThat(purchasePackageParams.purchasingData).isEqualTo(expectedPurchasingData)
+    }
+
+    @Test
+    fun `Initializing with Package containing TestStoreProduct throws error`() {
+        val storeProduct = TestStoreProduct(
+            "id",
+            "title",
+            "description",
+            Price("$1.99", 1_990_000, "US"),
+            Period(1, Period.Unit.MONTH, "P1M")
+        )
+        val (_, offerings) = stubOfferings(storeProduct)
+        try {
+            PurchaseParams.Builder(mockk(), offerings[STUB_OFFERING_IDENTIFIER]!!.monthly!!).build()
+            fail("Expected error")
+        } catch (e: PurchasesException) {
+            assertThat(e.code).isEqualTo(PurchasesErrorCode.ProductNotAvailableForPurchaseError)
+        }
     }
 
     @Test
