d83c25b305b38ecb88eddb4e11061c3edaf454bc
Toni Rico <antonio.rico.diez@revenuecat.com>
2023-10-17 09:51:55 +0200

    Move typography copy to extension

--- ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/fonts/FontProvider.kt
+++ ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/fonts/FontProvider.kt
@@ -1,7 +1,6 @@
 package com.revenuecat.purchases.ui.revenuecatui.fonts
 
 import androidx.compose.ui.text.font.FontFamily
-import com.revenuecat.purchases.ui.revenuecatui.PaywallView
 
 /**
  * Implement this interface to provide custom fonts to the [PaywallView]. If you don't, the current material3 theme
