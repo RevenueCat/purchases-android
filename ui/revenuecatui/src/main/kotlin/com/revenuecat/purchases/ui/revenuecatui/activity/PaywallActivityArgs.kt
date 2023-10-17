46b9cf963a803f269c3af7f363a0d1f7ec8aa2d7
Toni Rico <toni.rico.diez@revenuecat.com>
2023-10-17 10:08:26 +0200

    Paywalls: Support custom fonts through FontProvider (#1328)
    ### Description
    This adds a way for devs to give us a `FontProvider`, which will be used
    to override the font family in our views when a font family is provided.
    If it's not provided, the current material3 theme's font will be used.

--- ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/activity/PaywallActivityArgs.kt
+++ ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/activity/PaywallActivityArgs.kt
@@ -1,9 +1,17 @@
 package com.revenuecat.purchases.ui.revenuecatui.activity
 
 import android.os.Parcelable
+import com.revenuecat.purchases.ui.revenuecatui.fonts.FontResourceProvider
+import com.revenuecat.purchases.ui.revenuecatui.fonts.TypographyType
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 internal data class PaywallActivityArgs(
     val offeringId: String? = null,
-) : Parcelable
+    val fonts: Map<TypographyType, Int?>? = null,
+) : Parcelable {
+    constructor(offeringId: String? = null, fontProvider: FontResourceProvider?) : this(
+        offeringId,
+        fontProvider?.let { TypographyType.values().associateBy({ it }, { fontProvider.getFontResourceId(it) }) },
+    )
+}
---
+++ ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/extensions/TypographyExtensions.kt
@@ -0,0 +1,37 @@
+package com.revenuecat.purchases.ui.revenuecatui.extensions
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import com.revenuecat.purchases.ui.revenuecatui.fonts.FontProvider
+import com.revenuecat.purchases.ui.revenuecatui.fonts.TypographyType
+
+fun Typography.copyWithFontProvider(fontProvider: FontProvider): Typography {
+    with(this) {
+        return copy(
+            displayLarge = displayLarge.modifyFontIfNeeded(TypographyType.DISPLAY_LARGE, fontProvider),
+            displayMedium = displayMedium.modifyFontIfNeeded(TypographyType.DISPLAY_MEDIUM, fontProvider),
+            displaySmall = displaySmall.modifyFontIfNeeded(TypographyType.DISPLAY_SMALL, fontProvider),
+            headlineLarge = headlineLarge.modifyFontIfNeeded(TypographyType.HEADLINE_LARGE, fontProvider),
+            headlineMedium = headlineMedium.modifyFontIfNeeded(TypographyType.HEADLINE_MEDIUM, fontProvider),
+            headlineSmall = headlineSmall.modifyFontIfNeeded(TypographyType.HEADLINE_SMALL, fontProvider),
+            titleLarge = titleLarge.modifyFontIfNeeded(TypographyType.TITLE_LARGE, fontProvider),
+            titleMedium = titleMedium.modifyFontIfNeeded(TypographyType.TITLE_MEDIUM, fontProvider),
+            titleSmall = titleSmall.modifyFontIfNeeded(TypographyType.TITLE_SMALL, fontProvider),
+            bodyLarge = bodyLarge.modifyFontIfNeeded(TypographyType.BODY_LARGE, fontProvider),
+            bodyMedium = bodyMedium.modifyFontIfNeeded(TypographyType.BODY_MEDIUM, fontProvider),
+            bodySmall = bodySmall.modifyFontIfNeeded(TypographyType.BODY_SMALL, fontProvider),
+            labelLarge = labelLarge.modifyFontIfNeeded(TypographyType.LABEL_LARGE, fontProvider),
+            labelMedium = labelMedium.modifyFontIfNeeded(TypographyType.LABEL_MEDIUM, fontProvider),
+            labelSmall = labelSmall.modifyFontIfNeeded(TypographyType.LABEL_SMALL, fontProvider),
+        )
+    }
+}
+
+private fun TextStyle.modifyFontIfNeeded(typographyType: TypographyType, fontProvider: FontProvider): TextStyle {
+    val font = fontProvider.getFont(typographyType)
+    return if (font == null) {
+        this
+    } else {
+        this.copy(fontFamily = font)
+    }
+}
