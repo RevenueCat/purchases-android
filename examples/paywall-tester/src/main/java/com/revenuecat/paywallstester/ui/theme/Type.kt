12934f7aa57256f5f9ec94c13f06ed00f547252d
Toni Rico <toni.rico.diez@revenuecat.com>
2023-10-17 18:06:13 +0200

    Paywalls: Add custom font example to paywalls screen (#1358)
    ### Description
    This enables using a custom font from the `PaywallsScreen` in the
    paywalls tester app
    
    ![image](https://github.com/RevenueCat/purchases-android/assets/808417/c92a5995-3237-4e39-b76b-6618a5f43144)

--- examples/paywall-tester/src/main/java/com/revenuecat/paywallstester/ui/theme/Type.kt
+++ examples/paywall-tester/src/main/java/com/revenuecat/paywallstester/ui/theme/Type.kt
@@ -4,7 +4,21 @@ import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.unit.sp
+import com.revenuecat.paywallstester.R
+
+val googleFont = FontFamily(
+    Font(
+        GoogleFont("Lobster Two"),
+        GoogleFont.Provider(
+            "com.google.android.gms.fonts",
+            "com.google.android.gms",
+            R.array.com_google_android_gms_fonts_certs,
+        ),
+    ),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
