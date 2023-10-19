7d2ce3859e431101688e9ddae8574334fd4db12d
Toni Rico <toni.rico.diez@revenuecat.com>
2023-10-19 20:32:56 +0200

    Paywalls: Add offer details in template 2 when in condensed form (#1371)
    ### Description
    Followup to #1365. This adds the offer details to the template 2 when in
    condensed form.
    
    
    https://github.com/RevenueCat/purchases-android/assets/808417/567fa149-f026-4957-ba1c-28b5db88aec0

---
+++ ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/composables/OfferDetails.kt
@@ -0,0 +1,40 @@
+package com.revenuecat.purchases.ui.revenuecatui.composables
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import com.revenuecat.purchases.ui.revenuecatui.UIConstant
+import com.revenuecat.purchases.ui.revenuecatui.data.PaywallState
+import com.revenuecat.purchases.ui.revenuecatui.data.selectedLocalization
+import com.revenuecat.purchases.ui.revenuecatui.extensions.introEligibility
+
+/**
+ * This displays the offer details of the selected package. For templates that allow to select
+ * multiple packages, each package needs its own offer details so we won't use this composable.
+ */
+@Composable
+internal fun OfferDetails(
+    state: PaywallState.Loaded,
+) {
+    Box(
+        modifier = Modifier
+            .padding(bottom = UIConstant.defaultVerticalSpacing),
+    ) {
+        IntroEligibilityStateView(
+            textWithNoIntroOffer = state.selectedLocalization.offerDetails,
+            textWithIntroOffer = state.selectedLocalization.offerDetailsWithIntroOffer,
+            textWithMultipleIntroOffers = state.selectedLocalization.offerDetailsWithMultipleIntroOffers,
+            eligibility = state.selectedPackage.value.introEligibility,
+            color = state.templateConfiguration.getCurrentColors().text1,
+            style = MaterialTheme.typography.bodyMedium,
+            fontWeight = FontWeight.Normal,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth(),
+        )
+    }
+}
