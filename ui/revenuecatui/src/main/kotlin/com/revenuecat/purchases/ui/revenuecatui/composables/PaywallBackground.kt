d5bd0fd256dc58a97f0b9ebe1b6e058dc8560767
Andy Boedo <andresboedo@gmail.com>
2023-10-19 12:43:17 -0300

    Paywalls: backwards compatible blurring (#1327)
    Add backwards compatible blur. 
    
    This has 3 different ways of applying blur: 
    - through native blur in API 31+
    - through averaging the pixels in the image, based on
    https://github.com/T8RIN/BlurTransformation
    - through RenderScript in API < 31
    
    It also adds some logic to benchmark... But I've been having a tough
    time actually running the benchmarks.
    From multiDex to instrumentation tests and roboelectric, it's been fun.
    Will reach out tomorrow to android devs for a hand in actually running
    the benchmarks.
    
    ---------
    
    Co-authored-by: NachoSoto <ignaciosoto90@gmail.com>

--- ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/composables/PaywallBackground.kt
+++ ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/composables/PaywallBackground.kt
@@ -1,14 +1,18 @@
 package com.revenuecat.purchases.ui.revenuecatui.composables
 
+import BlurTransformation
+import android.os.Build
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.BlurredEdgeTreatment
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.blur
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.revenuecat.purchases.paywalls.PaywallData
 import com.revenuecat.purchases.ui.revenuecatui.R
@@ -16,14 +20,26 @@ import com.revenuecat.purchases.ui.revenuecatui.data.processed.TemplateConfigura
 import com.revenuecat.purchases.ui.revenuecatui.extensions.conditional
 import com.revenuecat.purchases.ui.revenuecatui.extensions.defaultBackgroundPlaceholder
 
+// Current implementation uses a transformation on API level < 31, modifier on > 31.
 @Composable
 internal fun BoxScope.PaywallBackground(templateConfiguration: TemplateConfiguration) {
+    val supportsNativeBlurring = Build.VERSION.SDK_INT >= BackgroundUIConstants.minSDKVersionSupportingBlur
+    val shouldBlur = templateConfiguration.configuration.blurredBackgroundImage
+    val imageAlpha = if (shouldBlur) { BackgroundUIConstants.blurAlpha } else 1.0f
+
+    val backwardsCompatibleTransformation = if (shouldBlur && !supportsNativeBlurring) {
+        BlurTransformation(
+            context = LocalContext.current,
+            radius = BackgroundUIConstants.blurSize.toFloatPx(),
+        )
+    } else {
+        null
+    }
+
     val modifier = Modifier
         .matchParentSize()
-        .conditional(templateConfiguration.configuration.blurredBackgroundImage) {
-            // TODO-PAYWALLS: backwards compatibility for blurring
+        .conditional(shouldBlur && supportsNativeBlurring) {
             blur(BackgroundUIConstants.blurSize, edgeTreatment = BlurredEdgeTreatment.Unbounded)
-                .alpha(BackgroundUIConstants.blurAlpha)
         }
 
     if (templateConfiguration.configuration.images.background == PaywallData.defaultBackgroundPlaceholder) {
@@ -32,6 +48,7 @@ internal fun BoxScope.PaywallBackground(templateConfiguration: TemplateConfigura
             painter = painterResource(id = R.drawable.default_background),
             contentDescription = null,
             contentScale = BackgroundUIConstants.contentScale,
+            alpha = imageAlpha,
         )
     } else {
         templateConfiguration.images.backgroundUri?.let {
@@ -39,6 +56,8 @@ internal fun BoxScope.PaywallBackground(templateConfiguration: TemplateConfigura
                 urlString = it.toString(),
                 modifier = modifier,
                 contentScale = BackgroundUIConstants.contentScale,
+                transformation = backwardsCompatibleTransformation,
+                alpha = imageAlpha,
             )
         }
     }
@@ -46,6 +65,13 @@ internal fun BoxScope.PaywallBackground(templateConfiguration: TemplateConfigura
 
 private object BackgroundUIConstants {
     val blurSize = 40.dp
-    const val blurAlpha = 0.7f
     val contentScale = ContentScale.Crop
+    const val blurAlpha = 0.7f
+    const val minSDKVersionSupportingBlur = 31
+}
+
+@Composable
+private fun Dp.toFloatPx(): Float {
+    val density = LocalDensity.current.density
+    return (this.value * density)
 }
