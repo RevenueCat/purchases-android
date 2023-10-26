14509e88881bf8a23c477208bb53cee1d7163e65
NachoSoto <ignaciosoto90@gmail.com>
2023-10-26 13:14:29 -0700

    `Paywalls`: ignore URL deserialization errors
    Note that this is only for the optional URLs, `assetBaseURL` is still required, but I'll handle errors there in a separate PR.

--- purchases/src/main/kotlin/com/revenuecat/purchases/paywalls/URLSerializer.kt
+++ purchases/src/main/kotlin/com/revenuecat/purchases/paywalls/URLSerializer.kt
@@ -1,13 +1,15 @@
 package com.revenuecat.purchases.paywalls
 
 import kotlinx.serialization.KSerializer
+import kotlinx.serialization.builtins.nullable
 import kotlinx.serialization.descriptors.PrimitiveKind
 import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
+import java.net.MalformedURLException
 import java.net.URL
 
-object URLSerializer : KSerializer<URL> {
+internal object URLSerializer : KSerializer<URL> {
     override val descriptor = PrimitiveSerialDescriptor("URL", PrimitiveKind.STRING)
 
     override fun deserialize(decoder: Decoder): URL {
@@ -16,5 +18,28 @@ object URLSerializer : KSerializer<URL> {
 
     override fun serialize(encoder: Encoder, value: URL) {
         encoder.encodeString(value.toString())
+    }
+}
+
+/**
+ * Equivalent to URLSerializer but allows empty or invalid URLs.
+ */
+internal object OptionalURLSerializer : KSerializer<URL?> {
+    private val delegate = URLSerializer.nullable
+    override val descriptor = PrimitiveSerialDescriptor("URL?", PrimitiveKind.STRING)
+
+    override fun deserialize(decoder: Decoder): URL? {
+        return try {
+            delegate.deserialize(decoder)
+        } catch (e: MalformedURLException) {
+            null
+        }
+    }
+
+    override fun serialize(encoder: Encoder, value: URL?) {
+        when (value) {
+            null -> encoder.encodeString("")
+            else -> delegate.serialize(encoder, value)
+        }
     }
 }
