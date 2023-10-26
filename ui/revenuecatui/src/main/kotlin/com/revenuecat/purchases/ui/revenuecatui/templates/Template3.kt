dceca775504abd57db4a43c60d50cd2c2658c506
NachoSoto <NachoSoto@users.noreply.github.com>
2023-10-26 09:15:33 -0700

    `Paywalls`: fix template 3 offer details color (#1394)

--- ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/templates/Template3.kt
+++ ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/templates/Template3.kt
@@ -79,7 +79,7 @@ internal fun Template3(
 
         Spacer(modifier = Modifier.height(UIConstant.defaultVerticalSpacing))
 
-        OfferDetails(state)
+        OfferDetails(state = state, color = state.templateConfiguration.getCurrentColors().text2)
         PurchaseButton(state, viewModel)
         Footer(templateConfiguration = state.templateConfiguration, viewModel = viewModel)
     }
