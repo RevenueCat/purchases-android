4f33593d09f97ea32cf30ed42a3e173973638bb4
Cesar de la Vega <cesarvegaro@gmail.com>
2023-10-27 18:29:20 +0200

    Animate transition between loading and loaded paywall (#1404)

--- ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/data/PaywallViewModel.kt
+++ ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/data/PaywallViewModel.kt
@@ -219,6 +219,7 @@ internal class PaywallViewModelImpl(
             Logger.w(validationError.associatedErrorString(offering))
             Logger.w(PaywallValidationErrorStrings.DISPLAYING_DEFAULT)
         }
+
         return offering.toPaywallState(
             variableDataProvider = variableDataProvider,
             activelySubscribedProductIdentifiers = customerInfo.activeSubscriptions,
