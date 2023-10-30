d1329ed892825de729e671d226dd639927a206a1
Toni Rico <toni.rico.diez@revenuecat.com>
2023-10-30 15:35:09 +0000

    Paywalls: Add bundled font to paywall tester (#1415)
    ### Description
    Using downloadable Google fonts when displaying the paywall has a small
    delay where the default font is first used, then changed to the new font
    the first time the font is used. While we could predownload the font
    during the initial paywall tester app, this switches to using a bundled
    font which is loaded immediately. This is the same font we were using
    previously through google fonts.

--- examples/paywall-tester/src/main/java/com/revenuecat/paywallstester/ui/theme/Type.kt
+++ examples/paywall-tester/src/main/java/com/revenuecat/paywallstester/ui/theme/Type.kt
@@ -2,7 +2,9 @@ package com.revenuecat.paywallstester.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.googlefonts.Font
 import androidx.compose.ui.text.googlefonts.GoogleFont
@@ -18,6 +20,13 @@ val googleFont = FontFamily(
             R.array.com_google_android_gms_fonts_certs,
         ),
     ),
+)
+
+val bundledLobsterTwoFontFamily = FontFamily(
+    Font(R.font.lobster_two_bold_italic, FontWeight.Bold, FontStyle.Italic),
+    Font(R.font.lobster_two_italic, FontWeight.Normal, FontStyle.Italic),
+    Font(R.font.lobster_two_bold, FontWeight.Bold),
+    Font(R.font.lobster_two_regular),
 )
 
 // Set of Material typography styles to start with
