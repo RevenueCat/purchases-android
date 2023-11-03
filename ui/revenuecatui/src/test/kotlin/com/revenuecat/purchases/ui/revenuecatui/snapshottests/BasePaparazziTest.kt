eefbeb931cfc044fa92aa37a6fe098c857271277
Toni Rico <antonio.rico.diez@revenuecat.com>
2023-11-03 12:53:24 +0100

    Small comment fix

--- ui/revenuecatui/src/test/kotlin/com/revenuecat/purchases/ui/revenuecatui/snapshottests/BasePaparazziTest.kt
+++ ui/revenuecatui/src/test/kotlin/com/revenuecat/purchases/ui/revenuecatui/snapshottests/BasePaparazziTest.kt
@@ -27,7 +27,7 @@ data class TestConfig(
  * - To run them locally you need:
  * `bundle exec fastlane verify_revenuecatui_snapshot_tests`
  * - If your PR requires updating snapshots, you can generate them on CI:
- * `bundle exec fastlane record_revenuecatui_snapshot_tests`
+ * `bundle exec fastlane generate_snapshots_RCUI`
  * - Once those PRs are merged in `purchases-android-snapshots`, you can update the commit:
  * `bundle exec fastlane update_snapshots_repo`
  */
