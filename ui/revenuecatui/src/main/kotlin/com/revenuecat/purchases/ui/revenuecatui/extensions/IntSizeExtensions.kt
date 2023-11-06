f044e9dccc328043dae430986c8094761aa79d9a
Toni Rico <antonio.rico.diez@revenuecat.com>
2023-11-06 18:39:24 +0100

    Make extension internal

--- ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/extensions/IntSizeExtensions.kt
+++ ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/extensions/IntSizeExtensions.kt
@@ -2,5 +2,5 @@ package com.revenuecat.purchases.ui.revenuecatui.extensions
 
 import androidx.compose.ui.unit.IntSize
 
-val IntSize.aspectRatio: Float
+internal val IntSize.aspectRatio: Float
     get() = width.toFloat() / height
