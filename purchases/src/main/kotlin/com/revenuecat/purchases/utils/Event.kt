5293788a03bfe02bee282ff48575ae5290f218dc
Toni Rico <antonio.rico.diez@revenuecat.com>
2023-11-10 10:18:02 +0100

    Add docstring

--- purchases/src/main/kotlin/com/revenuecat/purchases/utils/Event.kt
+++ purchases/src/main/kotlin/com/revenuecat/purchases/utils/Event.kt
@@ -1,5 +1,8 @@
 package com.revenuecat.purchases.utils
 
+/**
+ * Interface for events that can be tracked through an [EventsFileHelper]
+ */
 internal interface Event {
     override fun toString(): String
 }
