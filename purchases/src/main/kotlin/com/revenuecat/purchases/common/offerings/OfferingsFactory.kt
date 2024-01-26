7cb00588812cd60c368478a860f877341f2ed338
NachoSoto <NachoSoto@users.noreply.github.com>
2024-01-26 07:35:09 -0800

    Update OfferingsFactory.kt

--- purchases/src/main/kotlin/com/revenuecat/purchases/common/offerings/OfferingsFactory.kt
+++ purchases/src/main/kotlin/com/revenuecat/purchases/common/offerings/OfferingsFactory.kt
@@ -52,7 +52,7 @@ internal class OfferingsFactory(
                                     ),
                                 )
                             } else {
-                                log(LogIntent.DEBUG, OfferingStrings.CREATED_OFFERINGS.format(offerings))
+                                log(LogIntent.VERBOSE, OfferingStrings.CREATED_OFFERINGS.format(offerings))
                                 onSuccess(offerings)
                             }
                         } catch (error: Exception) {
