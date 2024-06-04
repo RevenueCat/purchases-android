88dc378d36187b2eec5f049731cad1a2a5f8cd15
Cesar de la Vega <cesarvegaro@gmail.com>
2024-06-04 14:08:40 +0200

    bump to koltin 1.9

--- test-apps/testpurchasesuiandroidcompatibility/build.gradle.kts
+++ test-apps/testpurchasesuiandroidcompatibility/build.gradle.kts
@@ -39,7 +39,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.0-alpha02"
+        kotlinCompilerExtensionVersion = "1.5.14"
     }
     packaging {
         resources {
