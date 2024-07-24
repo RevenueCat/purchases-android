e70187f79095760fbda6d28a59e06a504e12656f
James Borthwick <borthwick.james@gmail.com>
2024-07-24 16:10:56 -0700

    lint newline

--- ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/MyAppPurchaseLogic.kt
+++ ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/MyAppPurchaseLogic.kt
@@ -1,8 +1,8 @@
 package com.revenuecat.purchases.ui.revenuecatui
+
 import android.app.Activity
 import com.revenuecat.purchases.CustomerInfo
 import com.revenuecat.purchases.Package
-import androidx.compose.runtime.Composable
 
 class MyAppPurchaseLogic(
     val performPurchase: suspend ((Activity, Package) -> Unit),
