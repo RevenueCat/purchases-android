e5a303f5bc2d070b2f95ed4d96566c6f01d14716
James Borthwick <borthwick.james@gmail.com>
2024-08-12 15:24:45 -0700

    `storefrontCountryCode` API tests

--- api-tester/src/defaults/java/com/revenuecat/apitester/java/PurchasesAPI.java
+++ api-tester/src/defaults/java/com/revenuecat/apitester/java/PurchasesAPI.java
@@ -98,6 +98,8 @@ final class PurchasesAPI {
         purchases.onAppForegrounded();
 
         final Store store = purchases.getStore();
+
+        final String storefrontCountryCode = purchases.storefrontCountryCode
     }
 
     static void check(final Purchases purchases, final Map<String, String> attributes) {
