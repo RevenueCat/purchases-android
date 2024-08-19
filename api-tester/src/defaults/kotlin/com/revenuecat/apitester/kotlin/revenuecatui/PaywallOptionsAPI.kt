a75f6967039aa369113535237ebd80fc6791c29f
James Borthwick <borthwick.james@gmail.com>
2024-08-19 11:30:03 -0700

    Add `MyAppPurchaseLogic` to `PaywallOptions`.

--- api-tester/src/defaults/kotlin/com/revenuecat/apitester/kotlin/revenuecatui/PaywallOptionsAPI.kt
+++ api-tester/src/defaults/kotlin/com/revenuecat/apitester/kotlin/revenuecatui/PaywallOptionsAPI.kt
@@ -1,21 +1,29 @@
 package com.revenuecat.apitester.kotlin.revenuecatui
 
 import com.revenuecat.purchases.Offering
+import com.revenuecat.purchases.ui.revenuecatui.MyAppPurchaseLogic
 import com.revenuecat.purchases.ui.revenuecatui.PaywallListener
 import com.revenuecat.purchases.ui.revenuecatui.PaywallOptions
 import com.revenuecat.purchases.ui.revenuecatui.fonts.FontProvider
 
 @Suppress("unused", "UNUSED_VARIABLE")
 private class PaywallOptionsAPI {
-    fun check(offering: Offering?, listener: PaywallListener?, fontProvider: FontProvider?) {
+    suspend fun check(offering: Offering?,
+        listener: PaywallListener?,
+        fontProvider: FontProvider?,
+        myAppPurchaseLogic: MyAppPurchaseLogic?,
+        )
+    {
         val options: PaywallOptions = PaywallOptions.Builder(dismissRequest = {})
             .setOffering(offering)
             .setListener(listener)
             .setShouldDisplayDismissButton(true)
             .setFontProvider(fontProvider)
+            .setMyAppPurchaseLogic(myAppPurchaseLogic)
             .build()
         val listener2: PaywallListener? = options.listener
         val fontProvider2: FontProvider? = options.fontProvider
         val dismissRequest: () -> Unit = options.dismissRequest
+        val myAppPurchaseLogic2: MyAppPurchaseLogic? = options.myAppPurchaseLogic
     }
 }
