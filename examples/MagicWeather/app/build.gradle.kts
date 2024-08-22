0acf9fe8bb16f1410fc7a09de935664a86e0834a
JayShortway <29483617+JayShortway@users.noreply.github.com>
2024-08-22 13:15:31 +0200

    Converts the app module's build.gradle to build.gradle.kts.

---
+++ examples/MagicWeather/app/build.gradle.kts
@@ -0,0 +1,65 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    val compileVersion: Int by rootProject.extra
+    compileSdk = compileVersion
+
+    defaultConfig {
+        applicationId = "com.revenuecat.purchases_sample"
+        minSdk = 26
+        targetSdk = compileVersion
+        versionCode = rootProject.extra.get("versionCode")?.toString()?.toInt()
+        versionName = rootProject.extra.get("versionName")?.toString()
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    flavorDimensions += "store"
+    productFlavors {
+        create("amazon") {
+            dimension = "store"
+            buildConfigField("String", "STORE", "\"amazon\"")
+        }
+
+        create("google") {
+            dimension = "store"
+            buildConfigField("String", "STORE", "\"google\"")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        buildConfig = true
+    }
+    namespace = "com.revenuecat.sample"
+}
+
+dependencies {
+    implementation(libs.revenuecat)
+    implementation(libs.revenuecat.amazon)
+    implementation(libs.kotlin.stdlib)
+    implementation(libs.androidx.core)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.lifecycle.livedata)
+    implementation(libs.androidx.lifecycle.viewmodel)
+    implementation(libs.androidx.navigation.fragment)
+    implementation(libs.androidx.navigation.ui)
+}
