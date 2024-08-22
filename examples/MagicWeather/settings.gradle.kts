57ea6761b9fae8e3927e1fa905875f7aba7e7585
JayShortway <29483617+JayShortway@users.noreply.github.com>
2024-08-22 12:40:57 +0200

    Converts settings.gradle to settings.gradle.kts.

---
+++ examples/MagicWeather/settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+        maven(url = "https://oss.sonatype.org/content/repositories/snapshots")
+    }
+}
+
+include(":app")
+rootProject.name = "MagicWeatherKotlin"
