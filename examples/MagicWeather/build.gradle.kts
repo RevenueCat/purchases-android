aef0c00f58c53cd5fe26e952c77875b52376461c
JayShortway <29483617+JayShortway@users.noreply.github.com>
2024-08-23 11:32:43 +0200

    Converts MagicWeather's Gradle files to Kotlin (#1821)

---
+++ examples/MagicWeather/build.gradle.kts
@@ -0,0 +1,17 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application).apply(false)
+    alias(libs.plugins.kotlin.android).apply(false)
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://oss.sonatype.org/content/repositories/snapshots")
+    }
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
