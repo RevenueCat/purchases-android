075b1944aba5b569cef908130a27807c1af292cc
Toni Rico <antonio.rico.diez@revenuecat.com>
2024-09-06 16:52:46 +0200

    Remove advertising library

--- purchases/src/test/java/com/revenuecat/purchases/google/attribution/GoogleDeviceIdentifiersFetcherTests.kt
+++ purchases/src/test/java/com/revenuecat/purchases/google/attribution/GoogleDeviceIdentifiersFetcherTests.kt
@@ -1,11 +1,8 @@
 package com.revenuecat.purchases.google.attribution
 
 import android.app.Application
-import android.content.Context
 import android.provider.Settings
 import android.util.Log
-import com.google.android.gms.ads.identifier.AdvertisingIdClient
-import com.google.android.gms.common.GooglePlayServicesRepairableException
 import com.revenuecat.purchases.common.subscriberattributes.SubscriberAttributeKey
 import com.revenuecat.purchases.utils.SyncDispatcher
 import io.mockk.every
@@ -16,8 +13,6 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
-import java.io.IOException
-import java.util.concurrent.TimeoutException
 
 class GoogleDeviceIdentifiersFetcherTests {
 
@@ -41,20 +36,12 @@ class GoogleDeviceIdentifiersFetcherTests {
     }
 
     @Test
-    fun `getDeviceIdentifiers`() {
+    fun getDeviceIdentifiers() {
         val mockContext = mockk<Application>(relaxed = true)
-        mockAdvertisingInfo(
-            mockContext = mockContext,
-            expectedAdID = "12345",
-            expectedAndroidID = "androidid"
-        )
 
         var completionCalled = false
         underTest.getDeviceIdentifiers(mockContext) { identifiers ->
             completionCalled = true
-            val advertisingID = identifiers[SubscriberAttributeKey.DeviceIdentifiers.GPSAdID.backendKey]
-            assertThat(advertisingID).isNotNull()
-            assertThat(advertisingID).isEqualTo("12345")
 
             assertContainsNoAndroidId(identifiers)
 
@@ -63,215 +50,6 @@ class GoogleDeviceIdentifiersFetcherTests {
         }
 
         assertThat(completionCalled).isTrue()
-    }
-
-    @Test
-    fun `GooglePlayServicesNotAvailableException when calling getDeviceIdentifiers`() {
-        val mockContext = mockk<Application>(relaxed = true)
-        mockAdvertisingInfo(
-            mockContext = mockContext,
-            expectedAdID = "12345",
-            expectedAndroidID = "androidid",
-            expectedException = GooglePlayServicesRepairableException(1, "error", mockk())
-        )
-
-        var completionCalled = false
-        underTest.getDeviceIdentifiers(mockContext) { identifiers ->
-            completionCalled = true
-
-            val advertisingID = identifiers[SubscriberAttributeKey.DeviceIdentifiers.GPSAdID.backendKey]
-            assertThat(advertisingID).isNull()
-
-            assertContainsNoAndroidId(identifiers)
-
-            val ip = identifiers[SubscriberAttributeKey.DeviceIdentifiers.IP.backendKey]
-            assertThat(ip).isEqualTo("true")
-        }
-
-        assertThat(completionCalled).isTrue()
-    }
-
-    @Test
-    fun `GooglePlayServicesRepairableException when calling getDeviceIdentifiers`() {
-        val mockContext = mockk<Application>(relaxed = true)
-        mockAdvertisingInfo(
-            mockContext = mockContext,
-            expectedAdID = "12345",
-            expectedAndroidID = "androidid",
-            expectedException = GooglePlayServicesRepairableException(1, "error", mockk())
-        )
-
-        var completionCalled = false
-        underTest.getDeviceIdentifiers(mockContext) { identifiers ->
-            completionCalled = true
-
-            val advertisingID = identifiers[SubscriberAttributeKey.DeviceIdentifiers.GPSAdID.backendKey]
-            assertThat(advertisingID).isNull()
-
-            assertContainsNoAndroidId(identifiers)
-
-            val ip = identifiers[SubscriberAttributeKey.DeviceIdentifiers.IP.backendKey]
-            assertThat(ip).isEqualTo("true")
-        }
-
-        assertThat(completionCalled).isTrue()
-    }
-
-    @Test
-    fun `getDeviceIdentifiers when ad tracking is limited`() {
-        val mockContext = mockk<Application>(relaxed = true)
-        mockAdvertisingInfo(
-            mockContext = mockContext,
-            expectedAdID = "12345",
-            expectedAndroidID = "androidid",
-            expectedIsLimitAdTrackingEnabled = true
-        )
-
-        var completionCalled = false
-        underTest.getDeviceIdentifiers(mockContext) { identifiers ->
-            completionCalled = true
-
-            val advertisingID = identifiers[SubscriberAttributeKey.DeviceIdentifiers.GPSAdID.backendKey]
-            assertThat(advertisingID).isNull()
-
-            assertContainsNoAndroidId(identifiers)
-
-            val ip = identifiers[SubscriberAttributeKey.DeviceIdentifiers.IP.backendKey]
-            assertThat(ip).isEqualTo("true")
-        }
-
-        assertThat(completionCalled).isTrue()
-    }
-
-    @Test
-    fun `getDeviceIdentifiers when returns all zeros`() {
-        val mockContext = mockk<Application>(relaxed = true)
-        mockAdvertisingInfo(
-            mockContext = mockContext,
-            expectedAdID = "00000000-0000-0000-0000-000000000000",
-            expectedAndroidID = "androidid"
-        )
-
-        var completionCalled = false
-        underTest.getDeviceIdentifiers(mockContext) { identifiers ->
-            completionCalled = true
-
-            val advertisingID = identifiers[SubscriberAttributeKey.DeviceIdentifiers.GPSAdID.backendKey]
-            assertThat(advertisingID).isNull()
-
-            assertContainsNoAndroidId(identifiers)
-
-            val ip = identifiers[SubscriberAttributeKey.DeviceIdentifiers.IP.backendKey]
-            assertThat(ip).isEqualTo("true")
-        }
-
-        assertThat(completionCalled).isTrue
-    }
-
-    @Test
-    fun `IOException when getting device identifiers`() {
-        val mockContext = mockk<Application>(relaxed = true)
-        mockAdvertisingInfo(
-            mockContext = mockContext,
-            expectedAdID = "12345",
-            expectedAndroidID = "androidid",
-            expectedException = IOException(TimeoutException())
-        )
-
-        var completionCalled = false
-        underTest.getDeviceIdentifiers(mockContext) { identifiers ->
-            completionCalled = true
-
-            val advertisingID = identifiers[SubscriberAttributeKey.DeviceIdentifiers.GPSAdID.backendKey]
-            assertThat(advertisingID).isNull()
-
-            assertContainsNoAndroidId(identifiers)
-
-            val ip = identifiers[SubscriberAttributeKey.DeviceIdentifiers.IP.backendKey]
-            assertThat(ip).isEqualTo("true")
-        }
-
-        assertThat(completionCalled).isTrue()
-    }
-
-    @Test
-    fun `NullPointerException when getting device identifiers`() {
-        val mockContext = mockk<Application>(relaxed = true)
-        mockAdvertisingInfo(
-            mockContext = mockContext,
-            expectedAdID = "12345",
-            expectedAndroidID = "androidid",
-            expectedException = NullPointerException()
-        )
-
-        var completionCalled = false
-        underTest.getDeviceIdentifiers(mockContext) { identifiers ->
-            completionCalled = true
-
-            val advertisingID = identifiers[SubscriberAttributeKey.DeviceIdentifiers.GPSAdID.backendKey]
-            assertThat(advertisingID).isNull()
-
-            assertContainsNoAndroidId(identifiers)
-
-            val ip = identifiers[SubscriberAttributeKey.DeviceIdentifiers.IP.backendKey]
-            assertThat(ip).isEqualTo("true")
-        }
-
-        assertThat(completionCalled).isTrue()
-    }
-
-    @Test
-    fun `TimeoutException when getting device identifiers`() {
-        val mockContext = mockk<Application>(relaxed = true)
-        mockAdvertisingInfo(
-            mockContext = mockContext,
-            expectedAdID = "12345",
-            expectedAndroidID = "androidid",
-            expectedException = TimeoutException()
-        )
-
-        var completionCalled = false
-        underTest.getDeviceIdentifiers(mockContext) { identifiers ->
-            completionCalled = true
-
-            val advertisingID = identifiers[SubscriberAttributeKey.DeviceIdentifiers.GPSAdID.backendKey]
-            assertThat(advertisingID).isNull()
-
-            
-
-            val ip = identifiers[SubscriberAttributeKey.DeviceIdentifiers.IP.backendKey]
-            assertThat(ip).isEqualTo("true")
-        }
-
-        assertThat(completionCalled).isTrue()
-    }
-
-    private fun mockAdvertisingInfo(
-        mockContext: Context,
-        expectedAdID: String?,
-        expectedAndroidID: String?,
-        expectedIsLimitAdTrackingEnabled: Boolean = false,
-        expectedException: Exception? = null
-    ) {
-        val mockAdInfo = mockk<AdvertisingIdClient.Info>().apply {
-            every { isLimitAdTrackingEnabled } returns expectedIsLimitAdTrackingEnabled
-            every { id } returns expectedAdID
-        }
-        mockkStatic(AdvertisingIdClient::class)
-        if (expectedException == null) {
-            every {
-                AdvertisingIdClient.getAdvertisingIdInfo(mockContext)
-            } returns mockAdInfo
-        } else {
-            every {
-                AdvertisingIdClient.getAdvertisingIdInfo(mockContext)
-            } throws expectedException
-        }
-
-        mockkStatic(Settings.Secure::class)
-        every {
-            Settings.Secure.getString(mockContext.contentResolver, Settings.Secure.ANDROID_ID)
-        } returns expectedAndroidID
     }
 
     /**
