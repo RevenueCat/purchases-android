RevenueCat/purchases-android

commit 4c89ab6e9a1f30b16e40ef65efb69bbaf71323c8

Paywalls can use custom in-app purchase/restore code (#1777)
This allows someone who's adopting RevenueCat paywalls to use their own
app purchase/restore logic rather than RevenueCat's.

This is done by implementing `MyAppPurchaseLogic`, which has two
functions that implement the custom purchase and restore functions.

```kotlin
interface MyAppPurchaseLogic {

    suspend fun performPurchase(activity: Activity, rcPackage: Package): MyAppPurchaseResult

    suspend fun performRestore(customerInfo: CustomerInfo): MyAppRestoreResult

}
```
Users can also use completion handlers by implementing this abstract
class instead:

```kotlin
abstract class MyAppPurchaseLogicCompletion : MyAppPurchaseLogic {

    abstract fun performPurchaseWithCompletion(activity: Activity, rcPackage: Package, completion: (MyAppPurchaseResult) -> Unit)

    abstract fun performRestoreWithCompletion(completion: (MyAppRestoreResult) -> Unit)

    final override suspend fun performPurchase(activity: Activity, rcPackage: Package): MyAppPurchaseResult =
        suspendCoroutine { continuation ->
            performPurchaseWithCompletion(activity, rcPackage) { result ->
                continuation.resume(result)
            }
        }

    final override suspend fun performRestore(customerInfo: CustomerInfo): MyAppRestoreResult =
        suspendCoroutine { continuation ->
            performRestoreWithCompletion { result ->
                continuation.resume(result)
            }
        }
}
```

An nullable instance of this interface is now part of `PaywallOptions`
and `PaywallDialogOptions`, and settable via the builders. These classes
are used when creating a Paywall.

Builder function:
```kotlin
fun setMyAppPurchaseLogic(myAppPurchaseLogic: MyAppPurchaseLogic?) = apply {
    this.myAppPurchaseLogic = myAppPurchaseLogic
}
```
When a `MyAppPurchaseLogic` is provided, the paywall viewmodel will use

ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/data/PaywallViewModel.kt
@@ -13,6 +13,7 @@ import com.revenuecat.purchases.ExperimentalPreviewRevenueCatPurchasesAPI
 import com.revenuecat.purchases.Offering
 import com.revenuecat.purchases.Package
 import com.revenuecat.purchases.PurchaseParams
+import com.revenuecat.purchases.PurchasesAreCompletedBy
 import com.revenuecat.purchases.PurchasesError
 import com.revenuecat.purchases.PurchasesErrorCode
 import com.revenuecat.purchases.PurchasesException
@@ -21,6 +22,8 @@ import com.revenuecat.purchases.paywalls.events.PaywallEventType
 import com.revenuecat.purchases.ui.revenuecatui.PaywallListener
 import com.revenuecat.purchases.ui.revenuecatui.PaywallMode
 import com.revenuecat.purchases.ui.revenuecatui.PaywallOptions
+import com.revenuecat.purchases.ui.revenuecatui.PurchaseLogic
+import com.revenuecat.purchases.ui.revenuecatui.PurchaseLogicResult
 import com.revenuecat.purchases.ui.revenuecatui.data.processed.TemplateConfiguration
 import com.revenuecat.purchases.ui.revenuecatui.data.processed.VariableDataProvider
 import com.revenuecat.purchases.ui.revenuecatui.helpers.Logger
@@ -91,10 +94,14 @@ internal class PaywallViewModelImpl(
     private val mode: PaywallMode
         get() = options.mode
 
+    private val purchaseLogic: PurchaseLogic?
+        get() = options.purchaseLogic
+
     private var paywallPresentationData: PaywallEvent.Data? = null
 
     init {
         updateState()
+        validateState()
     }
 
     fun updateOptions(options: PaywallOptions) {
@@ -145,38 +152,73 @@ internal class PaywallViewModelImpl(
             Logger.e("Activity is null, not initiating package purchase")
             return
         }
-        when (val currentState = _state.value) {
-            is PaywallState.Loaded -> {
-                val selectedPackage = currentState.selectedPackage.value
-                if (!selectedPackage.currentlySubscribed) {
-                    purchasePackage(activity, selectedPackage.rcPackage)
-                } else {
-                    Logger.d("Ignoring purchase request for already subscribed package")
-                }
-            }
-
-            else -> {
-                Logger.e("Unexpected state trying to purchase package: $currentState")
-            }
+        viewModelScope.launch {
+            handlePackagePurchase(activity)
         }
     }
 
+    @Suppress("NestedBlockDepth", "CyclomaticComplexMethod", "LongMethod")
     override fun restorePurchases() {
-        if (verifyNoActionInProgressOrStartAction()) { return }
-
+        if (verifyNoActionInProgressOrStartAction()) {
+            return
+        }
         viewModelScope.launch {
             try {
-                listener?.onRestoreStarted()
+                val customRestoreHandler = purchaseLogic?.let { it::performRestore }
 
-                val customerInfo = purchases.awaitRestore()
+                when (purchases.purchasesAreCompletedBy) {
+                    PurchasesAreCompletedBy.MY_APP -> {
+                        checkNotNull(customRestoreHandler) {
+                            "myAppPurchaseLogic must not be null when purchases.purchasesAreCompletedBy " +
+                                "is PurchasesAreCompletedBy.MY_APP"
+                        }
+                        val customerInfo = purchases.awaitCustomerInfo()
+                        when (val result = customRestoreHandler(customerInfo)) {
+                            is PurchaseLogicResult.Success -> {
+                                purchases.syncPurchases()
 
-                Logger.i("Restore purchases successful: $customerInfo")
-                listener?.onRestoreCompleted(customerInfo)
+                                shouldDisplayBlock?.let {
+                                    if (!it(customerInfo)) {
+                                        Logger.d(
+                                            "Dismissing paywall after restore since display " +
+                                                "condition has not been met",
+                                        )
+                                        options.dismissRequest()
+                                    }
+                                }
+                            }
+                            is PurchaseLogicResult.Cancellation -> {
+                                // silently ignore
+                            }
+                            is PurchaseLogicResult.Error -> {
+                                result.errorDetails?.let { _actionError.value = it }
+                            }
+                        }
+                    }
 
-                shouldDisplayBlock?.let {
-                    if (!it(customerInfo)) {
-                        Logger.d("Dismissing paywall after restore since display condition has not been met")
-                        options.dismissRequest()
+                    PurchasesAreCompletedBy.REVENUECAT -> {
+                        listener?.onRestoreStarted()
+                        if (customRestoreHandler != null) {
+                            Logger.w(
+                                "myAppPurchaseLogic expected be null when " +
+                                    "purchases.purchasesAreCompletedBy is .REVENUECAT.\n" +
+                                    "myAppPurchaseLogic.performRestore will not be executed.",
+                            )
+                        }
+                        val customerInfo = purchases.awaitRestore()
+                        Logger.i("Restore purchases successful: $customerInfo")
+                        listener?.onRestoreCompleted(customerInfo)
+
+                        shouldDisplayBlock?.let {
+                            if (!it(customerInfo)) {
+                                Logger.d("Dismissing paywall after restore since display condition has not been met")
+                                options.dismissRequest()
+                            }
+                        }
+                    }
+
+                    else -> {
+                        Logger.e("Unsupported purchase completion type: ${purchases.purchasesAreCompletedBy}")
                     }
                 }
             } catch (e: PurchasesException) {
@@ -200,32 +242,88 @@ internal class PaywallViewModelImpl(
         }
     }
 
-    private fun purchasePackage(activity: Activity, packageToPurchase: Package) {
-        if (verifyNoActionInProgressOrStartAction()) { return }
-
-        viewModelScope.launch {
-            try {
-                listener?.onPurchaseStarted(packageToPurchase)
-                val purchaseResult = purchases.awaitPurchase(
-                    PurchaseParams.Builder(activity, packageToPurchase),
-                )
-                listener?.onPurchaseCompleted(purchaseResult.customerInfo, purchaseResult.storeTransaction)
-                Logger.d("Dismissing paywall after purchase")
-                options.dismissRequest()
-            } catch (e: PurchasesException) {
-                if (e.code == PurchasesErrorCode.PurchaseCancelledError) {
-                    trackPaywallCancel()
-                    listener?.onPurchaseCancelled()
+    private suspend fun handlePackagePurchase(activity: Activity) {
+        when (val currentState = _state.value) {
+            is PaywallState.Loaded -> {
+                val selectedPackage = currentState.selectedPackage.value
+                if (!selectedPackage.currentlySubscribed) {
+                    performPurchase(activity, selectedPackage.rcPackage)
                 } else {
-                    listener?.onPurchaseError(e.error)
-                    _actionError.value = e.error
+                    Logger.d("Ignoring purchase request for already subscribed package")
                 }
             }
-
-            finishAction()
+            else -> {
+                Logger.e("Unexpected state trying to purchase package: $currentState")
+            }
         }
     }
 
+    @Suppress("LongMethod", "NestedBlockDepth")
+    private suspend fun performPurchase(activity: Activity, packageToPurchase: Package) {
+        try {
+            val customPurchaseHandler = purchaseLogic?.let { it::performPurchase }
+
+            when (purchases.purchasesAreCompletedBy) {
+                PurchasesAreCompletedBy.MY_APP -> {
+                    checkNotNull(customPurchaseHandler) {
+                        "myAppPurchaseLogic must not be null when purchases.purchasesAreCompletedBy " +
+                            "is PurchasesAreCompletedBy.MY_APP"
+                    }
+                    when (val result = customPurchaseHandler.invoke(activity, packageToPurchase)) {
+                        is PurchaseLogicResult.Success -> {
+                            purchases.syncPurchases()
+                            Logger.d("Dismissing paywall after purchase")
+                            options.dismissRequest()
+                        }
+                        is PurchaseLogicResult.Cancellation -> {
+                            trackPaywallCancel()
+                        }
+                        is PurchaseLogicResult.Error -> {
+                            result.errorDetails?.let { _actionError.value = it }
+                        }
+                    }
+                }
+                PurchasesAreCompletedBy.REVENUECAT -> {
+                    listener?.onPurchaseStarted(packageToPurchase)
+                    if (customPurchaseHandler != null) {
+                        Logger.e(
+                            "myAppPurchaseLogic expected to be null " +
+                                "when purchases.purchasesAreCompletedBy is .REVENUECAT. \n" +
+                                "myAppPurchaseLogic.performPurchase will not be executed.",
+                        )
+                    }
+                    val purchaseResult = purchases.awaitPurchase(
+                        PurchaseParams.Builder(activity, packageToPurchase),
+                    )
+                    listener?.onPurchaseCompleted(purchaseResult.customerInfo, purchaseResult.storeTransaction)
+                    Logger.d("Dismissing paywall after purchase")
+                    options.dismissRequest()
+                }
+                else -> {
+                    Logger.e("Unsupported purchase completion type: ${purchases.purchasesAreCompletedBy}")
+                }
+            }
+        } catch (e: PurchasesException) {
+            if (e.code == PurchasesErrorCode.PurchaseCancelledError) {
+                trackPaywallCancel()
+                listener?.onPurchaseCancelled()
+            } else {
+                listener?.onPurchaseError(e.error)
+                _actionError.value = e.error
+            }
+        }
+
+        finishAction()
+    }
+
+    private fun validateState() {
+        if (purchases.purchasesAreCompletedBy == PurchasesAreCompletedBy.MY_APP && options.purchaseLogic == null) {
+            _state.value = PaywallState.Error(
+                "myAppPurchaseLogic is null, but is required when purchases.purchasesAreCompletedBy is " +
+                    ".MY_APP. App purchases will not be successful.",
+            )
+        }
+    }
     private fun updateState() {
         viewModelScope.launch {
             try {

api-tester/src/defaults/kotlin/com/revenuecat/apitester/kotlin/revenuecatui/PaywallOptionsAPI.kt
@@ -3,19 +3,28 @@ package com.revenuecat.apitester.kotlin.revenuecatui
 import com.revenuecat.purchases.Offering
 import com.revenuecat.purchases.ui.revenuecatui.PaywallListener
 import com.revenuecat.purchases.ui.revenuecatui.PaywallOptions
+import com.revenuecat.purchases.ui.revenuecatui.PurchaseLogic
 import com.revenuecat.purchases.ui.revenuecatui.fonts.FontProvider
 
 @Suppress("unused", "UNUSED_VARIABLE")
 private class PaywallOptionsAPI {
-    fun check(offering: Offering?, listener: PaywallListener?, fontProvider: FontProvider?) {
+
+    suspend fun check(
+        offering: Offering?,
+        listener: PaywallListener?,
+        fontProvider: FontProvider?,
+        purchaseLogic: PurchaseLogic?,
+    ) {
         val options: PaywallOptions = PaywallOptions.Builder(dismissRequest = {})
             .setOffering(offering)
             .setListener(listener)
             .setShouldDisplayDismissButton(true)
             .setFontProvider(fontProvider)
+            .setPurchaseLogic(purchaseLogic)
             .build()
         val listener2: PaywallListener? = options.listener
         val fontProvider2: FontProvider? = options.fontProvider
         val dismissRequest: () -> Unit = options.dismissRequest
+        val purchaseLogic2: PurchaseLogic? = options.purchaseLogic
     }
 }

api-tester/src/defaults/kotlin/com/revenuecat/apitester/kotlin/revenuecatui/PurchaseLogicAPI.kt
@@ -1,0 +1,62 @@
+package com.revenuecat.apitester.kotlin.revenuecatui
+
+import android.app.Activity
+import com.revenuecat.purchases.CustomerInfo
+import com.revenuecat.purchases.Package
+import com.revenuecat.purchases.PurchasesError
+import com.revenuecat.purchases.PurchasesErrorCode
+import com.revenuecat.purchases.ui.revenuecatui.PurchaseLogic
+import com.revenuecat.purchases.ui.revenuecatui.PurchaseLogicResult
+import com.revenuecat.purchases.ui.revenuecatui.PurchaseLogicWithCallback
+
+@Suppress("unused", "UNUSED_VARIABLE")
+private class PurchaseLogicAPI {
+
+    suspend fun check(
+        mySuspendLogic: PurchaseLogic,
+        activity: Activity,
+        rcPackage: Package,
+        customerInfo: CustomerInfo,
+    ) {
+        val suspendLogicPurchase: PurchaseLogicResult = mySuspendLogic.performPurchase(activity, rcPackage)
+        val suspendLogicRestore: PurchaseLogicResult = mySuspendLogic.performRestore(customerInfo)
+    }
+}
+
+@Suppress("unused")
+private class PurchaseLogicWithCallbackAPI : PurchaseLogicWithCallback() {
+
+    override fun performPurchaseWithCompletion(
+        activity: Activity,
+        rcPackage: Package,
+        completion: (PurchaseLogicResult) -> Unit,
+    ) {
+        val success = PurchaseLogicResult.Success
+        val cancelled = PurchaseLogicResult.Cancellation
+        val failed = PurchaseLogicResult.Error(PurchasesError(PurchasesErrorCode.StoreProblemError))
+        completion(success)
+    }
+
+    override fun performRestoreWithCompletion(customerInfo: CustomerInfo, completion: (PurchaseLogicResult) -> Unit) {
+        val success = PurchaseLogicResult.Success
+        val cancelled = PurchaseLogicResult.Cancellation
+        val failed = PurchaseLogicResult.Error(PurchasesError(PurchasesErrorCode.StoreProblemError))
+        completion(failed)
+    }
+
+    @Suppress("unused")
+    fun check(
+        activity: Activity,
+        rcPackage: Package,
+        customerInfo: CustomerInfo,
+    ) {
+        performPurchaseWithCompletion(
+            activity,
+            rcPackage,
+        ) { result: PurchaseLogicResult -> }
+
+        performRestoreWithCompletion(
+            customerInfo,
+        ) { result: PurchaseLogicResult -> }
+    }
+}

examples/paywall-tester/src/main/java/com/revenuecat/paywallstester/ui/screens/main/paywalls/PaywallsScreen.kt
@@ -1,5 +1,7 @@
 package com.revenuecat.paywallstester.ui.screens.main.paywalls
 
+import android.app.Activity
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -28,19 +30,88 @@ import com.revenuecat.paywallstester.SamplePaywalls
 import com.revenuecat.paywallstester.SamplePaywallsLoader
 import com.revenuecat.paywallstester.ui.screens.paywallfooter.SamplePaywall
 import com.revenuecat.paywallstester.ui.theme.bundledLobsterTwoFontFamily
+import com.revenuecat.purchases.CustomerInfo
 import com.revenuecat.purchases.Offering
+import com.revenuecat.purchases.Package
+import com.revenuecat.purchases.PurchasesError
+import com.revenuecat.purchases.PurchasesErrorCode
 import com.revenuecat.purchases.ui.revenuecatui.PaywallDialog
 import com.revenuecat.purchases.ui.revenuecatui.PaywallDialogOptions
 import com.revenuecat.purchases.ui.revenuecatui.PaywallFooter
 import com.revenuecat.purchases.ui.revenuecatui.PaywallOptions
+import com.revenuecat.purchases.ui.revenuecatui.PurchaseLogic
+import com.revenuecat.purchases.ui.revenuecatui.PurchaseLogicResult
+import com.revenuecat.purchases.ui.revenuecatui.PurchaseLogicWithCallback
 import com.revenuecat.purchases.ui.revenuecatui.fonts.CustomFontProvider
 import com.revenuecat.purchases.ui.revenuecatui.fonts.FontProvider
 
+private class TestAppPurchaseLogicSuspend : PurchaseLogic {
+
+    companion object { private const val TAG = "PaywallTester" }
+
+    override suspend fun performPurchase(
+        activity: Activity,
+        rcPackage: com.revenuecat.purchases.Package,
+    ): PurchaseLogicResult {
+        // Implement your app's custom purchase logic here.
+        // If you are using Google Play, RevenueCat will automatically call `purchases.syncPurchases()` if
+        // you return `.Success`. If you are using Amazon, you must call `purchases.syncAmazonPurchase()`.
+        Log.d(TAG, "Custom purchase code in performPurchase was called.")
+        return PurchaseLogicResult.Success
+    }
+
+    override suspend fun performRestore(customerInfo: CustomerInfo): PurchaseLogicResult {
+        // Implement your app's custom restore logic here.
+        // If you are using Google Play, RevenueCat will automatically call `purchases.syncPurchases()` if
+        // you return `.Success`. If you are using Amazon, you must call `purchases.syncAmazonPurchase()`.
+        Log.d(TAG, "Custom restore code in performRestore was called.")
+        return PurchaseLogicResult.Error(PurchasesError(PurchasesErrorCode.PurchaseCancelledError))
+    }
+}
+
+private class TestAppPurchaseLogicCallbacks : PurchaseLogicWithCallback() {
+
+    companion object { private const val TAG = "PaywallTester" }
+
+    override fun performPurchaseWithCompletion(
+        activity: Activity,
+        rcPackage: Package,
+        completion: (PurchaseLogicResult) -> Unit,
+    ) {
+        // Implement your app's custom purchase logic here.
+        // If you are using Google Play, RevenueCat will automatically call `purchases.syncPurchases()` if
+        // you return `.Success`. If you are using Amazon, you must call `purchases.syncAmazonPurchase()`.
+        Log.d(TAG, "Custom purchase code in performPurchaseWithCompletion was called.")
+        completion(PurchaseLogicResult.Success)
+    }
+
+    override fun performRestoreWithCompletion(customerInfo: CustomerInfo, completion: (PurchaseLogicResult) -> Unit) {
+        // Implement your app's custom restore logic here.
+        // If you are using Google Play, RevenueCat will automatically call `purchases.syncPurchases()` if
+        // you return `.Success`. If you are using Amazon, you must call `purchases.syncAmazonPurchase()`.
+        Log.d(TAG, "Custom restore code in performRestoreWithCompletion was called.")
+        completion(PurchaseLogicResult.Success)
+    }
+}
+
+@Suppress("LongMethod")
 @Composable
 fun PaywallsScreen(
     samplePaywallsLoader: SamplePaywallsLoader = SamplePaywallsLoader(),
 ) {
     var displayPaywallState by remember { mutableStateOf<DisplayPaywallState>(DisplayPaywallState.None) }
+
+    // This should be part of a view model so it survives activity recreations.
+    // Temporarily holding this here for this test app.
+    val useCallbackPurchaseLogic = true
+
+    val myAppPurchaseLogic = remember {
+        if (useCallbackPurchaseLogic) {
+            TestAppPurchaseLogicCallbacks()
+        } else {
+            TestAppPurchaseLogicSuspend()
+        }
+    }
 
     LazyColumn {
         items(SamplePaywalls.SampleTemplate.values()) { template ->
@@ -53,21 +124,32 @@ fun PaywallsScreen(
                 )
                 ButtonWithEmoji(
                     onClick = {
-                        displayPaywallState = DisplayPaywallState.FullScreen(offering)
+                        displayPaywallState = DisplayPaywallState.FullScreen(
+                            offering,
+                            purchaseLogic = myAppPurchaseLogic,
+                        )
                     },
                     emoji = "\uD83D\uDCF1",
                     label = "Full screen",
                 )
                 ButtonWithEmoji(
                     onClick = {
-                        displayPaywallState = DisplayPaywallState.Footer(offering, condensed = false)
+                        displayPaywallState = DisplayPaywallState.Footer(
+                            offering,
+                            condensed = false,
+                            purchaseLogic = myAppPurchaseLogic,
+                        )
                     },
                     emoji = "\uD83D\uDD3D",
                     label = "Footer",
                 )
                 ButtonWithEmoji(
                     onClick = {
-                        displayPaywallState = DisplayPaywallState.Footer(offering, condensed = true)
+                        displayPaywallState = DisplayPaywallState.Footer(
+                            offering,
+                            condensed = true,
+                            purchaseLogic = myAppPurchaseLogic,
+                        )
                     },
                     emoji = "\uD83D\uDDDC️",
                     label = "Condenser footer",
@@ -77,6 +159,7 @@ fun PaywallsScreen(
                         displayPaywallState = DisplayPaywallState.FullScreen(
                             offering,
                             CustomFontProvider(bundledLobsterTwoFontFamily),
+                            purchaseLogic = myAppPurchaseLogic,
                         )
                     },
                     emoji = "\uD83C\uDD70️",
@@ -104,6 +187,7 @@ private fun FullScreenDialog(currentState: DisplayPaywallState.FullScreen, onDis
             .setDismissRequest(onDismiss)
             .setOffering(currentState.offering)
             .setFontProvider(currentState.fontProvider)
+            .setCustomPurchaseLogic(currentState.purchaseLogic)
             .build(),
     )
 }
@@ -119,6 +203,7 @@ private fun FooterDialog(currentState: DisplayPaywallState.Footer, onDismiss: ()
                 PaywallFooter(
                     options = PaywallOptions.Builder(dismissRequest = onDismiss)
                         .setOffering(currentState.offering)
+                        .setPurchaseLogic(currentState.purchaseLogic)
                         .build(),
                     condensed = currentState.condensed,
                 ) { footerPadding ->
@@ -135,10 +220,12 @@ private sealed class DisplayPaywallState {
     constructor(
         val offering: Offering? = null,
         val fontProvider: FontProvider? = null,
+        var purchaseLogic: PurchaseLogic? = null,
     ) : DisplayPaywallState()
     data class Footer(
         val offering: Offering? = null,
         val condensed: Boolean = false,
+        var purchaseLogic: PurchaseLogic? = null,
     ) : DisplayPaywallState()
 }
 

ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/PaywallDialogOptions.kt
@@ -12,6 +12,7 @@ data class PaywallDialogOptions internal constructor(
     val shouldDisplayDismissButton: Boolean,
     val fontProvider: FontProvider?,
     val listener: PaywallListener?,
+    val purchaseLogic: PurchaseLogic?,
 ) {
 
     constructor(builder: Builder) : this(
@@ -21,6 +22,7 @@ data class PaywallDialogOptions internal constructor(
         shouldDisplayDismissButton = builder.shouldDisplayDismissButton,
         fontProvider = builder.fontProvider,
         listener = builder.listener,
+        purchaseLogic = builder.purchaseLogic,
     )
 
     internal fun toPaywallOptions(dismissRequest: () -> Unit): PaywallOptions {
@@ -32,6 +34,7 @@ data class PaywallDialogOptions internal constructor(
             .setShouldDisplayDismissButton(shouldDisplayDismissButton)
             .setFontProvider(fontProvider)
             .setListener(listener)
+            .setPurchaseLogic(purchaseLogic)
             .build()
     }
 
@@ -42,6 +45,7 @@ data class PaywallDialogOptions internal constructor(
         internal var shouldDisplayDismissButton: Boolean = true
         internal var fontProvider: FontProvider? = null
         internal var listener: PaywallListener? = null
+        internal var purchaseLogic: PurchaseLogic? = null
 
         /**
          * Allows to configure whether to display the paywall dialog depending on operations on the CustomerInfo
@@ -80,6 +84,10 @@ data class PaywallDialogOptions internal constructor(
 
         fun setListener(listener: PaywallListener?) = apply {
             this.listener = listener
+        }
+
+        fun setCustomPurchaseLogic(purchaseLogic: PurchaseLogic?) = apply {
+            this.purchaseLogic = purchaseLogic
         }
 
         fun build(): PaywallDialogOptions {

ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/PaywallOptions.kt
@@ -28,6 +28,7 @@ data class PaywallOptions internal constructor(
     internal val shouldDisplayDismissButton: Boolean,
     val fontProvider: FontProvider?,
     val listener: PaywallListener?,
+    val purchaseLogic: PurchaseLogic?,
     internal val mode: PaywallMode,
     val dismissRequest: () -> Unit,
 ) {
@@ -37,6 +38,7 @@ data class PaywallOptions internal constructor(
         shouldDisplayDismissButton = builder.shouldDisplayDismissButton,
         fontProvider = builder.fontProvider,
         listener = builder.listener,
+        purchaseLogic = builder.purchaseLogic,
         mode = builder.mode,
         dismissRequest = builder.dismissRequest,
     )
@@ -48,6 +50,7 @@ data class PaywallOptions internal constructor(
         internal var shouldDisplayDismissButton: Boolean = false
         internal var fontProvider: FontProvider? = null
         internal var listener: PaywallListener? = null
+        internal var purchaseLogic: PurchaseLogic? = null
         internal var mode: PaywallMode = PaywallMode.default
 
         fun setOffering(offering: Offering?) = apply {
@@ -74,6 +77,10 @@ data class PaywallOptions internal constructor(
 
         fun setListener(listener: PaywallListener?) = apply {
             this.listener = listener
+        }
+
+        fun setPurchaseLogic(purchaseLogic: PurchaseLogic?) = apply {
+            this.purchaseLogic = purchaseLogic
         }
 
         internal fun setMode(mode: PaywallMode) = apply {

commit a581da3bf015f6b5568ca59d3438a8d3b9978227

Fix interaction not disabled during purchases (#1850)
### Description
This was a regression in #1777, and was reported in #1849 

We weren't marking as an action in progress when performing purchases.

ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/data/PaywallViewModel.kt
@@ -152,8 +152,12 @@ internal class PaywallViewModelImpl(
             Logger.e("Activity is null, not initiating package purchase")
             return
         }
+        if (verifyNoActionInProgressOrStartAction()) {
+            return
+        }
         viewModelScope.launch {
             handlePackagePurchase(activity)
+            finishAction()
         }
     }
 