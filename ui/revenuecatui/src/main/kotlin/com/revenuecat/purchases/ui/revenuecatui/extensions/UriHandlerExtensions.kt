9b95b616aa0c8b367b926912f132eedc53be488a
Toni Rico <antonio.rico.diez@revenuecat.com>
2024-11-05 16:46:13 +0100

    Handle IllegalArgumentException when opening a Uri in the paywalls

--- ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/extensions/UriHandlerExtensions.kt
+++ ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/extensions/UriHandlerExtensions.kt
@@ -7,10 +7,12 @@ import androidx.compose.ui.platform.UriHandler
  * Opens the given [uri] in a browser. If no browser is installed, [fallbackAction] is called.
  */
 @Suppress("SwallowedException")
-fun UriHandler.openUriOrElse(uri: String, fallbackAction: () -> Unit) {
+fun UriHandler.openUriOrElse(uri: String, fallbackAction: (e: Exception) -> Unit) {
     try {
         openUri(uri)
     } catch (e: ActivityNotFoundException) {
-        fallbackAction()
+        fallbackAction(e)
+    } catch (e: IllegalArgumentException) {
+        fallbackAction(e)
     }
 }
