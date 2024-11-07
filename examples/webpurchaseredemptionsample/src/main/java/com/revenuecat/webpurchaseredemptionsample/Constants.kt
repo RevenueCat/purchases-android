1f18fc190a78c2819bdc330f637d66ac2535771a
Toni Rico <antonio.rico.diez@revenuecat.com>
2024-11-07 13:07:10 +0100

    Fix detekt

--- examples/webpurchaseredemptionsample/src/main/java/com/revenuecat/webpurchaseredemptionsample/Constants.kt
+++ examples/webpurchaseredemptionsample/src/main/java/com/revenuecat/webpurchaseredemptionsample/Constants.kt
@@ -3,6 +3,7 @@ package com.revenuecat.webpurchaseredemptionsample
 object Constants {
     // Put API key for the android app associated to the same project as your RCBilling project
     const val API_KEY = "api_key"
+
     // Put the same entitlement ID that the purchase unlocks in your RCBilling project
     const val ENTITLEMENT_ID = "catServices"
 }
