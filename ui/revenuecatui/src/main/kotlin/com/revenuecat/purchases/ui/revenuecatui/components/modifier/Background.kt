da1da68a77e5423cee8c60c6f1f50b8da8d04590
JayShortway <29483617+JayShortway@users.noreply.github.com>
2024-12-02 12:59:18 +0100

    Adds a BackgroundStyle-based modifier.

--- ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/components/modifier/Background.kt
+++ ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/components/modifier/Background.kt
@@ -5,8 +5,11 @@ package com.revenuecat.purchases.ui.revenuecatui.components.modifier
 import androidx.compose.foundation.background
 import androidx.compose.runtime.Stable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.Shape
+import com.revenuecat.purchases.ui.revenuecatui.components.BackgroundStyle
 import com.revenuecat.purchases.ui.revenuecatui.components.ColorStyle
 
 @JvmSynthetic
@@ -16,6 +19,17 @@ internal fun Modifier.background(
     shape: Shape = RectangleShape,
 ): Modifier =
     when (color) {
-        is ColorStyle.Solid -> background(color.color, shape)
-        is ColorStyle.Gradient -> background(color.brush, shape, alpha = 1f)
+        is ColorStyle.Solid -> this then background(color.color, shape)
+        is ColorStyle.Gradient -> this then background(color.brush, shape, alpha = 1f)
+    }
+
+@JvmSynthetic
+@Stable
+internal fun Modifier.background(
+    background: BackgroundStyle,
+    shape: Shape = RectangleShape,
+): Modifier =
+    when (background) {
+        is BackgroundStyle.Color -> this then background(color = background.color, shape = shape)
+        is BackgroundStyle.Image -> this then paint(background.painter) then clip(shape)
     }
