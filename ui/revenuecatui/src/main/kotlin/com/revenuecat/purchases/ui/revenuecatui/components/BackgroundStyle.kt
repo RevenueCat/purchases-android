71d209bf6e9c71c258f035faeef676ef7dcacc76
JayShortway <29483617+JayShortway@users.noreply.github.com>
2024-12-02 12:58:56 +0100

    Adds some previews to BackgroundStyle.

--- ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/components/BackgroundStyle.kt
+++ ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/components/BackgroundStyle.kt
@@ -1,11 +1,21 @@
 package com.revenuecat.purchases.ui.revenuecatui.components
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 import com.revenuecat.purchases.paywalls.components.common.Background
+import com.revenuecat.purchases.paywalls.components.properties.ColorInfo
+import com.revenuecat.purchases.paywalls.components.properties.ColorScheme
 import com.revenuecat.purchases.ui.revenuecatui.components.ktx.urlsForCurrentTheme
+import com.revenuecat.purchases.ui.revenuecatui.components.modifier.background
 
 /**
  * Ready to use background properties for the current theme.
@@ -41,3 +51,84 @@ internal fun Background.toBackgroundStyle(): BackgroundStyle =
             )
         }
     }
+
+@Preview
+@Composable
+private fun Background_Preview_ColorHex() {
+    Box(
+        modifier = Modifier
+            .requiredSize(100.dp)
+            .background(
+                Background.Color(
+                    ColorScheme(
+                        light = ColorInfo.Hex(Color.Red.toArgb()),
+                    ),
+                ).toBackgroundStyle(),
+            ),
+    )
+}
+
+@Preview
+@Composable
+private fun Background_Preview_ColorGradientLinear() {
+    Box(
+        modifier = Modifier
+            .requiredSize(100.dp)
+            .background(
+                Background.Color(
+                    ColorScheme(
+                        light = ColorInfo.Gradient.Linear(
+                            degrees = 0f,
+                            points = listOf(
+                                ColorInfo.Gradient.Point(
+                                    color = Color.Red.toArgb(),
+                                    percent = 0f,
+                                ),
+                                ColorInfo.Gradient.Point(
+                                    color = Color.Green.toArgb(),
+                                    percent = 0.5f,
+                                ),
+                                ColorInfo.Gradient.Point(
+                                    color = Color.Blue.toArgb(),
+                                    percent = 1f,
+                                ),
+                            ),
+                        ),
+                    ),
+                ).toBackgroundStyle(),
+            ),
+    )
+}
+
+@Preview
+@Composable
+private fun Background_Preview_ColorGradientRadial() {
+    Box(
+        modifier = Modifier
+            .requiredSize(100.dp)
+            .background(
+                Background.Color(
+                    ColorScheme(
+                        light = ColorInfo.Gradient.Radial(
+                            points = listOf(
+                                ColorInfo.Gradient.Point(
+                                    color = Color.Red.toArgb(),
+                                    percent = 0f,
+                                ),
+                                ColorInfo.Gradient.Point(
+                                    color = Color.Green.toArgb(),
+                                    percent = 0.5f,
+                                ),
+                                ColorInfo.Gradient.Point(
+                                    color = Color.Blue.toArgb(),
+                                    percent = 1f,
+                                ),
+                            ),
+                        ),
+                    ),
+                ).toBackgroundStyle(),
+            ),
+    )
+}
+
+// We cannot use a network image in Compose previews, so we don't have a preview for Background.Image.
