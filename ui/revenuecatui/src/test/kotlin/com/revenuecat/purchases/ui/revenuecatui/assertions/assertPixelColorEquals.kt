5c0423e154cd9918fc4fcb9a21a497a5dff5230b
JayShortway <29483617+JayShortway@users.noreply.github.com>
2024-12-09 14:10:39 +0100

    Updates assertPixelColorEquals.

--- ui/revenuecatui/src/test/kotlin/com/revenuecat/purchases/ui/revenuecatui/assertions/assertPixelColorEquals.kt
+++ ui/revenuecatui/src/test/kotlin/com/revenuecat/purchases/ui/revenuecatui/assertions/assertPixelColorEquals.kt
@@ -19,8 +19,8 @@ import com.revenuecat.purchases.ui.revenuecatui.helpers.captureToImageCompat
  *
  * @param startX The x-coordinate of the first pixel to read from the Composable.
  * @param startY The y-coordinate of the first pixel to read from the Composable.
- * @param width The number of pixels to read from each row
- * @param height The number of rows to read
+ * @param width The number of pixels to read from each row.
+ * @param height The number of rows to read.
  * @param color The color to assert.
  */
 internal fun SemanticsNodeInteraction.assertPixelColorEquals(
@@ -31,19 +31,133 @@ internal fun SemanticsNodeInteraction.assertPixelColorEquals(
     color: Color,
 ): SemanticsNodeInteraction {
     val colorArgbInt = color.toArgb()
+    val pixels = readPixels(
+        startX = startX,
+        startY = startY,
+        width = width,
+        height = height,
+    )
+
+    return assert(
+        SemanticsMatcher("All pixels have color '$color'") {
+            pixels.all { pixel ->
+                if (pixel == colorArgbInt) true
+                else {
+                    println("Found a pixel with a different color: ${Color(pixel)}")
+                    false
+                }
+            }
+        }
+    )
+}
+
+/**
+ * Assert the number of pixels in a rectangular area of this Composable that have the provided [color].
+ *
+ * When running on the JVM, make sure your test class or function has the following annotations. `sdk` has to be >= 26.
+ *
+ * ```kotlin
+ * @GraphicsMode(GraphicsMode.Mode.NATIVE)
+ * @Config(shadows = [ShadowPixelCopy::class], sdk = [26])
+ * ```
+ *
+ * @param startX The x-coordinate of the first pixel to read from the Composable.
+ * @param startY The y-coordinate of the first pixel to read from the Composable.
+ * @param width The number of pixels to read from each row.
+ * @param height The number of rows to read.
+ * @param color The color to assert.
+ * @param predicate The assertion you want to run.
+ */
+@Suppress("LongParameterList")
+internal fun SemanticsNodeInteraction.assertPixelColorCount(
+    startX: Int,
+    startY: Int,
+    width: Int,
+    height: Int,
+    color: Color,
+    predicate: (count: Int) -> Boolean,
+): SemanticsNodeInteraction {
+    val pixels = readPixels(
+        startX = startX,
+        startY = startY,
+        width = width,
+        height = height,
+    )
+
+    return assert(
+        SemanticsMatcher("Assert count of pixels with color '$color'") {
+            val count = pixels
+                .groupBy { color -> color }
+                .mapValues { (_, pixels) -> pixels.count() }
+                .getOrDefault(color.toArgb(), defaultValue = 0)
+
+            predicate(count)
+        }
+    )
+}
+
+/**
+ * Assert the percentage of pixels in a rectangular area of this Composable that have the provided [color].
+ *
+ * When running on the JVM, make sure your test class or function has the following annotations. `sdk` has to be >= 26.
+ *
+ * ```kotlin
+ * @GraphicsMode(GraphicsMode.Mode.NATIVE)
+ * @Config(shadows = [ShadowPixelCopy::class], sdk = [26])
+ * ```
+ *
+ * @param startX The x-coordinate of the first pixel to read from the Composable.
+ * @param startY The y-coordinate of the first pixel to read from the Composable.
+ * @param width The number of pixels to read from each row.
+ * @param height The number of rows to read.
+ * @param color The color to assert.
+ * @param predicate The assertion you want to run. The `percentage` parameter is in range 0..100.
+ */
+@Suppress("LongParameterList")
+internal fun SemanticsNodeInteraction.assertPixelColorPercentage(
+    startX: Int,
+    startY: Int,
+    width: Int,
+    height: Int,
+    color: Color,
+    predicate: (percentage: Float) -> Boolean,
+): SemanticsNodeInteraction =
+    assertPixelColorCount(
+        startX = startX,
+        startY = startY,
+        width = width,
+        height = height,
+        color = color,
+        predicate = { count ->
+            val percentage = count.toFloat() / (width * height)
+            predicate(percentage)
+        }
+    )
+
+
+/**
+ * When running on the JVM, make sure your test class or function has the following annotations. `sdk` has to be >= 26.
+ *
+ * ```kotlin
+ * @GraphicsMode(GraphicsMode.Mode.NATIVE)
+ * @Config(shadows = [ShadowPixelCopy::class], sdk = [26])
+ * ```
+ */
+private fun SemanticsNodeInteraction.readPixels(
+    startX: Int,
+    startY: Int,
+    width: Int,
+    height: Int,
+): IntArray {
     val pixels = IntArray(width * height)
 
     captureToImageCompat().readPixels(
         buffer = pixels,
         startX = startX,
         startY = startY,
-        width = 4,
-        height = 4,
+        width = width,
+        height = height,
     )
 
-    return assert(
-        SemanticsMatcher("Background has color '$color'") {
-            pixels.all { pixel -> pixel == colorArgbInt }
-        }
-    )
+    return pixels
 }
