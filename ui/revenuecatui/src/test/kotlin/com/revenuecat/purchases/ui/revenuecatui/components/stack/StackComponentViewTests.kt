2b5574d43384ee2b7b28f6c81d4ea649b9370919
JayShortway <29483617+JayShortway@users.noreply.github.com>
2024-12-12 17:10:07 +0100

    Adds a failing test to StackComponentViewTests.

--- ui/revenuecatui/src/test/kotlin/com/revenuecat/purchases/ui/revenuecatui/components/stack/StackComponentViewTests.kt
+++ ui/revenuecatui/src/test/kotlin/com/revenuecat/purchases/ui/revenuecatui/components/stack/StackComponentViewTests.kt
@@ -3,16 +3,20 @@ package com.revenuecat.purchases.ui.revenuecatui.components.stack
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.test.SemanticsNodeInteraction
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.unit.dp
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.revenuecat.purchases.paywalls.components.StackComponent
+import com.revenuecat.purchases.paywalls.components.properties.Border
 import com.revenuecat.purchases.paywalls.components.properties.ColorInfo
 import com.revenuecat.purchases.paywalls.components.properties.ColorScheme
 import com.revenuecat.purchases.paywalls.components.properties.Size
-import com.revenuecat.purchases.paywalls.components.properties.SizeConstraint
+import com.revenuecat.purchases.paywalls.components.properties.SizeConstraint.Fixed
 import com.revenuecat.purchases.ui.revenuecatui.assertions.assertPixelColorEquals
 import com.revenuecat.purchases.ui.revenuecatui.components.ComponentViewState
 import com.revenuecat.purchases.ui.revenuecatui.components.ScreenCondition
@@ -68,7 +72,7 @@ class StackComponentViewTests {
         val expectedDarkColor = Color.Yellow
         val component = StackComponent(
             components = emptyList(),
-            size = Size(SizeConstraint.Fixed(100u), SizeConstraint.Fixed(100u)),
+            size = Size(Fixed(100u), Fixed(100u)),
             backgroundColor = ColorScheme(
                 light = ColorInfo.Hex(expectedLightColor.toArgb()),
                 dark = ColorInfo.Hex(expectedDarkColor.toArgb()),
@@ -95,5 +99,120 @@ class StackComponentViewTests {
         )
     }
 
+    @GraphicsMode(GraphicsMode.Mode.NATIVE)
+    @Config(shadows = [ShadowPixelCopy::class], sdk = [26])
+    @Test
+    fun `Should change border color based on theme`(): Unit = with(composeTestRule) {
+        // Arrange
+        val sizeDp = 100
+        val borderWidthDp = 10.0
+        val expectedLightColor = Color.Red
+        val expectedDarkColor = Color.Yellow
+        val expectedBackgroundColor = Color.White
+        val component = StackComponent(
+            components = emptyList(),
+            size = Size(Fixed(sizeDp.toUInt()), Fixed(sizeDp.toUInt())),
+            backgroundColor = ColorScheme(light = ColorInfo.Hex(expectedBackgroundColor.toArgb())),
+            border = Border(
+                color = ColorScheme(
+                    light = ColorInfo.Hex(expectedLightColor.toArgb()),
+                    dark = ColorInfo.Hex(expectedDarkColor.toArgb()),
+                ),
+                width = borderWidthDp
+            ),
+        )
+        var borderWidthPx: Int? = null
+        var sizePx: Int? = null
 
+        themeChangingTest(
+            arrange = {
+                // Use the Composable context to calculate px equivalents of our dp values.
+                borderWidthPx = with(LocalDensity.current) { borderWidthDp.dp.roundToPx() }
+                sizePx = with(LocalDensity.current) { sizeDp.dp.roundToPx() }
+                // We don't want to recreate the entire tree every time the theme, or any other state, changes.
+                styleFactory.create(component).getOrThrow() as StackComponentStyle
+            },
+            act = { StackComponentView(style = it, modifier = Modifier.testTag("stack")) },
+            assert = { theme ->
+                theme.setLight()
+                onNodeWithTag("stack")
+                    .assertIsDisplayed()
+                    .assertSquareBorderColor(
+                        sizePx = sizePx!!,
+                        borderWidthPx = borderWidthPx!!,
+                        expectedBorderColor = expectedLightColor,
+                        expectedBackgroundColor = expectedBackgroundColor,
+                    )
+
+                theme.setDark()
+                onNodeWithTag("stack")
+                    .assertIsDisplayed()
+                    .assertSquareBorderColor(
+                        sizePx = sizePx!!,
+                        borderWidthPx = borderWidthPx!!,
+                        expectedBorderColor = expectedDarkColor,
+                        expectedBackgroundColor = expectedBackgroundColor,
+                    )
+            }
+        )
+    }
+
+    @GraphicsMode(GraphicsMode.Mode.NATIVE)
+    @Config(shadows = [ShadowPixelCopy::class], sdk = [26])
+    @Test
+    fun `Should change shadow color based on theme`(): Unit = with(composeTestRule) {
+        // TODO
+    }
+
+    /**
+     * Asserts the border color of a square element.
+     *
+     * @param sizePx The size (height & width) of the square element, in px.
+     */
+    private fun SemanticsNodeInteraction.assertSquareBorderColor(
+        sizePx: Int,
+        borderWidthPx: Int,
+        expectedBorderColor: Color,
+        expectedBackgroundColor: Color,
+    ): SemanticsNodeInteraction =
+        // Top edge
+        assertPixelColorEquals(
+            startX = 0,
+            startY = 0,
+            width = sizePx,
+            height = borderWidthPx,
+            color = expectedBorderColor
+        )
+            // Left edge
+            .assertPixelColorEquals(
+                startX = 0,
+                startY = 0,
+                width = borderWidthPx,
+                height = sizePx,
+                color = expectedBorderColor
+            )
+            // Right edge
+            .assertPixelColorEquals(
+                startX = sizePx - borderWidthPx,
+                startY = 0,
+                width = borderWidthPx,
+                height = sizePx,
+                color = expectedBorderColor
+            )
+            // Bottom edge
+            .assertPixelColorEquals(
+                startX = 0,
+                startY = sizePx - borderWidthPx,
+                width = sizePx,
+                height = borderWidthPx,
+                color = expectedBorderColor
+            )
+            // Inner area
+            .assertPixelColorEquals(
+                startX = borderWidthPx,
+                startY = borderWidthPx,
+                width = sizePx - borderWidthPx - borderWidthPx,
+                height = sizePx - borderWidthPx - borderWidthPx,
+                color = expectedBackgroundColor
+            )
 }
