063307b1ac66ba7d40a10dc5096542329e20e019
JayShortway <29483617+JayShortway@users.noreply.github.com>
2024-12-18 14:19:19 +0100

    Adds assertApproximatePixelColorPercentage().

--- ui/revenuecatui/src/test/kotlin/com/revenuecat/purchases/ui/revenuecatui/assertions/assertPixelColorEquals.kt
+++ ui/revenuecatui/src/test/kotlin/com/revenuecat/purchases/ui/revenuecatui/assertions/assertPixelColorEquals.kt
@@ -7,6 +7,7 @@ import androidx.compose.ui.test.SemanticsNodeInteraction
 import androidx.compose.ui.test.assert
 import androidx.compose.ui.unit.Dp
 import com.revenuecat.purchases.ui.revenuecatui.helpers.captureToImageCompat
+import kotlin.math.abs
 
 /**
  * Assert that the pixels in a rectangular area of this Composable have the provided [color].
@@ -40,15 +41,6 @@ internal fun SemanticsNodeInteraction.assertPixelColorEquals(
         width = widthToUse,
         height = heightToUse,
     )
-
-    pixels
-        .groupBy { color -> color }
-        .mapValues { (_, pixels) -> pixels.count() }
-        .also {
-            it.forEach { (color, count) ->
-                println("TESTING got $count pixel(s) of color ${Color(color)}")
-            }
-        }
 
     return assert(
         SemanticsMatcher("All pixels have color '$color'") {
@@ -181,6 +173,113 @@ internal fun SemanticsNodeInteraction.assertPixelColorPercentage(
         }
     )
 }
+
+/**
+ * Assert the percentage of pixels in a rectangular area of this Composable that have a color whose individual color
+ * channels satisfy the provided [red], [green], [blue] and [alpha] predicates.
+ *
+ * When running on the JVM, make sure your test class or function has the following annotations. `sdk` has to be >= 26.
+ *
+ * ```kotlin
+ * @GraphicsMode(GraphicsMode.Mode.NATIVE)
+ * @Config(shadows = [ShadowPixelCopy::class], sdk = [26])
+ * ```
+ *
+ * @param red Predicate for the red color channel. Return true if this color should be considered.
+ * @param green Predicate for the green color channel. Return true if this color should be considered.
+ * @param blue Predicate for the blue color channel. Return true if this color should be considered.
+ * @param alpha Predicate for the alpha color channel. Return true if this color should be considered.
+ * @param startX The x-coordinate of the first pixel to read from the Composable. Defaults to 0.
+ * @param startY The y-coordinate of the first pixel to read from the Composable. Defaults to 0.
+ * @param width The number of pixels to read from each row. Will read the entire row if this parameter is null.
+ * @param height The number of rows to read. Will read all rows if this parameter is null.
+ * @param predicate The assertion you want to run. The `percentage` parameter is in range 0..100.
+ */
+@Suppress("LongParameterList")
+internal fun SemanticsNodeInteraction.assertPixelColorChannelPercentage(
+    red: ((Float) -> Boolean)? = null,
+    green: ((Float) -> Boolean)? = null,
+    blue: ((Float) -> Boolean)? = null,
+    alpha: ((Float) -> Boolean)? = null,
+    startX: Int = 0,
+    startY: Int = 0,
+    width: Int? = null,
+    height: Int? = null,
+    predicate: (percentage: Float) -> Boolean,
+): SemanticsNodeInteraction {
+    require(red != null || green != null || blue != null || alpha != null) {
+        "At least one of `red`, `green`, `blue` or `alpha` should be non-null."
+    }
+    
+    val (widthToUse, heightToUse) = getDimensionsIfNull(width = width, height = height)
+    val pixels = readPixels(
+        startX = startX,
+        startY = startY,
+        width = widthToUse,
+        height = heightToUse,
+    )
+
+    return assert(
+        SemanticsMatcher("Assert percentage of pixel color channels") {
+            val count = pixels
+                .groupBy { color -> Color(color) }
+                .mapValues { (_, pixels) -> pixels.count() }
+                .filterKeys { color ->
+                    red?.invoke(color.red) ?: true &&
+                        green?.invoke(color.green) ?: true &&
+                        blue?.invoke(color.blue) ?: true &&
+                        alpha?.invoke(color.alpha) ?: true
+                }
+                .values
+                .sum()
+
+            val percentage = count.toFloat() / (widthToUse * heightToUse)
+
+            predicate(percentage)
+        }
+    )
+}
+
+/**
+ * Assert the percentage of pixels in a rectangular area of this Composable that have a color approximately equal to
+ * the provided [color].
+ *
+ * When running on the JVM, make sure your test class or function has the following annotations. `sdk` has to be >= 26.
+ *
+ * ```kotlin
+ * @GraphicsMode(GraphicsMode.Mode.NATIVE)
+ * @Config(shadows = [ShadowPixelCopy::class], sdk = [26])
+ * ```
+ *
+ * @param color The color to assert.
+ * @param threshold The maximum allowed absolute difference per color channel, for a color to be considered approximate.
+ * @param startX The x-coordinate of the first pixel to read from the Composable. Defaults to 0.
+ * @param startY The y-coordinate of the first pixel to read from the Composable. Defaults to 0.
+ * @param width The number of pixels to read from each row. Will read the entire row if this parameter is null.
+ * @param height The number of rows to read. Will read all rows if this parameter is null.
+ * @param predicate The assertion you want to run. The `percentage` parameter is in range 0..100.
+ */
+@Suppress("LongParameterList")
+internal fun SemanticsNodeInteraction.assertApproximatePixelColorPercentage(
+    color: Color,
+    threshold: Float,
+    startX: Int = 0,
+    startY: Int = 0,
+    width: Int? = null,
+    height: Int? = null,
+    predicate: (percentage: Float) -> Boolean,
+): SemanticsNodeInteraction =
+    assertPixelColorChannelPercentage(
+        red = { abs(color.red - it) <= threshold },
+        green = { abs(color.green - it) <= threshold },
+        blue = { abs(color.blue - it) <= threshold },
+        alpha = { abs(color.alpha - it) <= threshold },
+        startX = startX,
+        startY = startY,
+        width = width,
+        height = height,
+        predicate = predicate
+    )
 
 /**
  * Asserts the border color of a rectangular element.
