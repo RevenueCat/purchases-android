1edb09453f6781edd0c66e25d1bb751763fc835c
Josh Holtz <me@joshholtz.com>
2024-12-20 19:30:04 -0600

    This too

--- ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/data/PaywallViewModel.kt
+++ ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/data/PaywallViewModel.kt
@@ -154,7 +154,7 @@ internal class PaywallViewModelImpl(
             Logger.e("Activity is null, not initiating package purchase")
             return
         }
-        
+
         if (verifyNoActionInProgressOrStartAction()) {
             return
         }
@@ -390,6 +390,7 @@ internal class PaywallViewModelImpl(
                 variableDataProvider = variableDataProvider,
                 activelySubscribedProductIdentifiers = customerInfo.activeSubscriptions,
                 nonSubscriptionProductIdentifiers = customerInfo.nonSubscriptionTransactions
+                    // Filter out consumables so they don't get checked in valiation
                     .filter { !it.shouldConsume }
                     .map { it.productIdentifier }
                     .toSet(),
