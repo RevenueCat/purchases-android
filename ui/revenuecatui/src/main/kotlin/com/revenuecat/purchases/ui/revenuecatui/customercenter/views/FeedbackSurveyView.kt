44e3fd4884f4886d216df5056c06b1c2aa15913f
Cesar de la Vega <cesarvegaro@gmail.com>
2024-12-31 13:22:57 +0100

    Feedback Surveys (#2010)

---
+++ ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/customercenter/views/FeedbackSurveyView.kt
@@ -0,0 +1,86 @@
+@file:OptIn(ExperimentalPreviewRevenueCatPurchasesAPI::class)
+@file:JvmSynthetic
+
+package com.revenuecat.purchases.ui.revenuecatui.customercenter.views
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.revenuecat.purchases.ExperimentalPreviewRevenueCatPurchasesAPI
+import com.revenuecat.purchases.customercenter.CustomerCenterConfigData
+import com.revenuecat.purchases.ui.revenuecatui.customercenter.data.CustomerCenterConfigTestData
+import com.revenuecat.purchases.ui.revenuecatui.customercenter.data.FeedbackSurveyData
+
+@JvmSynthetic
+@Composable
+internal fun FeedbackSurveyView(
+    data: FeedbackSurveyData,
+) {
+    var loadingOption by remember { mutableStateOf<String?>(null) }
+    val feedbackSurvey = data.feedbackSurvey
+
+    Box {
+        FeedbackSurveyButtonsView(
+            options = feedbackSurvey.options,
+            onOptionSelect = { option ->
+                loadingOption = option.id
+                data.onOptionSelected(option)
+                loadingOption = null
+            },
+            loadingOption = loadingOption,
+        )
+    }
+}
+
+@JvmSynthetic
+@Composable
+internal fun FeedbackSurveyButtonsView(
+    options: List<CustomerCenterConfigData.HelpPath.PathDetail.FeedbackSurvey.Option>,
+    onOptionSelect: (CustomerCenterConfigData.HelpPath.PathDetail.FeedbackSurvey.Option) -> Unit,
+    loadingOption: String?,
+    modifier: Modifier = Modifier,
+) {
+    Column(modifier = modifier) {
+        options.forEach { option ->
+            Button(
+                onClick = { onOptionSelect(option) },
+                enabled = loadingOption == null,
+                modifier = Modifier.fillMaxWidth().padding(8.dp),
+            ) {
+                if (loadingOption == option.id) {
+                    CircularProgressIndicator()
+                } else {
+                    Text(option.title)
+                }
+            }
+        }
+    }
+}
+
+@JvmSynthetic
+@OptIn(ExperimentalPreviewRevenueCatPurchasesAPI::class)
+@Preview(showBackground = true)
+@Composable
+internal fun FeedbackSurveyPreview() {
+    FeedbackSurveyView(
+        FeedbackSurveyData(
+            feedbackSurvey = CustomerCenterConfigTestData.customerCenterData()
+                .getManagementScreen()?.paths?.first { it.type == CustomerCenterConfigData.HelpPath.PathType.CANCEL }!!
+                .feedbackSurvey!!,
+            onOptionSelected = { _ -> },
+        ),
+
+    )
+}
