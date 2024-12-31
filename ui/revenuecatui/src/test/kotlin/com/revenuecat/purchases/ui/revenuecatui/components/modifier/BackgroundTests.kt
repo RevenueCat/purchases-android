8af8d9ec0efb3b6ed4ec53cd9935d0f8885e4706
JayShortway <29483617+JayShortway@users.noreply.github.com>
2024-12-31 17:07:19 +0100

    Adds a failing test.

--- ui/revenuecatui/src/test/kotlin/com/revenuecat/purchases/ui/revenuecatui/components/modifier/BackgroundTests.kt
+++ ui/revenuecatui/src/test/kotlin/com/revenuecat/purchases/ui/revenuecatui/components/modifier/BackgroundTests.kt
@@ -1,8 +1,16 @@
 package com.revenuecat.purchases.ui.revenuecatui.components.modifier
 
-import android.graphics.drawable.ColorDrawable
+import android.app.Application
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.drawable.BitmapDrawable
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -13,6 +21,7 @@ import androidx.compose.ui.semantics.testTag
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import coil.Coil
@@ -20,8 +29,10 @@ import coil.ImageLoader
 import coil.annotation.ExperimentalCoilApi
 import coil.test.FakeImageLoaderEngine
 import com.revenuecat.purchases.paywalls.components.common.Background
+import com.revenuecat.purchases.paywalls.components.properties.FitMode
 import com.revenuecat.purchases.paywalls.components.properties.ImageUrls
 import com.revenuecat.purchases.paywalls.components.properties.ThemeImageUrls
+import com.revenuecat.purchases.ui.revenuecatui.assertions.assertNoPixelColorEquals
 import com.revenuecat.purchases.ui.revenuecatui.assertions.assertPixelColorEquals
 import com.revenuecat.purchases.ui.revenuecatui.assertions.assertPixelColorPercentage
 import com.revenuecat.purchases.ui.revenuecatui.components.properties.toBackgroundStyle
@@ -49,10 +60,12 @@ class BackgroundTests {
     @get:Rule
     val composeTestRule = createComposeRule()
 
+    private val imageSizePx = 100
+
     @Before
     fun setup() {
         val engine = FakeImageLoaderEngine.Builder()
-            .interceptAllTestUrls()
+            .interceptAllTestUrls(imageSizePx)
             .build()
         val imageLoader = ImageLoader.Builder(InstrumentationRegistry.getInstrumentation().targetContext)
             .components { add(engine) }
@@ -69,13 +82,12 @@ class BackgroundTests {
     @Test
     fun `Should properly set an image background`(): Unit = with(composeTestRule) {
         // Arrange
-        val sizePx = 100
         val testUrl = TestUrl.Blue
         val expectedColor = testUrl.color
-        val background = Background.Image(ThemeImageUrls(light = testUrl.toImageUrls(size = sizePx)))
+        val background = Background.Image(ThemeImageUrls(light = testUrl.toImageUrls(size = imageSizePx)))
         setContent {
             val backgroundStyle = background.toBackgroundStyle()
-            val sizeDp = with(LocalDensity.current) { sizePx.toDp() }
+            val sizeDp = with(LocalDensity.current) { imageSizePx.toDp() }
 
             // Act
             Box(
@@ -89,19 +101,24 @@ class BackgroundTests {
         // Assert
         onNodeWithTag("box")
             .assertIsDisplayed()
-            .assertPixelColorEquals(startX = 0, startY = 0, width = sizePx, height = sizePx, color = expectedColor)
+            .assertPixelColorEquals(
+                startX = 0,
+                startY = 0,
+                width = imageSizePx,
+                height = imageSizePx,
+                color = expectedColor
+            )
     }
 
     @Test
     fun `Should draw image background behind content`(): Unit = with(composeTestRule) {
         // Arrange
-        val sizePx = 100
         val testUrl = TestUrl.Blue
         val expectedColor = testUrl.color
-        val background = Background.Image(ThemeImageUrls(light = testUrl.toImageUrls(size = sizePx)))
+        val background = Background.Image(ThemeImageUrls(light = testUrl.toImageUrls(size = imageSizePx)))
         setContent {
             val backgroundStyle = background.toBackgroundStyle()
-            val sizeDp = with(LocalDensity.current) { sizePx.toDp() }
+            val sizeDp = with(LocalDensity.current) { imageSizePx.toDp() }
 
             // Act
             Text(
@@ -119,8 +136,8 @@ class BackgroundTests {
             .assertPixelColorPercentage(
                 startX = 0,
                 startY = 0,
-                width = sizePx,
-                height = sizePx,
+                width = imageSizePx,
+                height = imageSizePx,
                 color = expectedColor,
                 // Text rendering might not be fully deterministic (e.g. due to anti aliasing, font settings, etc.) so
                 // we're just verifying that the majority of the Composable shows the background, but not all of it.
@@ -128,11 +145,59 @@ class BackgroundTests {
             )
     }
 
-    private fun FakeImageLoaderEngine.Builder.interceptAllTestUrls(): FakeImageLoaderEngine.Builder = apply {
-        TestUrl.values().forEach { testUrl ->
-            intercept(testUrl.urlString, ColorDrawable(testUrl.color.toArgb()))
+    @Test
+    fun `Image background with FitMode FILL should fill the parent`(): Unit = with(composeTestRule) {
+        // Arrange
+        val testUrl = TestUrl.Blue
+        val expectedColor = testUrl.color
+        val unexpectedColor = Color.Yellow
+        val background = Background.Image(
+            value = ThemeImageUrls(light = testUrl.toImageUrls(size = imageSizePx)),
+            fitMode = FitMode.FILL
+        )
+        setContent {
+            val backgroundStyle = background.toBackgroundStyle()
+            val imageSizeDp = with(LocalDensity.current) { imageSizePx.toDp() }
+
+            // Act
+            Box(
+                modifier = Modifier
+                    .requiredWidth(imageSizeDp * 3)
+                    // Parent is taller than the image
+                    .requiredHeight(imageSizeDp * 4)
+                    .background(unexpectedColor)
+                    .semantics { testTag = "parent" }
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(backgroundStyle)
+                )
+            }
         }
+
+        // Assert
+        onNodeWithTag("parent")
+            .assertIsDisplayed()
+            .assertPixelColorEquals(expectedColor)
+            .assertNoPixelColorEquals(unexpectedColor)
     }
+
+    private fun FakeImageLoaderEngine.Builder.interceptAllTestUrls(imageSizePx: Int): FakeImageLoaderEngine.Builder =
+        apply {
+            TestUrl.values().forEach { testUrl ->
+                // Returning a BitmapDrawable instead of a ColorDrawable, as the latter does not have an intrinsic
+                // size and is thus "automatically" resized. We'll have bitmaps in production, and we want to test
+                // resizing and scaling, so we should use bitmaps in these tests too.
+                intercept(
+                    data = testUrl.urlString,
+                    drawable = BitmapDrawable(
+                        ApplicationProvider.getApplicationContext<Application>().resources,
+                        testUrl.toBitmap(imageSizePx)
+                    )
+                )
+            }
+        }
 
     private fun TestUrl.toImageUrls(size: Int): ImageUrls =
         ImageUrls(
@@ -142,4 +207,17 @@ class BackgroundTests {
             width = size.toUInt(),
             height = size.toUInt(),
         )
+
+    private fun TestUrl.toBitmap(size: Int): Bitmap =
+        Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888).also { bitmap ->
+            Canvas(bitmap).apply {
+                drawRect(
+                    0f,
+                    0f,
+                    size.toFloat(),
+                    size.toFloat(),
+                    Paint().apply { color = this@toBitmap.color.toArgb() },
+                )
+            }
+        }
 }
