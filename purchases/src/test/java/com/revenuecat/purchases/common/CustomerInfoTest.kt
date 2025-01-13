ab5f8b3eb06f4ef363d339e0594f04fdb36160c5
Cesar de la Vega <cesarvegaro@gmail.com>
2025-01-13 18:14:47 +0100

    add test

--- purchases/src/test/java/com/revenuecat/purchases/common/CustomerInfoTest.kt
+++ purchases/src/test/java/com/revenuecat/purchases/common/CustomerInfoTest.kt
@@ -8,6 +8,10 @@ package com.revenuecat.purchases.common
 import android.net.Uri
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.revenuecat.purchases.CustomerInfo
+import com.revenuecat.purchases.OwnershipType
+import com.revenuecat.purchases.PeriodType
+import com.revenuecat.purchases.Store
+import com.revenuecat.purchases.utils.Iso8601Utils
 import com.revenuecat.purchases.utils.Responses
 import org.assertj.core.api.Assertions.assertThat
 import org.json.JSONException
@@ -42,7 +46,6 @@ class CustomerInfoTest {
         assertThat(emptyCustomerInfo.latestExpirationDate).isNull()
     }
 
-    @Suppress("DEPRECATION")
     @Test
     fun `Given a full response with non subscription SKUs, all SKUs are parsed properly`() {
         val info = fullCustomerInfo
@@ -342,5 +345,82 @@ class CustomerInfoTest {
         assertThat(fullCustomerInfo.allPurchaseDatesByProduct["100_coins_pack"]).isNotNull
         assertThat(fullCustomerInfo.allPurchaseDatesByProduct["7_extra_lives"]).isNotNull
         assertThat(fullCustomerInfo.allPurchaseDatesByProduct["lifetime_access"]).isNotNull
+    }
+
+    @Test
+    fun `Test subscriptionsByProductIdentifier`() {
+        assertThat(fullCustomerInfo.subscriptionsByProductIdentifier).isNotEmpty
+        assertThat(fullCustomerInfo.subscriptionsByProductIdentifier.size).isEqualTo(2)
+
+        val proSubscription = fullCustomerInfo.subscriptionsByProductIdentifier["pro"]
+        assertThat(proSubscription).isNotNull
+        assertThat(proSubscription!!.productIdentifier).isEqualTo("pro")
+        assertThat(proSubscription.purchaseDate).isEqualTo(Iso8601Utils.parse("2019-07-26T23:45:40Z"))
+        assertThat(proSubscription.originalPurchaseDate).isEqualTo(Iso8601Utils.parse("2019-07-26T23:30:41Z"))
+        assertThat(proSubscription.expiresDate).isEqualTo(Iso8601Utils.parse("2100-04-06T20:54:45.975000Z"))
+        assertThat(proSubscription.store).isEqualTo(Store.APP_STORE)
+        assertThat(proSubscription.unsubscribeDetectedAt).isNull()
+        assertThat(proSubscription.isSandbox).isTrue()
+        assertThat(proSubscription.billingIssuesDetectedAt).isNull()
+        assertThat(proSubscription.gracePeriodExpiresDate).isNull()
+        assertThat(proSubscription.ownershipType).isEqualTo(OwnershipType.PURCHASED)
+        assertThat(proSubscription.periodType).isEqualTo(PeriodType.NORMAL)
+        assertThat(proSubscription.refundedAt).isNull()
+        assertThat(proSubscription.storeTransactionId).isEqualTo("GPA.3394-7009-4518-41945..6")
+        assertThat(proSubscription.requestDate).isEqualTo(Iso8601Utils.parse("2019-08-16T10:30:42Z"))
+        assertThat(proSubscription.isActive).isTrue()
+        assertThat(proSubscription.willRenew).isTrue()
+
+        val basicSubscription = fullCustomerInfo.subscriptionsByProductIdentifier["basic"]
+        assertThat(basicSubscription).isNotNull
+        assertThat(basicSubscription!!.productIdentifier).isEqualTo("basic")
+        assertThat(basicSubscription.purchaseDate).isEqualTo(Iso8601Utils.parse("2019-07-26T23:45:40Z"))
+        assertThat(basicSubscription.originalPurchaseDate).isEqualTo(Iso8601Utils.parse("2019-07-26T23:30:41Z"))
+        assertThat(basicSubscription.expiresDate).isEqualTo(Iso8601Utils.parse("1990-08-30T02:40:36Z"))
+        assertThat(basicSubscription.store).isEqualTo(Store.APP_STORE)
+        assertThat(basicSubscription.unsubscribeDetectedAt).isEqualTo(Iso8601Utils.parse("2023-07-26T23:45:40Z"))
+        assertThat(basicSubscription.isSandbox).isTrue()
+        assertThat(basicSubscription.billingIssuesDetectedAt).isNull()
+        assertThat(basicSubscription.gracePeriodExpiresDate).isNull()
+        assertThat(basicSubscription.ownershipType).isEqualTo(OwnershipType.PURCHASED)
+        assertThat(basicSubscription.periodType).isEqualTo(PeriodType.NORMAL)
+        assertThat(basicSubscription.refundedAt).isNull()
+        assertThat(basicSubscription.storeTransactionId).isEqualTo("GPA.3394-7009-4518-41945..8")
+        assertThat(basicSubscription.requestDate).isEqualTo(Iso8601Utils.parse("2019-08-16T10:30:42Z"))
+        assertThat(basicSubscription.isActive).isFalse()
+        assertThat(basicSubscription.willRenew).isFalse()
+    }
+
+    @Test
+    fun `Test subscriptionsByProductIdentifier nullable dates`() {
+        val jsonObject = JSONObject(Responses.validFullPurchaserResponse)
+        val subscriber = jsonObject.getJSONObject("subscriber")
+        val subscriptions = subscriber.getJSONObject("subscriptions")
+
+        val proSubscriptionJSON = subscriptions.getJSONObject("pro")
+        proSubscriptionJSON.put("unsubscribe_detected_at", "2023-07-26T23:45:40Z")
+        proSubscriptionJSON.put("billing_issues_detected_at", "2019-07-26T23:45:40Z")
+        proSubscriptionJSON.put("grace_period_expires_date", "2019-07-25T23:45:40Z")
+        proSubscriptionJSON.put("refunded_at", "2019-07-24T23:45:40Z")
+
+        subscriptions.put("pro", proSubscriptionJSON)
+        subscriber.put("subscriptions", subscriptions)
+        jsonObject.put("subscriber", subscriber)
+
+        val customerInfo = createCustomerInfo(jsonObject)
+
+        assertThat(customerInfo.subscriptionsByProductIdentifier).isNotEmpty
+        assertThat(customerInfo.subscriptionsByProductIdentifier.size).isEqualTo(2)
+
+        val proSubscription = customerInfo.subscriptionsByProductIdentifier["pro"]
+        assertThat(proSubscription).isNotNull
+        assertThat(proSubscription!!.productIdentifier).isEqualTo("pro")
+        assertThat(proSubscription.purchaseDate).isEqualTo(Iso8601Utils.parse("2019-07-26T23:45:40Z"))
+        assertThat(proSubscription.originalPurchaseDate).isEqualTo(Iso8601Utils.parse("2019-07-26T23:30:41Z"))
+        assertThat(proSubscription.expiresDate).isEqualTo(Iso8601Utils.parse("2100-04-06T20:54:45.975000Z"))
+        assertThat(proSubscription.unsubscribeDetectedAt).isEqualTo(Iso8601Utils.parse("2023-07-26T23:45:40Z"))
+        assertThat(proSubscription.billingIssuesDetectedAt).isEqualTo(Iso8601Utils.parse("2019-07-26T23:45:40Z"))
+        assertThat(proSubscription.gracePeriodExpiresDate).isEqualTo(Iso8601Utils.parse("2019-07-25T23:45:40Z"))
+        assertThat(proSubscription.refundedAt).isEqualTo(Iso8601Utils.parse("2019-07-24T23:45:40Z"))
     }
 }
