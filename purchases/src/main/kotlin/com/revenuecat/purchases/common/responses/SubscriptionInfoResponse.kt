4837cfd32bd23bc578733bf322eeb0cc983925ce
Cesar de la Vega <cesarvegaro@gmail.com>
2025-01-14 12:39:18 +0100

    PR comments

--- purchases/src/main/kotlin/com/revenuecat/purchases/common/responses/SubscriptionInfoResponse.kt
+++ purchases/src/main/kotlin/com/revenuecat/purchases/common/responses/SubscriptionInfoResponse.kt
@@ -11,26 +11,30 @@ import java.util.Date
 @Serializable
 @SuppressWarnings("LongParameterList")
 internal class SubscriptionInfoResponse(
-    @SerialName("purchase_date") @Serializable(with = ISO8601DateSerializer::class) val purchaseDate: Date,
-    @SerialName("original_purchase_date") @Serializable(with = ISO8601DateSerializer::class) val originalPurchaseDate:
-    Date?,
-    @SerialName("expires_date") @Serializable(with = ISO8601DateSerializer::class) val expiresDate: Date?,
-    @SerialName("store") val store: Store,
-    @SerialName("is_sandbox") val isSandbox: Boolean,
-    @SerialName("unsubscribe_detected_at") @Serializable(with = ISO8601DateSerializer::class) val unsubscribeDetectedAt:
-    Date?,
-    @SerialName(
-        "billing_issues_detected_at",
-    ) @Serializable(with = ISO8601DateSerializer::class) val billingIssuesDetectedAt:
-    Date?,
-    @SerialName(
-        "grace_period_expires_date",
-    ) @Serializable(with = ISO8601DateSerializer::class) val gracePeriodExpiresDate:
-    Date?,
-    @SerialName("ownership_type") val ownershipType: OwnershipType = OwnershipType.UNKNOWN,
-    @SerialName("period_type") val periodType: PeriodType,
-    @SerialName("refunded_at") @Serializable(with = ISO8601DateSerializer::class) val refundedAt: Date?,
-    @SerialName("store_transaction_id") val storeTransactionId: String?,
+    @SerialName("purchase_date") @Serializable(with = ISO8601DateSerializer::class)
+    val purchaseDate: Date,
+    @SerialName("original_purchase_date") @Serializable(with = ISO8601DateSerializer::class)
+    val originalPurchaseDate: Date?,
+    @SerialName("expires_date") @Serializable(with = ISO8601DateSerializer::class)
+    val expiresDate: Date?,
+    @SerialName("store")
+    val store: Store,
+    @SerialName("is_sandbox")
+    val isSandbox: Boolean,
+    @SerialName("unsubscribe_detected_at") @Serializable(with = ISO8601DateSerializer::class)
+    val unsubscribeDetectedAt: Date?,
+    @SerialName("billing_issues_detected_at") @Serializable(with = ISO8601DateSerializer::class)
+    val billingIssuesDetectedAt: Date?,
+    @SerialName("grace_period_expires_date") @Serializable(with = ISO8601DateSerializer::class)
+    val gracePeriodExpiresDate: Date?,
+    @SerialName("ownership_type")
+    val ownershipType: OwnershipType = OwnershipType.UNKNOWN,
+    @SerialName("period_type")
+    val periodType: PeriodType,
+    @SerialName("refunded_at") @Serializable(with = ISO8601DateSerializer::class)
+    val refundedAt: Date?,
+    @SerialName("store_transaction_id")
+    val storeTransactionId: String?,
 ) {
 
     override fun toString(): String {
--- purchases/src/main/kotlin/com/revenuecat/purchases/utils/EntitlementInfoHelper.kt
+++ purchases/src/main/kotlin/com/revenuecat/purchases/utils/EntitlementInfoHelper.kt
@@ -3,21 +3,18 @@ package com.revenuecat.purchases.utils
 import com.revenuecat.purchases.Store
 import java.util.Date
 
-internal class EntitlementInfoHelper private constructor() {
+internal object EntitlementInfoHelper {
 
-    companion object {
-
-        fun getWillRenew(
-            store: Store,
-            expirationDate: Date?,
-            unsubscribeDetectedAt: Date?,
-            billingIssueDetectedAt: Date?,
-        ): Boolean {
-            val isPromo = store == Store.PROMOTIONAL
-            val isLifetime = expirationDate == null
-            val hasUnsubscribed = unsubscribeDetectedAt != null
-            val hasBillingIssues = billingIssueDetectedAt != null
-            return !(isPromo || isLifetime || hasUnsubscribed || hasBillingIssues)
-        }
+    fun getWillRenew(
+        store: Store,
+        expirationDate: Date?,
+        unsubscribeDetectedAt: Date?,
+        billingIssueDetectedAt: Date?,
+    ): Boolean {
+        val isPromo = store == Store.PROMOTIONAL
+        val isLifetime = expirationDate == null
+        val hasUnsubscribed = unsubscribeDetectedAt != null
+        val hasBillingIssues = billingIssueDetectedAt != null
+        return !(isPromo || isLifetime || hasUnsubscribed || hasBillingIssues)
     }
 }
