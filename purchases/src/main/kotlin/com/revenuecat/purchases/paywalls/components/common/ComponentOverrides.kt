2bb5ed7dc215ae45b188373797379944891336f8
JayShortway <29483617+JayShortway@users.noreply.github.com>
2025-01-14 19:16:11 +0100

    Properly deserializes intro offer overrides.

--- purchases/src/main/kotlin/com/revenuecat/purchases/paywalls/components/common/ComponentOverrides.kt
+++ purchases/src/main/kotlin/com/revenuecat/purchases/paywalls/components/common/ComponentOverrides.kt
@@ -3,13 +3,15 @@ package com.revenuecat.purchases.paywalls.components.common
 import com.revenuecat.purchases.InternalRevenueCatAPI
 import com.revenuecat.purchases.paywalls.components.PartialComponent
 import dev.drewhamilton.poko.Poko
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @InternalRevenueCatAPI
 @Poko
 @Serializable
 class ComponentOverrides<T : PartialComponent>(
-    @get:JvmSynthetic val introOffer: T? = null,
+    @get:JvmSynthetic @SerialName("intro_offer") val introOffer: T? = null,
+    @get:JvmSynthetic @SerialName("multiple_intro_offers") val multipleIntroOffers: T? = null,
     @get:JvmSynthetic val states: ComponentStates<T>? = null,
     @get:JvmSynthetic val conditions: ComponentConditions<T>? = null,
 )
