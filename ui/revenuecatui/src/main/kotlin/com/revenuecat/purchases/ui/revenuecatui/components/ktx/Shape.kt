ed102f4259269f147527523c96852cf484bac8d2
JayShortway <29483617+JayShortway@users.noreply.github.com>
2025-01-15 13:15:36 +0100

    Updates a comment.

--- ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/components/ktx/Shape.kt
+++ ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/components/ktx/Shape.kt
@@ -13,7 +13,7 @@ import com.revenuecat.purchases.paywalls.components.properties.Shape as RcShape
 
 /**
  * The scale factor to use on the Y axis when drawing a Concave or Convex shape. This will be multiplied by Y to
- * determine the start and end point of the curve from the bottom of the shape.
+ * determine the maximum distance of the curve to the bottom of the shape.
  */
 private const val SCALE_Y_OFFSET_CONCAVE_CONVEX = 0.2f
 
