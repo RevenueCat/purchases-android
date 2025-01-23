87174595fa28839f0157ac0a879ee5525ea32959
JayShortway <29483617+JayShortway@users.noreply.github.com>
2025-01-23 12:43:54 +0100

    Refactors PurchasesFactoryTest to use shadows.

--- purchases/src/test/java/com/revenuecat/purchases/PurchasesFactoryTest.kt
+++ purchases/src/test/java/com/revenuecat/purchases/PurchasesFactoryTest.kt
@@ -5,6 +5,7 @@ import android.app.Application
 import android.content.Context
 import android.content.pm.PackageManager
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
 import io.mockk.clearAllMocks
 import io.mockk.every
 import io.mockk.just
@@ -16,14 +17,13 @@ import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.robolectric.Shadows.shadowOf
 
 @RunWith(AndroidJUnit4::class)
 class PurchasesFactoryTest {
 
-    private val applicationMock = mockk<Context>()
-    private val contextMock = mockk<Context>().apply {
-        every { applicationContext } returns applicationMock
-    }
+    private val contextMock = InstrumentationRegistry.getInstrumentation().targetContext
+    private val applicationMock = contextMock.applicationContext as Application
     private val apiKeyValidatorMock = mockk<APIKeyValidator>()
 
     private lateinit var purchasesFactory: PurchasesFactory
@@ -43,9 +43,7 @@ class PurchasesFactoryTest {
     @Test
     fun `creating purchase checks context has INTERNET permission`() {
         val configuration = createConfiguration()
-        every {
-            applicationMock.checkCallingOrSelfPermission(Manifest.permission.INTERNET)
-        } returns PackageManager.PERMISSION_DENIED
+        shadowOf(applicationMock).denyPermissions(Manifest.permission.INTERNET)
         assertThatExceptionOfType(IllegalArgumentException::class.java).isThrownBy {
             purchasesFactory.validateConfiguration(configuration)
         }.withMessage("Purchases requires INTERNET permission.")
@@ -54,9 +52,7 @@ class PurchasesFactoryTest {
     @Test
     fun `creating purchase checks api key is not empty`() {
         val configuration = createConfiguration(testApiKey = "")
-        every {
-            applicationMock.checkCallingOrSelfPermission(Manifest.permission.INTERNET)
-        } returns PackageManager.PERMISSION_GRANTED
+        shadowOf(applicationMock).grantPermissions(Manifest.permission.INTERNET)
         assertThatExceptionOfType(IllegalArgumentException::class.java).isThrownBy {
             purchasesFactory.validateConfiguration(configuration)
         }.withMessage("API key must be set. Get this from the RevenueCat web app")
@@ -64,14 +60,19 @@ class PurchasesFactoryTest {
 
     @Test
     fun `creating purchase checks context application context is an application`() {
-        val configuration = createConfiguration()
+        val applicationMock = mockk<Context>()
+        val contextMock = mockk<Context>().apply {
+            every { applicationContext } returns applicationMock
+        }
         val nonApplicationContextMock = mockk<Context>()
-        every {
-            applicationMock.checkCallingOrSelfPermission(Manifest.permission.INTERNET)
-        } returns PackageManager.PERMISSION_GRANTED
         every {
             applicationMock.applicationContext
         } returns nonApplicationContextMock
+        every {
+            applicationMock.checkCallingOrSelfPermission(Manifest.permission.INTERNET)
+        } returns PackageManager.PERMISSION_GRANTED
+        val configuration = createConfiguration(context = contextMock)
+
         assertThatExceptionOfType(IllegalArgumentException::class.java).isThrownBy {
             purchasesFactory.validateConfiguration(configuration)
         }.withMessage("Needs an application context.")
@@ -80,19 +81,16 @@ class PurchasesFactoryTest {
     @Test
     fun `creating purchase validates api key is valid`() {
         val configuration = createConfiguration()
-        val applicationContextMock = mockk<Application>()
-        every {
-            applicationMock.checkCallingOrSelfPermission(Manifest.permission.INTERNET)
-        } returns PackageManager.PERMISSION_GRANTED
-        every {
-            applicationMock.applicationContext
-        } returns applicationContextMock
+        shadowOf(applicationMock).grantPermissions(Manifest.permission.INTERNET)
         purchasesFactory.validateConfiguration(configuration)
         verify(exactly = 1) { apiKeyValidatorMock.validateAndLog("fakeApiKey", Store.PLAY_STORE) }
     }
 
-    private fun createConfiguration(testApiKey: String = "fakeApiKey"): PurchasesConfiguration {
-        return PurchasesConfiguration.Builder(contextMock, testApiKey)
+    private fun createConfiguration(
+        context: Context = contextMock,
+        testApiKey: String = "fakeApiKey",
+    ): PurchasesConfiguration {
+        return PurchasesConfiguration.Builder(context, testApiKey)
             .appUserID("appUserID")
             .store(Store.PLAY_STORE)
             .build()
