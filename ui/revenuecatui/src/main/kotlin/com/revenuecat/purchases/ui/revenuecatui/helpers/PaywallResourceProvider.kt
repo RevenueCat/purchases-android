b5d90c19685b226a808128889a8d0186a3b8b949
JayShortway <29483617+JayShortway@users.noreply.github.com>
2025-01-27 15:16:47 +0100

    Adds getResourceIdentifier() to ResourceProvider.

--- ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/helpers/PaywallResourceProvider.kt
+++ ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/helpers/PaywallResourceProvider.kt
@@ -1,5 +1,6 @@
 package com.revenuecat.purchases.ui.revenuecatui.helpers
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.res.Resources
 import androidx.annotation.StringRes
@@ -12,15 +13,17 @@ internal interface ResourceProvider {
     fun getApplicationName(): String
     fun getString(@StringRes resId: Int, vararg formatArgs: Any): String
     fun getLocale(): Locale
+    fun getResourceIdentifier(name: String, type: String): Int
 }
 
 internal class PaywallResourceProvider(
     private val applicationName: String,
+    private val packageName: String,
     private val resources: Resources,
 ) : ResourceProvider {
     constructor(
         context: Context,
-    ) : this(context.applicationContext.applicationName(), context.resources)
+    ) : this(context.applicationContext.applicationName(), context.packageName, context.resources)
 
     override fun getApplicationName(): String {
         return applicationName
@@ -33,6 +36,14 @@ internal class PaywallResourceProvider(
     override fun getLocale(): Locale {
         return resources.configuration.locales.get(0)
     }
+
+    /**
+     * Use sparingly. The underlying platform API is discouraged because
+     * > resource reflection makes it harder to perform build optimizations and compile-time verification of code.
+     */
+    @SuppressLint("DiscouragedApi")
+    override fun getResourceIdentifier(name: String, type: String): Int =
+        resources.getIdentifier(name, type, packageName)
 }
 
 internal fun Context.toResourceProvider(): ResourceProvider {
