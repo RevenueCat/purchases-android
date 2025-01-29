57b55a96d61ba083e21fdce1a25d9382f32c0786
Facundo Menzella <facumenzella@users.noreply.github.com>
2025-01-29 11:18:08 +0100

    feat: Add basic customer center events (#2075)
    ### Motivation
    This PR adds basic customer center events definition:
    - `customer_center_impression`
    - `customer_center_survey_option_chosen`
    
    ### Description
    Inspired in iOS
    [PR](https://github.com/RevenueCat/purchases-ios/pull/4392/files), and
    using my really really basic (almost negative) Kotlin expertise.
    
    Let me know what you think 🙏

---
+++ purchases/src/main/kotlin/com/revenuecat/purchases/customercenter/events/CustomerCenterEvent.kt
@@ -0,0 +1,54 @@
+package com.revenuecat.purchases.customercenter.events
+
+import androidx.annotation.VisibleForTesting
+import com.revenuecat.purchases.ExperimentalPreviewRevenueCatPurchasesAPI
+import com.revenuecat.purchases.utils.serializers.DateSerializer
+import com.revenuecat.purchases.utils.serializers.UUIDSerializer
+import dev.drewhamilton.poko.Poko
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import java.util.Date
+import java.util.UUID
+
+/**
+ * Type representing a customer center event and associated data. Meant for RevenueCatUI use.
+ */
+@ExperimentalPreviewRevenueCatPurchasesAPI
+@Poko
+@Serializable
+class CustomerCenterEvent(
+    val creationData: CreationData,
+    val eventData: Data,
+) {
+
+    companion object {
+        @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+        internal val json = Json.Default
+    }
+
+    @ExperimentalPreviewRevenueCatPurchasesAPI
+    @Poko
+    @Serializable
+    class CreationData(
+        @Serializable(with = UUIDSerializer::class)
+        val id: UUID,
+        @Serializable(with = DateSerializer::class)
+        val date: Date,
+    )
+
+    @ExperimentalPreviewRevenueCatPurchasesAPI
+    @Poko
+    @Serializable
+    @SuppressWarnings("LongParameterList")
+    class Data(
+        val type: CustomerCenterEventType,
+        @Serializable(with = DateSerializer::class)
+        val timestamp: Date,
+        val darkMode: Boolean,
+        val locale: String,
+        val isSandbox: Boolean,
+        val version: Int = 1,
+        val revisionId: Int = 1,
+        val displayMode: CustomerCenterDisplayMode = CustomerCenterDisplayMode.FULL_SCREEN,
+    )
+}
