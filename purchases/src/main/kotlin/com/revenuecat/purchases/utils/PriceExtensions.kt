aa7071ddcd9bf773d07b82a9613bbe017645f268
JayShortway <29483617+JayShortway@users.noreply.github.com>
2025-01-29 20:55:24 +0100

    Adds pricePerPeriod extensions.

--- purchases/src/main/kotlin/com/revenuecat/purchases/utils/PriceExtensions.kt
+++ purchases/src/main/kotlin/com/revenuecat/purchases/utils/PriceExtensions.kt
@@ -1,5 +1,6 @@
 package com.revenuecat.purchases.utils
 
+import com.revenuecat.purchases.InternalRevenueCatAPI
 import com.revenuecat.purchases.models.Period
 import com.revenuecat.purchases.models.Price
 import java.text.NumberFormat
@@ -8,6 +9,12 @@ import java.util.Locale
 
 private const val MICRO_MULTIPLIER = 1_000_000.0
 
+@OptIn(InternalRevenueCatAPI::class)
+internal fun Price.pricePerDay(billingPeriod: Period, locale: Locale): Price {
+    return pricePerPeriod(billingPeriod.valueInDays, locale)
+}
+
+@OptIn(InternalRevenueCatAPI::class)
 internal fun Price.pricePerWeek(billingPeriod: Period, locale: Locale): Price {
     return pricePerPeriod(billingPeriod.valueInWeeks, locale)
 }
@@ -16,6 +23,7 @@ internal fun Price.pricePerMonth(billingPeriod: Period, locale: Locale): Price {
     return pricePerPeriod(billingPeriod.valueInMonths, locale)
 }
 
+@OptIn(InternalRevenueCatAPI::class)
 internal fun Price.pricePerYear(billingPeriod: Period, locale: Locale): Price {
     return pricePerPeriod(billingPeriod.valueInYears, locale)
 }
