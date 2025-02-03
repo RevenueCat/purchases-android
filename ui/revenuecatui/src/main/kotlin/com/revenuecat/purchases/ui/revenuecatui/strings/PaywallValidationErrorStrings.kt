fc0dbe93ae2240fa5a4cf5e839c4b6c10f9ba5e2
Toni Rico <toni.rico.diez@revenuecat.com>
2025-02-03 14:39:01 +0100

    Update ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/strings/PaywallValidationErrorStrings.kt
    Co-authored-by: JayShortway <29483617+JayShortway@users.noreply.github.com>

--- ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/strings/PaywallValidationErrorStrings.kt
+++ ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/strings/PaywallValidationErrorStrings.kt
@@ -23,5 +23,5 @@ internal object PaywallValidationErrorStrings {
     const val ALIASED_COLOR_IS_ALIAS = "Aliased color '%s' has an aliased value '%s', which is not allowed."
     const val MISSING_FONT_ALIAS = "Aliased font '%s' does not exist."
     const val INVALID_MODE_FOR_COMPONENTS_PAYWALL =
-        "Components paywall does not support footer modes. Falling back to legacy fallback paywall."
+        "Paywalls V2 does not support footer modes. Falling back to legacy fallback paywall."
 }
