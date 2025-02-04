64b9fec3d6821614a4f121fd1766fc58a8a2dd66
JayShortway <29483617+JayShortway@users.noreply.github.com>
2025-02-04 17:46:54 +0100

    Fixes detekt.

--- ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/components/style/StyleFactory.kt
+++ ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/components/style/StyleFactory.kt
@@ -77,6 +77,7 @@ internal class StyleFactory(
         private val DEFAULT_SHAPE = Shape.Rectangle()
     }
 
+    @Suppress("CyclomaticComplexMethod")
     fun create(
         component: PaywallComponent,
         rcPackage: Package? = null,
