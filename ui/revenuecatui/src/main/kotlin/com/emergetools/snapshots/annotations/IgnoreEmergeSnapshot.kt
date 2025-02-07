333fda2433b6d944bcd9abd1812e302c007062c1
Cesar de la Vega <cesarvegaro@gmail.com>
2025-02-07 10:43:46 +0100

    Revert "pacakge annotation"
    This reverts commit 9d098fd92583d0d2323136f12265d96f5acfdd7d.

--- ui/revenuecatui/src/main/kotlin/com/emergetools/snapshots/annotations/IgnoreEmergeSnapshot.kt
+++
@@ -1,7 +0,0 @@
-package com.emergetools.snapshots.annotations
-
-/**
- * Specifies the annotated function is a Preview that should be ignored by Emerge snapshotting.
- */
-@Target(AnnotationTarget.FUNCTION)
-internal annotation class IgnoreEmergeSnapshot
