da78abb8d8d0c3338dd01953a1339e91068254ec
Facundo Menzella <facumenzella@gmail.com>
2025-02-07 13:40:56 +0100

    reverteed

--- ui/revenuecatui/src/test/kotlin/com/revenuecat/purchases/ui/revenuecatui/data/PaywallViewModelTest.kt
+++ ui/revenuecatui/src/test/kotlin/com/revenuecat/purchases/ui/revenuecatui/data/PaywallViewModelTest.kt
@@ -1281,7 +1281,7 @@ class PaywallViewModelTest {
                 withArg { event ->
                     val paywallEvent = event as? PaywallEvent
                         ?: error("Expected PaywallEvent but got ${event::class.simpleName}")
-                    assertThat(paywallEvent.type).isEqualTo(PaywallEventType.CLOSE)
+                    withArg { assertThat(paywallEvent.type).isEqualTo(eventType) }
                 }
             )
         }
