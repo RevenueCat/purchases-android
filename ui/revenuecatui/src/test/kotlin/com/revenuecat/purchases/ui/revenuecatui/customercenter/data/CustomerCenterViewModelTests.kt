fe9d065e29acbe13aaa7d60dcdb6802691e3afca
Facundo Menzella <facumenzella@gmail.com>
2025-02-12 13:53:04 +0100

    tests working

--- ui/revenuecatui/src/test/kotlin/com/revenuecat/purchases/ui/revenuecatui/customercenter/data/CustomerCenterViewModelTests.kt
+++ ui/revenuecatui/src/test/kotlin/com/revenuecat/purchases/ui/revenuecatui/customercenter/data/CustomerCenterViewModelTests.kt
@@ -4,8 +4,14 @@ import android.content.Context
 import androidx.compose.material3.ColorScheme
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.revenuecat.purchases.CustomerInfo
+import com.revenuecat.purchases.EntitlementInfos
 import com.revenuecat.purchases.ExperimentalPreviewRevenueCatPurchasesAPI
+import com.revenuecat.purchases.OwnershipType
+import com.revenuecat.purchases.PeriodType
 import com.revenuecat.purchases.PurchasesAreCompletedBy
+import com.revenuecat.purchases.Store
+import com.revenuecat.purchases.SubscriptionInfo
+import com.revenuecat.purchases.VerificationResult
 import com.revenuecat.purchases.customercenter.CustomerCenterConfigData
 import com.revenuecat.purchases.customercenter.CustomerCenterConfigData.HelpPath
 import com.revenuecat.purchases.customercenter.CustomerCenterConfigData.HelpPath.PathDetail
@@ -13,6 +19,7 @@ import com.revenuecat.purchases.customercenter.CustomerCenterConfigData.HelpPath
 import com.revenuecat.purchases.customercenter.CustomerCenterConfigData.Localization
 import com.revenuecat.purchases.customercenter.CustomerCenterConfigData.Screen
 import com.revenuecat.purchases.customercenter.CustomerCenterConfigData.Support
+import com.revenuecat.purchases.models.Transaction
 import com.revenuecat.purchases.paywalls.PaywallData
 import com.revenuecat.purchases.ui.revenuecatui.customercenter.viewmodel.CustomerCenterViewModel
 import com.revenuecat.purchases.ui.revenuecatui.customercenter.viewmodel.CustomerCenterViewModelImpl
@@ -29,6 +36,8 @@ import io.mockk.every
 import io.mockk.just
 import io.mockk.mockk
 import junit.framework.TestCase.fail
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.runTest
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
@@ -37,6 +46,8 @@ import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.util.Collections
+import java.util.Date
 import java.util.Locale
 
 @OptIn(ExperimentalPreviewRevenueCatPurchasesAPI::class)
@@ -47,13 +58,11 @@ class CustomerCenterViewModelTests {
     private lateinit var customerInfo: CustomerInfo
     private lateinit var configData: CustomerCenterConfigData
 
-    private lateinit var purchaseInformation: PurchaseInformation
     private lateinit var screens: Map<Screen.ScreenType, Screen>
 
     @Before
     fun setUp() {
         purchases = mockk()
-        purchaseInformation = mockk()
         customerInfo = mockk()
         configData = mockk()
 
@@ -84,59 +93,42 @@ class CustomerCenterViewModelTests {
     }
 
     @Test
-    fun `isSupportedPaths filters CANCEL when purchase is lifetime`() = runTest {
-        every { configData.getManagementScreen() } returns screens[Screen.ScreenType.MANAGEMENT]
-
-        coEvery { purchases.awaitCustomerInfo(any()) } returns customerInfo
-        coEvery { purchases.awaitCustomerCenterConfigData() } returns configData
-
-        every { purchases.purchasesAreCompletedBy } returns PurchasesAreCompletedBy.REVENUECAT
-        every { customerInfo.activeSubscriptions } returns setOf()
-        every { customerInfo.nonSubscriptionTransactions } returns listOf()
-        every { purchases.storefrontCountryCode } returns "US"
-        every { purchases.track(any()) } just Runs
-        every { purchases.syncPurchases() } just Runs
-        every { purchaseInformation.isLifetime } returns true
-
-        val model = CustomerCenterViewModelImpl(
-            purchases = purchases,
-            locale = Locale.US,
-            colorScheme = TestData.Constants.currentColorScheme,
-            isDarkMode = false
-        )
-        model.loadCustomerCenter()
-
-        val state = model.state.value
-        if (state !is CustomerCenterState.Success) {
-            fail("Invalid state")
-            return@runTest
-        }
-
-        val successState = state as CustomerCenterState.Success
-        val paths = successState.supportedPathsForManagementScreen ?: emptyList()
-
-        assertThat(paths.size).isEqualTo(1)
-        assertThat(paths.first()).isEqualTo(HelpPath(
-            id = "id1",
-            title = "title1",
-            type = HelpPath.PathType.MISSING_PURCHASE
-        ))
-    }
-
-    @Test
     fun `isSupportedPaths does not filter CANCEL when purchase is not lifetime`() = runTest {
         every { configData.getManagementScreen() } returns screens[Screen.ScreenType.MANAGEMENT]
 
+        coEvery { purchases.awaitGetProduct(any(), any()) } returns null
         coEvery { purchases.awaitCustomerInfo(any()) } returns customerInfo
         coEvery { purchases.awaitCustomerCenterConfigData() } returns configData
-
         every { purchases.purchasesAreCompletedBy } returns PurchasesAreCompletedBy.REVENUECAT
-        every { customerInfo.activeSubscriptions } returns setOf()
+
+        every { customerInfo.managementURL } returns null
+        every { customerInfo.activeSubscriptions } returns setOf(TestData.Packages.monthly.product.id)
+        every { customerInfo.entitlements } returns EntitlementInfos(
+            emptyMap(),
+            VerificationResult.VERIFIED,
+        )
+        every { customerInfo.subscriptionsByProductIdentifier } returns mapOf(
+            "productIdentifier" to SubscriptionInfo(
+                productIdentifier = "productIdentifier",
+                purchaseDate = Date(),
+                originalPurchaseDate = null,
+                expiresDate = null,
+                store = Store.APP_STORE,
+                unsubscribeDetectedAt = null,
+                isSandbox = false,
+                billingIssuesDetectedAt = null,
+                gracePeriodExpiresDate = null,
+                periodType = PeriodType.NORMAL,
+                refundedAt = null,
+                storeTransactionId = null,
+                requestDate = Date()
+            )
+        )
+
         every { customerInfo.nonSubscriptionTransactions } returns listOf()
         every { purchases.storefrontCountryCode } returns "US"
         every { purchases.track(any()) } just Runs
         every { purchases.syncPurchases() } just Runs
-        every { purchaseInformation.isLifetime } returns true
 
         val model = CustomerCenterViewModelImpl(
             purchases = purchases,
@@ -144,17 +136,74 @@ class CustomerCenterViewModelTests {
             colorScheme = TestData.Constants.currentColorScheme,
             isDarkMode = false
         )
-        model.loadCustomerCenter()
 
-        val state = model.state.value
-        if (state !is CustomerCenterState.Success) {
-            fail("Invalid state")
-            return@runTest
+        val job = launch {
+            model.state.collect { state ->
+                if (state is CustomerCenterState.Success) {
+                    val paths = state.supportedPathsForManagementScreen ?: emptyList()
+                    assertThat(paths.size).isEqualTo(2)
+                    cancel()
+                }
+            }
         }
 
-        val successState = model.state as CustomerCenterState.Success
-        val paths = successState.supportedPathsForManagementScreen ?: emptyList()
+        job.join()
+    }
 
-        assertThat(paths.size).isEqualTo(2)
+    @Test
+    fun `isSupportedPaths does filter CANCEL when purchase is lifetime`() = runTest {
+        every { configData.getManagementScreen() } returns screens[Screen.ScreenType.MANAGEMENT]
+
+        coEvery { purchases.awaitGetProduct(any(), any()) } returns null
+        coEvery { purchases.awaitCustomerInfo(any()) } returns customerInfo
+        coEvery { purchases.awaitCustomerCenterConfigData() } returns configData
+        every { purchases.purchasesAreCompletedBy } returns PurchasesAreCompletedBy.REVENUECAT
+
+        every { customerInfo.managementURL } returns null
+        every { customerInfo.activeSubscriptions } returns setOf(TestData.Packages.monthly.product.id)
+        every { customerInfo.entitlements } returns EntitlementInfos(
+            emptyMap(),
+            VerificationResult.VERIFIED,
+        )
+        every { customerInfo.subscriptionsByProductIdentifier } returns emptyMap()
+        every { customerInfo.nonSubscriptionTransactions } returns listOf(
+            Transaction(
+                transactionIdentifier = "transactionIdentifier",
+                revenuecatId = "revenuecatId",
+                productIdentifier = "productIdentifier",
+                productId = "productId",
+                purchaseDate = Date(),
+                storeTransactionId = null,
+                store = Store.PLAY_STORE
+            )
+        )
+
+        every { purchases.storefrontCountryCode } returns "US"
+        every { purchases.track(any()) } just Runs
+        every { purchases.syncPurchases() } just Runs
+
+        val model = CustomerCenterViewModelImpl(
+            purchases = purchases,
+            locale = Locale.US,
+            colorScheme = TestData.Constants.currentColorScheme,
+            isDarkMode = false
+        )
+
+        val job = launch {
+            model.state.collect { state ->
+                if (state is CustomerCenterState.Success) {
+                    val paths = state.supportedPathsForManagementScreen ?: emptyList()
+                    assertThat(paths.size).isEqualTo(1)
+                    assertThat(paths.first()).isEqualTo(HelpPath(
+                        id = "id1",
+                        title = "title1",
+                        type = HelpPath.PathType.MISSING_PURCHASE
+                    ))
+                    cancel()
+                }
+            }
+        }
+
+        job.join()
     }
 }
