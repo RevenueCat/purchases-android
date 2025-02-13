1dd61064d5234ebda22e1ca70069d1746259ba7f
Facundo Menzella <facumenzella@users.noreply.github.com>
2025-02-13 20:56:59 +0100

    chore: Unify Json encoding with JsonHelper (#2160)
    ### Motivation
    As promised to @tonidero and @JayShortway, here's a `JsonHelper` that
    unifies Json use.
    This PR introduces `JsonProvider`, and a `defaultJson`. 
    
    > Sadly, `classDiscriminator` cannot be dynamic so we need one per
    discriminator (paywalls will need one)

--- purchases/src/main/kotlin/com/revenuecat/purchases/common/Backend.kt
+++ purchases/src/main/kotlin/com/revenuecat/purchases/common/Backend.kt
@@ -497,7 +497,7 @@ internal class Backend(
         onSuccessHandler: () -> Unit,
         onErrorHandler: (error: PurchasesError, shouldMarkAsSynced: Boolean) -> Unit,
     ) {
-        val body = EventsRequest.json.encodeToJsonElement(paywallEventRequest).asMap()
+        val body = JsonProvider.defaultJson.encodeToJsonElement(paywallEventRequest).asMap()
         if (body == null) {
             onErrorHandler(
                 PurchasesError(
