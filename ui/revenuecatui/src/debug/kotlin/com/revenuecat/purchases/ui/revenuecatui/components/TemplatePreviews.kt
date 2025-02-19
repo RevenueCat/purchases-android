6443be0ec3bb5f578c0824f49179f7eb67b6c9ee
JayShortway <29483617+JayShortway@users.noreply.github.com>
2025-02-19 19:01:30 +0100

    Instantiates the OfferingParser using reflection.

--- ui/revenuecatui/src/debug/kotlin/com/revenuecat/purchases/ui/revenuecatui/components/TemplatePreviews.kt
+++ ui/revenuecatui/src/debug/kotlin/com/revenuecat/purchases/ui/revenuecatui/components/TemplatePreviews.kt
@@ -5,11 +5,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import com.revenuecat.purchases.Offering
-import com.revenuecat.purchases.PackageType
-import com.revenuecat.purchases.common.OfferingParser
+import com.revenuecat.purchases.Offerings
 import com.revenuecat.purchases.models.StoreProduct
 import com.revenuecat.purchases.ui.revenuecatui.BuildConfig
-import com.revenuecat.purchases.ui.revenuecatui.data.testdata.TestData
 import com.revenuecat.purchases.ui.revenuecatui.helpers.getOrThrow
 import com.revenuecat.purchases.ui.revenuecatui.helpers.toComponentsPaywallState
 import org.json.JSONArray
@@ -17,30 +15,7 @@ import org.json.JSONObject
 import java.io.File
 import java.util.Date
 
-private object PreviewOfferingParser : OfferingParser() {
-    override fun findMatchingProduct(
-        productsById: Map<String, List<StoreProduct>>,
-        packageJson: JSONObject,
-    ): StoreProduct? {
-        // Ignoring productsById and just returning a product based on the identifier in the packageJson.
-        val identifier = packageJson.getString("identifier")
-        val packageType = PackageType.values().first { packageType -> packageType.identifier == identifier }
-
-        return when (packageType) {
-            PackageType.LIFETIME -> TestData.Packages.lifetime.product
-            PackageType.ANNUAL -> TestData.Packages.annual.product
-            PackageType.SIX_MONTH -> TestData.Packages.semester.product
-            PackageType.THREE_MONTH -> TestData.Packages.quarterly.product
-            PackageType.TWO_MONTH -> TestData.Packages.bimonthly.product
-            PackageType.MONTHLY -> TestData.Packages.monthly.product
-            PackageType.WEEKLY -> TestData.Packages.weekly.product
-            else -> null
-        }
-    }
-}
-
 private class OfferingProvider : PreviewParameterProvider<Offering> {
-
     private val offeringsJsonFileName = "offerings_paywalls_v2_templates.json"
     private val packagesJsonFileName = "packages.json"
 
@@ -57,7 +32,14 @@ private class OfferingProvider : PreviewParameterProvider<Offering> {
             val hasPaywall = offering.optString("paywall_components").isNotBlank()
             if (hasPaywall) offering.getString("identifier") else null
         }
-    private val offerings = PreviewOfferingParser.createOfferings(offeringsJsonObject, emptyMap())
+
+    // Using reflection to create our Offerings, so the OfferingParser can stay internal to the purchases SDK.
+    private val offerings = Class.forName("com.revenuecat.purchases.utils.PreviewOfferingParser")
+        .let { cls ->
+            val offeringParser = cls.getDeclaredConstructor().apply { isAccessible = true }.newInstance()
+            val createOfferings = cls.getMethod("createOfferings", JSONObject::class.java, Map::class.java)
+            createOfferings(offeringParser, offeringsJsonObject, emptyMap<String, List<StoreProduct>>()) as Offerings
+        }
 
     override val values: Sequence<Offering> = offeringIds.asSequence()
         .sorted()
