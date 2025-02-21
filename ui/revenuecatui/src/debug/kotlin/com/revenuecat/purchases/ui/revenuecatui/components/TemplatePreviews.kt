5cb668131b0828cbe73dff71351feeb1fdb01ea7
JayShortway <29483617+JayShortway@users.noreply.github.com>
2025-02-21 18:24:56 +0100

    Not sorting the offerings by ID.

--- ui/revenuecatui/src/debug/kotlin/com/revenuecat/purchases/ui/revenuecatui/components/TemplatePreviews.kt
+++ ui/revenuecatui/src/debug/kotlin/com/revenuecat/purchases/ui/revenuecatui/components/TemplatePreviews.kt
@@ -69,7 +69,7 @@ private class OfferingProvider : PreviewParameterProvider<Offering> {
 
             index++
         }
-    }.sortedBy { it.identifier }
+    }
 
     private fun createOfferings(offeringsJsonObject: JSONObject): Offerings =
         createOfferingsMethod(offeringParser, offeringsJsonObject, emptyMap<String, List<StoreProduct>>()) as Offerings
