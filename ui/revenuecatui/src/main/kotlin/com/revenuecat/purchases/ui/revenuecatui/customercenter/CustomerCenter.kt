8e35c121be2c0bf9d99e30dbf49277fbdba15278
Cesar de la Vega <cesarvegaro@gmail.com>
2025-03-06 17:00:08 +0100

    Fix `CustomerCenterActivity` colors on dark mode (#2225)
    Dark mode colors were not correctly applied in React Native. We needed
    to set the correct theme and also set the correct `LocalContentColor`
    based on the theme to the whole `InternalCustomerCenter` otherwise
    composables would default to black.

--- ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/customercenter/CustomerCenter.kt
+++ ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/customercenter/CustomerCenter.kt
@@ -1,6 +1,9 @@
 package com.revenuecat.purchases.ui.revenuecatui.customercenter
 
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 
 /**
@@ -23,9 +26,12 @@ fun CustomerCenter(
     options: CustomerCenterOptions = CustomerCenterOptions.Builder().build(),
     onDismiss: () -> Unit,
 ) {
-    InternalCustomerCenter(
-        modifier = modifier,
-        listener = options.listener,
-        onDismiss = onDismiss,
-    )
+    // Ensure proper content color is set for the entire CustomerCenter
+    CompositionLocalProvider(LocalContentColor provides MaterialTheme.colorScheme.onBackground) {
+        InternalCustomerCenter(
+            modifier = modifier,
+            listener = options.listener,
+            onDismiss = onDismiss,
+        )
+    }
 }
