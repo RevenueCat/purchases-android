a24c9caa8767a98577cd7d34a63c1f31f1337e89
Toni Rico <antonio.rico.diez@revenuecat.com>
2025-03-12 10:23:02 +0100

    Add refresh button to PaywallTester app

--- examples/paywall-tester/src/main/java/com/revenuecat/paywallstester/ui/screens/main/offerings/OfferingsViewModel.kt
+++ examples/paywall-tester/src/main/java/com/revenuecat/paywallstester/ui/screens/main/offerings/OfferingsViewModel.kt
@@ -2,16 +2,24 @@ package com.revenuecat.paywallstester.ui.screens.main.offerings
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.revenuecat.paywallstester.MainApplication
 import com.revenuecat.purchases.Purchases
+import com.revenuecat.purchases.awaitOfferings
+import com.revenuecat.purchases.awaitSyncAttributesAndOfferingsIfNeeded
+import com.revenuecat.purchases.awaitSyncPurchases
 import com.revenuecat.purchases.getOfferingsWith
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 abstract class OfferingsViewModel : ViewModel() {
 
     abstract val offeringsState: StateFlow<OfferingsState>
+
+    abstract fun refreshOfferings()
 }
 
 class OfferingsViewModelImpl : OfferingsViewModel() {
@@ -22,6 +30,14 @@ class OfferingsViewModelImpl : OfferingsViewModel() {
 
     init {
         updateOfferings()
+    }
+
+    override fun refreshOfferings() {
+        _offeringsState.update { OfferingsState.Loading }
+        viewModelScope.launch {
+            val offerings = Purchases.sharedInstance.awaitSyncAttributesAndOfferingsIfNeeded()
+            _offeringsState.update { OfferingsState.Loaded(offerings) }
+        }
     }
 
     private fun updateOfferings() {
