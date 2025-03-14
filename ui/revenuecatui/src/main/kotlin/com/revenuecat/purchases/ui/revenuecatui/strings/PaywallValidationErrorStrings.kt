0c002dc7618f75c160f6d938c329c34c8e3f7a69
JayShortway <29483617+JayShortway@users.noreply.github.com>
2025-03-14 10:47:28 +0100

    Small tweak to the MISSING_PACKAGE error message.

--- ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/strings/PaywallValidationErrorStrings.kt
+++ ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/strings/PaywallValidationErrorStrings.kt
@@ -21,7 +21,8 @@ internal object PaywallValidationErrorStrings {
     const val ALL_VARIABLE_LOCALIZATIONS_MISSING_FOR_LOCALE =
         "All variable localizations for locale '%s' are missing."
     const val MISSING_PACKAGE =
-        "Offering with id '%s' does not have a package with id '%s'. It has these packages instead: [%s]."
+        "The Paywall references a package with id '%s', but Offering '%s' does not contain such a package. " +
+            "It has these packages instead: [%s]."
     const val MISSING_COLOR_ALIAS = "Aliased color '%s' does not exist."
     const val ALIASED_COLOR_IS_ALIAS = "Aliased color '%s' has an aliased value '%s', which is not allowed."
     const val MISSING_FONT_ALIAS = "Aliased font '%s' does not exist."
