2f700fbfbba19ffda9157a7e59f8ffc85773e0ac
Facundo Menzella <facumenzella@gmail.com>
2025-03-18 16:01:21 +0100

    update view

--- ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/views/CustomerCenterView.kt
+++ ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/views/CustomerCenterView.kt
@@ -2,32 +2,58 @@ package com.revenuecat.purchases.ui.revenuecatui.views
 
 import android.content.Context
 import android.util.AttributeSet
-import androidx.compose.runtime.Composable
+import android.util.Log
 import androidx.compose.ui.platform.AbstractComposeView
 import com.revenuecat.purchases.ui.revenuecatui.customercenter.CustomerCenter
 
 /**
  * View that wraps the [CustomerCenter] Composable to display the Customer Center through the View system.
  */
-class CustomerCenterView : AbstractComposeView {
 
-    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+public class CustomerCenterView : AbstractComposeView {
 
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
+        init()
+    }
 
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init()
+    }
+
+    /**
+     * Constructor for programmatic use.
+     */
+    @JvmOverloads
     constructor(
         context: Context,
-        dismissHandler: (CustomerCenterView) -> Unit,
+        dismissHandler: (() -> Unit)? = null,
     ) : super(context) {
+        this.dismissHandler = dismissHandler
+        init()
+    }
+
+    private var dismissHandler: (() -> Unit)? = null
+
+    /**
+     * Sets a dismiss handler for when the customer center is closed.
+     */
+    fun setDismissHandler(dismissHandler: (() -> Unit)?) {
         this.dismissHandler = dismissHandler
     }
 
-    private var dismissHandler: ((CustomerCenterView) -> Unit) = {}
+    private fun init() {
+        Log.d("CustomerCenterView", "Initialized CustomerCenterView")
+    }
 
     @Composable
     override fun Content() {
+        CustomerCenterUI(dismissHandler = dismissHandler)
+    }
+
+    @Composable
+    private fun CustomerCenterUI(dismissHandler: (() -> Unit)?) {
         CustomerCenter {
-            dismissHandler(this)
+            dismissHandler?.invoke()
         }
     }
 }
