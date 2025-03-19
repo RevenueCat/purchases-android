a35716b228ea083ef418d16c12ee86fd1e7c7e45
JayShortway <29483617+JayShortway@users.noreply.github.com>
2025-03-19 16:03:31 +0100

    [Paywalls V2] Ignores missing font alias if it's blank. (#2271)

--- ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/components/LocalizedTextPartial.kt
+++ ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/components/LocalizedTextPartial.kt
@@ -10,6 +10,7 @@ import com.revenuecat.purchases.ui.revenuecatui.components.ktx.stringForAllLocal
 import com.revenuecat.purchases.ui.revenuecatui.components.properties.ColorStyles
 import com.revenuecat.purchases.ui.revenuecatui.components.properties.FontSpec
 import com.revenuecat.purchases.ui.revenuecatui.components.properties.getFontSpec
+import com.revenuecat.purchases.ui.revenuecatui.components.properties.recoverFromBlankFontAlias
 import com.revenuecat.purchases.ui.revenuecatui.components.properties.toColorStyles
 import com.revenuecat.purchases.ui.revenuecatui.errors.PaywallValidationError
 import com.revenuecat.purchases.ui.revenuecatui.helpers.NonEmptyList
@@ -51,6 +52,7 @@ internal class LocalizedTextPartial private constructor(
                 third = from.backgroundColor?.toColorStyles(aliases).orSuccessfullyNull(),
                 fourth = from.fontName
                     ?.let { fontAliases.getFontSpec(it) }
+                    ?.recoverFromBlankFontAlias()
                     .orSuccessfullyNull()
                     .mapError { nonEmptyListOf(it) },
             ) { texts, color, backgroundColor, fontSpec ->
