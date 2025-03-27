a4c1467cc5b6648e4b5e3c1991f490077909aa65
Toni Rico <antonio.rico.diez@revenuecat.com>
2025-03-27 10:19:17 +0100

    Improve error message

--- purchases/src/main/kotlin/com/revenuecat/purchases/NoCoreLibraryDesugaringException.kt
+++ purchases/src/main/kotlin/com/revenuecat/purchases/NoCoreLibraryDesugaringException.kt
@@ -2,7 +2,8 @@ package com.revenuecat.purchases
 
 // TODO: Add link
 class NoCoreLibraryDesugaringException(cause: Throwable) : RuntimeException(
-    "Error building BillingFlowParams for subscriptions " +
-        "due to an issue in Google's Billing Client library. Please check TODO-LINK to fix this issue.",
+    "Error building BillingFlowParams which is required to " +
+        "perform purchases in the Play store. This is due to an issue in Google's Billing Client library. " +
+        "Please check TODO-LINK for more info and to fix this issue.",
     cause,
 )
