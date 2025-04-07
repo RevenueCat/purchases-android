e0efa0115375df95d27ce69cb641e0f71857ef5d
JayShortway <29483617+JayShortway@users.noreply.github.com>
2025-04-07 14:06:22 +0200

    Adds EnumDeserializerWithDefault.

---
+++ purchases/src/main/kotlin/com/revenuecat/purchases/utils/serializers/EnumDeserializerWithDefault.kt
@@ -0,0 +1,30 @@
+package com.revenuecat.purchases.utils.serializers
+
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+
+/**
+ * Deserializer for enums with a default value.
+ */
+internal abstract class EnumDeserializerWithDefault<T : Enum<T>>(
+    private val valuesByType: Map<String, T>,
+    private val defaultValue: T,
+) : KSerializer<T> {
+    private val enumName = defaultValue.javaClass.simpleName
+
+    override val descriptor: SerialDescriptor =
+        PrimitiveSerialDescriptor(enumName, PrimitiveKind.STRING)
+
+    override fun deserialize(decoder: Decoder): T {
+        val key = decoder.decodeString()
+        return valuesByType[key] ?: defaultValue
+    }
+
+    override fun serialize(encoder: Encoder, value: T) {
+        throw NotImplementedError("Serialization is not implemented because it is not needed.")
+    }
+}
