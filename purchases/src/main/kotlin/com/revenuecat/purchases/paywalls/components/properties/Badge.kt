677326e037f4f3527874725c262257b1b88f2b58
JayShortway <29483617+JayShortway@users.noreply.github.com>
2025-04-07 14:07:54 +0200

    Adds a default value to Badge.Style.

--- purchases/src/main/kotlin/com/revenuecat/purchases/paywalls/components/properties/Badge.kt
+++ purchases/src/main/kotlin/com/revenuecat/purchases/paywalls/components/properties/Badge.kt
@@ -2,6 +2,7 @@ package com.revenuecat.purchases.paywalls.components.properties
 
 import com.revenuecat.purchases.InternalRevenueCatAPI
 import com.revenuecat.purchases.paywalls.components.StackComponent
+import com.revenuecat.purchases.utils.serializers.EnumDeserializerWithDefault
 import dev.drewhamilton.poko.Poko
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
@@ -16,7 +17,7 @@ class Badge(
 ) {
 
     @InternalRevenueCatAPI
-    @Serializable
+    @Serializable(with = BadgeStyleSerializer::class)
     enum class Style {
         @SerialName("overlay")
         Overlay,
@@ -28,3 +29,13 @@ class Badge(
         Nested,
     }
 }
+
+@InternalRevenueCatAPI
+private object BadgeStyleSerializer : EnumDeserializerWithDefault<Badge.Style>(
+    valuesByType = mapOf(
+        "overlay" to Badge.Style.Overlay,
+        "edge_to_edge" to Badge.Style.EdgeToEdge,
+        "nested" to Badge.Style.Nested,
+    ),
+    defaultValue = Badge.Style.Overlay,
+)
