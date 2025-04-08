5f085a2463bfe46d8f1319499592409c4db75078
skydoves <skydoves@naver.com>
2025-04-08 09:53:18 +0900

    Resolve detekt foramts

--- bom/build.gradle.kts
+++ bom/build.gradle.kts
@@ -1,16 +1,16 @@
 plugins {
-  kotlin("jvm")
+    kotlin("jvm")
 }
 
 if (!project.properties["ANDROID_VARIANT_TO_PUBLISH"].toString().contains("customEntitlementComputation")) {
-  apply(plugin = "com.vanniktech.maven.publish")
+    apply(plugin = "com.vanniktech.maven.publish")
 }
 
 dependencies {
-  constraints {
-    api(project(":purchases"))
-    api(project(":ui:revenuecatui"))
-    api(project(":ui:debugview"))
-    api(project(":feature:amazon"))
-  }
+    constraints {
+        api(project(":purchases"))
+        api(project(":ui:revenuecatui"))
+        api(project(":ui:debugview"))
+        api(project(":feature:amazon"))
+    }
 }
