c18f44a8d31c6b725a83fdd1622ec7e8c8546771
skydoves <skydoves@naver.com>
2025-04-09 16:33:38 +0900

    Migrate root gradle file to KTS

---
+++ build.gradle.kts
@@ -0,0 +1,76 @@
+buildscript {
+    extra["compileVersion"] = 34
+    extra["minVersion"] = 21
+}
+
+plugins {
+    alias(libs.plugins.mavenPublish) apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.dokka)
+    alias(libs.plugins.kover) apply false
+    alias(libs.plugins.detekt)
+    alias(libs.plugins.dependencyGraph)
+    alias(libs.plugins.emerge) apply false
+    alias(libs.plugins.poko) apply false
+    //    Removing from here gives an error
+    //    The request for this plugin could not be satisfied because the plugin is already on the classpath with an
+    //    unknown version, so compatibility cannot be checked.
+    alias(libs.plugins.kotlin.parcelize) apply false
+}
+
+dependencies {
+    detektPlugins(libs.detekt.formatting)
+    detektPlugins(libs.detekt.compose)
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
+
+tasks.register<io.gitlab.arturbosch.detekt.Detekt>("detektAll") {
+    description = "Runs over the whole codebase without the startup overhead for each module."
+    buildUponDefaultConfig = true
+    autoCorrect = true
+    parallel = true
+    setSource(files(rootDir))
+    include("**/*.kt", "**/*.kts")
+    exclude(
+        "**/build/**",
+        "**/test/**/*.kt",
+        "**/testDefaults/**/*.kt",
+        "**/testCustomEntitlementComputation/**/*.kt",
+    )
+    config.setFrom(files("$rootDir/config/detekt/detekt.yml"))
+    baseline.set(file("$rootDir/config/detekt/detekt-baseline.xml"))
+    reports {
+        xml.required.set(true)
+        xml.outputLocation.set(file("build/reports/detekt/detekt.xml"))
+        html.required.set(false)
+        txt.required.set(false)
+    }
+}
+
+tasks.register<io.gitlab.arturbosch.detekt.DetektCreateBaselineTask>("detektAllBaseline") {
+    description = "Overrides the current top-level baseline with issues found during this run."
+    buildUponDefaultConfig.set(true)
+    ignoreFailures.set(true)
+    parallel.set(true)
+    setSource(files(rootDir))
+    config.setFrom(files("$rootDir/config/detekt/detekt.yml"))
+    baseline.set(file("$rootDir/config/detekt/detekt-baseline.xml"))
+    include("**/*.kt", "**/*.kts")
+    exclude(
+        "**/build/**",
+        "**/test/**/*.kt",
+        "**/testDefaults/**/*.kt",
+        "**/testCustomEntitlementComputation/**/*.kt",
+    )
+}
+
+tasks.named<org.jetbrains.dokka.gradle.DokkaMultiModuleTask>("dokkaHtmlMultiModule") {
+    outputDirectory.set(file("docs/${project.property("VERSION_NAME")}"))
+    includes.from("README.md")
+}
