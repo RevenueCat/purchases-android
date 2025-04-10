b0c194404908859a86141fc41b66fc8626698546
JayShortway <29483617+JayShortway@users.noreply.github.com>
2025-04-10 17:22:08 +0200

    Adds some fastlane lanes.

--- ui/revenuecatui/src/testDebug/kotlin/com/revenuecat/purchases/ui/revenuecatui/snapshottests/PaywallComponentsTemplatePreviewRecorder.kt
+++ ui/revenuecatui/src/testDebug/kotlin/com/revenuecat/purchases/ui/revenuecatui/snapshottests/PaywallComponentsTemplatePreviewRecorder.kt
@@ -38,8 +38,8 @@ class PaywallComponentsTemplatePreviewRecorder(
 
     companion object {
         @JvmStatic
-        // Placing the offering ID in double square brackets so we can easily parse it later.
-        @Parameters(name = "[[{0}]]")
+        // Placing the offering ID in inverted double diamond brackets so we can easily parse it later.
+        @Parameters(name = ">>{0}<<")
         fun data(): List<Array<Any>> {
             // The OfferingProvider uses an OfferingParser under the hood, which logs.
             // We have to replace the log handler, as the default one uses android.util.Log, which gives an
