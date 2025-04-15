2ef7d5d6f9a6392956ec088008859966c5a5a2b5
skydoves <skydoves@naver.com>
2025-04-15 14:25:54 +0900

    Add Purchases Coroutine extensions with receivers

--- purchases/src/main/kotlin/com/revenuecat/purchases/CoroutinesExtensionsCommon.kt
+++ purchases/src/main/kotlin/com/revenuecat/purchases/CoroutinesExtensionsCommon.kt
@@ -31,6 +31,32 @@ suspend fun Purchases.awaitOfferings(): Offerings {
 }
 
 /**
+ * Fetch the configured offerings for this users. Offerings allows you to configure your in-app
+ * products via RevenueCat and greatly simplifies management. See
+ * [the guide](https://docs.revenuecat.com/offerings) for more info.
+ *
+ * Offerings will be fetched and cached on instantiation so that, by the time they are needed,
+ * your prices are loaded for your purchase flow. Time is money.
+ *
+ * Coroutine friendly version of [Purchases.getOfferings].
+ *
+ * @throws [PurchasesException] with a [PurchasesError] if there's an error retrieving the offerings.
+ * @return The [Offerings] available to this user.
+ */
+@JvmSynthetic
+suspend fun Purchases.awaitOfferings(
+    onSuccess: (Offerings) -> Unit,
+    onError: (PurchasesException) -> Unit,
+) {
+    try {
+        val offering = awaitOfferings()
+        onSuccess.invoke(offering)
+    } catch (e: PurchasesException) {
+        onError.invoke(e)
+    }
+}
+
+/**
  * Initiate a purchase with the given [PurchaseParams].
  * Initialized with an [Activity] either a [Package], [StoreProduct], or [SubscriptionOption].
  *
@@ -65,6 +91,36 @@ suspend fun Purchases.awaitPurchase(purchaseParams: PurchaseParams): PurchaseRes
 }
 
 /**
+ * Initiate a purchase with the given [PurchaseParams].
+ * Initialized with an [Activity] either a [Package], [StoreProduct], or [SubscriptionOption].
+ *
+ * If a [Package] or [StoreProduct] is used to build the [PurchaseParams], the [StoreProduct.defaultOption] will
+ * be purchased.
+ * [StoreProduct.defaultOption] is selected via the following logic:
+ *   - Filters out offers with "rc-ignore-offer" and "rc-customer-center" tag
+ *   - Uses [SubscriptionOption] with the longest free trial or cheapest first phase
+ *   - Falls back to use base plan
+ *
+ * @params [purchaseParams] The parameters configuring the purchase. See [PurchaseParams.Builder] for options.
+ * @throws [PurchasesTransactionException] with a [PurchasesTransactionException] if there's an error when purchasing
+ * and a userCancelled boolean that indicates if the user cancelled the purchase flow.
+ * @return The [StoreTransaction] for this purchase and the updated [CustomerInfo] for this user.
+ */
+@JvmSynthetic
+suspend fun Purchases.awaitPurchase(
+    purchaseParams: PurchaseParams,
+    onSuccess: (PurchaseResult) -> Unit,
+    onError: (PurchasesTransactionException) -> Unit,
+) {
+    try {
+        val purchaseResult = awaitPurchase(purchaseParams)
+        onSuccess.invoke(purchaseResult)
+    } catch (e: PurchasesTransactionException) {
+        onError.invoke(e)
+    }
+}
+
+/**
  * Gets the StoreProduct(s) for the given list of product ids of type [type], or for all types if no type is specified.
  *
  * Coroutine friendly version of [Purchases.getProducts].
@@ -95,6 +151,33 @@ suspend fun Purchases.awaitGetProducts(
 }
 
 /**
+ * Gets the StoreProduct(s) for the given list of product ids of type [type], or for all types if no type is specified.
+ *
+ * Coroutine friendly version of [Purchases.getProducts].
+ *
+ * @param [productIds] List of productIds
+ * @param [type] A product type to filter by
+ *
+ * @throws [PurchasesException] with a [PurchasesError] if there's an error retrieving the products.
+ * @return A list of [StoreProduct] with the products that have been able to be fetched from the store successfully.
+ * Not found products will be ignored.
+ */
+@JvmSynthetic
+suspend fun Purchases.awaitGetProducts(
+    productIds: List<String>,
+    type: ProductType? = null,
+    onSuccess: (List<StoreProduct>) -> Unit,
+    onError: (PurchasesException) -> Unit,
+) {
+    try {
+        val products = awaitGetProducts(productIds, type)
+        onSuccess.invoke(products)
+    } catch (e: PurchasesException) {
+        onError.invoke(e)
+    }
+}
+
+/**
  * Restores purchases made with the current Play Store account for the current user.
  * This method will post all purchases associated with the current Play Store account to
  * RevenueCat and become associated with the current `appUserID`. If the receipt token is being
@@ -118,5 +201,34 @@ suspend fun Purchases.awaitRestore(): CustomerInfo {
             onSuccess = { continuation.resume(it) },
             onError = { continuation.resumeWithException(PurchasesException(it)) },
         )
+    }
+}
+
+/**
+ * Restores purchases made with the current Play Store account for the current user.
+ * This method will post all purchases associated with the current Play Store account to
+ * RevenueCat and become associated with the current `appUserID`. If the receipt token is being
+ * used by an existing user, the current `appUserID` will be aliased together with the
+ * `appUserID` of the existing user. Going forward, either `appUserID` will be able to reference
+ * the same user.
+ *
+ * You shouldn't use this method if you have your own account system. In that case
+ * "restoration" is provided by your app passing the same `appUserId` used to purchase originally.
+ *
+ * Coroutine friendly version of [Purchases.restorePurchases].
+ *
+ * @throws [PurchasesException] with a [PurchasesError] if there's an error login out the user.
+ * @return The [CustomerInfo] with the restored purchases.
+ */
+@JvmSynthetic
+suspend fun Purchases.awaitRestore(
+    onSuccess: (CustomerInfo) -> Unit,
+    onError: (PurchasesException) -> Unit,
+) {
+    try {
+        val customerInfo = awaitRestore()
+        onSuccess.invoke(customerInfo)
+    } catch (e: PurchasesException) {
+        onError.invoke(e)
     }
 }
