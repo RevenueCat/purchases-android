a171a012341089cc58f3b68245deac5a2d4ad9f0
Jaewoong Eum <skydoves@naver.com>
2025-04-30 08:48:59 +0900

    Add GitHub pre-commit hooks rule for checking the detekt and metalava (#2378)
    ### Motivation
    
    Add a GitHub pre-commit hook to optionally run Detekt and Metalava
    checks before each commit. While not mandatory, it can be a convenient
    way to catch issues early. To use it, simply copy the provided
    `pre-commit` file into your `.git/hooks` directory and that's all.
    
    If you face the following error: `The '.git/hooks/pre-commit' hook was
    ignored because it's not set as executable`
    
    You can resolve it by running the following command: `chmod ug+x
    .git/hooks/*`

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 echo "Running detekt check..."
 OUTPUT="/tmp/detekt-$(date +%s)"
 ./gradlew detektAll > $OUTPUT
@@ -13,3 +14,27 @@ if [ $EXIT_CODE -ne 0 ]; then
   exit $EXIT_CODE
 fi
 rm $OUTPUT
+
+echo "Running metalava (defaults) check..."
+./gradlew metalavaCheckCompatibilityDefaultsRelease -q
+EXIT_CODE=$?
+if [ $EXIT_CODE -ne 0 ]; then
+  echo "❌ metalava for defaults flavor failed, running it for you..."
+
+  ./gradlew metalavaGenerateSignatureDefaultsRelease -q
+
+  echo "API dump done. Please check the updated API dump and add it to your commit.
+  exit $EXIT_CODE
+fi
+
+echo "Running metalava (custom entitlement) check..."
+./gradlew metalavaCheckCompatibilityCustomEntitlementComputationRelease -q
+EXIT_CODE=$?
+if [ $EXIT_CODE -ne 0 ]; then
+  echo "❌ metalava for custom entitlement flavor failed, running it for you..."
+
+  ./gradlew metalavaGenerateSignatureCustomEntitlementComputationRelease -q
+
+  echo "API dump done. Please check the updated API dump and add it to your commit.
+  exit $EXIT_CODE
+fi
