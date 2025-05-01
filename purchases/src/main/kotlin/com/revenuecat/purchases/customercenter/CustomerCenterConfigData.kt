bcba4bb884e772d510dce2aa254ed1c5bfa4263d
Cesar de la Vega <cesarvegaro@gmail.com>
2025-05-01 19:08:22 +0200

    fix tests

--- purchases/src/main/kotlin/com/revenuecat/purchases/customercenter/CustomerCenterConfigData.kt
+++ purchases/src/main/kotlin/com/revenuecat/purchases/customercenter/CustomerCenterConfigData.kt
@@ -328,7 +328,31 @@ data class CustomerCenterConfigData(
                 class CrossProductPromotion(
                     @SerialName("store_offer_identifier") val storeOfferIdentifier: String,
                     @SerialName("target_product_id") val targetProductId: String,
-                )
+                ) {
+                    override fun equals(other: Any?): Boolean {
+                        if (this === other) return true
+                        if (javaClass != other?.javaClass) return false
+
+                        other as CrossProductPromotion
+
+                        if (storeOfferIdentifier != other.storeOfferIdentifier) return false
+                        if (targetProductId != other.targetProductId) return false
+
+                        return true
+                    }
+
+                    override fun hashCode(): Int {
+                        var result = storeOfferIdentifier.hashCode()
+                        result = 31 * result + targetProductId.hashCode()
+                        return result
+                    }
+
+                    override fun toString(): String {
+                        return "CrossProductPromotion(" +
+                            "storeOfferIdentifier='$storeOfferIdentifier'," +
+                            "targetProductId='$targetProductId')"
+                    }
+                }
             }
 
             @Serializable
