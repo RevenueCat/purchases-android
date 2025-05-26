95c02b67573ade5a65e722b5864840cf7d4b1f2c
JayShortway <29483617+JayShortway@users.noreply.github.com>
2025-05-26 22:01:28 +0200

    Sets jvmTarget to 17 for :dokka-hide-internal.

--- dokka-hide-internal/build.gradle.kts
+++ dokka-hide-internal/build.gradle.kts
@@ -1,5 +1,13 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     alias(libs.plugins.kotlin.jvm)
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
 
 dependencies {
