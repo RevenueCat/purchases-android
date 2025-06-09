dfeb37eee10639f0bbe3180e5ba5c87af04a400c
Cesar de la Vega <664544+vegaro@users.noreply.github.com>
2025-06-09 17:22:39 +0200

    fix colors

--- examples/paywall-tester/src/main/java/com/revenuecat/paywallstester/ui/theme/Theme.kt
+++ examples/paywall-tester/src/main/java/com/revenuecat/paywallstester/ui/theme/Theme.kt
@@ -10,8 +10,10 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val LightColorScheme = lightColorScheme(
     primary = deep_purple_primary,
@@ -52,6 +54,15 @@ fun PaywallTesterAndroidTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
+            // Set both status bar and navigation bar colors
+            window.statusBarColor = colorScheme.background.toArgb()
+            window.navigationBarColor = colorScheme.background.toArgb()
+
+            // Configure system bars' content color
+            WindowCompat.getInsetsController(window, view).apply {
+                isAppearanceLightStatusBars = !darkTheme
+                isAppearanceLightNavigationBars = !darkTheme
+            }
         }
     }
 
