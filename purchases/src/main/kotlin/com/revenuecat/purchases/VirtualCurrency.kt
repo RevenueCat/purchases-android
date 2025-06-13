990c6804a9eb1dc7e816ff4f3c5e01ab7305e175
Will Taylor <wtaylor151@gmail.com>
2025-06-13 10:08:03 -0500

    add additional fields to VirtualCurrency

--- purchases/src/main/kotlin/com/revenuecat/purchases/VirtualCurrency.kt
+++ purchases/src/main/kotlin/com/revenuecat/purchases/VirtualCurrency.kt
@@ -1,6 +1,7 @@
 package com.revenuecat.purchases
 
 import android.os.Parcelable
+import com.revenuecat.purchases.utils.getNullableString
 import dev.drewhamilton.poko.Poko
 import kotlinx.parcelize.Parcelize
 import org.json.JSONObject
@@ -8,12 +9,18 @@ import org.json.JSONObject
 /**
  * A class representing information about a virtual currency in the app.
  *
- * @property balance The current balance of the virtual currency.
+ * @property balance The customer's current balance of the virtual currency.
+ * @property name The virtual currency's name defined in the RevenueCat dashboard.
+ * @property code The virtual currency's code defined in the RevenueCat dashboard.
+ * @property serverDescription The virtual currency description defined in the RevenueCat dashboard.
  */
 @Poko
 @Parcelize
 class VirtualCurrency internal constructor(
     val balance: Int,
+    val name: String,
+    val code: String,
+    val serverDescription: String?
 ) : Parcelable {
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
@@ -30,7 +37,15 @@ class VirtualCurrency internal constructor(
     companion object {
         internal fun fromJson(json: JSONObject): VirtualCurrency {
             val balance = json.getInt("balance")
-            return VirtualCurrency(balance)
+            val name = json.getString("name")
+            val code = json.getString("code")
+            val serverDescription = json.getNullableString("description")
+            return VirtualCurrency(
+                balance = balance,
+                name = name,
+                code = code,
+                serverDescription = serverDescription
+            )
         }
     }
 }
