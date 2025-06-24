3a969f462fa1d8d7eb17e1906b995ed8a3758e97
Antonio Pallares <antonio.pallares@revenuecat.com>
2025-06-24 16:53:23 +0200

    compare `hashCode()` of `PaywallOptions`

--- ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/data/PaywallViewModel.kt
+++ ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/data/PaywallViewModel.kt
@@ -110,7 +110,7 @@ internal class PaywallViewModelImpl(
     }
 
     fun updateOptions(options: PaywallOptions) {
-        val needsUpdateState = this.options != options
+        val needsUpdateState = this.options.hashCode() != options.hashCode()
         // Some properties not considered for equality (hashCode) may have changed
         // (e.g. the listener may change in some re-renderers)
         this.options = options
