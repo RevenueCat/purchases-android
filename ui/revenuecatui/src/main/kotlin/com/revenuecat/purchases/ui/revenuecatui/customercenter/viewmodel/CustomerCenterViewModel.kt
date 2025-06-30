d3be430e2ea59290c966838cf65492c87daa98fc
Cesar de la Vega <664544+vegaro@users.noreply.github.com>
2025-06-30 21:15:04 +0200

    Issue when loading promotional offers when products are missing base plans (#2471)
    There's an issue when loading promotional offers for target products
    that miss a base plan
    
    For example this configuration is valid if the product is old and
    configured without base plans in the dashboard:
    
    <img width="728" alt="Screenshot 2025-06-27 at 12 05 36"
    src="https://github.com/user-attachments/assets/4d121b68-4214-478c-ad2d-05621ebfc810"
    />
    
    The SDK will fail to load the promotional becasue it looks for the
    `product.id`, which in that case would look like `annual_freetrial:p1y`.
    On the other hand `product.googleProduct?.productId` has only the
    product id `annual_freetrial` so we should be looking for that instead
    if we can't find a promotional matching the base plan.
    
    There was also another issue when loading the target product, since we
    were picking the first one returned, which is not ideal. For example, if
    the product "subscription" had "monthly" and "annual" base plans. And
    the target product was just "subscription", `Purchases.getProducts`
    without an specific base plan returns two products, one for each base
    plan. Which could load an unexpected product. I changed the logic to
    load the base plan that's equivalent to the currently active
    subscription.

--- ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/customercenter/viewmodel/CustomerCenterViewModel.kt
+++ ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/customercenter/viewmodel/CustomerCenterViewModel.kt
@@ -34,6 +34,7 @@ import com.revenuecat.purchases.models.GoogleSubscriptionOption
 import com.revenuecat.purchases.models.StoreProduct
 import com.revenuecat.purchases.models.SubscriptionOption
 import com.revenuecat.purchases.models.Transaction
+import com.revenuecat.purchases.models.googleProduct
 import com.revenuecat.purchases.ui.revenuecatui.customercenter.data.CustomerCenterState
 import com.revenuecat.purchases.ui.revenuecatui.customercenter.data.FeedbackSurveyData
 import com.revenuecat.purchases.ui.revenuecatui.customercenter.data.PromotionalOfferData
@@ -709,8 +710,13 @@ internal class CustomerCenterViewModelImpl(
         promotionalOffer: CustomerCenterConfigData.HelpPath.PathDetail.PromotionalOffer,
         product: StoreProduct,
     ): SubscriptionOption? {
+        val googleProduct = product.googleProduct
         val crossProductPromotion: CrossProductPromotion? =
             promotionalOffer.crossProductPromotions[product.id]
+                // Check for cross-product promotions first (product.id includes base plan ex: "sub:p1m")
+                // But it's possible the product is not configured with base plan in RevenueCat
+                // so we also check for the Google product ID (which does not include base plan ex: "sub")
+                ?: googleProduct?.let { promotionalOffer.crossProductPromotions[it.productId] }
                 ?: promotionalOffer.productMapping[product.id]?.let {
                     CrossProductPromotion(
                         storeOfferIdentifier = it,
@@ -725,10 +731,15 @@ internal class CustomerCenterViewModelImpl(
             return null
         }
 
-        val targetProduct: StoreProduct? = if (crossProductPromotion.targetProductId == product.id) {
-            product
-        } else {
-            findTargetProduct(crossProductPromotion.targetProductId)
+        val targetProduct: StoreProduct? = when {
+            crossProductPromotion.targetProductId == product.id -> product
+            googleProduct?.basePlanId != null ->
+                // Passing original product's base plan ID to find the target product
+                // in case the target product is missing the base plan ID in the dashboard
+                // which is common for old products. Purchases.getProducts would return all products
+                // with the same product ID but different base plan IDs. That way we can find the most relevant product.
+                findTargetProduct(crossProductPromotion.targetProductId, googleProduct.basePlanId!!)
+            else -> null
         }
 
         if (targetProduct == null) {
@@ -747,10 +758,11 @@ internal class CustomerCenterViewModelImpl(
 
     private suspend fun findTargetProduct(
         targetProductId: String,
+        sourceBasePlan: String,
     ): StoreProduct? {
         val splitProduct = targetProductId.split(":")
         val productId = splitProduct.first()
-        val basePlan = splitProduct.getOrNull(1)
+        val basePlan = splitProduct.getOrNull(1) ?: sourceBasePlan
         val targetProduct = purchases.awaitGetProduct(productId, basePlan)
         return targetProduct
     }
