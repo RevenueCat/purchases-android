1212a2f190dec072408f8cf0b144f1311ec8cf3e
JayShortway <29483617+JayShortway@users.noreply.github.com>
2025-07-02 12:24:11 +0200

    Only publishes the bom if our Kotlin version is at least 1.9.20.

--- bom/build.gradle.kts
+++ bom/build.gradle.kts
@@ -1,18 +1,21 @@
-import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
+import org.jetbrains.kotlin.gradle.plugin.KotlinBasePluginWrapper
+import java.lang.Runtime.Version
 
 plugins {
-    kotlin("jvm")
+    alias(libs.plugins.kotlin.jvm)
 }
 
-if (!project.properties["ANDROID_VARIANT_TO_PUBLISH"].toString().contains("customEntitlementComputation")) {
+private val kotlinPlugin = plugins
+    .withType<KotlinBasePluginWrapper>()
+    .firstOrNull()
+    ?: error("Kotlin plugin is not applied")
+
+if (
+    !project.properties["ANDROID_VARIANT_TO_PUBLISH"].toString().contains("customEntitlementComputation") &&
+    // This will ensure the bom automatically gets published once we update to Kotlin 1.9.20.
+    Version.parse(kotlinPlugin.pluginVersion) >= Version.parse("1.9.20")
+) {
     apply(plugin = "com.vanniktech.maven.publish")
-}
-
-kotlin {
-    compilerOptions {
-        languageVersion = KotlinVersion.fromVersion(libs.versions.kotlinLanguage.get())
-        apiVersion = KotlinVersion.fromVersion(libs.versions.kotlinApi.get())
-    }
 }
 
 dependencies {
