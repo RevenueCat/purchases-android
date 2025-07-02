4be2ce0afbb2a84cb88ac6b7faaebdb9851138d5
Will Taylor <wtaylor151@gmail.com>
2025-07-02 13:03:30 -0500

    add more VirtualCurrencyManager tests

--- purchases/src/test/java/com/revenuecat/purchases/virtualcurrencies/VirtualCurrencyManagerTest.kt
+++ purchases/src/test/java/com/revenuecat/purchases/virtualcurrencies/VirtualCurrencyManagerTest.kt
@@ -4,9 +4,11 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.revenuecat.purchases.common.AppConfig
 import com.revenuecat.purchases.common.caching.DeviceCache
 import com.revenuecat.purchases.identity.IdentityManager
+import com.revenuecat.purchases.interfaces.GetVirtualCurrenciesCallback
 import com.revenuecat.purchases.utils.Responses
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verify
 import org.assertj.core.api.Assertions.assertThat
 import org.json.JSONObject
 import org.junit.Test
@@ -19,6 +21,133 @@ class VirtualCurrencyManagerTest {
         JSONObject(Responses.validFullVirtualCurrenciesResponse)
     )
 
+    // region virtualCurrencies
+    @Test
+    fun `virtualCurrencies returns cached VirtualCurrencies when cache is not stale`() {
+        val appUserID = "appUserID"
+
+        val mockDeviceCache = mockk<DeviceCache>()
+        every {
+            mockDeviceCache.getCachedVirtualCurrencies(any())
+        } returns this.virtualCurrencies
+        every {
+            mockDeviceCache.isVirtualCurrenciesCacheStale(any(), any())
+        } returns false
+
+        val mockIdentityManager = mockk<IdentityManager>()
+        every {
+            mockIdentityManager.currentAppUserID
+        } returns appUserID
+
+        val mockAppConfig = mockk<AppConfig>()
+        every {
+            mockAppConfig.isAppBackgrounded
+        } returns false
+
+        val mockCallback = mockk<GetVirtualCurrenciesCallback>()
+        every {
+            mockCallback.onReceived(any())
+        } returns Unit
+
+        val virtualCurrencyManager = VirtualCurrencyManager(
+            identityManager = mockIdentityManager,
+            deviceCache = mockDeviceCache,
+            backend = mockk(),
+            appConfig = mockAppConfig
+        )
+
+        virtualCurrencyManager.virtualCurrencies(mockCallback)
+
+        verify {
+            mockDeviceCache.getCachedVirtualCurrencies(appUserID = appUserID)
+            mockDeviceCache.isVirtualCurrenciesCacheStale(appUserID = appUserID, appInBackground = false)
+            mockCallback.onReceived(this@VirtualCurrencyManagerTest.virtualCurrencies)
+        }
+    }
+
+    @Suppress("ForbiddenComment")
+    @Test
+    fun `virtualCurrencies fetches VirtualCurrencies from network when cache is stale`() {
+        val appUserID = "appUserID"
+
+        val mockDeviceCache = mockk<DeviceCache>()
+        every {
+            mockDeviceCache.getCachedVirtualCurrencies(any())
+        } returns this.virtualCurrencies
+        every {
+            mockDeviceCache.isVirtualCurrenciesCacheStale(any(), any())
+        } returns true
+
+        val mockIdentityManager = mockk<IdentityManager>()
+        every {
+            mockIdentityManager.currentAppUserID
+        } returns appUserID
+
+        val mockAppConfig = mockk<AppConfig>()
+        every {
+            mockAppConfig.isAppBackgrounded
+        } returns false
+
+        val mockCallback = mockk<GetVirtualCurrenciesCallback>()
+        every {
+            mockCallback.onReceived(any())
+        } returns Unit
+
+        val virtualCurrencyManager = VirtualCurrencyManager(
+            identityManager = mockIdentityManager,
+            deviceCache = mockDeviceCache,
+            backend = mockk(),
+            appConfig = mockAppConfig
+        )
+
+        virtualCurrencyManager.virtualCurrencies(mockCallback)
+
+        verify(exactly = 1) {
+            mockDeviceCache.isVirtualCurrenciesCacheStale(appUserID = appUserID, appInBackground = false)
+            // TODO: Verify that a network call was made
+
+            // TODO: Verify that the returned VCs match the ones returned from the network call
+//            mockCallback.onReceived(this@VirtualCurrencyManagerTest.virtualCurrencies)
+        }
+        
+        verify(exactly = 0) {
+            mockDeviceCache.getCachedVirtualCurrencies(any())
+        }
+    }
+
+    // endregion
+
+    // region invalidateVirtualCurrenciesCache
+    @Test
+    fun `invalidateVirtualCurrenciesCache clears the virtual currencies cache`() {
+        val appUserID = "appUserID"
+
+        val mockDeviceCache = mockk<DeviceCache>()
+        every {
+            mockDeviceCache.clearVirtualCurrenciesCache(any())
+        } returns Unit
+
+        val mockIdentityManager = mockk<IdentityManager>()
+        every {
+            mockIdentityManager.currentAppUserID
+        } returns appUserID
+
+        val virtualCurrencyManager = VirtualCurrencyManager(
+            identityManager = mockIdentityManager,
+            deviceCache = mockDeviceCache,
+            backend = mockk(),
+            appConfig = mockk()
+        )
+
+        virtualCurrencyManager.invalidateVirtualCurrenciesCache()
+
+        verify(exactly = 1) {
+            mockDeviceCache.clearVirtualCurrenciesCache(appUserID = appUserID)
+        }
+    }
+    // endregion
+
+    // region cachedVirtualCurrencies
     @Test
     fun `cachedVirtualCurrencies returns cached VirtualCurrencies when present`() {
         val appUserID = "appUserID"
@@ -76,4 +205,5 @@ class VirtualCurrencyManagerTest {
 
         assertThat(virtualCurrencyManager.cachedVirtualCurrencies()).isNull()
     }
+    // endregion
 }
