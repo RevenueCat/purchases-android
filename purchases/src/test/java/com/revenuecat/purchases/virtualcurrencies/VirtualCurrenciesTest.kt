f13837425813880424dfdd524606086c2894b237
Will Taylor <wtaylor151@gmail.com>
2025-07-08 10:17:59 -0500

    add equality tests

--- purchases/src/test/java/com/revenuecat/purchases/virtualcurrencies/VirtualCurrenciesTest.kt
+++ purchases/src/test/java/com/revenuecat/purchases/virtualcurrencies/VirtualCurrenciesTest.kt
@@ -3,14 +3,23 @@ package com.revenuecat.purchases.virtualcurrencies
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.json.JSONObject
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
 import org.junit.Assert.assertNull
+import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class VirtualCurrenciesTest {
 
-    // ----- Subscript Tests -----
+    private val virtualCurrency = VirtualCurrency(
+        name = "Test VC",
+        balance = 100,
+        code = "TEST",
+        serverDescription = "hello world"
+    )
+
+    // region Subscript Tests
     @Test
     fun `subscript returns correct virtual currency`() {
         val code = "COIN"
@@ -43,4 +52,52 @@ class VirtualCurrenciesTest {
 
         assertNull(virtualCurrencies["NON_EXISTENT"])
     }
+    // endregion
+
+    // region Equality Tests
+    @Test
+    fun `equals is true for VirtualCurrencies objects with identical VCs`() {
+
+        val vcClone = VirtualCurrency(
+            name = "Test VC",
+            balance = 100,
+            code = "TEST",
+            serverDescription = "hello world"
+        )
+
+        val virtualCurrencies1 = VirtualCurrencies(
+            all = mapOf("TEST" to virtualCurrency)
+        )
+        val virtualCurrencies2 = VirtualCurrencies(
+            all = mapOf("TEST" to virtualCurrency)
+        )
+        val virtualCurrencies3 = VirtualCurrencies(
+            all = mapOf("TEST" to vcClone)
+        )
+
+        assertTrue(virtualCurrencies1 == virtualCurrencies2)
+        assertTrue(virtualCurrencies1 == virtualCurrencies3)
+    }
+
+    @Test
+    fun `equals is false for VirtualCurrencies objects with different VCs`() {
+
+        val differentVirtualCurrency = VirtualCurrency(
+            name = "Test VC 2",
+            balance = 200,
+            code = "TEST2",
+            serverDescription = "lorem ipsum"
+        )
+
+        val virtualCurrencies1 = VirtualCurrencies(
+            all = mapOf("TEST" to virtualCurrency)
+        )
+        val virtualCurrencies2 = VirtualCurrencies(
+            all = mapOf("TEST2" to differentVirtualCurrency)
+        )
+
+        assertFalse(virtualCurrencies1 == virtualCurrencies2)
+    }
+
+    // endregion
 }
