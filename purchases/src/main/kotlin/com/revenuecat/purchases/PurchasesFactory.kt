b6b5bda7b3a1ad91a0a32c669db8770a7da3a32c
Will Taylor <wtaylor151@gmail.com>
2025-07-16 07:10:55 -0500

    remove unnecessary formatting change

--- purchases/src/main/kotlin/com/revenuecat/purchases/PurchasesFactory.kt
+++ purchases/src/main/kotlin/com/revenuecat/purchases/PurchasesFactory.kt
@@ -355,9 +355,7 @@ internal class PurchasesFactory(
                 virtualCurrencyManager = virtualCurrencyManager,
             )
 
-            return Purchases(
-                purchasesOrchestrator = purchasesOrchestrator,
-            )
+            return Purchases(purchasesOrchestrator = purchasesOrchestrator)
         }
     }
 
