ccd05fbeb15e3f638e6cf919e0ade78db7363a15
Will Taylor <wtaylor151@gmail.com>
2025-07-22 10:22:02 -0500

    Update VC Caching Log Message (#2552)
    ### Description
    
    Android equivalent of
    https://github.com/RevenueCat/purchases-ios/pull/5404
    
    This PR updates the
    `VirtualCurrencyStrings.NO_CACHED_VIRTUAL_CURRENCIES` log message from
    `"No cached VirtualCurrencies, fetching from network."` to `"There are
    no cached VirtualCurrencies."`.
    
    The previous message stated that a network fetch would occur, which
    isn't always the case.
    
    There are two scenarios where this log may appear:
    - When fetching virtual currencies and the cache is not stale but
    contains no virtual currencies object (an edge case that shouldn't occur
    with normal usage).
    - When accessing the cache directly using
    `Purchases.cachedVirtualCurrencies` and it's empty - in this case, the
    SDK returns nil without initiating a network fetch.
    
    The updated message is more descriptive of what actually happens in the
    second scenario. In the first scenario, subsequent log messages state
    that the SDK is fetching VCs from the network.

--- purchases/src/main/kotlin/com/revenuecat/purchases/strings/VirtualCurrencyStrings.kt
+++ purchases/src/main/kotlin/com/revenuecat/purchases/strings/VirtualCurrencyStrings.kt
@@ -3,7 +3,7 @@ package com.revenuecat.purchases.strings
 internal object VirtualCurrencyStrings {
     const val INVALIDATING_VIRTUAL_CURRENCIES_CACHE = "Invalidating VirtualCurrencies cache."
     const val VENDING_FROM_CACHE = "Vending VirtualCurrencies from cache."
-    const val NO_CACHED_VIRTUAL_CURRENCIES = "No cached VirtualCurrencies, fetching from network."
+    const val NO_CACHED_VIRTUAL_CURRENCIES = "There are no cached VirtualCurrencies."
     const val VIRTUAL_CURRENCIES_STALE_UPDATING_FROM_NETWORK = "VirtualCurrencies cache is stale, updating from " +
         "network."
     const val VIRTUAL_CURRENCIES_UPDATED_FROM_NETWORK = "VirtualCurrencies updated from the network."
