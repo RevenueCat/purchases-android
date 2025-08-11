1fc470d4d26f80b2835e573d8e53721c6a223a90
Facundo Menzella <facumenzella@users.noreply.github.com>
2025-08-11 11:23:22 +0200

    Update examples/paywall-tester/src/main/java/com/revenuecat/paywallstester/Constants.kt

--- examples/paywall-tester/src/main/java/com/revenuecat/paywallstester/Constants.kt
+++ examples/paywall-tester/src/main/java/com/revenuecat/paywallstester/Constants.kt
@@ -3,7 +3,7 @@ package com.revenuecat.paywallstester
 object Constants {
     // PaywallsTester supports switching between 2 API keys on the App Info screen. It uses "A" by default.
     const val GOOGLE_API_KEY_A = "API_KEY_A"
-    const val GOOGLE_API_KEY_B = "goog_TzjuPFWcFelhzMSOGvKoZWvrdcW"
+    const val GOOGLE_API_KEY_B = "API_KEY_B"
 
     // PaywallsTester will use these labels in its UI to differentiate the API keys.
     const val GOOGLE_API_KEY_A_LABEL = "API_KEY_A_LABEL"
