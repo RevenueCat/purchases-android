8f02065ceeaab86deead042c0b714562c20033c4
Toni Rico <antonio.rico.diez@revenuecat.com>
2025-08-27 10:50:52 +0200

    Update web purchase redemption sample target/compile version

--- examples/web-purchase-redemption-sample/build.gradle.kts
+++ examples/web-purchase-redemption-sample/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.revenuecat.webpurchaseredemptionsample"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.revenuecat.webpurchaseredemptionsample"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
         missingDimensionStrategy("apis", "defaults")
