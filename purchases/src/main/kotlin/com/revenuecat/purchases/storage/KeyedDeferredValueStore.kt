aa900964c6db2ba498bf9ae95db24aefc2a21bf5
Jacob Rakidzich <Jacob@JacobZivanDesign.com>
2025-08-29 07:52:33 -0500

    reduce scope

--- purchases/src/main/kotlin/com/revenuecat/purchases/storage/KeyedDeferredValueStore.kt
+++ purchases/src/main/kotlin/com/revenuecat/purchases/storage/KeyedDeferredValueStore.kt
@@ -2,7 +2,7 @@ package com.revenuecat.purchases.storage
 
 import kotlinx.coroutines.Deferred
 
-open class KeyedDeferredValueStore<H, T>(
+internal class KeyedDeferredValueStore<H, T>(
     private val lock: Any = object {},
 ) {
     val deferred: MutableMap<H, Deferred<T>> = mutableMapOf()
@@ -11,7 +11,7 @@ open class KeyedDeferredValueStore<H, T>(
         deferred.get(key) ?: forgettingFailure(key, task).also { deferred.put(key, it) }
     }
 
-    fun forgettingFailure(key: H, task: () -> Deferred<T>): Deferred<T> =
+    private fun forgettingFailure(key: H, task: () -> Deferred<T>): Deferred<T> =
         task().apply {
             invokeOnCompletion { exception ->
                 exception?.run {
