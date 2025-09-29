6e797e49fdb300b7ca49cf5f3624e64498668af5
skydoves <skydoves@naver.com>
2025-09-29 14:04:39 +0900

    Add comment for the 64 KB page sizes

--- ui/revenuecatui/build.gradle.kts
+++ ui/revenuecatui/build.gradle.kts
@@ -52,7 +52,7 @@ android {
         externalNativeBuild {
             cmake {
                 cppFlags += "-std=c++17"
-                arguments += listOf("-DANDROID_SUPPORT_FLEXIBLE_PAGE_SIZES=ON")
+                arguments += listOf("-DANDROID_SUPPORT_FLEXIBLE_PAGE_SIZES=ON") // support 64 KB page sizes
             }
         }
     }
