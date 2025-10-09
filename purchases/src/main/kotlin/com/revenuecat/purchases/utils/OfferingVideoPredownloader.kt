81d90bb1ac0ecb10d928c3a612f99037e9f7423a
Jacob Rakidzich <Jacob@JacobZivanDesign.com>
2025-10-09 10:42:30 -0500

    Finalize video component to include a checksum, and optimize memory usage for large file downloads (#2717)
    <!-- Thank you for contributing to Purchases! Before pressing the
    "Create Pull Request" button, please provide the following: -->
    
    ### Checklist
    - [x] If applicable, unit tests
    - [ ] If applicable, create follow-up issues for `purchases-ios` and
    hybrids
    
    ### Motivation
    <!-- Why is this change required? What problem does it solve? -->
    <!-- Please link to issues following this format: Resolves #999999 -->
    
    After getting the last video component PR up, the team had some great
    feedback, specifically that there was no way to prevent the corruption
    of a file before saving it and that storing the whole file in memory
    could be problematic when dealing with larger files like videos. So this
    PR sought to solve those issues.
    
    Description
    
    This PR does several things:
    
    - Stores the file using the checksum value if present -- ensuring that
    we get a cache miss if the checksum value changes
    - Streams the download into a temporary file to optimize memory usage
    for very large downloads (as opposed to keeping the whole thing in
    memory)
    - Only saves the data to the cache directory if the checksum matches the
    dowloaded content
    - Stops the cache warming of the videos to save on costs - we will be
    adding a flag in our backend to denote which paywalls can prewarm videos
    in the future
    - Removes the static object responsible for state management and
    replaces it with a view model to survive rotation
    
    
    ### Description
    <!-- Describe your changes in detail -->
    <!-- Please describe in detail how you tested your changes -->

--- purchases/src/main/kotlin/com/revenuecat/purchases/utils/OfferingVideoPredownloader.kt
+++ purchases/src/main/kotlin/com/revenuecat/purchases/utils/OfferingVideoPredownloader.kt
@@ -7,9 +7,12 @@ import com.revenuecat.purchases.InternalRevenueCatAPI
 import com.revenuecat.purchases.Offering
 import com.revenuecat.purchases.common.canUsePaywallUI
 import com.revenuecat.purchases.common.checkIfVideoComponentIsEnabled
+import com.revenuecat.purchases.models.Checksum
 import com.revenuecat.purchases.paywalls.components.VideoComponent
+import com.revenuecat.purchases.paywalls.components.properties.ThemeVideoUrls
 import com.revenuecat.purchases.storage.DefaultFileRepository
 import com.revenuecat.purchases.storage.FileRepository
+import java.net.URL
 
 internal class OfferingVideoPredownloader(
     context: Context,
@@ -21,20 +24,23 @@ internal class OfferingVideoPredownloader(
 
     fun downloadVideos(offering: Offering) {
         if (shouldPredownload) {
-            // WIP: We will add a remote flag in the offering metadata that will indicate if we should download
-            // the high res videos or not. For now, we want to only download the low-res to ensure we don't rack up
-            // high cloudfront costs
+            // WIP: We will add a remote flag in the offering metadata that will indicate if we should
+            // pre-download videos or not. For now, we want to only download the low-res to ensure we
+            // don't rack up high cloudfront costs
             offering.paywallComponents?.data?.componentsConfig?.base?.stack
                 ?.filter { it is VideoComponent }
                 ?.forEach { component ->
                     if (component is VideoComponent) {
-                        val lowResVideos = setOfNotNull(
-                            component.source.light.urlLowRes,
-                            component.source.dark?.urlLowRes,
-                        )
-                        fileRepository.prefetch(lowResVideos.toList())
+                        fileRepository.prefetch(component.source.checkedUrls())
                     }
                 }
         }
     }
 }
+
+private fun ThemeVideoUrls.checkedUrls(): List<Pair<URL, Checksum?>> = listOfNotNull(
+    light.url to light.checksum,
+    dark?.url?.let { it to dark.checksum },
+    light.urlLowRes?.let { it to light.checksumLowRes },
+    dark?.urlLowRes?.let { it to dark.checksumLowRes },
+)
