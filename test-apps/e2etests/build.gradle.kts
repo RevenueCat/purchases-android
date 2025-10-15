465d7243bec54dd3f32168ee359e31be4d794d51
Cesar de la Vega <664544+vegaro@users.noreply.github.com>
2025-10-15 19:24:37 +0200

    add kotlinCompilerExtensionVersion to e2etests

--- test-apps/e2etests/build.gradle.kts
+++ test-apps/e2etests/build.gradle.kts
@@ -26,6 +26,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.8"
+    }
     kotlinOptions {
         jvmTarget = "11"
     }
