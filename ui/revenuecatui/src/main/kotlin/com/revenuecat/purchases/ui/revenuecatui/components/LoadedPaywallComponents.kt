4a23382eee6ee756f077247ae5a047cd37f7b464
Jacob Rakidzich <Jacob@JacobZivanDesign.com>
2025-10-31 13:29:04 -0500

    detekt

--- ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/components/LoadedPaywallComponents.kt
+++ ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/components/LoadedPaywallComponents.kt
@@ -3,23 +3,15 @@
 package com.revenuecat.purchases.ui.revenuecatui.components
 
 import android.content.res.Configuration
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.currentCompositionLocalContext
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -521,14 +513,14 @@ private fun previewBottomSheet(
 
 private const val MILLIS_2025_01_25 = 1737763200000
 
-
+@Suppress("ModifierNotUsedAtRoot")
 @Composable
 internal fun ViewWithVideoBackground(
     state: PaywallState.Loaded.Components,
     background: BackgroundStyle?,
     modifier: Modifier = Modifier,
     shape: androidx.compose.ui.graphics.Shape = RectangleShape,
-    content: @Composable BoxScope.() -> Unit
+    content: @Composable BoxScope.() -> Unit,
 ) {
     Box {
         when (val background = background) {
@@ -576,7 +568,7 @@ internal fun WithOptionalVideoBackground(
     background: BackgroundStyle?,
     modifier: Modifier = Modifier,
     shape: androidx.compose.ui.graphics.Shape = RectangleShape,
-    content: @Composable () -> Unit
+    content: @Composable () -> Unit,
 ) {
     if (background is BackgroundStyle.Video) {
         ViewWithVideoBackground(
