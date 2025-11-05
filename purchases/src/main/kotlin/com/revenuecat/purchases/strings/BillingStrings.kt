1f0bc47aad2ed59faeb7a6713a5a97d55d8ac404
Toni Rico <toni.rico.diez@revenuecat.com>
2025-11-05 09:08:11 +0100

    Update purchases/src/main/kotlin/com/revenuecat/purchases/strings/BillingStrings.kt
    Co-authored-by: JayShortway <29483617+JayShortway@users.noreply.github.com>

--- purchases/src/main/kotlin/com/revenuecat/purchases/strings/BillingStrings.kt
+++ purchases/src/main/kotlin/com/revenuecat/purchases/strings/BillingStrings.kt
@@ -23,7 +23,7 @@ internal object BillingStrings {
     const val ILLEGAL_STATE_EXCEPTION_WHEN_CONNECTING = "There was an IllegalStateException when connecting to " +
         "BillingClient. This has been reported to occur on Samsung devices on unknown circumstances.\nException: %s"
     const val SECURITY_EXCEPTION_WHEN_CONNECTING = "There was a SecurityException when connecting to " +
-        "BillingClient. This has been reported to occur on some devices on unknown circumstances. " +
+        "BillingClient. This has been reported to occur on some devices in unknown circumstances. " +
         "See issuetracker: https://issuetracker.google.com/issues/457463701\nException: %s"
     const val BILLING_CONNECTION_ERROR_INAPP_MESSAGES = "Error connecting to billing client to display " +
         "in-app messages: %s"
