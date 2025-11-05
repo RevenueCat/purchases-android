e4034d7da568b3bd337dd5ae3814865ba92913eb
Jacob Rakidzich <Jacob@JacobZivanDesign.com>
2025-11-05 04:54:11 -0600

    Update the paywall tester app to use this feature

--- examples/paywall-tester/src/main/java/com/revenuecat/paywallstester/PaywallViewActivity.kt
+++ examples/paywall-tester/src/main/java/com/revenuecat/paywallstester/PaywallViewActivity.kt
@@ -1,8 +1,10 @@
 package com.revenuecat.paywallstester
 
+import android.content.DialogInterface
 import android.os.Bundle
 import android.util.Log
 import android.view.WindowManager
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import com.revenuecat.paywallstester.databinding.ActivityPaywallViewBinding
@@ -10,6 +12,7 @@ import com.revenuecat.purchases.CustomerInfo
 import com.revenuecat.purchases.Package
 import com.revenuecat.purchases.models.StoreTransaction
 import com.revenuecat.purchases.ui.revenuecatui.PaywallListener
+import com.revenuecat.purchases.ui.revenuecatui.utils.Resumable
 
 class PaywallViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,6 +29,32 @@ class PaywallViewActivity : AppCompatActivity() {
         setContentView(binding.root)
         binding.paywallView.setOfferingId(offeringId)
         binding.paywallView.setPaywallListener(object : PaywallListener {
+            override fun onPurchasePackageInitiated(packageId: String, resume: Resumable) {
+                val builder: AlertDialog.Builder = AlertDialog.Builder(this@PaywallViewActivity)
+                builder.setTitle("On Purchase Initiated Hook")
+                builder.setMessage(
+                    "Example of presenting other UI over the paywall. Do you want to proceed with the purchase?"
+                )
+                builder.setPositiveButton(
+                    "Proceed",
+                    object : DialogInterface.OnClickListener {
+                        override fun onClick(dialog: DialogInterface?, which: Int) {
+                            resume()
+                        }
+                    },
+                )
+                builder.setNegativeButton(
+                    "Cancel",
+                ) { dialog, which ->
+                    dialog.dismiss() // Dismiss the dialog
+                    resume(false)
+                }
+                builder.setCancelable(false) // Prevents dialog from being dismissed by tapping outside or back button
+
+                val dialog: AlertDialog = builder.create()
+                dialog.show()
+            }
+
             override fun onPurchaseStarted(rcPackage: Package) {
                 super.onPurchaseStarted(rcPackage)
                 Log.d("PaywallsTester", "onPurchaseStarted")
