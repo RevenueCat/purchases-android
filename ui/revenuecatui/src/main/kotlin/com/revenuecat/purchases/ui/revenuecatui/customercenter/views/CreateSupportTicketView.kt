234460ca48192a808a8e20d1d286f1a574358d55
Rosie Watson <rosie.watson@revenuecat.com>
2025-11-05 14:12:19 +0000

    test: add unit tests

--- ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/customercenter/views/CreateSupportTicketView.kt
+++ ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/customercenter/views/CreateSupportTicketView.kt
@@ -21,6 +21,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
@@ -61,7 +62,9 @@ internal fun CreateSupportTicketView(
                     CustomerCenterConfigData.Localization.CommonLocalizedString.ENTER_EMAIL,
                 )) },
                 enabled = !isSubmitting && !wasSuccessfullySent,
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .testTag("email_field"),
                 singleLine = true,
                 keyboardOptions = KeyboardOptions(
                     keyboardType = KeyboardType.Email,
@@ -80,7 +83,8 @@ internal fun CreateSupportTicketView(
                 enabled = !isSubmitting && !wasSuccessfullySent,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(200.dp),
+                    .height(200.dp)
+                    .testTag("description_field"),
                 minLines = 6,
                 maxLines = 10,
             )
