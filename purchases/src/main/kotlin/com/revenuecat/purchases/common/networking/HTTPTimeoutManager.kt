c970a893925c0f7692514165e2921f7c73480de7
Toni Rico <antonio.rico.diez@revenuecat.com>
2025-11-11 15:46:22 +0100

    Fix detekt

--- purchases/src/main/kotlin/com/revenuecat/purchases/common/networking/HTTPTimeoutManager.kt
+++ purchases/src/main/kotlin/com/revenuecat/purchases/common/networking/HTTPTimeoutManager.kt
@@ -15,7 +15,7 @@ internal class HTTPTimeoutManager(
     private val dateProvider: DateProvider = DefaultDateProvider(),
 ) {
     companion object {
-        const val READ_TIMEOUT_MS = 30000L // We always use 30 seconds for read timeout, once we have established a connection
+        const val READ_TIMEOUT_MS = 30000L // We always use 30 seconds for read timeout
         const val SUPPORTED_FALLBACK_TIMEOUT_MS = 5000L // 5 seconds for requests with fallback support
         const val REDUCED_TIMEOUT_MS = 2000L // 2 seconds for requests with fallback support after timeout
         const val DEFAULT_TIMEOUT_MS = 30000L // 30 seconds for requests without fallback support and fallback requests
