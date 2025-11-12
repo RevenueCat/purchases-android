ef712b7be7c5f86a0a6a8a9c211a8ca4c939a885
Toni Rico <antonio.rico.diez@revenuecat.com>
2025-11-12 11:04:54 +0100

    Add metalava bc7 check

--- scripts/api-dump.sh
+++ scripts/api-dump.sh
@@ -4,5 +4,6 @@ exit_code=0
 
 ./gradlew metalavaGenerateSignatureDefaultsBc8Release || exit_code=$?
 ./gradlew metalavaGenerateSignatureCustomEntitlementComputationBc8Release || exit_code=$?
+./gradlew metalavaGenerateSignatureDefaultsBc7Release || exit_code=$?
 
 exit $exit_code
