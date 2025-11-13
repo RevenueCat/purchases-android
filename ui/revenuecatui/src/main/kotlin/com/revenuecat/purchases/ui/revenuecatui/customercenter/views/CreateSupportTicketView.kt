87c0024b8efe4c393db775c4d52a496c78817a5c
Rosie Watson <rosie.watson@revenuecat.com>
2025-11-13 11:02:25 +0000

    chore: update keybaord focus

--- ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/customercenter/views/CreateSupportTicketView.kt
+++ ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/customercenter/views/CreateSupportTicketView.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
@@ -25,13 +26,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -142,6 +146,7 @@ private fun CreateSupportTicketContent(
         EmailInputField(
             state = emailState,
             localization = localization,
+            focusManager = LocalFocusManager.current,
         )
 
         Spacer(modifier = Modifier.height(SECTION_TITLE_BOTTOM_PADDING))
@@ -193,6 +198,7 @@ private fun CreateSupportTicketView_Preview() {
 private fun EmailInputField(
     state: EmailInputState,
     localization: CustomerCenterConfigData.Localization,
+    focusManager: FocusManager,
 ) {
     OutlinedTextField(
         value = state.email,
@@ -233,8 +239,8 @@ private fun EmailInputField(
         singleLine = true,
         keyboardOptions = KeyboardOptions(
             keyboardType = KeyboardType.Email,
-            imeAction = ImeAction.Next,
         ),
+        keyboardActions = KeyboardActions(onNext = { focusManager.moveFocus(FocusDirection.Down) }),
     )
 }
 
@@ -303,7 +309,7 @@ private fun ErrorSnackbar(
     localization: CustomerCenterConfigData.Localization,
     modifier: Modifier = Modifier,
 ) {
-    val errorSnackbarHostState = rememberSaveable { SnackbarHostState() }
+    val errorSnackbarHostState = remember { SnackbarHostState() }
     val errorMessage = localization.commonLocalizedString(
         CustomerCenterConfigData.Localization.CommonLocalizedString.SUPPORT_TICKET_FAILED,
     )
