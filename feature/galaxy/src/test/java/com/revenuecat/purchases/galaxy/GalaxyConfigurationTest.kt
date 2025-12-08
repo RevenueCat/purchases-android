91c4e8406a90ba9744ea84d3488027765d17134e
Will Taylor <wtaylor151@gmail.com>
2025-12-08 11:36:14 -0600

    remove test config

--- feature/galaxy/src/test/java/com/revenuecat/purchases/galaxy/GalaxyConfigurationTest.kt
+++ feature/galaxy/src/test/java/com/revenuecat/purchases/galaxy/GalaxyConfigurationTest.kt
@@ -1,9 +1,7 @@
 package com.revenuecat.purchases.galaxy
 
 import android.content.Context
-import android.os.Build
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.revenuecat.purchases.Purchases
 import com.revenuecat.purchases.Store
 import io.mockk.every
 import io.mockk.mockk
@@ -11,10 +9,8 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.robolectric.annotation.Config
 
 @RunWith(AndroidJUnit4::class)
-@Config(sdk = [Build.VERSION_CODES.LOLLIPOP])
 class GalaxyConfigurationTest {
 
     private val apiKey = "test-api-key"
