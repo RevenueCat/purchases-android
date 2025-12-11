df6fa9dee6547d6e1597f750a14980e5a7cc78be
Toni Rico <antonio.rico.diez@revenuecat.com>
2025-12-11 17:13:38 +0100

    Test 2: Verifying snapshots

--- ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/customercenter/theme/CustomerCenterPreviewTheme.kt
+++ ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/customercenter/theme/CustomerCenterPreviewTheme.kt
@@ -8,7 +8,6 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
