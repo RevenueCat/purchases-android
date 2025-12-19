c054ab1e172302200ae75c8ac3bcaafe4841a5ce
Toni Rico <antonio.rico.diez@revenuecat.com>
2025-12-19 10:19:21 +0100

    Fix detekt

--- test-apps/sdksizetesting/app/src/main/java/com/revenuecat/testapps/sdksizetesting/ui/theme/Theme.kt
+++ test-apps/sdksizetesting/app/src/main/java/com/revenuecat/testapps/sdksizetesting/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.revenuecat.testapps.sdksizetesting.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -14,13 +13,13 @@ import androidx.compose.ui.platform.LocalContext
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Pink80,
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -30,7 +29,7 @@ private val LightColorScheme = lightColorScheme(
     onTertiary = Color.White,
     onBackground = Color(0xFF1C1B1F),
     onSurface = Color(0xFF1C1B1F),
-    */
+     */
 )
 
 @Composable
@@ -38,7 +37,7 @@ fun SDKSizeTestingTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
+    content: @Composable () -> Unit,
 ) {
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
@@ -52,6 +51,6 @@ fun SDKSizeTestingTheme(
 
     MaterialTheme(
         colorScheme = colorScheme,
-        content = content
+        content = content,
     )
 }
