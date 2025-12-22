e42328e2bf0362cf441e15a43259d55fce68f7e0
Cesar de la Vega <664544+vegaro@users.noreply.github.com>
2025-12-22 12:57:21 +0100

    Retry creation of font folder (#2965)
    Followup on #2961 
    
    If the cache directory failed to create we never tried to create again

--- purchases/src/main/kotlin/com/revenuecat/purchases/paywalls/FontLoader.kt
+++ purchases/src/main/kotlin/com/revenuecat/purchases/paywalls/FontLoader.kt
@@ -79,7 +79,9 @@ internal class FontLoader(
                 errorLog { "Cannot download font: cache directory is not available" }
                 return@launch
             }
-            ensureFoldersExist(cacheDir)
+            if (!ensureFoldersExist(cacheDir)) {
+                return@launch
+            }
 
             val urlHash = md5Hex(url.toByteArray(Charsets.UTF_8))
             val extension = url.substringAfterLast('.', missingDelimiterValue = "")
@@ -164,14 +166,22 @@ internal class FontLoader(
         }
     }
 
-    private fun ensureFoldersExist(cacheDir: File) {
-        if (hasCheckedFoldersExist.getAndSet(true)) return
+    private fun ensureFoldersExist(cacheDir: File): Boolean {
+        if (hasCheckedFoldersExist.get()) return true
 
-        if (!cacheDir.exists() && !cacheDir.mkdirs()) {
-            errorLog { "Unable to create cache directory for remote fonts: ${cacheDir.absolutePath}" }
-        } else if (!cacheDir.isDirectory) {
-            errorLog { "Remote fonts cache path exists but is not a directory: ${cacheDir.absolutePath}" }
+        val success = when {
+            !cacheDir.exists() && !cacheDir.mkdirs() -> {
+                errorLog { "Unable to create cache directory for remote fonts: ${cacheDir.absolutePath}" }
+                false
+            }
+            !cacheDir.isDirectory -> {
+                errorLog { "Remote fonts cache path exists but is not a directory: ${cacheDir.absolutePath}" }
+                false
+            }
+            else -> true
         }
+        hasCheckedFoldersExist.set(success)
+        return success
     }
 
     @Throws(IOException::class)
