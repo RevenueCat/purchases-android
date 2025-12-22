fd1079edf23bf3cdab537df069f254219f968f1c
revenuecat-ops <ops@revenuecat.com>
2025-12-22 13:46:39 +0000

    Preparing for next version

--- purchases/src/main/kotlin/com/revenuecat/purchases/common/Config.kt
+++ purchases/src/main/kotlin/com/revenuecat/purchases/common/Config.kt
@@ -6,5 +6,5 @@ import com.revenuecat.purchases.api.BuildConfig
 internal object Config {
     var logLevel = LogLevel.debugLogsEnabled(BuildConfig.DEBUG)
 
-    const val frameworkVersion = "9.17.1"
+    const val frameworkVersion = "9.18.0-SNAPSHOT"
 }
