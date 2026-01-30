27fc8b69e33cc8037d7c650800a2aea7220bf915
Cesar de la Vega <664544+vegaro@users.noreply.github.com>
2026-01-30 17:17:36 +0100

    cleanup

--- ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/data/PaywallViewModel.kt
+++ ui/revenuecatui/src/main/kotlin/com/revenuecat/purchases/ui/revenuecatui/data/PaywallViewModel.kt
@@ -18,10 +18,7 @@ import com.revenuecat.purchases.PurchasesError
 import com.revenuecat.purchases.PurchasesErrorCode
 import com.revenuecat.purchases.PurchasesException
 import com.revenuecat.purchases.models.GoogleStoreProduct
-import com.revenuecat.purchases.models.Period
-import com.revenuecat.purchases.models.StoreProduct
 import com.revenuecat.purchases.models.SubscriptionOption
-import com.revenuecat.purchases.paywalls.components.common.ProductChangeConfig
 import com.revenuecat.purchases.paywalls.events.ExitOfferType
 import com.revenuecat.purchases.paywalls.events.PaywallEvent
 import com.revenuecat.purchases.paywalls.events.PaywallEventType
@@ -435,7 +432,6 @@ internal class PaywallViewModelImpl(
                 activity = activity,
                 packageToPurchase = packageInfo.rcPackage,
                 subscriptionOption = packageInfo.resolvedOffer?.subscriptionOption,
-                productChangeConfig = productChangeConfig,
             )
         }
     }
@@ -445,7 +441,6 @@ internal class PaywallViewModelImpl(
         activity: Activity,
         packageToPurchase: Package,
         subscriptionOption: SubscriptionOption?,
-        productChangeConfig: ProductChangeConfig? = null,
     ) {
         // Call onPurchasePackageInitiated and wait for resume() to be called
 
@@ -461,13 +456,11 @@ internal class PaywallViewModelImpl(
         }
 
         try {
-            val customPurchaseHandler = purchaseLogic?.let { it::performPurchase }
-
             trackPaywallPurchaseInitiated(packageToPurchase)
 
             when (purchases.purchasesAreCompletedBy) {
                 PurchasesAreCompletedBy.MY_APP -> {
-                    checkNotNull(customPurchaseHandler) {
+                    val logic = checkNotNull(purchaseLogic) {
                         "myAppPurchaseLogic must not be null when purchases.purchasesAreCompletedBy " +
                             "is PurchasesAreCompletedBy.MY_APP"
                     }
@@ -476,7 +469,6 @@ internal class PaywallViewModelImpl(
                             activity,
                             packageToPurchase,
                             subscriptionOption,
-                            productChangeConfig,
                         )
                     ) {
                         is PurchaseLogicResult.Success -> {
@@ -498,7 +490,7 @@ internal class PaywallViewModelImpl(
                 }
                 PurchasesAreCompletedBy.REVENUECAT -> {
                     listener?.onPurchaseStarted(packageToPurchase)
-                    if (customPurchaseHandler != null) {
+                    if (purchaseLogic != null) {
                         Logger.e(
                             "myAppPurchaseLogic expected to be null " +
                                 "when purchases.purchasesAreCompletedBy is .REVENUECAT. \n" +
@@ -511,17 +503,6 @@ internal class PaywallViewModelImpl(
                             .presentedOfferingContext(packageToPurchase.presentedOfferingContext)
                     } else {
                         PurchaseParams.Builder(activity, packageToPurchase)
-                    }
-
-                    // Apply product change configuration if detected
-                    if (productChangeInfo != null) {
-                        Logger.d(
-                            "Performing product change from ${productChangeInfo.oldProductId} " +
-                                "with mode ${productChangeInfo.replacementMode}",
-                        )
-                        purchaseParamsBuilder
-                            .oldProductId(productChangeInfo.oldProductId)
-                            .googleReplacementMode(productChangeInfo.replacementMode)
                     }
 
                     val purchaseResult = purchases.awaitPurchase(purchaseParamsBuilder)
