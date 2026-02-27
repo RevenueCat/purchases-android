fbcf55cdeda0bb22be2bc764f05d95ccf9ec27d9
Cesar de la Vega <664544+vegaro@users.noreply.github.com>
2026-02-27 12:37:13 +0100

    cleaner

--- purchases/src/main/kotlin/com/revenuecat/purchases/paywalls/components/common/PromoOfferConfig.kt
+++ purchases/src/main/kotlin/com/revenuecat/purchases/paywalls/components/common/PromoOfferConfig.kt
@@ -1,17 +1,10 @@
 package com.revenuecat.purchases.paywalls.components.common
 
 import com.revenuecat.purchases.InternalRevenueCatAPI
-import com.revenuecat.purchases.common.warnLog
+import com.revenuecat.purchases.utils.serializers.EmptyObjectToNullSerializer
 import dev.drewhamilton.poko.Poko
-import kotlinx.serialization.KSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.SerializationException
-import kotlinx.serialization.descriptors.SerialDescriptor
-import kotlinx.serialization.encoding.Decoder
-import kotlinx.serialization.encoding.Encoder
-import kotlinx.serialization.json.JsonDecoder
-import kotlinx.serialization.json.JsonObject
 
 /**
  * Configuration for a promo offer to use for a package in a paywall.
@@ -31,29 +24,6 @@ public class PromoOfferConfig(
 )
 
 @OptIn(InternalRevenueCatAPI::class)
-internal object ResilientPromoOfferConfigSerializer : KSerializer<PromoOfferConfig?> {
-    private val delegate = PromoOfferConfig.serializer()
-
-    override val descriptor: SerialDescriptor = delegate.descriptor
-
-    override fun deserialize(decoder: Decoder): PromoOfferConfig? {
-        return try {
-            val jsonDecoder = decoder as? JsonDecoder ?: return delegate.deserialize(decoder)
-            val element = jsonDecoder.decodeJsonElement()
-            if (element is JsonObject) {
-                jsonDecoder.json.decodeFromJsonElement(delegate, element)
-            } else {
-                null
-            }
-        } catch (e: SerializationException) {
-            warnLog { "Failed to deserialize PromoOfferConfig, defaulting to null: ${e.message}" }
-            null
-        }
-    }
-
-    override fun serialize(encoder: Encoder, value: PromoOfferConfig?) {
-        if (value != null) {
-            delegate.serialize(encoder, value)
-        }
-    }
-}
+internal object ResilientPromoOfferConfigSerializer : EmptyObjectToNullSerializer<PromoOfferConfig>(
+    PromoOfferConfig.serializer(),
+)
