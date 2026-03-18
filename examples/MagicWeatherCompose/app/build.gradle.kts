67c238efa409859147da7a6e065a728165ffe9a8
Toni Rico <antonio.rico.diez@revenuecat.com>
2026-03-18 10:49:45 +0100

    update all minSdk versions to 23

--- examples/MagicWeatherCompose/app/build.gradle.kts
+++ examples/MagicWeatherCompose/app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.revenuecat.purchases_sample"
-        minSdk = 21
+        minSdk = 23
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
