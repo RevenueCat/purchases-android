79c0aa24bce980b469b7b6750c4472564f5ab600
Pol Miro <polmiro@gmail.com>
2026-05-12 17:39:05 +0200

    fix: address review — remove redundant SerialName annotation

--- purchases/src/main/kotlin/com/revenuecat/purchases/common/networking/AdMobRewardVerificationStatusResponse.kt
+++ purchases/src/main/kotlin/com/revenuecat/purchases/common/networking/AdMobRewardVerificationStatusResponse.kt
@@ -2,12 +2,10 @@ package com.revenuecat.purchases.common.networking
 
 import com.revenuecat.purchases.AdMobRewardVerificationStatus
 import com.revenuecat.purchases.InternalRevenueCatAPI
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 internal data class AdMobRewardVerificationStatusResponse(
-    @SerialName("status")
     val status: String,
 ) {
     @OptIn(InternalRevenueCatAPI::class)
