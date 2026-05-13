bb245d443713b9ec92690fb59dab6f752b3f7d52
Pol Miro <polmiro@gmail.com>
2026-05-13 12:22:23 +0200

    fix: make outcome Kotlin 1.8 compatible
    Replace data object usage and add explicit experimental opt-in in reward verification outcome mapping to fix module compilation on CI.

--- feature/admob/src/main/kotlin/com/revenuecat/purchases/admob/rewardverification/Outcome.kt
+++ feature/admob/src/main/kotlin/com/revenuecat/purchases/admob/rewardverification/Outcome.kt
@@ -1,12 +1,15 @@
+@file:OptIn(ExperimentalPreviewRevenueCatPurchasesAPI::class)
+
 package com.revenuecat.purchases.admob.rewardverification
 
+import com.revenuecat.purchases.ExperimentalPreviewRevenueCatPurchasesAPI
 import com.revenuecat.purchases.admob.RewardVerificationResult
 import com.revenuecat.purchases.admob.VerifiedReward
 
 internal sealed interface Outcome {
     data class Verified(val reward: VerifiedReward) : Outcome
 
-    data object Failed : Outcome
+    object Failed : Outcome
 }
 
 internal fun Outcome.toResult(): RewardVerificationResult {
