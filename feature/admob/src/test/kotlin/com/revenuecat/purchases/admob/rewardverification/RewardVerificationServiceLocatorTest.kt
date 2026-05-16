5407f8996d2ae3432cb6b8a6041657e4e2a40bfb
Pol Miro <polmiro@gmail.com>
2026-05-16 04:21:12 -0600

    fix: avoid lifecycle replay deadlock in reward verification locator

--- feature/admob/src/test/kotlin/com/revenuecat/purchases/admob/rewardverification/RewardVerificationServiceLocatorTest.kt
+++ feature/admob/src/test/kotlin/com/revenuecat/purchases/admob/rewardverification/RewardVerificationServiceLocatorTest.kt
@@ -11,6 +11,7 @@ import io.mockk.verify
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Test
 
@@ -98,5 +99,77 @@ internal class RewardVerificationServiceLocatorTest {
         } finally {
             executor.shutdownNow()
         }
+    }
+
+    @Test
+    fun `replayed configured callback can mutate locator from another thread without deadlock`() {
+        val purchases = mockk<Purchases>(relaxed = true)
+        lateinit var locator: RewardVerificationServiceLocator
+        val listenerRegistrar = RewardVerificationListenerRegistrar { listener ->
+            listener.onPurchasesConfigured(purchases)
+        }
+        locator = RewardVerificationServiceLocator(listenerRegistrar)
+        val lateHook = mockk<RewardVerificationLifecycleHook>(relaxed = true)
+        val callbackCompleted = CountDownLatch(1)
+        val executor = Executors.newSingleThreadExecutor()
+
+        val reentrantHook = object : RewardVerificationLifecycleHook {
+            override fun onPurchasesConfigured(purchases: Purchases) {
+                val currentHook = this
+                executor.submit<Unit> {
+                    locator.unregisterHook(currentHook)
+                    locator.registerHook(lateHook)
+                }.get(1, TimeUnit.SECONDS)
+                callbackCompleted.countDown()
+            }
+
+            override fun onPurchasesClosed(purchases: Purchases) = Unit
+        }
+
+        try {
+            locator.registerHook(reentrantHook)
+
+            assertTrue(callbackCompleted.await(1, TimeUnit.SECONDS))
+            verify(exactly = 0) { lateHook.onPurchasesConfigured(any()) }
+
+            locator.onPurchasesConfigured(purchases)
+
+            verify(exactly = 1) { lateHook.onPurchasesConfigured(purchases) }
+        } finally {
+            executor.shutdownNow()
+        }
+    }
+
+    @Test
+    fun `replayed configured callback that registers a hook does not register locator twice`() {
+        val purchases = mockk<Purchases>(relaxed = true)
+        lateinit var locator: RewardVerificationServiceLocator
+        var registerCount = 0
+        val listenerRegistrar = RewardVerificationListenerRegistrar { listener ->
+            registerCount += 1
+            listener.onPurchasesConfigured(purchases)
+        }
+        locator = RewardVerificationServiceLocator(listenerRegistrar)
+        val lateHook = mockk<RewardVerificationLifecycleHook>(relaxed = true)
+        var hookCallbackCount = 0
+        var lateHookRegistered = false
+
+        val reentrantHook = object : RewardVerificationLifecycleHook {
+            override fun onPurchasesConfigured(purchases: Purchases) {
+                hookCallbackCount += 1
+                if (!lateHookRegistered) {
+                    lateHookRegistered = true
+                    locator.registerHook(lateHook)
+                }
+            }
+
+            override fun onPurchasesClosed(purchases: Purchases) = Unit
+        }
+
+        locator.registerHook(reentrantHook)
+
+        assertEquals(1, registerCount)
+        assertEquals(1, hookCallbackCount)
+        verify(exactly = 0) { lateHook.onPurchasesConfigured(any()) }
     }
 }
