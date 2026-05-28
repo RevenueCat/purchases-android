33dfdeecefbf2160be4ed7c134fddd696f80ef8f
Pol Miro <polmiro@gmail.com>
2026-05-28 14:49:19 +0200

    refactor: narrow reward verification outcome opt-in scope
    Replace file-level OptIn with declaration-level annotations on the only two sites that reference experimental APIs.

--- feature/admob/src/main/kotlin/com/revenuecat/purchases/admob/rewardverification/Outcome.kt
+++ feature/admob/src/main/kotlin/com/revenuecat/purchases/admob/rewardverification/Outcome.kt
@@ -1,17 +1,17 @@
-@file:OptIn(ExperimentalPreviewRevenueCatPurchasesAPI::class)
-
 package com.revenuecat.purchases.admob.rewardverification
 
 import com.revenuecat.purchases.ExperimentalPreviewRevenueCatPurchasesAPI
 import com.revenuecat.purchases.admob.RewardVerificationResult
 import com.revenuecat.purchases.admob.VerifiedReward
 
+@OptIn(ExperimentalPreviewRevenueCatPurchasesAPI::class)
 internal sealed interface Outcome {
     data class Verified(val reward: VerifiedReward) : Outcome
 
     object Failed : Outcome
 }
 
+@OptIn(ExperimentalPreviewRevenueCatPurchasesAPI::class)
 internal fun Outcome.toResult(): RewardVerificationResult {
     return when (this) {
         is Outcome.Verified -> RewardVerificationResult.verified(reward)
