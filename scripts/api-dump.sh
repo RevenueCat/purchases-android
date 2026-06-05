af2082be699c123565c2a9de6d732e63ca6b6c35
Will Taylor <wtaylor151@gmail.com>
2026-06-05 11:30:33 -0500

    remove test-galaxy ci job

--- scripts/api-dump.sh
+++ scripts/api-dump.sh
@@ -5,5 +5,6 @@ exit_code=0
 ./gradlew metalavaGenerateSignatureDefaultsBc8Release || exit_code=$?
 ./gradlew metalavaGenerateSignatureCustomEntitlementComputationBc8Release || exit_code=$?
 ./gradlew metalavaGenerateSignatureDefaultsBc7Release || exit_code=$?
+./gradlew :feature:galaxy:metalavaGenerateSignatureBc8Release || exit_code=$?
 
 exit $exit_code
