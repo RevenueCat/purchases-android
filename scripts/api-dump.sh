00b54c659b575e4f0dc7b3c6dc87cac3f80cf341
Will Taylor <wtaylor151@gmail.com>
2026-06-05 12:44:57 -0500

    remove explicit galaxy metalava

--- scripts/api-dump.sh
+++ scripts/api-dump.sh
@@ -5,6 +5,5 @@ exit_code=0
 ./gradlew metalavaGenerateSignatureDefaultsBc8Release || exit_code=$?
 ./gradlew metalavaGenerateSignatureCustomEntitlementComputationBc8Release || exit_code=$?
 ./gradlew metalavaGenerateSignatureDefaultsBc7Release || exit_code=$?
-./gradlew :feature:galaxy:metalavaGenerateSignatureBc8Release || exit_code=$?
 
 exit $exit_code
