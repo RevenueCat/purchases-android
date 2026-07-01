9a8feed858b2aca1b7cef466f890b402f5861975
Peter Porfy <peter.porfy@revenuecat.com>
2026-07-01 14:27:39 +0200

    feat(ads): typealiases for moved public classes

---
+++ feature/admob/src/main/kotlin/com/revenuecat/purchases/admob/RewardVerificationResult.kt
@@ -0,0 +1,17 @@
+package com.revenuecat.purchases.admob
+
+import com.revenuecat.purchases.ExperimentalPreviewRevenueCatPurchasesAPI
+
+/**
+ * Moved to [com.revenuecat.purchases.ads.rewardverification.RewardVerificationResult].
+ */
+@Deprecated(
+    "Moved to com.revenuecat.purchases.ads.rewardverification.RewardVerificationResult",
+    ReplaceWith(
+        "RewardVerificationResult",
+        "com.revenuecat.purchases.ads.rewardverification.RewardVerificationResult",
+    ),
+)
+@OptIn(ExperimentalPreviewRevenueCatPurchasesAPI::class)
+public typealias RewardVerificationResult =
+    com.revenuecat.purchases.ads.rewardverification.RewardVerificationResult
