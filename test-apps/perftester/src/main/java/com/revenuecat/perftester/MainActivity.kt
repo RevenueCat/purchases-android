f02448d26562e63d561c1b3b088f79af409bb2bc
Cesar de la Vega <664544+vegaro@users.noreply.github.com>
2026-07-09 12:38:17 +0200

    test: scaffold perftester app module

---
+++ test-apps/perftester/src/main/java/com/revenuecat/perftester/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.revenuecat.perftester
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Text("perftester")
+            }
+        }
+    }
+}
