2e770730009b79a18de2b2a583d7062fb39aea96
Cesar de la Vega <664544+vegaro@users.noreply.github.com>
2026-07-16 14:31:07 +0200

    test(workflows): dispatch e2etests config/screen on Maestro launch arg

--- test-apps/e2etests/src/main/java/com/revenuecat/e2etests/E2ETestsApplication.kt
+++ test-apps/e2etests/src/main/java/com/revenuecat/e2etests/E2ETestsApplication.kt
@@ -3,20 +3,12 @@ package com.revenuecat.e2etests
 import android.app.Application
 import com.revenuecat.purchases.LogLevel
 import com.revenuecat.purchases.Purchases
-import com.revenuecat.purchases.PurchasesConfiguration
 
 class E2ETestsApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-
-        // Configure RevenueCat SDK
+        // Configuration is performed in MainActivity, which can read the Maestro launch
+        // arguments (delivered as intent extras) to pick the flow, API key, and settings.
         Purchases.logLevel = LogLevel.DEBUG
-
-        Purchases.configure(
-            PurchasesConfiguration.Builder(
-                context = this,
-                apiKey = Constants.API_KEY,
-            ).build(),
-        )
     }
 }
--- test-apps/e2etests/src/main/java/com/revenuecat/e2etests/MainActivity.kt
+++ test-apps/e2etests/src/main/java/com/revenuecat/e2etests/MainActivity.kt
@@ -6,15 +6,47 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.revenuecat.e2etests.main.MainPage
 import com.revenuecat.e2etests.ui.theme.PurchasesandroidTheme
+import com.revenuecat.e2etests.workflow.WorkflowScreen
+import com.revenuecat.purchases.DangerousSettings
+import com.revenuecat.purchases.InternalRevenueCatAPI
+import com.revenuecat.purchases.Purchases
+import com.revenuecat.purchases.PurchasesConfiguration
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val flow = E2ETestFlow.fromRawValue(intent.getStringExtra(E2ETestFlow.INTENT_EXTRA_KEY))
+        configurePurchases(flow)
+
         enableEdgeToEdge()
         setContent {
             PurchasesandroidTheme {
-                MainPage()
+                when (flow) {
+                    E2ETestFlow.OPEN_WORKFLOW -> WorkflowScreen()
+                    null -> MainPage()
+                }
             }
         }
+    }
+
+    @OptIn(InternalRevenueCatAPI::class)
+    private fun configurePurchases(flow: E2ETestFlow?) {
+        if (Purchases.isConfigured) return
+
+        val builder = when (flow) {
+            E2ETestFlow.OPEN_WORKFLOW -> PurchasesConfiguration.Builder(
+                context = this,
+                apiKey = BuildConfig.WORKFLOWS_API_KEY,
+            ).dangerousSettings(DangerousSettings.forWorkflows())
+
+            null -> PurchasesConfiguration.Builder(
+                context = this,
+                apiKey = Constants.API_KEY,
+            )
+        }
+
+        Purchases.configure(builder.build())
     }
 }
