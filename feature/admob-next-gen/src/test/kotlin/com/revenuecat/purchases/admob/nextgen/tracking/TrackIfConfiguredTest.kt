595fb22c209fa8c86e36130061141fc1cc656576
Pol Miro <polmiro@gmail.com>
2026-08-13 11:36:05 +0200

    test(admob): drop redundant OptIn from TrackIfConfiguredTest
    The file opted into ExperimentalPreviewRevenueCatPurchasesAPI but touches no
    experimental API: trackIfConfigured is plain internal and Purchases is stable.

--- feature/admob-next-gen/src/test/kotlin/com/revenuecat/purchases/admob/nextgen/tracking/TrackIfConfiguredTest.kt
+++ feature/admob-next-gen/src/test/kotlin/com/revenuecat/purchases/admob/nextgen/tracking/TrackIfConfiguredTest.kt
@@ -1,8 +1,5 @@
-@file:OptIn(ExperimentalPreviewRevenueCatPurchasesAPI::class)
-
 package com.revenuecat.purchases.admob.nextgen.tracking
 
-import com.revenuecat.purchases.ExperimentalPreviewRevenueCatPurchasesAPI
 import com.revenuecat.purchases.Purchases
 import io.mockk.every
 import io.mockk.mockk
