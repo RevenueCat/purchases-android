7bc7fc7e6dce3246e3b7d9c7c2cfa6f50f589411
Pol Miro <polmiro@gmail.com>
2026-08-17 18:07:19 +0200

    refactor(admob): limit next-gen extensions to interstitials

--- feature/admob-next-gen/src/main/kotlin/com/revenuecat/purchases/admob/nextgen/AdTrackerAdMobExtensions.kt
+++ feature/admob-next-gen/src/main/kotlin/com/revenuecat/purchases/admob/nextgen/AdTrackerAdMobExtensions.kt
@@ -5,11 +5,6 @@
 package com.revenuecat.purchases.admob.nextgen
 
 import android.annotation.SuppressLint
-import com.google.android.libraries.ads.mobile.sdk.banner.AdView
-import com.google.android.libraries.ads.mobile.sdk.banner.BannerAd
-import com.google.android.libraries.ads.mobile.sdk.banner.BannerAdEventCallback
-import com.google.android.libraries.ads.mobile.sdk.banner.BannerAdRefreshCallback
-import com.google.android.libraries.ads.mobile.sdk.banner.BannerAdRequest
 import com.google.android.libraries.ads.mobile.sdk.common.AdLoadCallback
 import com.google.android.libraries.ads.mobile.sdk.common.AdRequest
 import com.google.android.libraries.ads.mobile.sdk.interstitial.InterstitialAd
@@ -20,39 +15,6 @@ import com.revenuecat.purchases.admob.nextgen.tracking.TrackingInterstitialAdEve
 import com.revenuecat.purchases.ads.events.AdTracker
 import com.revenuecat.purchases.ads.events.types.AdFormat
 import kotlin.jvm.JvmSynthetic
-
-/**
- * Sets up RevenueCat ad-event tracking for the given [AdView] and loads a banner ad.
- *
- * Call via `Purchases.sharedInstance.adTracker`. For a call that does not take the tracker
- * explicitly, use [AdView.loadAndTrackAd].
- *
- * @param adView The [AdView] that will display the loaded banner.
- * @param adRequest The [BannerAdRequest] to load. Its ad unit ID is used for tracking.
- * @param placement Optional placement identifier used in RevenueCat tracking.
- * @param loadCallback Optional callback to receive load success and failure events.
- * @param adEventCallback Optional callback for banner lifecycle and paid events.
- * @param bannerAdRefreshCallback Optional callback for automatic banner refresh events.
- */
-@ExperimentalPreviewRevenueCatPurchasesAPI
-@JvmSynthetic
-@Suppress("LongParameterList")
-public fun AdTracker.loadAndTrackBannerAd(
-    adView: AdView,
-    adRequest: BannerAdRequest,
-    placement: String? = null,
-    loadCallback: AdLoadCallback<BannerAd>? = null,
-    adEventCallback: BannerAdEventCallback? = null,
-    bannerAdRefreshCallback: BannerAdRefreshCallback? = null,
-) {
-    adView.loadAndTrackBannerAdInternal(
-        adRequest = adRequest,
-        placement = placement,
-        loadCallback = loadCallback,
-        adEventCallback = adEventCallback,
-        bannerAdRefreshCallback = bannerAdRefreshCallback,
-    )
-}
 
 /**
  * Loads an [InterstitialAd] and automatically tracks RevenueCat ad events.
