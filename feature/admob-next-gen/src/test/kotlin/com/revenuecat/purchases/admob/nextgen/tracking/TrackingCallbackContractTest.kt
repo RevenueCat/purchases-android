fd4d7d4644680eed08b8660cfb4a89a4f839ee39
Pol Miro <polmiro@gmail.com>
2026-08-17 15:47:23 +0200

    test(admob): guard next-gen native loader callbacks

--- feature/admob-next-gen/src/test/kotlin/com/revenuecat/purchases/admob/nextgen/tracking/TrackingCallbackContractTest.kt
+++ feature/admob-next-gen/src/test/kotlin/com/revenuecat/purchases/admob/nextgen/tracking/TrackingCallbackContractTest.kt
@@ -2,6 +2,7 @@ package com.revenuecat.purchases.admob.nextgen.tracking
 
 import com.google.android.libraries.ads.mobile.sdk.banner.BannerAdRefreshCallback
 import com.google.android.libraries.ads.mobile.sdk.common.AdLoadCallback
+import com.google.android.libraries.ads.mobile.sdk.nativead.NativeAdLoaderCallback
 import org.junit.Test
 
 class TrackingCallbackContractTest {
@@ -14,5 +15,10 @@ class TrackingCallbackContractTest {
     @Test
     fun `banner refresh callback overrides every SDK callback`() {
         assertOverridesAllSdkCallbacks(BannerAdRefreshCallback::class.java, TrackingBannerAdRefreshCallback::class.java)
+    }
+
+    @Test
+    fun `native loader callback overrides every SDK callback`() {
+        assertOverridesAllSdkCallbacks(NativeAdLoaderCallback::class.java, TrackingNativeAdLoaderCallback::class.java)
     }
 }
