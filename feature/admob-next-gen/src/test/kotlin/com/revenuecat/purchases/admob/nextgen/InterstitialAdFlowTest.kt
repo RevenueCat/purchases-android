875e1b577609c24bfa97103b7208b82d2e48f1c0
Pol Miro <polmiro@gmail.com>
2026-08-19 18:10:10 +0200

    Run ad flow tests without Robolectric

--- feature/admob-next-gen/src/test/kotlin/com/revenuecat/purchases/admob/nextgen/InterstitialAdFlowTest.kt
+++ feature/admob-next-gen/src/test/kotlin/com/revenuecat/purchases/admob/nextgen/InterstitialAdFlowTest.kt
@@ -39,12 +39,6 @@ import org.junit.Assert.assertSame
 import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.robolectric.RobolectricTestRunner
-import org.robolectric.annotation.Config
-
-@RunWith(RobolectricTestRunner::class)
-@Config(sdk = [35])
 class InterstitialAdFlowTest {
 
     private val adTracker = mockk<AdTracker>(relaxed = true)
